347d4bd29728504afec46153edc0da36f5e5f722
Erik Troan <ewt@redhat.com>
1999-04-25 22:01:46 +0000

    *** empty log message ***

---
+++ text.py
@@ -0,0 +1,21 @@
+import _balkan
+from snack import *
+
+screen = SnackScreen()
+
+device = 'hda';
+table = _balkan.readTable('/dev/' + device)
+partList = []
+for i in range(0, len(table) - 1):
+    (type, start, size) = table[i]
+    if (type == 0x83 and size):
+	fullName = '/dev/%s%d' % (device, i)
+	partList.append((fullName, fullName))
+    
+rc = ListboxChoiceWindow(screen, 'Root Partition', 'What partition would you '
+		    'like to use for your root partition?', partList,
+		    buttons = ['Ok'])
+
+screen.finish()
+
+print rc
