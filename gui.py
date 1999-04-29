d6d2cdaa7615a2c6cbed7d7c8778e845236dbcb1
Matt Wilson <msw@redhat.com>
1999-04-29 19:11:03 +0000

    added toplevel class

--- gui.py
+++ gui.py
@@ -3,60 +3,143 @@
 from gtk import *
 import GdkImlib
 import sys
+import _balkan
 
-def destroy(win):
-    win.destroy()
-    mainquit()
+class WelcomeWindow:		
+    def next(self, win):
+        mainquit()
 
-def WelcomeWindow():
-    window = GtkWindow()
-    window.connect("destroy", destroy)
-    window.set_border_width(10)
-    window.set_title("Welcome to Red Hat Linux!")
+    def __init__(self):
+        self.rc = 0
 
-    label = GtkLabel("Welcome to Red Hat Linux!\n\n"
-	"This installation process is outlined in detail in the "
-	"Official Red Hat Linux Installation Guide available from "
- 	"Red Hat Software. If you have access to this manual, you "
-	"should read the installation section before continuing.\n\n"
-	"If you have purchased Official Red Hat Linux, be sure to "
-	"register your purchase through our web site, "
-	"http://www.redhat.com.");
-    label.set_line_wrap (TRUE);
-    button = GtkButton("Ok")
-    button.connect("clicked", mainquit)
-    vbox = GtkVBox (FALSE, 10)
-    vbox.pack_start(label, FALSE, FALSE, 0)
-    vbox.pack_start(button, FALSE, FALSE, 0)
+    def run(self):
+        window = GtkWindow()
+        window.set_border_width(10)
+        window.set_title("Welcome to Red Hat Linux!")
 
-    hbox = GtkHBox (FALSE, 10)
+        label = GtkLabel("Welcome to Red Hat Linux!\n\n"
+	    "This installation process is outlined in detail in the "
+    	    "Official Red Hat Linux Installation Guide available from "
+	    "Red Hat Software. If you have access to this manual, you "
+	    "should read the installation section before continuing.\n\n"
+	    "If you have purchased Official Red Hat Linux, be sure to "
+	    "register your purchase through our web site, "
+	    "http://www.redhat.com/.")
+        label.set_line_wrap (TRUE)
+        buttonbox = GtkHButtonBox()
+        buttonbox.set_layout(BUTTONBOX_END)
+        button = GtkButton("Next >>")
+        button.connect("clicked", self.next)
+        buttonbox.add(button)
+        vbox = GtkVBox (FALSE, 10)
+        vbox.pack_start(label, TRUE, TRUE, 0)
+        vbox.pack_start(buttonbox, FALSE, FALSE, 0)
 
-    try:
-        im = GdkImlib.Image("shadowman-200.png")
-        im.render()
-        pix = im.make_pixmap()
-        hbox.pack_start(pix, TRUE, TRUE, 0)
+        hbox = GtkHBox (FALSE, 10)
 
-    except:
-        print "Unable to load shadowman-200.png"
+        try:
+            im = GdkImlib.Image("shadowman-200.png")
+            im.render()
+            pix = im.make_pixmap()
+            hbox.pack_start(pix, TRUE, TRUE, 0)
 
-    hbox.pack_start(vbox, TRUE, TRUE, 0)
+        except:
+            print "Unable to load shadowman-200.png"
 
-    window.add(hbox)
-    window.set_position(WIN_POS_CENTER)
-    window.show_all()
+        hbox.pack_start(vbox, TRUE, TRUE, 0)
 
-if(__name__=="__main__"):
-    for arg in sys.argv:
-	if(arg=="-d"):
-	    import pdb
-            pdb.set_trace()
-
-steps = {
-    "Welcome Window": WelcomeWindow,
-}
-
-for step in steps.keys():
-    if steps[step]:
-	steps[step]()
+        window.add(hbox)
+        window.set_position(WIN_POS_CENTER)
+        window.set_default_size(640, 480)
+        window.show_all()
         mainloop()
+        window.destroy()
+        return self.rc
+
+class PartitionWindow:
+    def back(self, win):
+        self.rc = -1
+	mainquit()
+
+    def next(self, win):
+	self.rc = 0
+        mainquit()
+
+    def __init__(self):
+        self.rc = 0
+
+    def run(self):
+        window = GtkWindow()
+        window.set_border_width(10)
+        window.set_title("Choose a partition")
+
+        label = GtkLabel("What partition would you like to use for your root "
+                         "partition?")
+        label.set_line_wrap (TRUE)
+
+        hbox = GtkHBox (FALSE, 10)
+
+        device = 'hda'
+        try:
+            table = _balkan.readTable('/dev/' + device)
+            if len(table) - 1 > 0:
+                partbox = GtkVBox (FALSE, 5)
+                button1 = None;
+                for i in range(0, len(table) - 1):
+                    (type, start, size) = table[i]
+                    if (type == 0x83 and size):
+                        if button1:
+                            button = GtkRadioButton(button1,
+                                                '/dev/%s%d' % (device, i + 1))
+                            partbox.pack_start(button, FALSE, FALSE, 0)
+                        else:
+                            button1 = GtkRadioButton(None,
+                                                 '/dev/%s%d' % (device, i + 1))
+                            partbox.pack_start(button1, FALSE, FALSE, 0)
+            hbox.pack_start(partbox, FALSE, FALSE, 0)
+            hbox.pack_start(label, FALSE, FALSE, 0)
+        except:
+            label = GtkLabel("Unable to read partition information")
+            hbox.pack_start(label, TRUE, TRUE, 0)
+            print "unable to read partitions"
+
+        buttonbox = GtkHButtonBox()
+        buttonbox.set_spacing(5)
+        buttonbox.set_layout(BUTTONBOX_END)
+        button = GtkButton("<< Back")
+        button.connect("clicked", self.back)
+        buttonbox.add(button)
+        button = GtkButton("Next >>")
+        button.connect("clicked", self.next)
+        buttonbox.add(button)
+
+        vbox = GtkVBox (FALSE, 10)
+        vbox.pack_start(hbox, TRUE, TRUE, 0)
+        vbox.pack_start(buttonbox, FALSE, FALSE, 0)
+
+        window.add(vbox)
+        window.set_position(WIN_POS_CENTER)
+        window.set_default_size(640, 480)
+        window.show_all()
+        mainloop()
+        window.destroy()
+        return self.rc
+
+class InstallMethod:
+    def run(self):
+        rc_parse("gtkrc")
+
+        steps = [
+            ["Welcome", WelcomeWindow],
+            ["Partition", PartitionWindow]
+        ]
+
+        step = 0
+        while step >= 0 and step < len(steps) and steps[step]:
+            print steps[step]
+
+            if steps[step][1]().run() == -1:
+                step = step - 1
+            else:
+                step = step + 1
+
