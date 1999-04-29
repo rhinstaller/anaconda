7ebdc7c48630cc69e3f9f8b37b1e56efc9ccb01b
Matt Wilson <msw@redhat.com>
1999-04-29 19:27:51 +0000

    commit to new interface

--- text.py
+++ text.py
@@ -1,24 +1,58 @@
 import _balkan
 from snack import *
-import rpm
+import _snack
+
+class WelcomeWindow:
+    def run(self, screen):
+        _snack.message("Red Hat Linux", "Welcome to Red Hat Linux!\n\n"
+            "This installation process is outlined in detail in the "
+	    "Official Red Hat Linux Installation Guide available from "
+	    "Red Hat Software. If you have access to this manual, you "
+	    "should read the installation section before continuing.\n\n"
+	    "If you have purchased Official Red Hat Linux, be sure to "
+	    "register your purchase through our web site, "
+	    "http://www.redhat.com/.")
+        return 0
+
+class PartitionWindow:
+    def run(self, screen):
+        device = 'hda';
+	try:
+            table = _balkan.readTable('/dev/' + device)
+            partList = []
+            for i in range(0, len(table) - 1):
+                (type, start, size) = table[i]
+                if (type == 0x83 and size):
+	            fullName = '/dev/%s%d' % (device, i + 1)
+    	            partList.append((fullName, fullName))
+    
+            rc = ListboxChoiceWindow(screen, 'Root Partition',
+                                     'What partition would you '
+                                     'like to use for your root partition?',
+                                     partList, buttons = ['Ok'])
+
+            print rc
+        except:
+            print "unable to read partition table"
+
+        return 0
 
 class InstallInterface:
+
     def run(self):
-	screen = SnackScreen()
+        screen = SnackScreen()
+  
+        steps = [
+            ["Welcome", WelcomeWindow],
+            ["Partition", PartitionWindow]
+        ]
 
-	device = 'hda';
-	table = _balkan.readTable('/dev/' + device)
-	partList = []
-	for i in range(0, len(table) - 1):
-	    (type, start, size) = table[i]
-	    if (type == 0x83 and size):
-		fullName = '/dev/%s%d' % (device, i + 1)
-		partList.append((fullName, fullName))
-	    
-	rc = ListboxChoiceWindow(screen, 'Root Partition', 'What partition would you '
-			    'like to use for your root partition?', partList,
-			    buttons = ['Ok'])
+        step = 0
+        while step >= 0 and step < len(steps) and steps[step]:
 
-	screen.finish()
+            if steps[step][1]().run(screen) == -1:
+                step = step - 1
+            else:
+                step = step + 1
 
-	print rc
+        screen.finish()
