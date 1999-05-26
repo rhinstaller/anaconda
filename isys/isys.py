1b5fe89a03b361c06b18d141372461e441acdb37
Erik Troan <ewt@redhat.com>
1999-05-26 21:00:40 +0000

    1) added device probing 2) added makeDevInode

--- isys/isys.py
+++ isys/isys.py
@@ -8,3 +8,30 @@ def umount(what):
 
 def smpAvailable():
     return _isys.smpavailable()
+
+def probeDevices():
+    # probes all probeable buses and returns a list of 
+    # ( driver, major, minor, description, args ) tuples, where args is a
+    # list of (argName, argDescrip) tuples
+    devices = _isys.pciprobe()
+    if (not devices): return None
+
+    result = []
+    for dev in devices:
+	info = _isys.findmoduleinfo(dev)
+	if not info:
+	    raise KeyError, "module " + dev + " is not in the module list"
+	result.append(info)
+
+    return result
+
+def moduleListByType(type):
+    return _isys.modulelist(type)
+
+def makeDevInode(name, fn):
+    return _isys.mkdevinode(name, fn)
+	
+try:
+    _isys.readmoduleinfo("/etc/module-info")
+except IOError:
+    _isys.readmoduleinfo("/boot/module-info")
