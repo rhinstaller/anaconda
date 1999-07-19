c3541881dd27d39c3b1e7c00b8cab3ee4f317d3d
Erik Troan <ewt@redhat.com>
1999-07-19 01:58:55 +0000

    1) probe for ide/scsi/net 2) moved pci probe to before newt startup

--- loader/loader.c
+++ loader/loader.c
@@ -20,11 +20,19 @@
  *
  */
 
-#include <unistd.h>
-#include <popt.h>
-#include <newt.h>
-#include <fcntl.h>
 #include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <net/if.h>
+#include <newt.h>
+#include <popt.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include "isys/imount.h"
 #include "isys/inet.h"
@@ -39,87 +47,295 @@
 #define LOADER_BACK 1
 #define LOADER_ERROR -1
 
-
-struct loaderState {
-    enum {
-	INTERFACE_TEXT,
-	INTERFACE_GUI,
-    } interface;
+struct device {
+    char * name;		/* malloced */
+    char * model;
+    enum deviceClass { DEVICE_UNKNOWN, DEVICE_DISK, DEVICE_CDROM, DEVICE_NET,
+    		       DEVICE_TAPE }
+    	class;
 };
 
-typedef int (*loaderStepFn)(struct loaderState *);
+int testing = 0;
+struct device knownDevices[100];		/* arbitrary limit <shrug> */
+int numKnownDevices = 0;
 
-struct loaderStep {
-    char * name;
-    loaderStepFn fn;
-    int skipOnLocal;
-};
+int deviceKnown(char * dev) {
+    int i;
 
-static int welcomeScreen(struct loaderState * state);
-static int detectHardware(struct loaderState * state);
-static int selectMedia(struct loaderState * state);
-static int selectInterface(struct loaderState * state);
+    for (i = 0; i < numKnownDevices; i++)
+    	if (!strcmp(knownDevices[i].name, dev)) return 1;
 
-static struct loaderStep loaderSteps[] = { 
-    { N_("Welcome"), welcomeScreen, 0 },
-    { N_("Hardware Detection"), detectHardware, 1 },
-    { N_("Select Media"), selectMedia, 0 },
-    { N_("Select Interface"), selectInterface, 0 },
-};
-
-static int numSteps = sizeof(loaderSteps) / sizeof(struct loaderStep);
-int testing;
-
-static int welcomeScreen(struct loaderState *state) {
-    return LOADER_OK;
+    return 0;
 }
 
-static int detectHardware(struct loaderState *state) {
+static int findNetList(void) {
+    struct ifconf ifc;
+    struct ifreq intfs[50];		/* should be enough <shrug> */
+    int s;
+
+    ifc.ifc_req = intfs;
+    ifc.ifc_len = sizeof(intfs);
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (ioctl(s, SIOCGIFCONF, &ifc)) {
+        logMessage("failed to get list of networking interfaces");
+	close(s);
+	return 1;
+    }
+
+    close(s);
+
+    for (s = 0; s < ifc.ifc_len / sizeof(struct ifreq); s++) {
+    	if (!strcmp(intfs[s].ifr_name, "lo")) continue;
+	if (deviceKnown(intfs[s].ifr_name)) continue;
+
+	knownDevices[numKnownDevices].name = strdup(intfs[s].ifr_name);
+	knownDevices[numKnownDevices].model = NULL;
+	knownDevices[numKnownDevices++].class = DEVICE_NET;
+    }
+
+    return 0;
+}
+
+static int findIdeList(void) {
+    DIR * dir;
+    char path[80];
+    int fd, i;
+    struct dirent * ent;
+    struct device device;
+
+    if (access("/proc/ide", R_OK)) return 0;
+
+    if (!(dir = opendir("/proc/ide"))) {
+        logMessage("failed to open /proc/ide for reading");
+	return 1;
+    }
+
+    /* set errno to 0, so we can tell when readdir() fails */
+    errno = 0;
+    while ((ent = readdir(dir))) {
+    	if (!deviceKnown(ent->d_name)) {
+	    sprintf(path, "/proc/ide/%s/media", ent->d_name);
+	    if ((fd = open(path, O_RDONLY)) >= 0) {
+		i = read(fd, path, 50);
+		close(fd);
+		path[i - 1] = '\0';		/* chop off trailing \n */
+
+		device.class = DEVICE_UNKNOWN;
+		if (!strcmp(path, "cdrom")) 
+		    device.class = DEVICE_CDROM;
+		else if (!strcmp(path, "disk"))
+		    device.class = DEVICE_DISK;
+
+		if (device.class != DEVICE_UNKNOWN) {
+		    device.name = strdup(ent->d_name);
+
+		    sprintf(path, "/proc/ide/%s/model", ent->d_name);
+		    if ((fd = open(path, O_RDONLY)) >= 0) {
+			i = read(fd, path, 50);
+			close(fd);
+			path[i - 1] = '\0';	/* chop off trailing \n */
+			device.model = strdup(path);
+		    }
+
+		    knownDevices[numKnownDevices++] = device;
+		}
+	    }
+	}
+
+        errno = 0;          
+    }
+
+    closedir(dir);
+
+    return 0;
+}
+
+#define SCSISCSI_TOP	0
+#define SCSISCSI_HOST 	1
+#define SCSISCSI_VENDOR 2
+#define SCSISCSI_TYPE 	3
+
+int findScsiList(void) {
+    int fd;
+    char buf[16384];
+    char linebuf[80];
+    char typebuf[10];
+    int i, state = SCSISCSI_TOP;
+    char * start, * chptr, * next, *end;
+    char driveName = 'a';
+    char cdromNum = '0';
+    char tapeNum = '0';
+
+    if (access("/proc/scsi/scsi", R_OK)) return 0;
+
+    fd = open("/proc/scsi/scsi", O_RDONLY);
+    if (fd < 0) return 1;
+    
+    i = read(fd, buf, sizeof(buf) - 1);
+    if (i < 1) {
+        close(fd);
+	return 1;
+    }
+    close(fd);
+    buf[i] = '\0';
+
+    start = buf;
+    while (*start) {
+	chptr = start;
+ 	while (*chptr != '\n') chptr++;
+	*chptr = '\0';
+	next = chptr + 1;
+
+	switch (state) {
+	  case SCSISCSI_TOP:
+	    if (strcmp("Attached devices: ", start)) {
+		logMessage("unexpected line in /proc/scsi/scsi: %s", start);
+		return LOADER_ERROR;
+	    }
+	    state = SCSISCSI_HOST;
+	    break;
+
+	  case SCSISCSI_HOST:
+	    if (strncmp("Host: ", start, 6)) {
+		logMessage("unexpected line in /proc/scsi/scsi: %s", start);
+		return LOADER_ERROR;
+	    }
+
+	    start = strstr(start, "Id: ");
+	    if (!start) {
+		logMessage("Id: missing in /proc/scsi/scsi");
+		return LOADER_ERROR;
+	    }
+	    start += 4;
+
+	    /*id = strtol(start, NULL, 10);*/
+
+	    state = SCSISCSI_VENDOR;
+	    break;
+
+	  case SCSISCSI_VENDOR:
+	    if (strncmp("  Vendor: ", start, 10)) {
+		logMessage("unexpected line in /proc/scsi/scsi: %s", start);
+		return LOADER_ERROR;
+	    }
+
+	    start += 10;
+	    end = chptr = strstr(start, "Model:");
+	    if (!chptr) {
+		logMessage("Model missing in /proc/scsi/scsi");
+		return LOADER_ERROR;
+	    }
+
+	    chptr--;
+	    while (*chptr == ' ') chptr--;
+	    *(chptr + 1) = '\0';
+
+	    strcpy(linebuf, start);
+	    *linebuf = toupper(*linebuf);
+	    chptr = linebuf + 1;
+	    while (*chptr) {
+		*chptr = tolower(*chptr);
+		chptr++;
+	    }
+
+	    start = end;  /* beginning of "Model:" */
+	    start += 7;
+		
+	    chptr = strstr(start, "Rev:");
+	    if (!chptr) {
+		logMessage("Rev missing in /proc/scsi/scsi");
+		return LOADER_ERROR;
+	    }
+	   
+	    chptr--;
+	    while (*chptr == ' ') chptr--;
+	    *(chptr + 1) = '\0';
+
+	    strcat(linebuf, " ");
+	    strcat(linebuf, start);
+
+	    state = SCSISCSI_TYPE;
+
+	    break;
+
+	  case SCSISCSI_TYPE:
+	    if (strncmp("  Type:", start, 7)) {
+		logMessage("unexpected line in /proc/scsi/scsi: %s", start);
+		return LOADER_ERROR;
+	    }
+	    *typebuf = '\0';
+	    if (strstr(start, "Direct-Access")) {
+		sprintf(typebuf, "sd%c", driveName++);
+		knownDevices[numKnownDevices].class = DEVICE_DISK;
+	    } else if (strstr(start, "Sequential-Access")) {
+		sprintf(typebuf, "st%c", tapeNum++);
+		knownDevices[numKnownDevices].class = DEVICE_DISK;
+	    } else if (strstr(start, "CD-ROM")) {
+		sprintf(typebuf, "scd%c", cdromNum++);
+		knownDevices[numKnownDevices].class = DEVICE_CDROM;
+	    }
+
+	    if (*typebuf && !deviceKnown(typebuf)) {
+		knownDevices[numKnownDevices].name = strdup(typebuf);
+		knownDevices[numKnownDevices].model = strdup(linebuf);
+
+		/* Do we need this for anything?
+		sdi[numMatches].bus = 0;
+		sdi[numMatches].id = id;
+		*/
+
+		numKnownDevices++;
+	    }
+
+	    state = SCSISCSI_HOST;
+	}
+
+	start = next;
+    }
+
+    return 0;
+}
+
+static int detectHardware(struct moduleInfo *** modules) {
     struct pciDevice **devices, *device;
+    struct moduleInfo * mod, ** modList;
+    int numMods, i;
 
     if (probePciReadDrivers(testing ? "../isys/pci/pcitable" :
 			              "/etc/pcitable")) {
-	newtWinMessage(_("Error"), _("OK"),
-		       _("An error occured while reading the PCI ID table"));
+        logMessage("An error occured while reading the PCI ID table");
 	return LOADER_ERROR;
     }
     
-    devices = probePci(0, 1);
+    devices = probePci(0, 0);
     if (devices == NULL) {
-	printf("No PCI devices found\n");
+        *modules = NULL;
 	return LOADER_OK;
     }
+
+    modList = malloc(sizeof(*modList) * 50);	/* should be enough */
+    numMods = 0;
+
     while ((device = *devices++)) {
-	if (!testing) {
-	    insmod(device->driver, NULL);
-	    newtPopWindow();
-	} else {
-	    newtWinMessage("Testing", "OK",
-			   "Test mode: I would run insmod(%s, args);\n",
-			   device->driver);
+	if ((mod = isysFindModuleInfo(device->driver))) {
+	    for (i = 0; i < numMods; i++) 
+	        if (modList[i] == mod) break;
+	    if (i == numMods) 
+		modList[numMods++] = mod;
 	}
     }
-    return LOADER_OK;
-}
 
-static int selectMedia(struct loaderState *state) {
-    return LOADER_OK;
-}
+    if (numMods) {
+        *modules = modList;
+	modList[numMods]->moduleName = NULL;
+    } else {
+        free(modList);
+	*modules = NULL;
+    }
 
-static int selectInterface(struct loaderState *state) {
-    int rc;
-    
-    rc = newtWinChoice(_("Install Interface"), _("Text"), _("Graphical"),
-		       _("You can install Red Hat Linux using one of two "
-			 "interfaces, Text or Graphical.  The text mode "
-			 "is similar to older Red Hat Linux installers. "
-			 "The Graphical installer is new and offers point "
-			 "and click installation of Red Hat Linux."));
-    if (rc == 1 || rc == 0)
-	state->interface = INTERFACE_TEXT;
-    else
-	state->interface = INTERFACE_GUI;
-    
+    free(devices);
+
     return LOADER_OK;
 }
 
@@ -128,16 +344,16 @@ int main(int argc, char ** argv) {
     char * anacondaArgs[30];
     char * arg;
     poptContext optCon;
-    int network, local, rc;
-    struct intfInfo eth0;    
-    struct loaderState state;
+    int network = 0;
+    int local = 0;
+    int i, rc;
+    struct moduleInfo ** modList;
     struct poptOption optionTable[] = {
 	    { "test", '\0', POPT_ARG_NONE, &testing, 0 },
 	    { "network", '\0', POPT_ARG_NONE, &network, 0 },
 	    { "local", '\0', POPT_ARG_NONE, &local, 0 },
 	    { 0, 0, 0, 0, 0 }
     };
-    int step = 0;
 
     optCon = poptGetContext(NULL, argc, argv, optionTable, 0);
 
@@ -153,74 +369,47 @@ int main(int argc, char ** argv) {
 	exit(1);
     }
 
+    if (isysReadModuleInfo("/boot/module-info")) {
+        fprintf(stderr, "failed to read /boot/module-info");
+	sleep(5);
+	exit(1);
+    }
+
     openLog(testing);
-    
-    newtInit();
-    newtCls();
-    newtDrawRootText(0, 0, _("Welcome to Red Hat Linux"));
-    
-    newtPushHelpLine(_("  <Tab>/<Alt-Tab> between elements  | <Space> selects | <F12> next screen "));
 
-    while (step < numSteps) {
-	rc = loaderSteps[step].fn(&state);
-	switch (rc) {
-	case LOADER_OK:
-	    step++;
-	    break;
-	case LOADER_BACK:
-	    step--;
-	    break;
-	case LOADER_ERROR:
-	    newtWinMessage(_("Error"), _("OK"),
-			   _("An error occured while running the '%s' step"),
-			   loaderSteps[step].name);
-	    break;
-	}
-    }
-    
-    if (!testing) {
-	strcpy(eth0.device, "eth0");
-	eth0.isPtp=0;
-	eth0.isUp=0;
-	eth0.ip.s_addr=inet_addr("207.175.42.47");
-	eth0.netmask.s_addr=inet_addr("255.255.254.0");
-	eth0.broadcast.s_addr=inet_addr("207.175.42.255");
-	eth0.network.s_addr=inet_addr("207.175.42.0");
+    logMessage("looking around for statically compiled devices");
 
-	configureNetDevice(&eth0);
+    findIdeList();
+    findScsiList();
+    findNetList();
 
-	mkdir("/mnt", 777);
-	mkdir("/mnt/source", 777);
-
-	insmod("sunrpc.o", NULL);
-	insmod("lockd.o", NULL);
-	insmod("nfs.o", NULL);
-    
-	doPwMount("207.175.42.68:/mnt/test/msw/i386",
-		  "/mnt/source", "nfs", 1, 0, NULL, NULL);
-
-	symlink("mnt/source/RedHat/instimage/usr", "/usr");
-	symlink("mnt/source/RedHat/instimage/lib", "/lib");
-
-	if (access("/usr/bin/anaconda", R_OK)) {
-	    perror("NFS mount does not appear to be a Red Hat 6.1 tree");
-	    exit (1);
+    if (!access("/proc/bus/pci/devices", R_OK)) {
+        /* autodetect whatever we can */
+        if (detectHardware(&modList)) {
+	    fprintf(stderr, "failed to scan for pci devices\n");
+	    sleep(5);
+	    exit(1);
+	} else if (modList) {
+	    printf("here\n");
+	    for (i = 0; modList[i]->moduleName; i++)
+	        printf("should try %s\n", modList[i]->moduleName);
 	}
     }
 
-    argptr = anacondaArgs;
-    *argptr++ = testing ? "../anaconda" : "/usr/bin/anaconda";
-    *argptr++ = "-p";
-    *argptr++ = "/mnt/source";
-    *argptr++ = (state.interface == INTERFACE_GUI) ? "-g" : "-T";
+    for (i = 0; i < numKnownDevices; i++) {
+    	printf("%-5s ", knownDevices[i].name);
+	if (knownDevices[i].class == DEVICE_CDROM)
+	    printf("cdrom");
+	else if (knownDevices[i].class == DEVICE_DISK)
+	    printf("disk ");
+	else if (knownDevices[i].class == DEVICE_NET)
+	    printf("net  ");
+    	if (knownDevices[i].model)
+	    printf(" %s\n", knownDevices[i].model);
+	else
+	    printf("\n");
+    }
 
-    newtFinished();
-    
-    printf("Launching anaconda (%s), please wait...\n", anacondaArgs[0]);
-
-    execv(anacondaArgs[0], anacondaArgs);
-    perror("exec");
- 
-    return 1;
+    closeLog();
 }
 
