f1f25f6b6cd5ac0613e1853bf5075875fff5535c
Erik Troan <ewt@redhat.com>
1999-08-06 19:31:18 +0000

    moved loader flags and test macros here

--- loader/loader.h
+++ loader/loader.h
@@ -2,4 +2,11 @@
 #define LOADER_BACK 1
 #define LOADER_ERROR -1
 
+#define LOADER_FLAGS_TESTING		(1 << 0)
+#define LOADER_FLAGS_EXPERT		(1 << 1)
+#define LOADER_FLAGS_TEXT		(1 << 2)
+
+#define FL_TESTING(a) ((a) && LOADER_FLAGS_TESTING)
+#define FL_EXPERT(a) ((a) && LOADER_FLAGS_EXPERT)
+#define FL_TEXT(a) ((a) && LOADER_FLAGS_TEXT)
 
