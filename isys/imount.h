f5dd4dcfdd7fe8859ed9fbcb929bf472f3ab3b68
Erik Troan <ewt@redhat.com>
1999-08-07 17:17:10 +0000

    include sys/mount.h

--- isys/imount.h
+++ isys/imount.h
@@ -4,6 +4,8 @@
 #define IMOUNT_ERR_ERRNO	1
 #define IMOUNT_ERR_OTHER	2
 
+#include <sys/mount.h>		/* for umount() */
+
 int doPwMount(char * dev, char * where, char * fs, int rdonly, int istty,
 		     char * acct, char * pw);
 
