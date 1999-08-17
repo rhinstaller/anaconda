afec71827a6fe88d66f52ea97ad838f84ff0f1a7
pnfisher <pnfisher>
1999-08-17 14:57:20 +0000

    added

---
+++ xkb.py
@@ -0,0 +1,27 @@
+import _xkb
+import tree
+import string
+
+class XKB:
+    def __init__ (self):
+        self.rules = _xkb.list_rules ()
+
+    def getRules (self):
+        return self.rules
+
+    def getModels (self):
+        return self.rules[0]
+
+    def getLayouts (self):
+        return self.rules[1]
+
+    def getVariants (self):
+        return self.rules[2]
+
+    def getOptions (self):
+        keys = self.rules[3].keys (); keys.sort ()
+        groups = ()
+        for x in keys:
+            groups = tree.merge (groups, string.split (x, ":"))
+        return (groups, self.rules[3])
+        
