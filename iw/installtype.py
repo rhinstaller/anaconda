dd3e5c02a2086408d1d422b65a3640cf03a280af
pnfisher <pnfisher>
1999-08-19 18:28:19 +0000

    added

---
+++ iw/installtype.py
@@ -0,0 +1,37 @@
+from gtk import *
+from iw import *
+from thread import *
+
+WORKSTATION_GNOME = 1
+WORKSTATION_KDE   = 2
+SERVER            = 3
+CUSTOM            = 4
+
+class InstallTypeWindow (InstallWindow):
+
+    def __init__ (self, ics):
+	InstallWindow.__init__ (self, ics)
+
+        self.todo = ics.getToDo ()
+        ics.setTitle ("Installation Type")
+	ics.setNextEnabled (TRUE)
+
+        self.installTypes = ((WORKSTATION_GNOME, "Workstation (Gnome)"),
+                             (WORKSTATION_KDE, "Workstation (KDE)"),
+                             (SERVER, "Server"),
+                             (CUSTOM, "Custom"))
+
+        self.type = self.installTypes[0][0]
+
+    def typeSelected (self, button, data):
+        self.type = data
+        
+    def getScreen (self):
+        box = GtkVBox (FALSE, 10)
+        group = None
+        for i in range (len (self.installTypes)):
+            group = GtkRadioButton (group, self.installTypes[i][1])
+            group.connect ("clicked", self.typeSelected, self.installTypes[i][0])
+            box.pack_start (group, FALSE)
+        return box
+
