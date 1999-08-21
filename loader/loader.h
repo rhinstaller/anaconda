68f00d4352d68b4c1181ea0e739ca25cdc7a24ed
Matt Wilson <msw@redhat.com>
1999-08-21 19:36:41 +0000

    added rescue stuff

--- loader/loader.h
+++ loader/loader.h
@@ -5,8 +5,10 @@
 #define LOADER_FLAGS_TESTING		(1 << 0)
 #define LOADER_FLAGS_EXPERT		(1 << 1)
 #define LOADER_FLAGS_TEXT		(1 << 2)
+#define LOADER_FLAGS_RESCUE		(1 << 3)
 
 #define FL_TESTING(a) ((a) & LOADER_FLAGS_TESTING)
 #define FL_EXPERT(a) ((a) & LOADER_FLAGS_EXPERT)
 #define FL_TEXT(a) ((a) & LOADER_FLAGS_TEXT)
+#define FL_RESCUE(a) ((a) & LOADER_FLAGS_RESCUE)
 
