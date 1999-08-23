9189036e60d9e98e88a6453fd259a1d77f177c9d
pnfisher <pnfisher>
1999-08-23 03:35:25 +0000

    Initial commit.

---
+++ iw/examine.py
@@ -0,0 +1,52 @@
+from gtk import *
+from iw import *
+
+class UpgradeExamineWindow (InstallWindow):		
+
+    def __init__ (self, ics):
+	InstallWindow.__init__ (self, ics)
+
+        ics.setTitle ("Upgrade Examine")
+
+    def toggled (self, widget, part):
+        if widget.get_active ():
+            self.root = part
+
+    def getNext (self):
+        self.todo.upgradeFindPackages (self.root)
+        if self.individualPackages.get_active ():
+            return IndividualPackageSelectionWindow
+        return None
+
+    def getScreen (self):
+        threads_leave ()
+        parts = self.todo.upgradeFindRoot ()
+        threads_enter ()
+
+	box = GtkHBox (FALSE)
+        if not parts:
+            box.pack_start (GtkLabel ("You don't have any Linux partitions.\n You can't upgrade this sytem!"),
+                            FALSE)
+            return box
+
+        self.ics.setNextEnabled (TRUE)
+        self.root = parts[0]
+        group = None
+        for part in parts:
+            group = GtkRadioButton (group, part)
+            group.connect ("toggled", self.toggled, part)
+            box.pack_start (part, FALSE)
+
+        sw = GtkScrolledWindow ()
+        sw.add_with_viewport (box)
+
+        vbox = GtkVBox (FALSE, 5)
+        self.individualPackages = GtkCheckButton ("Customized packages to be upgraded")
+        self.individualPackages.set_active (FALSE)
+        align = GtkAlignment (0.5, 0.5)
+        align.add (self.individualPackages)
+
+        vbox.pack_start (sw, TRUE)
+        vbox.pack_start (align, FALSE)
+
+        return vbox
---
+++ iw/installpath.py
@@ -0,0 +1,60 @@
+from gtk import *
+from iw import *
+from language import *
+from welcome import *
+from progress import *
+from package import *
+from network import *
+from account import *
+from rootpartition import *
+from auth import *
+from mouse import *
+from keyboard import *
+from format import *
+from congrats import *
+from autopartition import *
+from installtype import *
+from dependencies import *
+from lilo import *
+from examine import *
+
+UPGRADE = 0
+INSTALL = 1
+
+class InstallPathWindow (InstallWindow):		
+
+    def __init__ (self, ics):
+	InstallWindow.__init__ (self, ics)
+
+        ics.setTitle ("Install Path")
+        ics.setNextEnabled (1)
+
+        self.commonSteps = [LanguageWindow, KeyboardWindow, 
+                            WelcomeWindow, InstallPathWindow]
+
+        self.installSteps = [NetworkWindow, PartitionWindow, FormatWindow, PackageSelectionWindow,
+	                     UnresolvedDependenciesWindow, LiloWindow, AuthWindow, AccountWindow,
+                             InstallProgressWindow, CongratulationWindow]
+
+	self.upgradeSteps = [UpgradeExamineWindow,
+	                     UnresolvedDependenciesWindow, InstallProgressWindow,
+                             CongratulationWindow]
+
+    def toggled (self, widget, type):
+        if not widget.get_active (): return
+        if type == INSTALL:
+            self.ics.getICW ().setStateList (self.commonSteps + self.installSteps, len (self.commonSteps)-1)
+        else:
+            self.ics.getICW ().setStateList (self.commonSteps + self.upgradeSteps, len (self.commonSteps)-1)
+
+    def getScreen (self):
+	box = GtkVBox (FALSE, 5)
+	group = GtkRadioButton (None, "Install")
+        group.connect ("toggled", self.toggled, INSTALL)
+        self.toggled (group, INSTALL)
+	box.pack_start (group, FALSE)
+	group = GtkRadioButton (group, "Upgrade")
+        group.connect ("toggled", self.toggled, UPGRADE)
+        box.pack_start (group, FALSE)
+
+        return box
