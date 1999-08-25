4334b258c742b99f789d6619168fbc7d49d535ad
Matt Wilson <msw@redhat.com>
1999-08-25 19:55:27 +0000

    check for bad blocks

--- iw/format.py
+++ iw/format.py
@@ -3,12 +3,15 @@ from iw import *
 from thread import *
 import isys
 
+def _(x):
+    return x
+
 class FormatWindow (InstallWindow):
     def __init__ (self, ics):
 	InstallWindow.__init__ (self, ics)
 
         self.todo = ics.getToDo ()
-        ics.setTitle ("Choose partitions to Format")
+        ics.setTitle (_("Choose partitions to Format"))
         ics.setNextEnabled (1)
         ics.setHTML ("<HTML><BODY>Choose partitions to Format</BODY></HTML>")
 
@@ -20,6 +23,9 @@ class FormatWindow (InstallWindow):
             else:
                 (dev, fstype, format) = todo.mounts[mount]
                 todo.mounts[mount] = (dev, fstype, 0)
+
+        def check (widget, todo):
+            todo.badBlockCheck = widget.get_active ()
 
         box = GtkVBox (FALSE, 10)
 
@@ -35,6 +41,13 @@ class FormatWindow (InstallWindow):
                 box.pack_start (checkButton)
 
         vbox = GtkVBox (FALSE, 10)
-        vbox.pack_start (box, FALSE)
+        vbox.pack_start (box, FALSE, TRUE)
+        
+        self.check = GtkCheckButton (_("Check for bad blocks while formatting"))
+        self.check.set_active (self.todo.badBlockCheck)
+        self.check.connect ("toggled", check, self.todo)
+        vbox.pack_start (self.check, FALSE)
+        
+        self.check = GtkCheckButton 
 
         return vbox
