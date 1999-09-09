3e507fcde9690216b766d0d3c984dcba19f94bcd
Matt Wilson <msw@redhat.com>
1999-09-09 21:14:29 +0000

    always return GCONV_OK

--- loader/stubs.c
+++ loader/stubs.c
@@ -18,22 +18,24 @@ void *__libc_stack_end;
 int
 __gconv_OK () {return GCONV_OK;}
 
+/*
 int
 __gconv_NOCONV () {return GCONV_NOCONV;}
+*/
 
 strong_alias (__gconv_OK, __gconv_close_transform); 
-strong_alias (__gconv_NOCONV, __gconv);
-strong_alias (__gconv_NOCONV, __gconv_find_transform);
-strong_alias (__gconv_NOCONV, __gconv_open);
-strong_alias (__gconv_NOCONV, __gconv_transform_ascii_internal);
-strong_alias (__gconv_NOCONV, __gconv_transform_internal_ascii);
-strong_alias (__gconv_NOCONV, __gconv_transform_internal_ucs2);
-strong_alias (__gconv_NOCONV, __gconv_transform_internal_ucs2little);
-strong_alias (__gconv_NOCONV, __gconv_transform_internal_ucs4);
-strong_alias (__gconv_NOCONV, __gconv_transform_internal_utf16);
-strong_alias (__gconv_NOCONV, __gconv_transform_internal_utf8);
-strong_alias (__gconv_NOCONV, __gconv_transform_ucs2_internal);
-strong_alias (__gconv_NOCONV, __gconv_transform_ucs2little_internal);
-strong_alias (__gconv_NOCONV, __gconv_transform_utf16_internal);
-strong_alias (__gconv_NOCONV, __gconv_transform_utf8_internal);
+strong_alias (__gconv_OK, __gconv);
+strong_alias (__gconv_OK, __gconv_find_transform);
+strong_alias (__gconv_OK, __gconv_open);
+strong_alias (__gconv_OK, __gconv_transform_ascii_internal);
+strong_alias (__gconv_OK, __gconv_transform_internal_ascii);
+strong_alias (__gconv_OK, __gconv_transform_internal_ucs2);
+strong_alias (__gconv_OK, __gconv_transform_internal_ucs2little);
+strong_alias (__gconv_OK, __gconv_transform_internal_ucs4);
+strong_alias (__gconv_OK, __gconv_transform_internal_utf16);
+strong_alias (__gconv_OK, __gconv_transform_internal_utf8);
+strong_alias (__gconv_OK, __gconv_transform_ucs2_internal);
+strong_alias (__gconv_OK, __gconv_transform_ucs2little_internal);
+strong_alias (__gconv_OK, __gconv_transform_utf16_internal);
+strong_alias (__gconv_OK, __gconv_transform_utf8_internal);
 
