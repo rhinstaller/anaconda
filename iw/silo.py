f02fc362d6c25bd1891bda32e86b8181909dcdf0
Matt Wilson <msw@redhat.com>
1999-09-22 06:26:13 +0000

    from jj

---
+++ iw/silo.py
@@ -0,0 +1,222 @@
+from iw import *
+from gtk import *
+from gui import _
+from xpms import SMALL_CHECK
+import GdkImlib
+
+class SiloWindow (InstallWindow):
+    foo = GdkImlib.create_image_from_xpm (SMALL_CHECK)
+    foo.render()
+    checkMark = foo.make_pixmap()
+    del foo
+
+    def __init__ (self, ics):
+	InstallWindow.__init__ (self, ics)
+
+        ics.readHTML ("silo")
+
+        ics.setTitle (_("Silo Configuration"))
+        ics.setNextEnabled (1)
+        self.type = None
+        self.bootdisk = None
+        self.silo = None
+
+    def getNext (self):
+        # XXX
+        if not self.bootdisk: return None
+
+        if self.bootdisk.get_active ():
+            self.todo.bootdisk = 1
+        else:
+            self.todo.bootdisk = 0
+
+        if self.silo.get_active ():
+            self.todo.setLiloLocation (None)
+        else:
+            if self.mbr.get_active ():
+                self.todo.setLiloLocation ("mbr")
+            else:
+                self.todo.setLiloLocation ("partition")
+
+	self.todo.setLiloImages(self.images)
+
+    def typeName(self, type):
+	if (type == 2):
+	    return "Linux Native"
+	elif (type == 6):
+	    return "SunOS/Solaris"
+	else:
+	    return "Other"
+
+    def toggled (self, widget, *args):
+        if widget.get_active ():
+	    state = FALSE
+        else:
+	    state = TRUE
+
+	for n in [self.radioBox, self.editBox, self.imageList ]:
+            n.set_sensitive (state)
+
+    def labelUpdated(self, *args):
+	index = self.imageList.selection[0]
+	device = self.imageList.get_text(index, 1)
+
+	label = self.labelEntry.get_text()
+	self.imageList.set_text(index, 3, label)
+
+        if label:
+            self.defaultCheck.set_sensitive (TRUE)
+        else:
+            self.defaultCheck.set_sensitive (FALSE)
+
+    def defaultUpdated(self, *args):
+	if self.ignoreSignals: return
+
+	index = self.imageList.selection[0]
+
+	if self.defaultCheck.get_active():
+	    if self.default != None:
+		self.imageList.set_text(self.default, 0, "")
+
+	    self.imageList.set_pixmap(index, 0, self.checkMark)
+	    self.default = index
+	else:
+	    self.imageList.set_text(index, 0, "")
+	    self.default = None
+
+    def labelSelected(self, *args):
+	index = self.imageList.selection[0]
+	device = self.imageList.get_text(index, 1)
+	label = self.imageList.get_text(index, 3)
+
+	self.deviceLabel.set_text(_("Partition") + ": " + device)
+	device = device[5:]
+
+	type = self.images[device][1]
+
+	self.typeLabel.set_text(_("Type") + ":" + self.typeName(type))
+	self.labelEntry.set_text(label)
+
+        if not label:
+            self.defaultCheck.set_sensitive (FALSE)
+
+        self.ignoreSignals = 1
+	if index == self.default:
+	    self.defaultCheck.set_active(1)
+	else:
+	    self.defaultCheck.set_active(0)
+        self.ignoreSignals = 0
+
+    def getScreen (self):
+	self.images = self.todo.getLiloImages()
+        self.ignoreSignals = 0
+
+        if '/' not in self.todo.mounts.keys (): return None
+	(bootpart, boothd) = self.todo.getLiloOptions()
+            
+        format = "/dev/%s"
+
+        self.radioBox = GtkTable(2, 6)
+        self.radioBox.set_border_width (5)
+        
+	spacer = GtkLabel("")
+	spacer.set_usize(10, 1)
+	self.radioBox.attach(spacer, 0, 1, 2, 4, FALSE)
+
+	label = GtkLabel(_("Install SILO boot record on:"))
+	label.set_alignment(0.0, 0.5)
+	self.radioBox.attach(label, 0, 2, 1, 2)
+
+        part = GtkRadioButton(None,
+	    ("/dev/%s %s" % (bootpart, 
+		_("First sector of boot partition (recommended)"))))
+        self.mbr = GtkRadioButton(part, 
+	    ("/dev/%s %s" % (boothd, _("Master Boot Record (MBR)"))))
+	self.radioBox.attach(part, 1, 2, 2, 3)
+	self.radioBox.attach(self.mbr, 1, 2, 3, 4)
+
+	label = GtkLabel(_("Kernel parameters") + ":")
+	label.set_alignment(0.0, 0.5)
+	self.appendEntry = GtkEntry(15)
+	box = GtkHBox(FALSE, 5)
+	box.pack_start(label)
+	box.pack_start(self.appendEntry)
+	alignment = GtkAlignment()
+	alignment.set(0.0, 0.5, 0, 1.0)
+	alignment.add(box)
+	self.radioBox.attach(alignment, 0, 2, 5, 6)
+	
+        box = GtkVBox (FALSE, 0)
+
+        optionBox = GtkVBox (FALSE, 5)
+        optionBox.set_border_width (5)
+        self.bootdisk = GtkCheckButton (_("Create boot disk"))
+        self.bootdisk.set_active (TRUE)
+        optionBox.pack_start (self.bootdisk)
+
+        self.silo = GtkCheckButton (_("Do not install SILO"))
+        self.silo.set_active (FALSE)
+        self.silo.connect ("toggled", self.toggled)
+        optionBox.pack_start (self.silo, FALSE)
+
+        box.pack_start (optionBox, FALSE)
+
+        box.pack_start (GtkHSeparator (), FALSE)
+        box.pack_start (self.radioBox, FALSE)
+
+	self.imageList = GtkCList (4,
+	    ( _("Default"), _("Device"), _("Partition type"), _("Boot label")))
+        self.imageList.set_selection_mode (SELECTION_BROWSE)
+
+	sortedKeys = self.images.keys()
+	sortedKeys.sort()
+
+        self.default = None
+	count = 0
+	for n in sortedKeys:
+	    (label, type) = self.images[n]
+	    self.imageList.append(("", "/dev/" + n, self.typeName(type), 
+				    label))
+	    if (label == "linux"):
+		self.default = count
+		self.imageList.set_pixmap(count, 0, self.checkMark)
+	    count = count + 1
+
+        self.imageList.columns_autosize ()
+        self.imageList.column_title_passive (1)
+        self.imageList.set_border_width (5)
+	self.imageList.connect("select_row", self.labelSelected)
+	self.imageList.set_column_justification(2, JUSTIFY_CENTER)
+
+	self.deviceLabel = GtkLabel(_("Partition") + ":")
+	self.typeLabel = GtkLabel(_("Type") + ":")
+
+	tempBox = GtkHBox(TRUE)
+	self.deviceLabel.set_alignment(0.0, 0.0)
+	self.typeLabel.set_alignment(0.0, 0.0)
+	tempBox.pack_start(self.deviceLabel, FALSE)
+	tempBox.pack_start(self.typeLabel, FALSE)
+	self.defaultCheck = GtkCheckButton("Default boot image")
+	self.defaultCheck.connect("toggled", self.defaultUpdated)
+
+	# Alliteration!
+	self.labelLabel = GtkLabel(_("Boot label") + ":")
+	self.labelEntry = GtkEntry(15)
+	self.labelEntry.connect("changed", self.labelUpdated)
+
+	tempBox2 = GtkHBox(FALSE, 5)
+	self.labelLabel.set_alignment(0.0, 0.5)
+	tempBox2.pack_start(self.labelLabel, FALSE)
+	tempBox2.pack_start(self.labelEntry, FALSE)
+
+	self.editBox = GtkVBox ()
+	self.editBox.pack_start (tempBox, FALSE)
+	self.editBox.pack_start (self.defaultCheck, FALSE)
+	self.editBox.pack_start (tempBox2, FALSE)
+	self.editBox.set_border_width (5)
+
+        box.pack_start (GtkHSeparator (), FALSE)
+        box.pack_start (self.editBox, FALSE)
+        box.pack_start (self.imageList, TRUE)
+
+        return box
---
+++ textw/silo.py
@@ -0,0 +1,197 @@
+import gettext
+import iutil
+from snack import *
+from textw.constants import *
+from text import _
+
+#cat = gettext.Catalog ("anaconda", "/usr/share/locale")
+#_ = cat.gettext
+
+class SiloAppendWindow:
+
+    def __call__(self, screen, todo):
+	t = TextboxReflowed(53,
+		     _("A few systems will need to pass special options "
+		       "to the kernel at boot time for the system to function "
+		       "properly. If you need to pass boot options to the "
+		       "kernel, enter them now. If you don't need any or "
+		       "aren't sure, leave this blank."))
+
+	entry = Entry(48, scroll = 1, returnExit = 1)
+	buttons = ButtonBar(screen, [(_("OK"), "ok"), (_("Skip"), "skip"),  
+			     (_("Back"), "back") ] )
+
+	grid = GridForm(screen, _("SILO Configuration"), 1, 3)
+	grid.add(t, 0, 0)
+	grid.add(entry, 0, 1, padding = (0, 0, 0, 1))
+	grid.add(buttons, 0, 2, growx = 1)
+
+        result = grid.runOnce ()
+        button = buttons.buttonPressed(result)
+        
+        if button == "back":
+            return INSTALL_BACK
+
+	if button == "skip":
+	    todo.skipLilo = 1
+	    todo.liloDevice = None
+	else:
+	    todo.skipLilo = 0
+
+	return INSTALL_OK
+
+class SiloWindow:
+    def __call__(self, screen, todo):
+        if '/' not in todo.mounts.keys (): return INSTALL_NOOP
+	if todo.skipLilo: return INSTALL_NOOP
+
+	(bootpart, boothd) = todo.getLiloOptions()
+
+	if (todo.getLiloLocation () == "mbr"):
+	    default = 1
+	elif (todo.getLiloLocation () == "partition"):
+	    default = 0
+	else:
+	    default = 1
+            
+        format = "/dev/%-11s %s" 
+        locations = []
+        locations.append (format % (bootpart, "First sector of boot partition"))
+        locations.append (format % (boothd, "Master Boot Record (MBR)"))
+
+	title = _("SILO Configuration")
+        (rc, sel) = ListboxChoiceWindow (screen, title,
+                                         _("Where do you want to install the bootloader?"),
+                                         locations, default = default,
+                                         buttons = [ _("OK"), _("Back") ])
+
+        if sel == 1:
+            todo.setLiloLocation("mbr")
+        else:
+            todo.setLiloLocation("partition")
+
+        if rc == string.lower (_("Back")):
+            return INSTALL_BACK
+        return INSTALL_OK
+
+class SiloImagesWindow:
+    def editItem(self, screen, partition, itemLabel):
+	devLabel = Label(_("Device") + ":")
+	bootLabel = Label(_("Boot label") + ":")
+	device = Label("/dev/" + partition)
+        newLabel = Entry (20, scroll = 1, returnExit = 1, text = itemLabel)
+
+	buttons = ButtonBar(screen, [_("Ok"), _("Clear"), _("Cancel")])
+
+	subgrid = Grid(2, 2)
+	subgrid.setField(devLabel, 0, 0, anchorLeft = 1)
+	subgrid.setField(device, 1, 0, padding = (1, 0, 0, 0), anchorLeft = 1)
+	subgrid.setField(bootLabel, 0, 1, anchorLeft = 1)
+	subgrid.setField(newLabel, 1, 1, padding = (1, 0, 0, 0), anchorLeft = 1)
+
+	g = GridForm(screen, _("Edit Boot Label"), 1, 2)
+	g.add(subgrid, 0, 0, padding = (0, 0, 0, 1))
+	g.add(buttons, 0, 1, growx = 1)
+
+	result = ""
+	while (result != string.lower(_("Ok")) and result != newLabel):
+	    result = g.run()
+	    if (buttons.buttonPressed(result)):
+		result = buttons.buttonPressed(result)
+
+	    if (result == string.lower(_("Cancel"))):
+		screen.popWindow ()
+		return itemLabel
+	    elif (result == string.lower(_("Clear"))):
+		newLabel.set("")
+
+	screen.popWindow()
+
+	return newLabel.value()
+
+    def formatDevice(self, type, label, device, default):
+	if (type == 2):
+	    type = "Linux extended"
+	elif (type == 6):
+	    type = "SunOS/Solaris"
+	else:
+	    type = "Other"
+
+	if default == device:
+	    default = '*'
+	else:
+	    default = ""
+	    
+	return "%-10s  %-25s %-7s %-10s" % ( "/dev/" + device, type, default, label)
+
+    def __call__(self, screen, todo):
+	images = todo.getLiloImages()
+	if not images: return INSTALL_NOOP
+	if todo.skipLilo: return INSTALL_NOOP
+
+	sortedKeys = images.keys()
+	sortedKeys.sort()
+
+	listboxLabel = Label("%-10s  %-25s %-7s %-10s" % 
+		( _("Device"), _("Partition type"), _("Default"), _("Boot label")))
+	listbox = Listbox(5, scroll = 1, returnExit = 1)
+
+	default = ""
+
+	for n in sortedKeys:
+	    (label, type) = images[n]
+	    listbox.append(self.formatDevice(type, label, n, default), n)
+
+	buttons = ButtonBar(screen, [ (_("Ok"), "ok"), (_("Edit"), "edit"), 
+				      (_("Back"), "back") ] )
+
+	text = TextboxReflowed(55, _("The boot manager Red Hat uses can boot other " 
+		      "operating systems as well. You need to tell me " 
+		      "what partitions you would like to be able to boot " 
+		      "and what label you want to use for each of them."))
+
+	title = _("SILO Configuration")
+	g = GridForm(screen, title, 1, 4)
+	g.add(text, 0, 0, anchorLeft = 1)
+	g.add(listboxLabel, 0, 1, padding = (0, 1, 0, 0), anchorLeft = 1)
+	g.add(listbox, 0, 2, padding = (0, 0, 0, 1), anchorLeft = 1)
+	g.add(buttons, 0, 3, growx = 1)
+	g.addHotKey("F2")
+
+	result = None
+	while (result != "ok" and result != "back" and result != "F12"):
+	    result = g.run()
+	    if (buttons.buttonPressed(result)):
+		result = buttons.buttonPressed(result)
+
+	    if (result == string.lower(_("Edit")) or result == listbox):
+		item = listbox.current()
+		(label, type) = images[item]
+		label = self.editItem(screen, item, label)
+		images[item] = (label, type)
+		if (default == item and not label):
+		    default = ""
+		listbox.replace(self.formatDevice(type, label, item, default), item)
+		listbox.setCurrent(item)
+	    elif result == "F2":
+		item = listbox.current()
+		(label, type) = images[item]
+		if (label):
+		    if (default):
+			(oldLabel, oldType) = images[default]
+			listbox.replace(self.formatDevice(oldType, oldLabel, default, 
+					""), default)
+		    default = item
+		    listbox.replace(self.formatDevice(type, label, item, default), 
+				    item)
+		    listbox.setCurrent(item)
+
+	screen.popWindow()
+
+	if (result == "back"):
+	    return INSTALL_BACK
+
+	todo.setLiloImages(images)
+
+	return INSTALL_OK
+
