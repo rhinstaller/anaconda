27537c495d5275f6cec9ac5ba36d61c2ac4ce22d
Matt Wilson <msw@redhat.com>
2000-02-05 15:25:40 +0000

    fix for missing XKB

--- gui.py
+++ gui.py
@@ -246,8 +246,10 @@ class InstallInterface:
         if todo.instClass.keyboard:
             from xkb import XKB
             kb = XKB()
-            (rules, model, layout, variant, options) = todo.keyboard.getXKB()
-            kb.setRule (model, layout, variant, "complete")
+	    info = todo.keyboard.getXKB()
+	    if info:
+                (rules, model, layout, variant, options) = info
+                kb.setRule (model, layout, variant, "complete")
         self.icw = InstallControlWindow (self, commonSteps, todo)
         self.icw.start ()
         self.finishedTODO.wait ()
