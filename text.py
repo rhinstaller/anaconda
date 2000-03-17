ea677e91667a6142c979afa53b9f2b6ed37f4056
Erik Troan <ewt@redhat.com>
2000-03-17 13:56:35 +0000

    turned killSelf on ^Z back on

--- text.py
+++ text.py
@@ -993,7 +993,7 @@ class InstallInterface:
         self.screen.pushHelpLine (_("  <Tab>/<Alt-Tab> between elements   |  <Space> selects   |  <F12> next screen"))
 # uncomment this line to make the installer quit on <Ctrl+Z>
 # handy for quick debugging.
-	#self.screen.suspendCallback(killSelf, self.screen)
+	self.screen.suspendCallback(killSelf, self.screen)
 # uncomment this line to drop into the python debugger on <Ctrl+Z>
 # --VERY handy--
 	#self.screen.suspendCallback(debugSelf, self.screen)
