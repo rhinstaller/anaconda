e96f9861b94282f7f22cb3fa661c69334b957aa1
Erik Troan <ewt@redhat.com>
2000-03-27 20:56:31 +0000

    copyfile should be static

--- isys/uncpio.c
+++ isys/uncpio.c
@@ -691,7 +691,7 @@ const char * myCpioStrerror(int rc)
     return msg;
 }
 
-int copyFile(struct ourfd * inFd, struct ourfd * outFd, 
+static int copyfile(struct ourfd * inFd, struct ourfd * outFd, 
 	     struct cpioHeader * chp, struct cpioCrcPhysicalHeader * pHdr) {
     char buf[8192];
     int amount;
