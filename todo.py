dc1dd64bd90fa1947751ca8fe8d5581fa4129935
Mike Fulbright <msf@redhat.com>
2000-04-03 20:47:34 +0000

    (drmike) Fixed kickstart setting root password using crypted value when md5 and shadow enabled (bug #10437)

--- todo.py
+++ todo.py
@@ -575,21 +575,13 @@ class ToDo:
 	if pure:
 	    self.setPassword("root", pure)
 	else:
-	    # we need to splice in an already crypted password for kickstart
-	    f = open (self.instPath + "/etc/passwd", "r")
-	    lines = f.readlines ()
-	    f.close ()
-	    index = 0
-	    for line in lines:
-		if line[0:4] == "root":
-		    entry = string.splitfields (line, ':')
-		    entry[1] = self.rootpassword.getCrypted ()
-		    lines[index] = string.joinfields (entry, ':')
-		    break
-		index = index + 1
-	    f = open (self.instPath + "/etc/passwd", "w")
-	    f.writelines (lines)
-	    f.close ()
+            crypt = self.rootpassword.getCrypted ()
+            devnull = os.open("/dev/null", os.O_RDWR)
+
+            argv = [ "/usr/sbin/usermod", "-p", crypt, "root" ]
+            iutil.execWithRedirect(argv[0], argv, root = self.instPath, 
+                                   stdout = devnull, stderr = None)
+            os.close(devnull)
 
     def setupAuthentication (self):
         args = [ "/usr/sbin/authconfig", "--kickstart", "--nostart" ]
