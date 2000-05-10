6f75d59f74ce5d7650838293a1095480e4722136
Erik Troan <ewt@redhat.com>
2000-05-10 23:41:16 +0000

    1) split into nfs and cdrom methods 2) switch loopback fs to file in installed system

--- image.py
+++ image.py
@@ -1,8 +1,12 @@
 # Install method for disk image installs (CD & NFS)
 
 from comps import ComponentSet, HeaderListFromFile
+from installmethod import InstallMethod
+import iutil
+import os
+import isys
 
-class InstallMethod:
+class ImageInstallMethod(InstallMethod):
 
     def readComps(self, hdlist):
 	return ComponentSet(self.tree + '/RedHat/base/comps', hdlist)
@@ -13,14 +17,27 @@ class InstallMethod:
     def readHeaders(self):
 	return HeaderListFromFile(self.tree + "/RedHat/base/hdlist")
 
-    def targetFstab(self, fstab):
-	pass
+    def __init__(self, tree):
+	InstallMethod.__init__(self)
+	self.tree = tree
+
+class CdromInstallMethod(ImageInstallMethod):
+
+    def systemMounted(self, fstab, mntPoint):
+	target = "%s/rhinstall-stage2.img" % mntPoint
+	iutil.copyFile("%s/RedHat/base/stage2.img" % self.tree, target)
+	isys.makeDevInode("loop0", "/tmp/loop")
+	isys.lochangefd("/tmp/loop", target)
 
     def filesDone(self):
-	pass
-
-    def unlinkFilename(self, fullName):
-	pass
+	# this isn't the exact right place, but it's close enough
+	target = "%s/rhinstall-stage2.img" % mntPoint
+	os.unlink(target)
 
     def __init__(self, tree):
-	self.tree = tree
+	ImageInstallMethod.__init__(self, tree)
+
+class NfsInstallMethod(ImageInstallMethod):
+
+    def __init__(self, tree):
+	ImageInstallMethod.__init__(self, tree)
