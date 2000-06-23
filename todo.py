97a74931b87ed703e264e203d83998c876c78eee
Mike Fulbright <msf@redhat.com>
2000-06-23 19:13:54 +0000

    Ok have to setupAuthentication FIRST

--- todo.py
+++ todo.py
@@ -1233,11 +1233,10 @@ class ToDo:
         self.writeMouse ()
         self.writeKeyboard ()
         self.writeNetworkConfig ()
+        self.setupAuthentication ()
         self.writeRootPassword ()
         self.createAccounts ()
 
-        # should do this AFTER created accounts
-        self.setupAuthentication ()
         self.writeTimezone()
 
     def sortPackages(self, first, second):
