ea3a37a104e4c8fbc9ad5abedb7006bd2af52f58
Erik Troan <ewt@redhat.com>
2000-07-15 16:12:09 +0000

    new parameter for loading driver disks

--- loader/devices.h
+++ loader/devices.h
@@ -15,8 +15,8 @@ int devDeviceMenu(enum driverMajor type, moduleInfoSet modInfo,
 		  moduleList modLoaded, moduleDeps * modDepsPtr, 
 		  char * ddDevice, int flags, char ** moduleName);
 int devLoadDriverDisk(moduleInfoSet modInfo, moduleList modLoaded,
-		     moduleDeps *modDepsPtr, int flags, int cancelNotBack,
-		     char * device);
+		      moduleDeps *modDepsPtr, int flags, int cancelNotBack,
+		      int askForExistence, char * device);
 int devInitDriverDisk(moduleInfoSet modInfo, moduleList modLoaded, 
 		      moduleDeps *modDepsPtr, int flags, char * mntPoint,
 		      struct driverDiskInfo * ddi);
