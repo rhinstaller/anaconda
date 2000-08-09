d5f74b3f6b4400888b46db94bdb09c21378743c5
Erik Troan <ewt@redhat.com>
2000-08-09 14:13:34 +0000

    handle no FNM_PATHNAME

--- loader/fnmatch-stub.c
+++ loader/fnmatch-stub.c
@@ -8,7 +8,7 @@ int fnmatch(const char *pattern, const char *string, int flags)
 {
   const char *p, *q, *r;
 
-  if (flags == (FNM_PATHNAME | FNM_PERIOD)
+  if (!(flags & ~(FNM_PATHNAME | FNM_PERIOD))
       && strpbrk (pattern, "[?{") == NULL
       && (p = strchr (pattern, '*')) != NULL
       && strchr (p + 1, '*') == NULL)
@@ -17,11 +17,13 @@ int fnmatch(const char *pattern, const char *string, int flags)
 	return FNM_NOMATCH;
       q = strstr (string + (p - pattern), p + 1);
       r = strchr (string + (p - pattern), '/');
-      if (q == NULL || strlen (q) != strlen (p + 1)
-	  || (r != NULL && r < q))
+      if (q == NULL || strlen (q) != strlen (p + 1))
+	return FNM_NOMATCH;
+
+      if ((flags & FNM_PATHNAME) && (r != NULL && r < q))
 	return FNM_NOMATCH;
       return 0;
     }
   fprintf (stderr, "fnmatch stub does not support '%s' patterns\n", pattern);
-  exit (1);
+  abort ();
 }
