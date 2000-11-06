40ec129d4e408bd0e11f91e147967f0081777b35
Erik Troan <ewt@redhat.com>
2000-11-06 19:15:14 +0000

    *** empty log message ***

---
+++ installclasses/laptop.py
@@ -0,0 +1,31 @@
+from installclass import BaseInstallClass
+from installclass import FSEDIT_CLEAR_LINUX
+from translate import N_
+import os
+
+class InstallClass(BaseInstallClass):
+    name = N_("Laptop")
+    pixmap = "workstation.png"
+
+    sortPriority = 1
+
+    def __init__(self, expert):
+	BaseInstallClass.__init__(self)
+	self.setGroups(["Workstation Common"])
+	self.setHostname("localhost.localdomain")
+	if not expert:
+	    self.addToSkipList("lilo")
+	self.addToSkipList("authentication")
+	self.setMakeBootdisk(1)
+
+        self.showgroups = [ "KDE",
+                            "GNOME",
+                            "Games" ]
+
+	if os.uname ()[4] != 'sparc64':
+	    self.addNewPartition('/boot', (16, -1, 0), (None,-1,0), (0,0))
+	self.addNewPartition('/', (700, -1, 1), (None, -1, 0), (0,0))
+	self.addNewPartition('swap', (64, -1, 0), (None, -1, 0), (0,0))
+	self.setClearParts(FSEDIT_CLEAR_LINUX, 
+	    warningText = N_("You are about to erase any preexisting Linux "
+			     "installations on your system."))
