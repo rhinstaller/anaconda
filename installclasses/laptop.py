bf5981a5ff55a10b8eb80174f467d493572f208a
Erik Troan <ewt@redhat.com>
2000-11-20 16:20:53 +0000

    set as default on machines w/ pcmcia

--- installclasses/laptop.py
+++ installclasses/laptop.py
@@ -1,12 +1,16 @@
 import workstation
 from translate import N_
 import os
+import pcmcia
 
 class InstallClass(workstation.InstallClass):
     name = N_("Laptop")
     pixmap = "laptop-support.png"
 
     sortPriority = 5000
+
+    if pcmcia.pcicType():
+	default = 1
 
     def __init__(self, expert):
 	workstation.InstallClass.__init__(self, expert)
