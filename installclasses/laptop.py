3a95635fc82cfca64545d362a6118f9d1ec19b24
Erik Troan <ewt@redhat.com>
2000-11-20 15:15:55 +0000

    descend from normal workstation

--- installclasses/laptop.py
+++ installclasses/laptop.py
@@ -1,31 +1,12 @@
-from installclass import BaseInstallClass
-from installclass import FSEDIT_CLEAR_LINUX
+import workstation
 from translate import N_
 import os
 
-class InstallClass(BaseInstallClass):
+class InstallClass(workstation.InstallClass):
     name = N_("Laptop")
     pixmap = "laptop-support.png"
 
     sortPriority = 5000
 
     def __init__(self, expert):
-	BaseInstallClass.__init__(self)
-	self.setGroups(["Workstation Common"])
-	self.setHostname("localhost.localdomain")
-	if not expert:
-	    self.addToSkipList("lilo")
-	self.addToSkipList("authentication")
-	self.setMakeBootdisk(1)
-
-        self.showgroups = [ "KDE",
-                            "GNOME",
-                            "Games" ]
-
-	if os.uname ()[4] != 'sparc64':
-	    self.addNewPartition('/boot', (16, -1, 0), (None,-1,0), (0,0))
-	self.addNewPartition('/', (700, -1, 1), (None, -1, 0), (0,0))
-	self.addNewPartition('swap', (64, -1, 0), (None, -1, 0), (0,0))
-	self.setClearParts(FSEDIT_CLEAR_LINUX, 
-	    warningText = N_("You are about to erase any preexisting Linux "
-			     "installations on your system."))
+	workstation.InstallClass.__init__(self, expert)
