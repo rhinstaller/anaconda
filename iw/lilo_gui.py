d8fdc7ac97fed72c9db3f1fea7a511128cd31531
Mike Fulbright <msf@redhat.com>
2000-12-08 23:09:45 +0000

    fix label string

--- iw/lilo_gui.py
+++ iw/lilo_gui.py
@@ -353,7 +353,7 @@ class LiloWindow (InstallWindow):
         self.typeLabel.set_alignment(0.0, 0.0)
         tempBox.pack_start(self.deviceLabel, FALSE)
         tempBox.pack_start(self.typeLabel, FALSE)
-        self.defaultCheck = GtkCheckButton(_("Defaultcheck boot image"))
+        self.defaultCheck = GtkCheckButton(_("Default boot image"))
         self.defaultCheck.connect("toggled", self.defaultUpdated)
 
         # Alliteration!
