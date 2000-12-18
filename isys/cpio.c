e7c7b3358b3f19f1528a9b9123af30184861d11d
Mike Fulbright <msf@redhat.com>
2000-12-18 16:12:30 +0000

    reverting to 1.6, I screwed up a commit

--- isys/cpio.c
+++ isys/cpio.c
@@ -12,10 +12,6 @@ int installCpioFile(gzFile fd, char * cpioName, char * outName, int inWin) {
     int rc;
     const char * failedFile;
 
-    FILE *myfile;
-
-    myfile = fopen("/dev/tty3", "rw");
-
     if (outName) {
 	map.archivePath = cpioName;
 	map.fsPath = outName;
@@ -24,8 +20,6 @@ int installCpioFile(gzFile fd, char * cpioName, char * outName, int inWin) {
 
     rc = myCpioInstallArchive(fd, outName ? &map : NULL, 1, NULL, NULL, 
 			    &failedFile);
-
-    fprintf(myfile, "Failed file = %s\n", failedFile);
 
     if (rc || access(outName, R_OK)) {
 	return -1;
