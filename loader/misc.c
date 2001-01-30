2d8704857ed4a0a182b29c7eab696578dcd96a08
Erik Troan <ewt@redhat.com>
2001-01-30 19:12:39 +0000

    added readLine() [merge from 7.0 branch]

--- loader/misc.c
+++ loader/misc.c
@@ -9,21 +9,41 @@ int copyFile(char * source, char * dest) {
     int infd = -1, outfd = -1;
     char buf[4096];
     int i;
+    int rc = 0;
 
     outfd = open(dest, O_CREAT | O_RDWR, 0666);
     infd = open(source, O_RDONLY);
 
     if (infd < 0) {
 	logMessage("failed to open %s: %s", source, strerror(errno));
+	return 1;
+    } else if (outfd < 0) {
+	close(infd);
+	logMessage("failed to open %s: %s", dest, strerror(errno));
+	return 1;
     }
 
     while ((i = read(infd, buf, sizeof(buf))) > 0) {
-	if (write(outfd, buf, i) != i) break;
+	if (write(outfd, buf, i) != i) {
+	    rc = 1;
+	    break;
+	}
     }
 
     close(infd);
     close(outfd);
 
-    return 0;
+    return rc;
+}
+
+char * readLine(FILE * f) {
+    char buf[1024];
+
+    fgets(buf, sizeof(buf), f);
+
+    /* chop */
+    buf[strlen(buf) - 1] = '\0';
+
+    return strdup(buf);
 }
 
--- loader/misc.h
+++ loader/misc.h
@@ -2,5 +2,6 @@
 #define H_LOADER_MISC_H
 
 int copyFile(char * source, char * dest);
+char * readLine(FILE * f);
 
 #endif
