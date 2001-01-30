6d3c9a8d68817d5e0953b0c16ae1792ea95d550d
Erik Troan <ewt@redhat.com>
2001-01-30 19:19:53 +0000

    merged ddReadDriverDiskModInfo() from anaconda 7.0 branch

--- loader/loader.c
+++ loader/loader.c
@@ -2154,22 +2154,6 @@ int kickstartFromFloppy(char * location, moduleList modLoaded,
     return 0;
 }
 
-void readExtraModInfo(moduleInfoSet modInfo) {
-    int num = 0;
-    char fileName[80];
-    char * dirName;
-
-    sprintf(fileName, "/tmp/DD-%d/modinfo", num);
-    while (!access(fileName, R_OK)) {
-	dirName = malloc(50);
-	sprintf(dirName, "/tmp/DD-%d", num);
-
-	isysReadModuleInfo(fileName, modInfo, dirName);
-
-	sprintf(fileName, "/tmp/DD-%d/modinfo", ++num);
-    }
-}
-
 /* Recursive */
 int copyDirectory(char * from, char * to) {
     DIR * dir;
@@ -2727,15 +2711,22 @@ int main(int argc, char ** argv) {
     /* merge in any new pci ids */
     pciReadDrivers("/modules/pcitable");
 
-    /* XXX should free old modInfo set */
+    /* We reinit this from the beginning because we could have lost drivers
+       when we switched media, and we don't want to list ones that don't
+       exist. This is a bit unfortunate in that we lose information on
+       drivers we've loaded as well, which could include ISA drivers which
+       kudzu won't reprobe! */
     modInfo = isysNewModuleInfoSet();
+
     if (isysReadModuleInfo(arg, modInfo, NULL)) {
         fprintf(stderr, "failed to read %s\n", arg);
 	sleep(5);
 	exit(1);
     }
 
-    readExtraModInfo(modInfo);
+    /* merge in drivers we know about from a driver disk so we probe things
+       properly */
+    ddReadDriverDiskModInfo(modInfo);
 
     if (ksFile)
 	kickstartDevices(&kd, modInfo, modLoaded, &modDeps, flags);
