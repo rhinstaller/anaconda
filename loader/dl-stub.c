57a06965e911ceaa573dfc5d8ff791fc17cd841d
Erik Troan <ewt@redhat.com>
2001-03-14 14:33:22 +0000

    added patches from jakub for new glibc

--- loader/dl-stub.c
+++ loader/dl-stub.c
@@ -5,3 +5,10 @@ void _dl_mcount_wrapper (void *selfpc)
 void _dl_mcount_wrapper_check (void *selfpc)
 {
 }
+
+int _dl_osversion;
+int __libc_multiple_libcs;
+int __libc_enable_secure;
+
+void __libc_check_standard_fds (void) { }
+
