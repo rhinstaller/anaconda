a5161a665fbba094ff76dc25824b56378b111f1f
bfox <bfox>
2001-06-20 23:27:02 +0000

    a parser for anaconda.conf

---
+++ readConfigFile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import string
+
+
+def getConfigFile():
+    import string
+
+    f = open("anaconda.conf", "r")
+    lines = f.readlines()
+    f.close()
+
+    dict = {}
+
+    for line in lines:
+        line = string.strip(line)
+
+        if string.find (line, "#") > -1 or line == "": 
+            pass
+        else:
+            tokens = string.split(line)
+            dict[tokens[0]] = tokens[1]   
+
+    return dict
