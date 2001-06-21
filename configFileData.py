213c3f6df6b571a75d661ad89581d12f8c40768c
bfox <bfox>
2001-06-21 18:45:47 +0000

    data class for configuration file

---
+++ configFileData.py
@@ -0,0 +1,17 @@
+import readConfigFile
+
+class configFileData:
+    def __init__(self):
+        self.data = readConfigFile.getConfigFile()
+        
+    def getConfigData (self):
+        return self.data
+
+    def getSplashscreen (self):
+        return self.data["Splashscreen"]
+
+    def getTitleBar (self):
+        return self.data["TitleBar"]
+
+    def getTitle (self):
+        return self.data["Title"]
