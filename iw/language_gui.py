0d2e1a59dd614ad64d141efe1a9dccf858ad74fc
bfox <bfox>
2001-06-25 16:25:35 +0000

    can't go back beyond the beginning

--- iw/language_gui.py
+++ iw/language_gui.py
@@ -9,6 +9,7 @@ class LanguageWindow (InstallWindow):
 
     def __init__ (self, ics):
 	InstallWindow.__init__ (self, ics)
+        ics.setPrevEnabled(FALSE)
 
     def getNext (self):
 	self.instLang.setRuntimeLanguage(self.lang)
