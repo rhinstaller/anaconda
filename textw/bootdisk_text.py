ddc96989d08b26fe5c42655cebef67013a4b7d4f
Matt Wilson <msw@redhat.com>
2001-07-09 01:30:45 +0000

    copyright

--- textw/bootdisk_text.py
+++ textw/bootdisk_text.py
@@ -1,3 +1,16 @@
+#
+# bootdisk_text.py: text mode bootdisk creation
+#
+# Copyright 2001 Red Hat, Inc.
+#
+# This software may be freely redistributed under the terms of the GNU
+# library public license.
+#
+# You should have received a copy of the GNU Library Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
 import iutil
 from translate import _
 from snack import *
