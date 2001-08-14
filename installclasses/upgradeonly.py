e543d309588e8330246b471eaa5152c425d72316
Jeremy Katz <katzj@redhat.com>
2001-08-14 22:05:05 +0000

    we should migrate filesystems in upgradeonly... really... (51762)

--- installclasses/upgradeonly.py
+++ installclasses/upgradeonly.py
@@ -31,6 +31,7 @@ class InstallClass(BaseInstallClass):
 		    "dependencies",
 		    "confirmupgrade",
 		    "install",
+                    "migratefilesystems",                    
                     "preinstallconfig",
                     "installpackages",
                     "postinstallconfig",
