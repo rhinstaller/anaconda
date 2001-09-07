94270a2291522090755119730b2ba69099950a41
harald <harald>
2001-09-07 10:56:04 +0000

    no error message for uncpio, but would be nice to have one for ramdisk full

--- isys/otherinsmod.c
+++ isys/otherinsmod.c
@@ -57,7 +57,7 @@ int ourInsmodCommand(int argc, char ** argv) {
 	    /* it might be having a ball */
 	    fd = gzopen(ballPath, "r");
 	    if (!fd) {
-	      fprintf(stderr, "cannot unzip %s\n", ballPath);
+	        fprintf(stderr, "cannot unzip %s\n", ballPath);
 		free(ballPath);
 		return 1;
 	    }
@@ -74,7 +74,6 @@ int ourInsmodCommand(int argc, char ** argv) {
 		    continue;
 		}
 		free(ballPath);
-		fprintf(stderr, "cannot uncpio %s\n", ballPath);
 		return 1;
 	    }
 
