554b552aa3ae889c1e56786ba170df66f4245d79
harald <harald>
2001-09-18 14:22:18 +0000

    removed s390 stuff accidently added

--- loader/s390/common.h
+++
@@ -1,8 +0,0 @@
-static void
-doNewtInit (int *width, int *height)
-{
-	newtInit ();
-	newtCls ();
-	newtGetScreenSize (width, height);
-	newtDrawRootText (0, 0, "Red Hat Linux for S/390   (C) 2001 Red Hat, Inc.");
-}
