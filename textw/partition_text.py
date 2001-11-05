02f7d5e3b294b81ca74f4691c02f304f866afce4
Karsten Hopp <karsten@redhat.de>
2001-11-05 10:25:32 +0000

    rename button (S/390)

--- textw/partition_text.py
+++ textw/partition_text.py
@@ -992,7 +992,7 @@ class PartitionWindow:
         self.g.add(self.lb, 0, 1)
 
         if iutil.getArch() == "s390" or iutil.getArch() == "s390x":
-            self.bb = ButtonBar (screen, ((_("Edit"), "edit", "F3"), TEXT_OK_BUTTON, TEXT_BACK_BUTTON))
+            self.bb = ButtonBar (screen, ((_("Edit partitions"), "edit", "F3"), TEXT_OK_BUTTON, TEXT_BACK_BUTTON))
         else:
             self.bb = ButtonBar (screen, ((_("New"), "new", "F2"), (_("Edit"), "edit", "F3"), (_("Delete"), "delete", "F4"), (_("RAID"), "raid", "F11"), TEXT_OK_BUTTON, TEXT_BACK_BUTTON))
             
