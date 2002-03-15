17cdc6469e111acf7bd3aca2763e579cc1610ae7
Jeremy Katz <katzj@redhat.com>
2002-03-15 05:31:42 +0000

    handled quotes in lilo.conf correctly

--- lilo.py
+++ lilo.py
@@ -183,6 +183,9 @@ class LiloConfigFile:
 	    if (len(fields) == 2):
 		f0 = string.strip (fields [0])
 		f1 = string.strip (fields [1])
+                # people can be silly and put quotes in their lilo.conf
+                f1 = string.replace(f1, '"', '')
+                f1 = string.replace(f1, "'", "")
 		if (f0 == "image" or f0 == "other"):
 		    if image: self.addImage(image)
 		    image = LiloConfigFile(imageType = f0, 
