c96e4da0c0ff9cc4514c24324f1cf1abc4aa3df9
Jeremy Katz <katzj@redhat.com>
2002-12-09 18:10:03 +0000

    for sanity's sake..

--- loader2/mediacheck.h
+++ loader2/mediacheck.h
@@ -1,6 +1,11 @@
+#ifndef MEDIACHECK_H
+#define MEDIACHECK_H
+
 /* simple program to check implanted md5sum in an iso 9660 image   */
 /* Copyright 2001 Red Hat, Inc.                                    */
 /* Michael Fulbright msf@redhat.com                                */
 
 int mediaCheckFile(char *file, char *descr);
 int parsepvd(int isofd, char *mediasum, int *skipsectors, long long *isosize, int *isostatus);
+
+#endif
