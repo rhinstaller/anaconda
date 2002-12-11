0dfe2add501b92b2a3ef75c4972f5a88f3ee4ea6
Jeremy Katz <katzj@redhat.com>
2002-12-11 22:12:00 +0000

    collage is dead now

--- collage/commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_MOUNT
-#define H_MOUNT
-
-int mknodCommand(int argc, char ** argv);
-int mountCommand(int argc, char ** argv);
-int umountCommand(int argc, char ** argv);
-int uncpioCommand(int argc, char ** argv);
-
-#endif
