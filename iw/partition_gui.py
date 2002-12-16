c4600137d9f0eecf2f82d107da04d3f95abde2e0
Karsten Hopp <karsten@redhat.de>
2002-12-16 16:26:46 +0000

    display fdasd help when trying to delete a partition with diskdruid

--- iw/partition_gui.py
+++ iw/partition_gui.py
@@ -529,10 +529,13 @@ class DiskTreeModel(gtk.TreeStore):
 
 class PartitionWindow(InstallWindow):
     def __init__(self, ics):
-	InstallWindow.__init__(self, ics)
+        InstallWindow.__init__(self, ics)
         ics.setTitle(_("Partitioning"))
         ics.setNextEnabled(gtk.TRUE)
-        ics.readHTML("partition")
+        if iutil.getArch() == "s390":
+            ics.readHTML("dasd-s390")
+        else:
+            ics.readHTML("partition")
         self.parent = ics.getICW().window
 
     def quit(self):
@@ -931,6 +934,12 @@ class PartitionWindow(InstallWindow):
 
     def deleteCb(self, widget):
         curselection = self.tree.getCurrentPartition()
+
+	# FIXME: is this really required with the latest parted with S/390 patches ?
+        if iutil.getArch() == "s390" and type(partition) != type("RAID"):
+            self.intf.messageWindow(_("Error"),
+                                    _("DASD partitions can only be deleted with fdasd"))
+            return
 
         if curselection:
             if doDeletePartitionByRequest(self.intf, self.partitions, curselection):
