449a6417dee0e50c91bdd35868f6045aa1981284
Jeremy Katz <katzj@redhat.com>
2003-07-07 20:32:59 +0000

    null terminate

--- loader2/net.c
+++ loader2/net.c
@@ -48,7 +48,7 @@ char *netServerPrompt = \
 
 struct intfconfig_s {
     newtComponent ipEntry, nmEntry, gwEntry, nsEntry;
-    char * ip, * nm, * gw, * ns;
+    const char * ip, * nm, * gw, * ns;
 };
 
 typedef int int32;
@@ -156,7 +156,7 @@ static void dhcpBoxCallback(newtComponent co, void * ptr) {
 
 static int getDnsServers(struct networkDeviceConfig * cfg) {
     int rc;
-    char * ns = "";
+    const char * ns = "";
     struct newtWinEntry entry[] = { { N_("Nameserver IP"), &ns, 0 },
                                       { NULL, NULL, 0 } };
 
