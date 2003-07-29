a46f3cc4698e09cab44aecf8e68ee4a42fe7d038
Mike Fulbright <msf@redhat.com>
2003-07-29 20:54:27 +0000

    no need to send us a copy of the release notes everytime anaconda goes legs up

--- exception.py
+++ exception.py
@@ -40,6 +40,7 @@ def dumpClass(instance, fd, level=0, parentkey=""):
 		    "id.hdList",		    
 		    "id.keyboard.modelDict",
 		    "intf.icw.buff",
+		    "intf.icw.releaseNotesContents",
 		    "intf.icw.stockButtons",
 		    "id.instLanguage.nativeLangNames",
 		    "id.instLanguage.runtimeLangs",
