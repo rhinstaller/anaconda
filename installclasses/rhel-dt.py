c686d8f9eeb6e92975108729baad721463a1465d
Jeremy Katz <katzj@redhat.com>
2003-08-04 21:21:05 +0000

    add rhel installclasses as hidden so they can get translated

---
+++ installclasses/rhel-dt.py
@@ -0,0 +1,45 @@
+from installclass import BaseInstallClass
+from rhpl.translate import N_
+from constants import *
+import os
+import iutil
+
+# custom installs are easy :-)
+class InstallClass(BaseInstallClass):
+    name = N_("Red Hat Enterprise Linux DT")
+    pixmap = "workstation.png"
+    description = N_("Red Hat Enterprise Linux DT")
+    sortPriority = 100
+    showLoginChoice = 0
+    hidden = 1
+
+    pkgstext = _("\tDesktop shell (GNOME)\n"
+                 "\tOffice suite (OpenOffice)\n"
+                 "\tWeb browser (Mozilla) \n"
+                 "\tEmail (Evolution)\n"
+                 "\tInstant messaging\n"
+                 "\tSound and video applications\n"
+                 "\tGames\n")
+
+    def setSteps(self, dispatch):
+	BaseInstallClass.setSteps(self, dispatch);
+	dispatch.skipStep("partition")
+	dispatch.skipStep("authentication")
+
+        dispatch.skipStep("desktopchoice", skip = 0)
+        dispatch.skipStep("package-selection", skip = 1)
+
+    def setGroupSelection(self, grpset, intf):
+	BaseInstallClass.__init__(self, grpset)
+
+        grpset.unselectAll()
+        grpset.selectGroup("workstation-common", asMeta = 1)
+        grpset.selectGroup("gnome-desktop")
+
+    def setInstallData(self, id):
+	BaseInstallClass.setInstallData(self, id)
+        BaseInstallClass.setDefaultPartitioning(self, id.partitions,
+                                                CLEARPART_TYPE_ALL)
+
+    def __init__(self, expert):
+	BaseInstallClass.__init__(self, expert)
