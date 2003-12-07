cb44209e595b7d8c5b1c1e3a4c08ec04a16df60e
Jeremy Katz <katzj@redhat.com>
2003-12-07 04:12:24 +0000

    lvm2 command

--- packages.py
+++ packages.py
@@ -1189,13 +1189,13 @@ def doPostInstall(method, id, intf, instPath):
         instLog.write(line)
     
 
-    # XXX hack - we should really write a proper /etc/lvmtab.  but for now
-    # just create the lvmtab if they have /sbin/vgscan and some VGs
-    if (os.access(instPath + "/sbin/vgscan", os.X_OK) and
-        os.access(instPath + "/proc/lvm", os.R_OK) and
-        len(os.listdir("/proc/lvm/VGs")) > 0):
-        rc = iutil.execWithRedirect("/sbin/vgscan",
-                                    ["vgscan", "-v"],
+    # XXX hack - we should really write a proper lvm "config".  but for now
+    # just vgscan if they have /sbin/lvm and some appearance of volumes
+    if (os.access(instPath + "/sbin/lvm", os.X_OK) and
+        os.access(instPath + "/dev/mapper", os.X_OK) and
+        len(os.listdir("/dev/mapper") > 1)):
+        rc = iutil.execWithRedirect("/sbin/lvm",
+                                    ["lvm", "vgscan", "-v"],
                                     stdout = "/dev/tty5",
                                     stderr = "/dev/tty5",
                                     root = instPath,
