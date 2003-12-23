76554f848542b17b3f6999a57830c1971f9f32b3
Jeremy Katz <katzj@redhat.com>
2003-12-23 00:19:56 +0000

    build fix

--- isys/dasd.c
+++ isys/dasd.c
@@ -12,54 +12,7 @@
 #include "isys.h"
 
 #if defined(__s390__) || defined(__s390x__)
-/*
- * struct dasd_information_t
- * represents any data about the data, which is visible to userspace
- */
-typedef struct dasd_information_t {
-        unsigned int devno; /* S/390 devno */
-        unsigned int real_devno; /* for aliases */
-        unsigned int schid; /* S/390 subchannel identifier */
-        unsigned int cu_type  : 16; /* from SenseID */
-        unsigned int cu_model :  8; /* from SenseID */
-        unsigned int dev_type : 16; /* from SenseID */
-        unsigned int dev_model : 8; /* from SenseID */
-        unsigned int open_count;
-        unsigned int req_queue_len;
-        unsigned int chanq_len;
-        char type[4]; /* from discipline.name, 'none' for unknown */
-        unsigned int status; /* current device level */
-        unsigned int label_block; /* where to find the VOLSER */
-        unsigned int FBA_layout; /* fixed block size (like AIXVOL) */
-        unsigned int characteristics_size;
-        unsigned int confdata_size;
-        char characteristics[64]; /* from read_device_characteristics */
-        char configuration_data[256]; /* from read_configuration_data */
-} dasd_information_t;
-
-typedef struct cchhb
-{
-        u_int16_t cc;
-        u_int16_t hh;
-        u_int8_t b;
-} __attribute__ ((packed)) cchhb_t;
-
-typedef struct volume_label
-{
-        char volkey[4];         /* volume key = volume label                 */
-        char vollbl[4];         /* volume label                              */
-        char volid[6];          /* volume identifier                         */
-        u_int8_t security;              /* security byte                             */
-        cchhb_t vtoc;           /* VTOC address                              */
-        char res1[5];           /* reserved                                  */
-        char cisize[4];         /* CI-size for FBA,...                       */
-                                /* ...blanks for CKD                         */
-        char blkperci[4];       /* no of blocks per CI (FBA), blanks for CKD */
-        char labperci[4];       /* no of labels per CI (FBA), blanks for CKD */
-        char res2[4];           /* reserved                                  */
-        char lvtoc[14];         /* owner code for LVTOC                      */
-        char res3[29];          /* reserved                                  */
-} __attribute__ ((packed)) volume_label_t;
+#include <asm/vtoc.h>
 #endif
 
 
