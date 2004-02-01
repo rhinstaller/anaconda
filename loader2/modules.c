cd76ecc0f4a326030c5c0e6ca711311fcf5f979f
Bill Nottingham <notting@redhat.com>
2004-02-01 21:24:34 +0000

    write the include so we get all the stock aliases

--- loader2/modules.c
+++ loader2/modules.c
@@ -526,7 +526,8 @@ static int doLoadModules(const char * origModNames, moduleList modLoaded,
             logMessage("error appending to /tmp/modprobe.conf: %s\n",
                        strerror(errno));
         } else {
-            writeModulesConf(modLoaded, fd);
+            write(fd, "include /etc/modprobe.conf.dist\n", strlen("include /etc/modprobe.conf.dist\n"));
+	    writeModulesConf(modLoaded, fd);
             close(fd);
         }
     }
