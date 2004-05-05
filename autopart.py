25221ad41075f61979ffc9c8242fabd8f3a85096
Jeremy Katz <katzj@redhat.com>
2004-05-05 18:44:28 +0000

    we can't make boot disks anymore and this isn't much of a problem for "modern" hardware. (#122535)

--- autopart.py
+++ autopart.py
@@ -1008,6 +1008,10 @@ def doPartitioning(diskset, requests, doRefresh = 1):
         raise PartitioningError, _("Boot partition %s isn't a VFAT partition.  EFI won't be able to boot from this partition.") %(requests.getBootableRequest()[0].mountpoint,)
     elif ret == BOOTIPSERIES_TOO_HIGH:
         raise PartitioningError, _("Boot partition isn't located early enough on the disk.  OpenFirmware won't be able to boot this installation.")
+    elif ret == BOOT_ABOVE_1024:
+        # we can't make boot disks anymore and this isn't much of a problem
+        # for "modern" hardware. (#122535)
+        pass
     elif ret != PARTITION_SUCCESS:
         # more specific message?
         raise PartitioningWarning, _("Boot partition %s may not meet booting constraints for your architecture.  Creation of a boot disk is highly encouraged.") %(requests.getBootableRequest()[0].mountpoint,)
