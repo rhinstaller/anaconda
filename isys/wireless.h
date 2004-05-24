fc2ae88ce331dc39317bbb2f5033b2e89a393653
Jeremy Katz <katzj@redhat.com>
2004-05-24 21:49:52 +0000

    add initial wireless bits.  allows determining if this is a wireless interface, get/set essid and set wep key

---
+++ isys/wireless.h
@@ -0,0 +1,9 @@
+#ifndef WIRELESS_H
+#define WIRELESS_H
+
+int is_wireless_interface(char * ifname);
+int set_essid(char * ifname, char * essid);
+char * get_essid(char * ifname);
+int set_wep_key(char * ifname, char * key);
+
+#endif
