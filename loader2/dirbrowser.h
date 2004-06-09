f74841eefae1ccafbc292b8cc8acf314a0916978
Jeremy Katz <katzj@redhat.com>
2004-06-09 05:09:30 +0000

    warning fix

--- loader2/dirbrowser.h
+++ loader2/dirbrowser.h
@@ -1,6 +1,8 @@
 #ifndef DIRBROWSER_H
 #define DIRBROWSER_H
 
+#include <dirent.h>
+
 char * newt_select_file(char * title, char * text, char * dirname,
                         int (*filterfunc)(char *, struct dirent *));
 
