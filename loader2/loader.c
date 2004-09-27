04210487375cd382a46d82d0a41c085f05733adb
Jeremy Katz <katzj@redhat.com>
2004-09-27 18:07:37 +0000

    2004-09-27  Jeremy Katz  <katzj@redhat.com>
    	* loader2/loader.c: Load dm-zero, dm-snapshot and dm-mirror (#132001)
    
    	* scripts/mk-images.i386:
    	* scripts/mk-images.ia64:
    	* scripts/mk-images.ppc:
    	* scripts/mk-images.s390:
    	* scripts/mk-images.x86_64:
    	Use a variable for raid and lvm modules.  Add dm-zero, dm-snapshot
    	and dm-mirror (#132001)

--- loader2/loader.c
+++ loader2/loader.c
@@ -1351,7 +1351,7 @@ int main(int argc, char ** argv) {
     /* look for cards which require the agpgart module */
     agpgartInitialize(modLoaded, modDeps, modInfo, flags);
 
-    mlLoadModuleSetLocation("md:raid0:raid1:raid5:raid6:fat:msdos:jbd:ext3:reiserfs:jfs:xfs:dm-mod",
+    mlLoadModuleSetLocation("md:raid0:raid1:raid5:raid6:fat:msdos:jbd:ext3:reiserfs:jfs:xfs:dm-mod:dm-zero:dm-mirror:dm-snapshot",
 			    modLoaded, modDeps, modInfo, flags, 
 			    secondStageModuleLocation);
 
