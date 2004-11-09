b910c683b6d7ea2dd48c97628b5eaf6a68cfae7d
Jeremy Katz <katzj@redhat.com>
2004-11-09 18:40:32 +0000

    2004-11-09  Jeremy Katz  <katzj@redhat.com>
    	* packages.py (doPreInstall): If we go back, we need to try
    	unmount /dev, too (#133773)

--- packages.py
+++ packages.py
@@ -678,10 +678,11 @@ def handleMiscPackages(intf, id, dir):
 
 def doPreInstall(method, id, intf, instPath, dir):
     if dir == DISPATCH_BACK:
-        try:
-            isys.umount(instPath + "/selinux")
-        except:
-            pass
+        for d in ("/selinux", "/dev"):
+            try:
+                isys.umount(instPath + d)
+            except Exception, e:
+                log("unable to unmount %s: %s" %(d, e))
         return
 
     if flags.test:
