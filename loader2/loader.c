b5a7e8c6e0b70a1e132995ec24ae7498b4fc0335
Jeremy Katz <katzj@redhat.com>
2004-11-10 23:03:49 +0000

    2004-11-10  Jeremy Katz  <katzj@redhat.com>
    	* loader2/loader.c: Make serial imply nofb (#134167)

--- loader2/loader.c
+++ loader2/loader.c
@@ -1189,6 +1189,8 @@ int main(int argc, char ** argv) {
 
     if (FL_SERIAL(flags) && !hasGraphicalOverride())
         flags |= LOADER_FLAGS_TEXT;
+    if (FL_SERIAL(flags)
+        flags |= LOADER_FLAGS_NOFB;
 
     setupRamfs();
 
