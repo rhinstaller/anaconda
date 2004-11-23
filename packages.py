b790110599c6122e9146558f2f35642f69d071b7
Jeremy Katz <katzj@redhat.com>
2004-11-23 13:16:03 +0000

    (merge from rhel4-branch) 2004-11-23  Jeremy Katz  <katzj@redhat.com>
    	* textw/upgrade_bootloader_text.py
    	(UpgradeBootloaderWindow.__call__): Set a default when we can't
    	determine the boot loader to avoid indeterminate state (#139603)
    
    	* lvm.py: Don't disable LVM if vgscan, et al segfault (#134263)
    
    	* packages.py (doPreInstall): Don't remove the directories as
    	they may not be empty (#133773)

--- packages.py
+++ packages.py
@@ -681,7 +681,7 @@ def doPreInstall(method, id, intf, instPath, dir):
     if dir == DISPATCH_BACK:
         for d in ("/selinux", "/dev"):
             try:
-                isys.umount(instPath + d)
+                isys.umount(instPath + d, removeDir = 0)
             except Exception, e:
                 log("unable to unmount %s: %s" %(d, e))
         return
