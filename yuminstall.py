3df37fa9ee85f001449cbbadadac45c73dafb454
Paul Nasrat <pnasrat@redhat.com>
2005-07-07 20:29:15 +0000

    more yum prep/test work

--- yuminstall.py
+++ yuminstall.py
@@ -11,73 +11,173 @@
 
 from flags import flags
 
+import sys
+import os
+import timer
+
+import rpm
+import rpmUtils
+import urlgrabber.progress
 import yum
 import yum.repos
-from yum.config import yumconf
+import yum.packages
 
-class AnacondaYumConf(yumconf):
+from rhpl.log import log
+from rhpl.translate import _
+
+import iutil
+
+class simpleCallback:
+
+    def __init__(self, messageWindow, progress, pkgTimer, method,
+                 progressWindowClass, instLog, modeText, ts):
+        self.messageWindow = messageWindow
+        self.progress = progress
+        self.pkgTimer = pkgTimer
+        self.method = method
+        self.progressWindowClass = progressWindowClass
+        self.progressWindow = None
+        self.lastprogress = 0
+        self.incr = 20
+        self.instLog = instLog
+        self.modeText = modeText
+        self.beenCalled = 0
+        self.initWindow = None
+        self.ts = ts
+        self.fdnos = {}
+
+    def callback(self, what, amount, total, h, user):
+        # first time here means we should pop the window telling
+        # user to wait until we get here
+        if not self.beenCalled:
+            self.beenCalled = 1
+            self.initWindow.pop()
+
+        if what == rpm.RPMCALLBACK_TRANS_START:
+            # step 6 is the bulk of the ts processing time
+            if amount == 6:
+                self.progressWindow = \
+                    self.progressWindowClass (_("Processing"), 
+                                              _("Preparing to install..."),
+                                              total)
+                try:
+                    self.incr = total / 10
+                except:
+                    pass
+
+        if what == rpm.RPMCALLBACK_TRANS_PROGRESS:
+            if self.progressWindow and amount > self.lastprogress + self.incr:
+                self.progressWindow.set(amount)
+                self.lastprogress = amount
+
+        if what == rpm.RPMCALLBACK_TRANS_STOP and self.progressWindow:
+            self.progressWindow.pop()
+
+        if what == rpm.RPMCALLBACK_INST_OPEN_FILE:
+            self.pkgTimer.start()
+
+            hdr, path = h
+
+            self.progress.setPackage(hdr)
+            self.progress.setPackageScale(0, 1)
+
+            self.instLog.write(self.modeText % (hdr[rpm.RPMTAG_NAME],
+                                                hdr[rpm.RPMTAG_VERSION],
+                                                hdr[rpm.RPMTAG_RELEASE],
+                                                hdr[rpm.RPMTAG_ARCH]))
+
+            self.instLog.flush()
+            self.size = h[rpm.RPMTAG_SIZE]
+
+            fd = os.open(path, os.O_RDONLY)
+            nvr = '%s-%s-%s' % ( hdr['name'], hdr['version'], hdr['release'] )
+            self.fdnos[nvr] = fd
+            return fd
+
+        elif what == rpm.RPMCALLBACK_INST_PROGRESS:
+            if amount > total:
+                amount = total
+            if not total or total == 0 or total == "0":
+                total = amount
+            self.progress.setPackageScale(amount, total)
+
+        elif what == rpm.RPMCALLBACK_INST_CLOSE_FILE:
+            hdr, path =h
+            nvr = '%s-%s-%s' % ( hdr['name'], hdr['version'], hdr['release'] )
+            os.close(self.fdnos[nvr])
+            self.progress.completePackage(hdr, self.pkgTimer)
+            self.progress.processEvents()
+
+        else:
+            pass
+
+        self.progress.processEvents()
+
+
+class AnacondaYumConf:
     """Dynamic yum configuration"""
 
     def __init__( self, configfile = None, root = '/'):
-        self.configdata = {
-                'debuglevel': 2,
-                'errorlevel': 2,
-                'retries': 10,
-                'recent': 7, 
-                'cachedir': '/var/cache/yum', 
-                'logfile': '/tmp/anaconda-yum.log', 
-                'reposdir': [],
-                'syslog_ident': None,
-                'syslog_facility': 'LOG_USER',
-                'distroverpkg': 'fedora-release',
-                'installroot': root,
-                'commands': [],
-                'exclude': [],
-                'failovermethod': 'roundrobin',
-                'yumversion': 'unversioned',
-                'proxy': None,
-                'proxy_username': None,
-                'proxy_password': None,
-                'pluginpath': '/usr/lib/yum-plugins',
-                'installonlypkgs': [],
-                'kernelpkgnames': ['kernel','kernel-smp',
-                                             'kernel-enterprise', 'kernel-bigmem',
-                                             'kernel-BOOT']),
-                'exactarchlist': ['kernel', 'kernel-smp', 'glibc',
-                                            'kernel-hugemem', 'kernel-enterprise',
-                                            'kernel-bigmem']),
-                'tsflags': [],
-                'assumeyes': False,
-                'alwaysprompt': True,
-                'exactarch': True,
-                'tolerant': True,
-                'diskspacecheck': True,
-                'overwrite_groups': False,
-                'keepalive': True,
-                'gpgcheck': False,
-                'obsoletes': False,
-                'showdupesfromrepos': False,
-                'enabled': True,
-                'plugins': False,
-                'enablegroups': True,
-                'uid': 0,
-                'cache': 0,
-                'progress_obj': None,
-                'timeout', 30.0,
-                }
+        yumconfstr = """
+[main]
+cachedir=/var/cache/yum
+reposdir=/tmp/repos.d
+debuglevel=2
+logfile=/var/log/yum.log
+pkgpolicy=newest
+distroverpkg=redhat-release
+tolerant=1
+exactarch=1
+retries=5
+obsoletes=1
+gpgcheck=1
+installroot=/mnt/sysimage
 
+[anaconda]
+baseurl=file:///mnt/source
+enabled=1
+gpgcheck=1
+gpgkey=file:///mnt/source/RPM-GPG-KEY-fedora
+"""
 
-class AnacondaYum(yum.YumBase):
+        if configfile is None:
+            configfile = "/tmp/yum.conf"
+
+        self.file = configfile
+        f = open(configfile, 'w')
+        f.write(yumconfstr)
+        f.close()
+
     
+class AnacondaYum(yum.YumBase):
     def __init__(self, method, id, intf, instPath):
+        self.method = method
+        self.id = id
+        self.intf = intf
         self.updates = []
         self.localPackages = []
+        yum.YumBase.__init__(self)
 
-    def doConfigSetup(self, fn=None, root='/'):
-        self.conf = AnacondaYumConf()
-        repo = yum.repos.Repository()
-        repo.set('baseurl','file:///mnt/source')
-        self.repos.add(repo)
+
+    def setGroupSelection(self, grpset, intf):
+        if grpset is None:
+            return 0
+
+        pkgs = []
+
+        availpackages = {}
+        d = yum.packages.buildPkgRefDict(self.pkgSack.returnPackages())
+        for po in self.pkgSack.returnPackages():
+            availpackages[po.name] = po
+        
+        for group in grpset:
+            if not self.groupInfo.groupExists(group):
+                continue
+            pkglist = self.groupInfo.pkgTree(group)
+            for pkg in pkglist:
+                if availpackages.has_key(pkg):
+                    pkgs.append(pkg)
+                    self.tsInfo.addInstall(availpackages[pkg])
 
     def errorlog(self, value, msg):
         pass
@@ -95,7 +195,74 @@ def doYumInstall(method, id, intf, instPath):
 # XXX: Only operate on nfs trees for now
     if not id.methodstr.startswith('nfs:/'):
         from packages import doInstall
-            return doInstall(method, id, intf, instPath)
-    
-    
+        return doInstall(method, id, intf, instPath)
+
+    upgrade = id.getUpgrade()
+
+    if upgrade:
+        logname = '/root/upgrade.log'
+    else:
+        logname = '/root/install.log'
+
+    instLogName = instPath + logname
+    try:
+        iutil.rmrf (instLogName)
+    except OSError:
+        pass
+
+    instLog = open(instLogName, "w+")
+    if upgrade:
+        logname = '/root/upgrade.log'
+    else:
+        logname = '/root/install.log'
+
+    instLogName = instPath + logname
+    try:
+        iutil.rmrf (instLogName)
+    except OSError:
+        pass
+
+    instLog = open(instLogName, "w+")
+
+    if upgrade:
+        modeText = _("Upgrading %s-%s-%s.%s.\n")
+    else:
+        modeText = _("Installing %s-%s-%s.%s.\n")
+
+    ac = AnacondaYumConf(configfile="/tmp/yum.conf", root=instPath)
+    ayum = AnacondaYum(method, id, intf, instPath)
+    ayum.doConfigSetup(fn="/tmp/yum.conf", root=instPath)
+    ayum.doTsSetup()
+    # XXX: handle RepoError
+    ayum.doRepoSetup()
+    for x in ayum.repos.repos.values():
+        x.dirSetup()
+    ayum.doGroupSetup()
+    ayum.doSackSetup()
+    ayum.doRpmDBSetup()
+    ayum.setGroupSelection(["Core"], intf)
+
+    downloadpkgs = []
+    for txmbr in ayum.tsInfo.getMembers():
+        if txmbr.ts_state in ['i', 'u']:
+            po = txmbr.po
+        if po:
+            downloadpkgs.append(po)
+
+    ayum.downloadPkgs(downloadpkgs, callback=None)
+
+    ayum.initActionTs()
+    ayum.populateTs()
+
+    pkgTimer = timer.Timer(start = 0)
+
+
+    cb = simpleCallback(intf.messageWindow, id.instProgress, pkgTimer, method, intf.progressWindow, instLog, modeText, ayum.ts)
+
+    cb.initWindow = intf.waitWindow(_("Install Starting"),
+                                    _("Starting install process.  This may take several minutes..."))
+
+    ayum.ts.check()
+    ayum.ts.order()
+    ayum.runTransaction(cb=cb)
 
