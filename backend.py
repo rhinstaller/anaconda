b3a636f84f28a77a0a52a6a88e1d5758ec9843f9
Paul Nasrat <pnasrat@redhat.com>
2005-08-24 19:10:30 +0000

    Stub backend

---
+++ backend.py
@@ -0,0 +1,32 @@
+#
+# backend.py: Interface for installation backends
+#
+# Paul Nasrat <pnasrat@redhat.com> 
+# Copyright (c) 2005 Red Hat, Inc.
+#
+# This software may be freely redistributed under the terms of the GNU
+# library public license.
+#
+# You should have received a copy of the GNU Library Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import logging
+log = logging.getLogger("anaconda")
+
+class AnacondaBackend:
+    def __init__(self):
+        pass
+
+    def doPreSelection(self):
+        pass
+
+    def doPostSelection(self):
+        pass
+
+    def doPreInstall(self):
+        pass
+
+    def doPostInstall(self):
+        pass
