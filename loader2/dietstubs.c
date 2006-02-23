029be4f7918e1903843fdaf1476c1a19cc5b3b66
David Cantrell <dcantrell@redhat.com>
2006-02-23 21:40:53 +0000

    * loader2/dietstubs.c: Removed bogl stuff.

--- loader2/dietstubs.c
+++ loader2/dietstubs.c
@@ -10,10 +10,6 @@
 #include "../isys/gzlib/gzlib.h"
 #endif
 
-#define WLITE_REDEF_STDC 0
-#include <wlite_wchar.h>
-#include <wlite_wctype.h>
-
 int __libc_enable_secure = 0;
 
 struct glibc_stat {
@@ -169,27 +165,6 @@ void * __rawmemchr (void* s, int c) {
 
 char * dcgettext (const char *domainname, const char *msgid, int category) {
     return (char *) msgid;
-}
-
-int wcwidth (wchar_t c) {
-    return wlite_wcwidth(c);
-}
-
-size_t mbrtowc (wchar_t *pwc, const char *s, size_t n, void *ps) {
-    return wlite_mbrtowc (pwc, s, n, ps);
-}
-
-#define mbstate_t wlite_mbstate_t
-size_t mbsrtowcs(wchar_t *pwc, const char **src, size_t n, mbstate_t *ps) {
-    return wlite_mbsrtowcs (pwc, src, n, ps);
-}
-
-int iswspace (wchar_t c) {
-    return wlite_iswctype((c), wlite_space_);
-}
-
-size_t wcrtomb(char *s, wchar_t wc, void *ps) {
-    return wlite_wcrtomb (s, wc, ps);
 }
 
 /* lie to slang to trick it into using unicode chars for linedrawing */
