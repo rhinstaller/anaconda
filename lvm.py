6e6cccb09e0b91bf5efa4edf93bde5643c154c34
David Cantrell <dcantrell@redhat.com>
2006-05-03 16:53:32 +0000

    * lvm.py (has_lvm):  Return if we're on S/390 and don't see a        /proc/devices file.

--- lvm.py
+++ lvm.py
@@ -34,7 +34,10 @@ def has_lvm():
     if not (os.access("/usr/sbin/lvm", os.X_OK) or
             os.access("/sbin/lvm", os.X_OK)):
         return
-    
+
+    if iutil.getArch() == "s390" and not os.access("/proc/devices", os.R_OK):
+        return
+
     f = open("/proc/devices", "r")
     lines = f.readlines()
     f.close()
