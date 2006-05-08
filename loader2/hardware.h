b699b18726739a81fda1a631e0beb528dddffc10
Peter Jones <pjones@redhat.com>
2006-05-08 21:54:48 +0000

    - add lapicSetup declaration.

--- loader2/hardware.h
+++ loader2/hardware.h
@@ -17,6 +17,8 @@ int earlyModuleLoad(moduleInfoSet modInfo, moduleList modLoaded,
 int busProbe(moduleInfoSet modInfo, moduleList modLoaded, moduleDeps modDeps,
              int justProbe, int flags);
 
+void lapicSetup(moduleList modLoaded, moduleDeps modDeps,
+                moduleInfoSet modInfo, int flags);
 void scsiSetup(moduleList modLoaded, moduleDeps modDeps,
                moduleInfoSet modInfo, int flags);
 void ideSetup(moduleList modLoaded, moduleDeps modDeps,
