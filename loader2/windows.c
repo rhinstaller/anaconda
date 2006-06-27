a1332af7fb1221441dcd820e12ccf5a90f2b4b9e
David Cantrell <dcantrell@redhat.com>
2006-06-27 15:31:43 +0000

    Use vasprintf().

--- loader2/windows.c
+++ loader2/windows.c
@@ -25,42 +25,36 @@
 
 #include "windows.h"
 
-void winStatus(int width, int height, char * title,
-		char * text, ...) {
+void winStatus(int width, int height, char * title, char * text, ...) {
     newtComponent t, f;
     char * buf = NULL;
-    int size = 0;
-    int i = 0;
     va_list args;
 
     va_start(args, text);
 
-    do {
-	size += 1000;
-	if (buf) free(buf);
-	buf = malloc(size);
-	i = vsnprintf(buf, size, text, args);
-    } while (i == size);
+    if (vasprintf(&buf, text, args) != -1) {
+        newtCenteredWindow(width, height, title);
+
+        t = newtTextbox(1, 1, width - 2, height - 2, NEWT_TEXTBOX_WRAP);
+        newtTextboxSetText(t, buf);
+        f = newtForm(NULL, NULL, 0);
+
+        free(buf);
+
+        newtFormAddComponent(f, t);
+
+        newtDrawForm(f);
+        newtRefresh();
+        newtFormDestroy(f);
+    }
 
     va_end(args);
-
-    newtCenteredWindow(width, height, title);
-
-    t = newtTextbox(1, 1, width - 2, height - 2, NEWT_TEXTBOX_WRAP);
-    newtTextboxSetText(t, buf);
-    f = newtForm(NULL, NULL, 0);
-
-    free(buf);
-
-    newtFormAddComponent(f, t);
-
-    newtDrawForm(f);
-    newtRefresh();
-    newtFormDestroy(f);
 }
 
 
 void scsiWindow(const char * driver) {
-    winStatus(40, 3, _("Loading SCSI driver"), 
-	      _("Loading %s driver..."), driver);
+    winStatus(40, 3, _("Loading SCSI driver"),
+        _("Loading %s driver..."), driver);
 }
+
+/* vim:set shiftwidth=4 softtabstop=4: */
