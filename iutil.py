03abe5a091ed97fa0c582bbc5bf0af041ed42b3f
Chris Lumens <clumens@redhat.com>
2006-08-17 20:21:18 +0000

    Flush the drive cache so we don't mistake hard drives for CD/DVD drives.

--- iutil.py
+++ iutil.py
@@ -215,6 +215,7 @@ def makeCharDeviceNodes():
 
 # make the device nodes for all of the drives on the system
 def makeDriveDeviceNodes():
+    isys.flushDriveDict()
     hardDrives = isys.hardDriveDict()
     for drive in hardDrives.keys():
         if drive.startswith("mapper"):
@@ -238,6 +239,7 @@ def makeDriveDeviceNodes():
             dev = "%s%s%d" % (drive, sep, i)
             isys.makeDevInode(dev, "/dev/%s" % (dev,))
 
+    isys.flushDriveDict()
     cdroms = isys.cdromList()
     for drive in cdroms:
         isys.makeDevInode(drive, "/dev/%s" % (drive,))
