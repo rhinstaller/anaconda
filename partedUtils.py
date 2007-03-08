75bae4beeda2415bbdabcfb9c4aaeaf74bf0ada9
Chris Lumens <clumens@redhat.com>
2007-03-08 16:43:35 +0000

    Ignore disks listed in ignoredisks, even if we have clearpart --all (#186438).

--- partedUtils.py
+++ partedUtils.py
@@ -827,7 +827,8 @@ class DiskSet:
         if self.disks:
             return
         for drive in self.driveList ():
-            if drive in DiskSet.skippedDisks and not initAll:
+            # ignoredisk takes precedence over clearpart (#186438).
+            if drive in DiskSet.skippedDisks:
                 continue
             deviceFile = isys.makeDevInode(drive)
             if isys.driveIsRemovable(drive) and not flags.expert:
