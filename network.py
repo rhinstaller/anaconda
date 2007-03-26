887019e4e3406837df6da1399df1e05bcf26a9aa
David Cantrell <dcantrell@redhat.com>
2007-03-26 21:33:59 +0000

    * network.py: When gathering network device description from kudzu,    only probe for devices with modules currently loaded (#233507).

--- network.py
+++ network.py
@@ -255,7 +255,7 @@ class Network:
 		self.netdevices[self.firstnetdevice].set(("onboot", "yes"))
 
 	    # assign description to each device based on kudzu information
-	    probedevs = kudzu.probe(kudzu.CLASS_NETWORK, kudzu.BUS_UNSPEC, 0)
+	    probedevs = kudzu.probe(kudzu.CLASS_NETWORK, kudzu.BUS_UNSPEC, kudzu.PROBE_LOADED)
 	    for netdev in probedevs:
 		device = netdev.device
 		if device in self.netdevices.keys():
