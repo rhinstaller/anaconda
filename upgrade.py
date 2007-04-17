d472197f9a4fc3bc12eaadb2fa08f541873e7751
Jeremy Katz <katzj@redhat.com>
2007-04-17 21:05:30 +0000

    2007-04-17  Jeremy Katz  <katzj@redhat.com>
    	* upgrade.py (upgrade_remove_blacklist): Add hal and mysql to
    	upgrade blacklists

--- upgrade.py
+++ upgrade.py
@@ -40,10 +40,10 @@ log = logging.getLogger("anaconda")
 upgrade_remove_blacklist = [("system-config-mouse",), ("dev",)] 
 
 if rhpl.getArch() == "x86_64":
-    upgrade_remove_blacklist.extend( [("perl","i386"), ("e2fsprogs", "i386")] )
+    upgrade_remove_blacklist.extend( [("perl","i386"), ("e2fsprogs", "i386"), ("hal", "i386"), ("mysql", "i386")] )
 
 if rhpl.getArch() == "ppc":
-    upgrade_remove_blacklist.extend( [("e2fsprogs", "ppc64")] )
+    upgrade_remove_blacklist.extend( [("e2fsprogs", "ppc64"), ("hal", "ppc64"), ("mysql", "ppc64")] )
 
 def queryUpgradeContinue(anaconda):
     if anaconda.dir == DISPATCH_FORWARD:
