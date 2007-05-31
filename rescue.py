8f9646a9ee947dd037e8c2594fd40afd5a42df78
dlehman <dlehman>
2007-05-31 15:43:54 +0000

    * rescue.py (runShell): mount /dev/pts in rescue mode (#228714)
    	* scripts/upd-instroot: include dmidecode on ia64 (#232947)
    
    	* loader2/modules.c (scsiDiskCount): correctly count SCSI disk
    	devices (#230526)

--- rescue.py
+++ rescue.py
@@ -381,6 +381,9 @@ def runRescue(anaconda, instClass):
                 # now that dev is udev, bind mount the installer dev there
                 isys.mount("/dev", "%s/dev" %(anaconda.rootPath,), bindMount = 1)
 
+                # and /dev/pts
+                isys.mount("/dev/pts", "%s/dev/pts" %(anaconda.rootPath,), bindMount = 1)
+
                 # and /selinux too
                 if flags.selinux and os.path.isdir("%s/selinux" %(anaconda.rootPath,)):
                     try:
