698d8b2001a6ab71e2fddbd945c5e06afcd9f869
Jeremy Katz <katzj@redhat.com>
2007-12-07 15:14:12 -0500

    Remove isomd5sum in favor of standalone version

--- isomd5sum/libcheckisomd5.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LIBCHECKISOMD5_H__
-#define  __LIBCHECKISOMD5_H__
-
-int mediaCheckFile(char *iso, int quiet);
-void printMD5SUM(char *file);
-
-#endif
--- isomd5sum/libimplantisomd5.h
+++
@@ -1,5 +0,0 @@
-#ifndef __LIBIMPLANTISOMD5_H__
-#define __LIBIMPLANTISOMD5_H__
-int implantISOFile(char *iso, int supported, int forceit, int quiet, char **errstr);
-#endif
-
