f25a4624b200604b72a933cfebd1d36138bc2c43
David Cantrell <dcantrell@redhat.com>
2007-12-14 19:17:32 -1000

    GPLv2+ license boilerplates for textw/*.py files.

--- textw/grpselect_text.py
+++ textw/grpselect_text.py
@@ -1,15 +1,22 @@
 #
 # grpselect_text - Text mode group/package selection UI
 #
-# Jeremy Katz <katzj@redhat.com>
-# Copyright 2005-2007   Red Hat, Inc.
+# Copyright (C) 2005, 2006, 2007  Red Hat, Inc.  All rights reserved.
 #
-# This software may be freely redistributed under the terms of the GNU
-# general public license.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Red Hat Author(s): Jeremy Katz <katzj@redhat.com>
 #
 
 import yum.Errors
--- textw/upgrade_bootloader_text.py
+++ textw/upgrade_bootloader_text.py
@@ -1,17 +1,22 @@
 #
 # upgrade_bootloader_text.py: text bootloader dialog for upgrades
 #
-# Jeremy Katz <katzj@redhat.com>
+# Copyright (C) 2002  Red Hat, Inc.  All rights reserved.
 #
-# Copyright, 2002 Red Hat, Inc.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# This software may be freely redistributed under the terms of the GNU
-# library public license.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU Library Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# Red Hat Author(s): Jeremy Katz <katzj@redhat.com>
 #
 
 from snack import *
