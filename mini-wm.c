04e3669dfac00ecb6ae001fc600cadba7c700a72
David Cantrell <dcantrell@redhat.com>
2007-12-14 19:06:29 -1000

    More GPLv2+ license boilerplate updates.

--- mini-wm.c
+++ mini-wm.c
@@ -1,19 +1,23 @@
 /*
  * mini-wm.c - simple keyboard focus handling 'wm'.
  *
- * Owen Taylor <otaylor@redhat.com>
+ * Copyright (C) 2002  Red Hat, Inc.  All rights reserved.
  *
- * Copyright 2002 Red Hat, Inc.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This software may be freely redistributed under the terms of the GNU
- * public license.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * Red Hat Author(s): Owen Taylor <otaylor@redhat.com>
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
