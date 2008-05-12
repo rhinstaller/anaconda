6631c8e66e18dd578208f56fc74381d1e6a4ec34
David Lehman <dlehman@redhat.com>
2008-05-12 13:04:55 -0500

    Don't unlink an image we retrieved but could not mount as it could be .cgz.

--- loader2/method.c
+++ loader2/method.c
@@ -582,7 +582,6 @@ int copyFileAndLoopbackMount(int fd, char * dest,
         /* JKFIXME: this used to be fatal, but that seems unfriendly */
         logMessage(ERROR, "Error mounting %s on %s (%s)", device,
                    mntpoint, strerror(errno));
-        unlink(dest);
         return 1;
     }
 
