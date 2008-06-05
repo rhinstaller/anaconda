6b3a61fa8b01df58cf37f2fb97230e6bcc997f78
Peter Jones <pjones@redhat.com>
2008-06-05 12:42:24 -0400

    raise "NotImplementedError", not "NotImplemented"

--- backend.py
+++ backend.py
@@ -98,7 +98,7 @@ class AnacondaBackend:
 
     def doInstall(self, anaconda):
         log.warning("doInstall not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def initLog(self, id, instPath):
         upgrade = id.getUpgrade()
@@ -139,50 +139,50 @@ class AnacondaBackend:
         return []
 
     def doInitialSetup(self, anaconda):
-        raise NotImplemented
+        raise NotImplementedError
 
     def doRepoSetup(self, anaconda):
         log.warning("doRepoSetup not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def groupExists(self, group):
         log.warning("groupExists not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def selectGroup(self, group, *args):
         log.warning("selectGroup not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def deselectGroup(self, group, *args):
         log.warning("deselectGroup not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def packageExists(self, pkg):
         log.warning("packageExists not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
     
     def selectPackage(self, pkg, *args):
         log.warning("selectPackage not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def deselectPackage(self, pkg, *args):
         log.warning("deselectPackage not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def getDefaultGroups(self, anaconda):
         log.warning("getDefaultGroups not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     # write out the %packages section of anaconda-ks.cfg
     def writePackagesKS(self, f, anaconda):
         log.warning("writePackagesKS not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     # write out any config files that live on the installed system
     # (e.g., /etc/yum.repos.d/* files)
     def writeConfiguration(self):
         log.warning("writeConfig not implemented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     # write out any other kickstart bits the backend requires - no warning
     # here because this may not be needed
@@ -191,7 +191,7 @@ class AnacondaBackend:
 
     def getRequiredMedia(self):
         log.warning("getRequiredMedia not implmented for backend!")
-        raise NotImplemented
+        raise NotImplementedError
 
     def complete(self, anaconda):
         pass
