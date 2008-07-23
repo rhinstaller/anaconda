594864adce6e5ac58b914adcfa1b90aa33e739cb
Chris Lumens <clumens@redhat.com>
2008-07-23 16:53:11 -0400

    MD_NEW_SIZE_BLOCKS no longer exists in newer kernel headers.

--- isys/isys.c
+++ isys/isys.c
@@ -692,7 +692,7 @@ static PyObject * doWipeRaidSuperblock(PyObject * s, PyObject * args) {
     /* put the size in 1k blocks */
     size >>= 1;
 
-    if (lseek64(fd, ((off64_t) 1024) * (off64_t) MD_NEW_SIZE_BLOCKS(size), SEEK_SET) < 0) {
+    if (lseek64(fd, ((off64_t) 512) * (off64_t) MD_NEW_SIZE_SECTORS(size), SEEK_SET) < 0) {
 	PyErr_SetFromErrno(PyExc_SystemError);
 	return NULL;
     } 
@@ -724,7 +724,7 @@ static PyObject * doGetRaidSuperblock(PyObject * s, PyObject * args) {
     /* put the size in 1k blocks */
     size >>= 1;
 
-    if (lseek64(fd, ((off64_t) 1024) * (off64_t) MD_NEW_SIZE_BLOCKS(size), SEEK_SET) < 0) {
+    if (lseek64(fd, ((off64_t) 512) * (off64_t) MD_NEW_SIZE_SECTORS(size), SEEK_SET) < 0) {
 	PyErr_SetFromErrno(PyExc_SystemError);
 	return NULL;
     } 
@@ -762,7 +762,7 @@ static PyObject * doGetRaidChunkSize(PyObject * s, PyObject * args) {
     /* put the size in 1k blocks */
     size >>= 1;
 
-    if (lseek64(fd, ((off64_t) 1024) * (off64_t) MD_NEW_SIZE_BLOCKS(size), SEEK_SET) < 0) {
+    if (lseek64(fd, ((off64_t) 512) * (off64_t) MD_NEW_SIZE_SECTORS(size), SEEK_SET) < 0) {
 	PyErr_SetFromErrno(PyExc_SystemError);
 	return NULL;
     } 
