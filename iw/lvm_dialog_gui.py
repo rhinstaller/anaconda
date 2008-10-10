ae3201aa10014e74e9be195063333eab22b58498
Peter Jones <pjones@redhat.com>
2008-10-10 16:55:02 -0400

    Revert "Don't clamp down extra PE in getSmallestPVSize."
    There's no bz# here filed against 5.3 with ACKs.  Please re-commit this
    with the appropriate documentation.
    
    This reverts commit 840bc3dc2a3a66a7761d6b0f310239fc1e9434ec.

--- iw/lvm_dialog_gui.py
+++ iw/lvm_dialog_gui.py
@@ -80,7 +80,7 @@ class VolumeGroupEditor:
                 pesize = 32768
 	    pvreq = self.partitions.getRequestByID(id)
 	    pvsize = pvreq.getActualSize(self.partitions, self.diskset)
-            pvsize = lvm.clampPVSize(pvsize, pesize)
+            pvsize = lvm.clampPVSize(pvsize, pesize) - (pesize/1024)
 	    if first:
 		minpvsize = pvsize
 		first = 0
