978533e996dabdab2e07b07a980e3decb29312b3
Chris Lumens <clumens@redhat.com>
2009-03-23 10:00:50 -0400

    Add a missing import (#491605).

--- kickstart.py
+++ kickstart.py
@@ -18,6 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from storage.deviceaction import *
 from storage.devices import LUKSDevice
 from storage.devicelibs.lvm import getPossiblePhysicalExtents
 from storage.formats import getFormat
