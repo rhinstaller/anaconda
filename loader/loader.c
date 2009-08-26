rhinstaller/anaconda

commit 245d1951c40ecb61cee11f3ecc00e483bc8dd142

Retry network configuration in loader (#492009)
Rewrite disabled network configuration scripts in loader if we ask the
user to retry network configuration.  nm-system-settings will pick up
the change to those files and down those interfaces.

loader/loader.c
@@ -1378,7 +1378,7 @@ static char *doLoaderMain(struct loaderData_s *loaderData,
                                      _("OK"), _("Back"), NULL);
                 }
 
-                if (rc && rc != 1) {
+                if (rc && (rc != 1) && (loaderData->method == -1)) {
                     loaderData->method = -1;
                     step = STEP_KBD;
                     dir = -1;

loader/net.c
@@ -2,7 +2,7 @@
  * net.c
  *
  * Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005  Red Hat, Inc.
- *               2006, 2007, 2008
+ *               2006, 2007, 2008, 2009
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1939,6 +1939,11 @@ int kickstartNetworkUp(struct loaderData_s * loaderData, iface_t * iface) {
             /* Going back to the interface selection screen, so unset anything
              * we set before attempting to bring the incorrect interface up.
              */
+            if ((rc = writeDisabledNetInfo()) != 0) {
+                logMessage(ERROR, "writeDisabledNetInfo failure (%s): %d",
+                           __func__, rc);
+            }
+
             loaderData->netDev_set = 0;
             loaderData->ipinfo_set = 0;
             free(loaderData->ipv4);

commit ec146852176e2b41ace7725ed8eda337842d3160

Fix going back in "Inst. Method" and "Configure TCP/IP" screens in stage 1 (#515450)
Going back from method screen was broken with commit
245d1951c40ecb61cee11f3ecc00e483bc8dd142. This patch implements Retry flow differently -
by staying on the Configure TCP/IP screen instead of going there through hacked
(skipped) method screen. The interface can still be changed by going Back
from Configure TCP/IP which is fixed in this patch too.

loader/loader.c
@@ -1387,10 +1387,12 @@ static char *doLoaderMain(struct loaderData_s *loaderData,
                                        "image?"),
                                      30, 10, 20, 6, installNames, &loaderData->method,
                                      _("OK"), _("Back"), NULL);
+                    if (rc == 2) {
+                        loaderData->method = -1;
+                    }
                 }
 
-                if (rc && (rc != 1) && (loaderData->method == -1)) {
-                    loaderData->method = -1;
+                if (rc && (rc != 1)) {
                     step = STEP_KBD;
                     dir = -1;
                 } else {
@@ -1504,6 +1506,10 @@ static char *doLoaderMain(struct loaderData_s *loaderData,
                 rc = chooseNetworkInterface(loaderData);
                 if ((rc == LOADER_BACK) || (rc == LOADER_ERROR) ||
                     ((dir == -1) && (rc == LOADER_NOOP))) {
+                    /* don't skip method dialog iff we don't have url from ks or boot params */
+                    if (!loaderData->stage2Data) {
+                        loaderData->method = -1;
+                    }
                     step = STEP_METHOD;
                     dir = -1;
                     break;
@@ -1555,11 +1561,16 @@ static char *doLoaderMain(struct loaderData_s *loaderData,
                 free(ret);
                 ret = NULL;
 
-                if ((rc == LOADER_BACK) || (rc == LOADER_ERROR) ||
+                if ((rc == LOADER_BACK) ||
                     ((dir == -1) && (rc == LOADER_NOOP))) {
                     needsNetwork = 1;
                     step = STEP_IFACE;
                     dir = -1;
+                    break;
+                }
+                /* retry */
+                if (rc == LOADER_ERROR) {
+                    needsNetwork = 1;
                     break;
                 }
 

loader/net.c
@@ -522,7 +522,7 @@ int readNetConfig(char * device, iface_t * iface,
                              "interface."));
             iface->ipv4method = IPV4_UNUSED_METHOD;
             iface->ipv6method = IPV6_UNUSED_METHOD;
-            return LOADER_BACK;
+            return LOADER_ERROR;
         }
     }
 