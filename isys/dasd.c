9c061a2b4ee5602c13ff5f786e29a3a7628b074b
Chris Lumens <clumens@redhat.com>
2009-09-03 10:14:33 -0400

    The vtoc.h header has moved from the kernel to s390utils (karsten, #520830).

--- isys/dasd.c
+++ isys/dasd.c
@@ -35,9 +35,8 @@
 #define u16 __u16
 #define u32 __u32
 #define u64 __u64
-#include <asm/vtoc.h>
+#include <s390utils/vtoc.h>
 #include <asm/dasd.h>
-typedef struct vtoc_volume_label volume_label_t;
 #endif
 
 #if defined(__s390__) || defined(__s390x__)
