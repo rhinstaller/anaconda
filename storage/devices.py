81a0a090d8e811905449d0d682712371f916798d
David Cantrell <dcantrell@redhat.com>
2009-10-12 11:14:58 -1000

    Add dracutSetupData() method to DASDDevice (#526354).
    Return kernel parameter information for the DASD.  NOTE:  This is using
    the current rd_DASD parameter in dracut, which is just a copy of the
    dasd= kernel parameter.  Will change this once dracut gets a better
    rd_DASD parameter.

--- storage/devices.py
+++ storage/devices.py
@@ -3163,6 +3163,31 @@ class DASDDevice(DiskDevice):
         self.opts = kwargs.get('opts')
         DiskDevice.__init__(self, device, kwargs)
 
+    def dracutSetupString(self):
+        arg = "rd_DASD=%s" % (self.busid,)
+
+        # XXX: Temporary parameter until dracut gets a new rd_DASD option.
+        # Using the old dasd= kernel parameter syntax for now.  When dracut
+        # is fixed up, we can use these lines instead:
+        #
+        #for opt in self.opts.keys():
+        #    arg += ",%s=%s" % (opt, self.opts[opt],)
+
+        flags = []
+        for opt in self.opts.keys():
+            if self.opts[opt] == "1":
+                if opt == "readonly":
+                    flags.append("ro")
+                elif opt == "use_diag":
+                    flags.append("diag")
+                else:
+                    flags.append(opt)
+
+        if len(flags):
+            arg += "(" + ":".join(flags) + ")"
+
+        return arg
+
 
 class NFSDevice(StorageDevice, NetworkStorageDevice):
     """ An NFS device """
