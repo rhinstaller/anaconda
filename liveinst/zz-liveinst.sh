9bfe661b0e69d1aa87ecbeb5b9f5a88ddb6cc931
Chris Lumens <clumens@redhat.com>
2009-10-14 16:36:41 -0400

    Use /dev/mapper/live-osimg-min instead of the old device node name (#526789).

--- liveinst/zz-liveinst.sh
+++ liveinst/zz-liveinst.sh
@@ -4,7 +4,7 @@
 
 # don't run on geode (olpc)
 if [ `grep -c Geode /proc/cpuinfo` -eq 0 ]; then
-  if [ -b /dev/live-osimg ]; then
+  if [ -b /dev/mapper/live-osimg-min ]; then
     test -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs && source ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs
     cp /usr/share/applications/liveinst.desktop "${XDG_DESKTOP_DIR:-$HOME/Desktop}"
   elif [ -f /.livecd-configured ]; then  # FIXME: old way... this should go away
