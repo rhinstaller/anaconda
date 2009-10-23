cc7d403c2c338c0d71de0bca8ff9b316b336a6b2
David Cantrell <dcantrell@redhat.com>
2009-10-22 15:16:53 -1000

    Find and format any unformatted DASD devices (#528386).
    Before the device tree is built in the storage code, run through the
    DASD devices on the system and format any with a status of
    'unformatted'.  We have to run dasdfmt on these devices before we can
    partition them.
    
    Under RHEL-5, this process was serial and the user had to click Yes for
    each unformatted DASD found, which could take a really long time if
    you had thousands of DASDs.
    
    The idea now is that if the DASD is seen by anaconda, we want to use it
    for installation.  The stage 1 device initialization routines as well as
    the CMS conf file provided at boot time allow the user to restrict the
    range of devices we see during installation.  If any of the devices we
    see are unformatted, run dasdfmt before building the devicetree.
    
    This process is implemented in a DASD class that works similar to the
    iscsi and zfcp classes.  That is, devices may need to be started or
    brought in to a working state before the anaconda storage code can use
    them.

---
+++ storage/dasd.py
@@ -0,0 +1,172 @@
+#
+# dasd.py - DASD class
+#
+# Copyright (C) 2009  Red Hat, Inc.  All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Red Hat Author(s): David Cantrell <dcantrell@redhat.com>
+#
+
+import iutil
+import sys
+import os
+from storage.devices import deviceNameToDiskByPath
+from constants import *
+from flags import flags
+
+import logging
+log = logging.getLogger("anaconda")
+
+import gettext
+_ = lambda x: gettext.ldgettext("anaconda", x)
+P_ = lambda x, y, z: gettext.ldngettext("anaconda", x, y, z)
+
+class DASD:
+    """ Controlling class for DASD interaction before the storage code in
+        anaconda has initialized.
+
+        The DASD class can determine if any DASD devices on the system are
+        unformatted and can perform a dasdfmt on them.
+    """
+
+    def __init__(self):
+        self._dasdlist = []
+        self._totalCylinders = 0
+        self._completedCylinders = 0.0
+        self._maxFormatJobs = 0
+        self.started = False
+
+    def startup(self, intf=None):
+        """ Look for any unformatted DASDs in the system and offer the user
+            the option for format them with dasdfmt or exit the installer.
+        """
+        if self.started:
+            return
+
+        self.started = True
+
+        if not iutil.isS390():
+            return
+
+        log.info("Checking for unformatted DASD devices:")
+
+        for device in os.listdir("/sys/block"):
+            if not device.startswith("dasd"):
+                continue
+
+            statusfile = "/sys/block/%s/device/status" % (device,)
+            if not os.path.isfile(statusfile):
+                continue
+
+            f = open(statusfile, "r")
+            status = f.read().strip()
+            f.close()
+
+            if status == "unformatted":
+                log.info("    %s is an unformatted DASD" % (device,))
+                self._dasdlist.append(device)
+
+        if not len(self._dasdlist):
+            log.info("    no unformatted DASD devices found")
+            return
+
+        tmplist = map(lambda s: "/dev/" + s, self._dasdlist)
+        self._dasdlist = map(lambda s: deviceNameToDiskByPath(s), tmplist)
+
+        if intf:
+            c = len(self._dasdlist)
+
+            title = P_("Unformatted DASD Device Found",
+                       "Unformatted DASD Devices Found", c)
+            msg = P_("Format uninitialized DASD device?\n\n"
+                     "There is %d uninitialized DASD device on this "
+                     "system.  To continue installation, the device must "
+                     "be formatted.  Formatting will remove any data on "
+                     "this device." % c,
+                     "Format uninitialized DASD devices?\n\n"
+                     "There are %d uninitialized DASD devices on this "
+                     "system.  To continue installation, the devices must "
+                     "be formatted.  Formatting will remove any data on "
+                     "these devices." % c,
+                     c)
+
+            devs = ''
+            for dasd in self._dasdlist:
+                devs += "%s\n" % (dasd,)
+
+            icon = "/usr/share/icons/gnome/32x32/status/dialog-error.png"
+            buttons = [_("_Format"), _("_Exit installer")]
+            rc = intf.detailedMessageWindow(title, msg, devs.strip(),
+                                            type="custom",
+                                            custom_icon=icon,
+                                            custom_buttons=buttons)
+            if rc == 1:
+                sys.exit(0)
+
+        argv = ["-y", "-P", "-d", "cdl", "-b", "4096"]
+
+        if intf:
+            title = P_("Formatting DASD Device", "Formatting DASD Devices", c)
+            msg = P_("Preparing %d DASD device for use with Linux..." % c,
+                     "Preparing %d DASD devices for use with Linux..." % c, c)
+            pw = intf.progressWindow(title, msg, 1.0)
+
+            for dasd in self._dasdlist:
+                log.info("Running dasdfmt on %s" % (dasd,))
+                iutil.execWithCallback("/sbin/dasdfmt", argv + [dasd],
+                                       stdout="/dev/tty5", stderr="/dev/tty5",
+                                       callback=self._updateProgressWindow,
+                                       callback_data=pw)
+
+            pw.pop()
+        else:
+            for dasd in self._dasdlist:
+                log.info("Running dasdfmt on %s" % (dasd,))
+                iutil.execWithRedirect("/sbin/dasdfmt", argv + [dasd],
+                                       stdout="/dev/tty5", stderr="/dev/tty5")
+
+    def _updateProgressWindow(self, data, callback_data=None):
+        """ Reads progress output from dasdfmt and collects the number of
+            cylinders completed so the progress window can update.
+        """
+        if not callback_data:
+            return
+
+        if data == '\n':
+            # each newline we see in this output means one more cylinder done
+            self._completedCylinders += 1.0
+            callback_data.set(self._completedCylinders / self.totalCylinders)
+
+    @property
+    def totalCylinders(self):
+        """ Total number of cylinders of all unformatted DASD devices. """
+        if self._totalCylinders:
+            return self._totalCylinders
+
+        argv = ["-t", "-v", "-y", "-d", "cdl", "-b", "4096"]
+        for dasd in self._dasdlist:
+            buf = iutil.execWithCapture("/sbin/dasdfmt", argv + [dasd],
+                                        stderr="/dev/tty5")
+            for line in buf.splitlines():
+                if line.startswith("Drive Geometry: "):
+                    # line will look like this:
+                    # Drive Geometry: 3339 Cylinders * 15 Heads =  50085 Tracks
+                    cyls = long(filter(lambda s: s, line.split(' '))[2])
+                    self._totalCylinders += cyls
+                    break
+
+        return self._totalCylinders
+
+# vim:tw=78:ts=4:et:sw=4
