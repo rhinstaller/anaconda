2ac86ad86976efcb536ab077f33620f53af7a0fe
David Cantrell <dcantrell@redhat.com>
2009-10-28 12:47:03 -1000

    Use 'zerombr' kickstart command for DASDs needing dasdfmt (#528386).
    If performing a kickstart install on s390, dasdfmt is only run if:
    a) There is a UI available to prompt the user.
    b) 'zerombr' is specified in the kickstart file.
    
    The zerombr command will also skip the UI if there is one.  The UI
    prompt is a fallback.

--- storage/__init__.py
+++ storage/__init__.py
@@ -322,7 +322,7 @@ class Storage(object):
         self.iscsi.startup(self.anaconda.intf)
         self.fcoe.startup(self.anaconda.intf)
         self.zfcp.startup()
-        self.dasd.startup(self.anaconda.intf)
+        self.dasd.startup(intf=self.anaconda.intf, zeroMbr=self.zeroMbr)
         if self.anaconda.id.getUpgrade():
             clearPartType = CLEARPART_TYPE_NONE
         else:
