0f7af6258e98169dbf3f6ffd254877e078ccd400
Peter Jones <pjones@redhat.com>
2010-01-14 16:25:40 -0500

    Show call depth with spaces in log_method_call()
    Prepend log_method_call()'s output with spaces corresponding to the
    depth of the current call stack, so you can see what's calling what.

--- storage/storage_log.py
+++ storage/storage_log.py
@@ -4,9 +4,13 @@ import inspect
 
 def log_method_call(d, *args, **kwargs):
     classname = d.__class__.__name__
-    methodname = inspect.stack()[1][3]
-    fmt = "%s.%s:"
-    fmt_args = [classname, methodname]
+    stack = inspect.stack()
+    methodname = stack[1][3]
+
+    spaces = len(stack) * ' '
+    fmt = "%s%s.%s:"
+    fmt_args = [spaces, classname, methodname]
+
     for arg in args:
         fmt += " %s ;"
         fmt_args.append(arg)
