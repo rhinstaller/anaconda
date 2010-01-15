3e20055db38f2d9445b4854b0571579c59c342ac
Peter Jones <pjones@redhat.com>
2010-01-15 12:28:33 -0500

    Add udev accessor for ID_MODEL_FROM_DATABASE/ID_MODEL.
    We get model info from udev, and it's useful to expose it.

--- storage/udev.py
+++ storage/udev.py
@@ -272,6 +272,10 @@ def udev_device_get_vendor(udev_info):
     """ Get the vendor of the device as reported by udev. """
     return udev_info.get("ID_VENDOR_FROM_DATABASE", udev_info.get("ID_VENDOR"))
 
+def udev_device_get_model(udev_info):
+    """ Get the model of the device as reported by udev. """
+    return udev_info.get("ID_MODEL_FROM_DATABASE", udev_info.get("ID_MODEL"))
+
 def udev_device_get_bus(udev_info):
     """ Get the bus a device is connected to the system by. """
     return udev_info.get("ID_BUS", "").upper()
