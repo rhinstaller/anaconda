5cce4b7329c8db705e3a7daa1d587df351b0615e
David Lehman <dlehman@redhat.com>
2010-03-09 18:05:29 -0600

    Preserve encryption setting when re-editing new encrypted LVs. (#568547)
    The part of the if clause that I'm removing will never be true anyhow
    -- it requires a preexisting format, but it is inside a clause that
    only gets entered for non-existing LVs, which obviously cannot contain
    pre-existing formats.

--- iw/lvm_dialog_gui.py
+++ iw/lvm_dialog_gui.py
@@ -722,12 +722,15 @@ class VolumeGroupEditor:
                     continue
 
                 format = fmt_class(mountpoint=mountpoint)
-                if self.lukscb and self.lukscb.get_active() and \
-                   (templv.format.type != "luks" or
-                    (templv.format.exists and not templv.format.hasKey)):
-                    newluks = format
-                    format = getFormat("luks",
+                if self.lukscb and self.lukscb.get_active():
+                    if templv.format.type != "luks":
+                        newluks = format
+                        format = getFormat("luks",
                                        passphrase=self.storage.encryptionPassphrase)
+                    else:
+                        newluks = format
+                        format = templv.format
+
                 templv.format = format
             else:
                 # existing lv
