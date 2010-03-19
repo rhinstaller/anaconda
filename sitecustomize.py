6e0f49f769235a6dddbb9555d81d452ede67a03e
Ales Kozumplik <akozumpl@redhat.com>
2010-03-19 10:30:32 +0100

    Make python start with correct default unicode encoding (#539904).
    Currently it's ascii and that's bad. import gtk; changes it to utf-8
    anyway but that doesn't happen in text install of course. The default
    encoding is set vie sitecustomize.py, it is not possible to do so in any
    other way.
    
    The patch has been tested with a custom compose.

---
+++ sitecustomize.py
@@ -0,0 +1,22 @@
+#
+# sitecustomize.py: The python site init code for the Red Hat Installer
+#
+# Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+# Red Hat, Inc.  All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+sys.setdefaultencoding('utf-8')
