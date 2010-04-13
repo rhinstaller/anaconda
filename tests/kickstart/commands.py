5fbd314619addb0009f9e5f079a822e0cf768bc2
Chris Lumens <clumens@redhat.com>
2010-04-13 13:38:09 -0400

    Add a test case to verify that kickstart commands use the right handler.
    The aim of this is to prevent bugs like 581829, where a later version of a
    command is added in pykickstart but anaconda is never updated to use the
    right version.  Of course, this shifts the burden from updating kickstart.py
    to running the test case.  But it's better than nothing.

---
+++ tests/kickstart/commands.py
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2010  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Chris Lumens <clumens@redhat.com>
+import unittest
+
+import kickstart
+import pykickstart.version
+
+# Verify that each kickstart command in anaconda uses the correct version of
+# that command as provided by pykickstart.  That is, if there's an FC3 and an
+# F10 version of a command, make sure anaconda >= F10 uses the F10 version.
+class CommandVersionTestCase(unittest.TestCase):
+    def setUp(self):
+        self.handler = pykickstart.version.makeVersion(kickstart.ver)
+
+    def runTest(self):
+        for (commandName, commandObj) in kickstart.commandMap.iteritems():
+            baseClass = commandObj().__class__.__bases__[0]
+            pykickstartClass = self.handler.commands[commandName].__class__
+            self.assertEqual(baseClass.__name__, pykickstartClass.__name__)
+
+# Do the same thing as CommandVersionTestCase, but for data objects.
+class DataVersionTestCase(unittest.TestCase):
+    def setUp(self):
+        self.handler = pykickstart.version.makeVersion(kickstart.ver)
+
+    def runTest(self):
+        for (dataName, dataObj) in kickstart.dataMap.iteritems():
+            baseClass = dataObj().__class__.__bases__[0]
+
+            # pykickstart does not expose data objects a mapping the way it
+            # does command objects.
+            pykickstartClass = eval("self.handler.%s" % dataName)
+
+            self.assertEqual(baseClass.__name__, pykickstartClass.__name__)
+
+if __name__ == "__main__":
+    unittest.main()
