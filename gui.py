5745f4b7224bfddfd1ee8c0c1fb0639277401023
Radek Vykydal <rvykydal@redhat.com>
2010-04-26 15:26:46 +0200

    Don't ask when configuring net if we have only one network device (#520146)
    This is only for [Configure Network] button, otoh when enabling network,
    still offer pre-nmce dialog to inform user that he needs to configure
    network.

--- gui.py
+++ gui.py
@@ -957,6 +957,7 @@ class InstallInterface(InstallInterfaceBase):
 
 
     # TODORV: handle one device case - don't ask
+    # just_setup is used for [Configure Network] button
     def enableNetwork(self, just_setup=False):
 
         if len(self.anaconda.network.netdevices) == 0:
@@ -967,11 +968,15 @@ class InstallInterface(InstallInterfaceBase):
 
         networkEnabled = False
         while not networkEnabled:
-            choice = selectNetDevicesDialog(self.anaconda.network,
-                                            select_install_device=(not just_setup))
-            if not choice:
-                break
-            nm_controlled_devices, install_device = choice
+            if just_setup and len(self.anaconda.network.netdevices) <= 1:
+                nm_controlled_devices = self.anaconda.network.netdevices.keys()
+                install_device = None
+            else:
+                choice = selectNetDevicesDialog(self.anaconda.network,
+                                                select_install_device=(not just_setup))
+                if not choice:
+                    break
+                nm_controlled_devices, install_device = choice
 
             # update ifcfg files for nm-c-e
             self.anaconda.network.setNMControlledDevices(nm_controlled_devices)
