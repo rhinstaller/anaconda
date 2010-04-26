9024c01a29891acf6261f5792462571970dc89c3
David Cantrell <dcantrell@redhat.com>
2010-04-26 09:33:24 -1000

    Revert "Set minswap suggestion on s390x to 1 (#466289)."
    This reverts commit bb743f32bec4e1b7ee347550b139051edaedd8a5.  After
    further investigation, there is no need to change the minswap
    suggestion on s390x.  The original request was to have anaconda not warn
    if you created a swap partition lower than this minimum, which is what
    happens already.  So there is nothing to do for this bug after all.

--- iutil.py
+++ iutil.py
@@ -454,9 +454,6 @@ def swapSuggestion(quiet=0):
             minswap = mem
             maxswap = 2*mem
 
-    if isS390():
-        minswap = 1
-
     if not quiet:
 	log.info("Swap attempt of %sM to %sM", minswap, maxswap)
 
