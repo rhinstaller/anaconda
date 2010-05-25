cab8e2947450ce069b910664519c788994cb9cbb
Martin Sivak <msivak@redhat.com>
2010-05-25 15:08:30 +0200

    Fix rescue mode startup with kickstart file and without (#515896)
    We had several reboot problems in rescue mode when starting with kickstart or when using kickstart and 'rescue' option on the boot prompt.
    
    1) Kickstart's install command had higher priority over command line options. This was really bad as it prevented user to start rescue using our standard PXE targets. This changes it so cmdline option has higher priority.
    
    2) Rescue automatically and unconditionally rebooted after processing the kickstart. This was equally bad as it wasn't possible to use kickstart to specify all location data and start rescue with it. This patch changes the code to honour the reboot command in kickstart and to start a shell if no such command is present in the kickstart.

--- rescue.py
+++ rescue.py
@@ -37,6 +37,7 @@ import shutil
 import time
 import network
 import subprocess
+from pykickstart.constants import *
 
 import gettext
 _ = lambda x: gettext.ldgettext("anaconda", x)
@@ -466,7 +467,8 @@ def runRescue(anaconda, instClass):
                       "automatically when you exit from the shell.") % (anaconda.rootPath,),
                       [_("OK")] )
     else:
-        if anaconda.isKickstart:
+        if anaconda.isKickstart and \
+               anaconda.id.ksdata.reboot.action in [KS_REBOOT, KS_SHUTDOWN]:
             log.info("No Linux partitions found")
             screen.finish()
             print(_("You don't have any Linux partitions.  Rebooting.\n"))
@@ -487,15 +489,22 @@ def runRescue(anaconda, instClass):
         except Exception, e:
             log.error("error making a resolv.conf: %s" %(e,))
         msgStr = _("Your system is mounted under the %s directory.") % (anaconda.rootPath,)
+        ButtonChoiceWindow(screen, _("Rescue"), msgStr, [_("OK")] )
 
+    # we do not need ncurses anymore, shut them down
+    screen.finish()
+    
     #create /etc/fstab in ramdisk, so it is easier to work with RO mounted filesystems
     makeFStab()
 
     # run %post if we've mounted everything
-    if anaconda.isKickstart:
+    if rootmounted and not readOnly and anaconda.isKickstart:
         from kickstart import runPostScripts
         runPostScripts(anaconda)
-    else:
+
+    # start shell if reboot wasn't requested
+    if not anaconda.isKickstart or \
+           not anaconda.id.ksdata.reboot.action in [KS_REBOOT, KS_SHUTDOWN]:
         runShell(screen, msgStr)
 
     sys.exit(0)
