16cec862f7420943c5bc852826ff2dbc87c9240e
Hans de Goede <hdegoede@redhat.com>
2010-05-25 13:48:42 +0200

    Move init questions to InstallInterfaceBase
    The code is identical for text mode and gui mode (except for one
    copy paste error fixed by the previous commit GRRRR).

--- gui.py
+++ gui.py
@@ -940,8 +940,6 @@ class InstallInterface(InstallInterfaceBase):
         root = gtk.gdk.get_default_root_window()
         cursor = gtk.gdk.Cursor(gtk.gdk.LEFT_PTR)
         root.set_cursor(cursor)
-        self._initLabelAnswers = {}
-        self._inconsistentLVMAnswers = {}
 
     def __del__ (self):
         pass
@@ -1172,114 +1170,6 @@ class InstallInterface(InstallInterfaceBase):
         rc = d.run()
         d.destroy()
         return rc
-
-    def resetInitializeDiskQuestion(self):
-        self._initLabelAnswers = {}
-
-    def questionInitializeDisk(self, path, description, size, details=""):
-
-        retVal = False # The less destructive default
-
-        if not path:
-            return retVal
-
-        # we are caching answers so that we don't
-        # ask in each storage.reset() again
-        if path in self._initLabelAnswers:
-            log.info("UI not asking about disk initialization, "
-                     "using cached answer: %s" % self._initLabelAnswers[path])
-            return self._initLabelAnswers[path]
-        elif "all" in self._initLabelAnswers:
-            log.info("UI not asking about disk initialization, "
-                     "using cached answer: %s" % self._initLabelAnswers["all"])
-            return self._initLabelAnswers["all"]
-
-        rc = self.messageWindow(_("Warning"),
-                _("Error processing drive:\n\n"
-                  "%(path)s\n%(size)-0.fMB\n%(description)s\n\n"
-                  "This device may need to be reinitialized.\n\n"
-                  "REINITIALIZING WILL CAUSE ALL DATA TO BE LOST!\n\n"
-                  "This action may also be applied to all other disks "
-                  "needing reinitialization.%(details)s")
-                % {'path': path, 'size': size,
-                   'description': description, 'details': details},
-                type="custom",
-                custom_buttons = [ _("_Ignore"),
-                                   _("Ignore _all"),
-                                   _("_Re-initialize"),
-                                   _("Re-ini_tialize all") ],
-                custom_icon="question")
-        if rc == 0:
-            retVal = False
-        elif rc == 1:
-            path = "all"
-            retVal = False
-        elif rc == 2:
-            retVal = True
-        elif rc == 3:
-            path = "all"
-            retVal = True
-
-        self._initLabelAnswers[path] = retVal
-        return retVal
-
-    def resetReinitInconsistentLVMQuestion(self):
-        self._inconsistentLVMAnswers = {}
-
-    def questionReinitInconsistentLVM(self, pv_names=None, lv_name=None, vg_name=None):
-
-        retVal = False # The less destructive default
-        allSet = frozenset(["all"])
-
-        if not pv_names or (lv_name is None and vg_name is None):
-            return retVal
-
-        # We are caching answers so that we don't ask for ignoring
-        # in each storage.reset() again (note that reinitialization is
-        # done right after confirmation in dialog, not as a planned
-        # action).
-        key = frozenset(pv_names)
-        if key in self._inconsistentLVMAnswers:
-            log.info("UI not asking about disk initialization, "
-                     "using cached answer: %s" % self._inconsistentLVMAnswers[key])
-            return self._inconsistentLVMAnswers[key]
-        elif allSet in self._inconsistentLVMAnswers:
-            log.info("UI not asking about disk initialization, "
-                     "using cached answer: %s" % self._inconsistentLVMAnswers[allSet])
-            return self._inconsistentLVMAnswers[allSet]
-
-        if vg_name is not None:
-            message = "Volume Group %s" % vg_name
-        elif lv_name is not None:
-            message = "Logical Volume %s" % lv_name
-
-        na = {'msg': message, 'pvs': ", ".join(pv_names)}
-        rc = self.messageWindow(_("Warning"),
-                  _("Error processing LVM.\n"
-                    "There is inconsistent LVM data on %(msg)s.  You can "
-                    "reinitialize all related PVs (%(pvs)s) which will erase "
-                    "the LVM metadata, or ignore which will preserve the "
-                    "contents.  This action may also be applied to all other "
-                    "PVs with inconsistent metadata.") % na,
-                type="custom",
-                custom_buttons = [ _("_Ignore"),
-                                   _("Ignore _all"),
-                                   _("_Re-initialize"),
-                                   _("Re-ini_tialize all") ],
-                custom_icon="question")
-        if rc == 0:
-            retVal = False
-        elif rc == 1:
-            key = allSet
-            retVal = False
-        elif rc == 2:
-            retVal = True
-        elif rc == 3:
-            key = allSet
-            retVal = True
-
-        self._inconsistentLVMAnswers[key] = retVal
-        return retVal
 
     def beep(self):
         gtk.gdk.beep()
