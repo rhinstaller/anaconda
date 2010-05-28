29c0ed968b2521815ba31581413bb61b6260c3d1
Hans de Goede <hdegoede@redhat.com>
2010-05-28 16:13:28 +0200

    booty make getDiskPart use the devicetree

--- booty/util.py
+++ booty/util.py
@@ -1,33 +1,11 @@
-import string
-from flags import flags
-
 def getDiskPart(dev, storage):
-    path = storage.devicetree.getDeviceByName(dev).path[5:]
-    cut = len(dev)
-    if dev[-1] in string.digits:
-        if (path.startswith('rd/') or path.startswith('ida/') or
-                path.startswith('cciss/') or path.startswith('sx8/') or
-                path.startswith('mapper/') or path.startswith('mmcblk') or
-                path.startswith('md')):
-            if dev[-2] == 'p':
-                cut = -2
-            elif dev[-3] == 'p' and dev[-2] in string.digits:
-                cut = -3
-        else:
-            if dev[-2] in string.digits:
-                cut = -2
-            else:
-                cut = -1
+    dev = storage.devicetree.getDeviceByName(dev)
 
-    name = dev[:cut]
-
-    if cut < 0:
-        part = dev[cut:]
-        if part[0] == 'p':
-            part = part[1:]
-        partNum = int(part) - 1
+    if dev.type == "partition":
+        partNum = dev.partedPartition.number - 1
+        disk = dev.disk
     else:
         partNum = None
-
-    return (name, partNum)
-
+        disk = dev
+    
+    return (disk.name, partNum)
