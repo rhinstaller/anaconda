ec7b9d45beda5a1eac151bfef42f68abe270a222
Brian C. Lane <bcl@redhat.com>
2010-06-09 11:42:27 -0700

    Properly rescan storage with Reset in partition GUI (#635778)
    Temporarily reset the partition type when rescanning so that
    the original settings will be discovered when hitting 'reset'
    in the partition dialog.
    
    Related: rhbz#635778

--- pyanaconda/iw/partition_gui.py
+++ pyanaconda/iw/partition_gui.py
@@ -1561,7 +1561,12 @@ class PartitionWindow(InstallWindow):
             return
 
         self.stripeGraph.shutDown()
+        # temporarily unset storage.clearPartType so that all devices will be
+        # found during storage reset
+        clearPartType = self.storage.clearPartType
+        self.storage.clearPartType = None
         self.storage.reset()
+        self.storage.clearPartType = clearPartType
         self.tree.clear()
         self.populate()
 
