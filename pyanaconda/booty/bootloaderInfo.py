d1e4bd6341c069e718dd1982ca0e95bbe527b33f
David Cantrell <dcantrell@redhat.com>
2010-06-29 09:19:21 -1000

    Make sure swap devices are included in dracut args (#607646)
    dracut needs swap devices specified in rd_* arguments for power
    management to know what to use for hibernate.

--- pyanaconda/booty/bootloaderInfo.py
+++ pyanaconda/booty/bootloaderInfo.py
@@ -118,7 +118,7 @@ class KernelArguments:
         args = ""
         bootArgs = []
         rootDev = self.anaconda.storage.rootDevice
-        neededDevs = [ rootDev ]
+        neededDevs = [ rootDev ] + self.anaconda.storage.swaps
 
         if flags.cmdline.get("fips") == "1":
             bootDev = self.anaconda.storage.mountpoints.get("/boot", rootDev)
