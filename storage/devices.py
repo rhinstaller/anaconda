5c83bbf01800ca86d1c8e4cd95d14480165b344c
Ales Kozumplik <akozumpl@redhat.com>
2010-08-17 18:41:30 +0200

    mpath: do not deactivate mpath device upon its teardown.
    So /dev/mapper/mpathx is still existing after the findrootparts step tears
    the mpath device down. As we change the format of mpathx to disklabel,
    parted device initialization will need /dev/mapper/mpathx to exist.
    
    Related: rhbz#624175

--- storage/devices.py
+++ storage/devices.py
@@ -3213,6 +3213,16 @@ class MultipathDevice(DMDevice):
                 self.format.teardown()
             udev_settle()
 
+        if recursive:
+            self.teardownParents(recursive=recursive)
+
+    def deactivate(self):
+        """ 
+        This is never called, included just for documentation.
+
+        If we called this during teardown(), we wouldn't be able to get parted
+        object because /dev/mapper/mpathX wouldn't exist.
+        """
         if self.exists and os.path.exists(self.path):
             #self.teardownPartitions()
             #rc = iutil.execWithRedirect("multipath",
@@ -3231,9 +3241,6 @@ class MultipathDevice(DMDevice):
             except Exception as e:
                 raise MPathError("failed to tear down multipath device %s: %s"
                                 % (self.name, e))
-
-        if recursive:
-            self.teardownParents(recursive=recursive)
 
     def setup(self, intf=None, orig=False):
         """ Open, or set up, a device. """
