27f47a38aeef7806c54e2c9d079f89df401d26ab
Radek Vykydal <rvykydal@redhat.com>
2010-10-07 12:20:24 +0200

    Fix non-dhcp network enablement in stage 2 (#640951)
    IPv4 DHCP configuration for enabled device was applied no matter
    what the configuration in nm-c-e had been.
    
    The fix is to set ONBOOT=yes (which brings the device up) not
    before, but after running nm-c-e.

--- pyanaconda/gui.py
+++ pyanaconda/gui.py
@@ -922,8 +922,6 @@ class InstallInterface(InstallInterfaceBase):
 
             # update ifcfg files for nm-c-e
             self.anaconda.network.setNMControlledDevices(nm_controlled_devices)
-            if not just_setup:
-                self.anaconda.network.updateActiveDevices([install_device])
 
             # we might want to do this only once
             if self.anaconda.network.hasWirelessDev():
@@ -952,6 +950,7 @@ class InstallInterface(InstallInterfaceBase):
                 waited_devs = self.anaconda.network.getOnbootControlledIfaces()
             else:
                 waited_devs = [install_device]
+                self.anaconda.network.updateActiveDevices([install_device])
 
             self.anaconda.network.write()
 
