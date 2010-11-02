0567bb5253c57bcc6deb2c0f897319a14f8c093d
David Cantrell <dcantrell@redhat.com>
2010-11-02 08:39:12 -1000

    Add libarchive helper functions for loader in unpack.c
    Created some helper functions for loader's interaction with
    libarchive.  There are 3 functions currently:
    
    1) A function to initialize a new archive object
    2) A function to extract all archive members to a specific
       destination (if NULL, extract to current directory)
    3) A function wrapping #1 and #2 to extract a compressed
       archive file

---
+++ loader/unpack.h
@@ -0,0 +1,31 @@
+/*
+ * unpack.h - libarchive helper functions
+ *
+ * Copyright (C) 2010  Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author(s): David Cantrell <dcantrell@redhat.com>
+ */
+
+#ifndef UNPACK_H
+#define UNPACK_H
+
+#include <archive.h>
+
+int unpack_init(struct archive **);
+int unpack_members_and_finish(struct archive *, char *);
+int unpack_archive_file(char *, char *);
+
+#endif
