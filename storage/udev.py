66b307504448e168d0fd6b11a6d6b9ac8524b71e
David Lehman <dlehman@redhat.com>
2010-11-16 13:17:55 -0500

    Generalize some of the device-mapper partition handling.
    Related: rhbz#709653

--- storage/udev.py
+++ storage/udev.py
@@ -437,7 +437,7 @@ def udev_device_is_dm_raid(info):
     return udev_device_dm_subsystem_match(info, "dmraid")
 
 def udev_device_is_dm_mpath(info):
-    """ Return True if the device is an. """
+    """ Return True if the device is a multipath device. """
     return udev_device_dm_subsystem_match(info, "mpath")
 
 def udev_device_is_biosraid(info):
@@ -467,6 +467,13 @@ def udev_device_get_dm_partition_disk(info):
         return None
 
     return info["DM_NAME"][:p_index]
+
+def udev_device_is_dm_partition(info):
+    if not udev_device_is_dm(info):
+        return False
+
+    diskname = udev_device_get_dm_partition_disk(info)
+    return diskname not in ("", None)
 
 def udev_device_is_dmraid_partition(info):
     if not udev_device_is_dm_raid(info):
