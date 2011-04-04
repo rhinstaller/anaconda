8016c41cb3ce16527b440900b2e9abde1097c678
Ales Kozumplik <akozumpl@redhat.com>
2011-04-04 16:16:35 +0200

    Fix syntax error from commit 9e696b62.

--- loader/nfsinstall.c
+++ loader/nfsinstall.c
@@ -243,7 +243,7 @@ int promptForNfs(struct loaderData_s *loaderData) {
         }
 
         if (mountOpts) {
-            checked_asprintf(&loaderData->instRepo, "nfs:%s:%s:%s", mountOpts, host
+            checked_asprintf(&loaderData->instRepo, "nfs:%s:%s:%s", mountOpts, host,
                              directory);
         } else {
             checked_asprintf(&loaderData->instRepo, "nfs:%s:%s", host, directory);
