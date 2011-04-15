dcdd025e9d727776791c8836f085d7787f3ac49b
Radek Vykydal <rvykydal@redhat.com>
2011-04-15 14:31:52 +0200

    Revert "Fix traceback of network --device=<MAC> (#694768)"
    This reverts commit 5ee8d1e3000d903bcc1687e0181586e9cbe15050.
    
    Resolves: rhbz#696127

--- kickstart.py
+++ kickstart.py
@@ -587,8 +587,8 @@ class NetworkData(commands.network.RHEL6_NetworkData):
             dev = devices[device]
         else:
             for devname in devices:
-                if isys.getMacAddress(devname) == device.upper():
-                    dev = devices[devname]
+                if isys.getMacAddress(devname).lower() == device.lower():
+                    dev = val
                     break
 
         if self.hostname != "":
