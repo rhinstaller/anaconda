70ba3e1ca554604e16f44c58660d705161250bcc
David Lehman <dlehman@redhat.com>
2011-05-06 09:10:30 -0500

    Add btrfs min size of 256 MB. (#702603)

--- pyanaconda/storage/formats/fs.py
+++ pyanaconda/storage/formats/fs.py
@@ -1115,6 +1115,7 @@ class BTRFS(FS):
     _dump = True
     _check = True
     _packages = ["btrfs-progs"]
+    _minSize = 256
     _maxSize = 16 * 1024 * 1024
     # FIXME parted needs to be thaught about btrfs so that we can set the
     # partition table type correctly for btrfs partitions
