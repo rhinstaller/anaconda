b33da47289bf13bd0bf38af49950fb46a1ea61dc
Chris Lumens <clumens@redhat.com>
2011-08-03 15:20:35 -0400

    Handle rpmdb open errors by throwing out the root candidate (#723167).
    This will mostly occur because we're attempting to check a non-RPM system
    to see if it can be upgraded or not.  It can't.
    (cherry picked from commit 336e9f2a7be3c9df3e76333f112efe040d979143)

--- pyanaconda/storage/__init__.py
+++ pyanaconda/storage/__init__.py
@@ -1446,7 +1446,14 @@ def getReleaseString(mountpoint):
 
     # We get the arch from the initscripts package, but the version and name
     # must come from reading the release file.
-    mi = ts.dbMatch('provides', 'initscripts')
+    try:
+        mi = ts.dbMatch('provides', 'initscripts')
+    except Exception:
+        # This could happen in a variety of cases, but the biggest one is we're
+        # examining an installed system that doesn't use RPM.  Raise an
+        # exception for the caller to handle.
+        raise ValueError
+
     for h in mi:
         relArch = h['arch']
         break
@@ -1505,7 +1512,12 @@ def findExistingRootDevices(anaconda, upgradeany=False):
             continue
 
         if os.access(anaconda.rootPath + "/etc/fstab", os.R_OK):
-            (arch, product, version) = getReleaseString(anaconda.rootPath)
+            try:
+                (arch, product, version) = getReleaseString(anaconda.rootPath)
+            except ValueError:
+                # This likely isn't our product, so don't even count it as
+                # notUpgradable.
+                continue
 
             if upgradeany or \
                anaconda.instClass.productUpgradable(arch, product, version):
