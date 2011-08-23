a1d6e3c3f3a49814b80e85b8026bc9a50d7cb022
Ales Kozumplik <akozumpl@redhat.com>
2011-08-23 15:20:39 +0200

    ut: cleanup after firewall_test.py

--- tests/pyanaconda_test/firewall_test.py
+++ tests/pyanaconda_test/firewall_test.py
@@ -1,22 +1,25 @@
 import mock
 
-class ImportTest(mock.TestCase):
+class FirewallTest(mock.TestCase):
     def setUp(self):
         self.setupModules(["_isys", "block", "ConfigParser"])
-        
+
         self.fs = mock.DiskIO()
-        
+
         import pyanaconda.firewall
 
         self.modifiedModule("pyanaconda.firewall")
         self.modifiedModule("os.path")
-        
+
         pyanaconda.firewall.open = self.fs.open
         pyanaconda.firewall.log = mock.Mock()
         pyanaconda.firewall.iutil = mock.Mock()
         pyanaconda.firewall.os = mock.Mock()
         pyanaconda.firewall.os.path = mock.Mock()
         pyanaconda.firewall.os.path.exists = self.fs.os_path_exists
+
+    def tearDown(self):
+        self.tearDownModules()
 
     def default_write_test(self):
         import pyanaconda.firewall
@@ -39,11 +42,10 @@ class ImportTest(mock.TestCase):
 
     def default_ks_test(self):
         """Simulate writing default fw to kickstart"""
-        
+
         import pyanaconda.firewall
         fw = pyanaconda.firewall.Firewall()
         f = self.fs.open("/tmp/fw.txt", "w")
         fw.writeKS(f)
         f.close()
         self.assertEquals(self.fs["/tmp/fw.txt"], "firewall --service=ssh\n")
-    
