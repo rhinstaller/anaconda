281f19135748181065168868526212dded1ab81b
Chris Lumens <clumens@redhat.com>
2011-11-17 14:09:09 -0500

    Remove the AnacondaGUI class, moving its contents to GraphicalUserInterface.
    I don't know what I was thinking when I divided things up this way, but
    AnacondaGUI was almost completely redundant and there's no need for yet more
    layers here.  Everything AnacondaGUI did has been moved into the
    GraphicalUserInterface class.

--- pyanaconda/ui/gui/__init__.py
+++ pyanaconda/ui/gui/__init__.py
@@ -22,6 +22,9 @@ import importlib, inspect, os
 
 from pyanaconda.ui import UserInterface
 
+import gettext
+_ = lambda x: gettext.ldgettext("anaconda", x)
+
 class GraphicalUserInterface(UserInterface):
     """This is the standard GTK+ interface we try to steer everything to using.
        It is suitable for use both directly and via VNC.
@@ -39,15 +42,88 @@ class GraphicalUserInterface(UserInterface):
 
     def setup(self, data):
         from hubs.summary import SummaryHub
-        from gui import AnacondaGUI
+        from spokes import StandaloneSpoke
+
+        from pyanaconda.product import isFinal, productName, productVersion
 
         self._hubs.append(SummaryHub)
 
-        self._ui = AnacondaGUI(data, self._hubs)
-        self._ui.setup()
+        # First, grab a list of all the standalone spokes.
+        standalones = collect("spokes", lambda obj: issubclass(obj, StandaloneSpoke) and \
+                                                    getattr(obj, "preForHub", False) or getattr(obj, "postForHub", False))
+
+        actionClasses = []
+        for hub in self._hubs:
+            actionClasses.extend(sorted(filter(lambda obj: getattr(obj, "preForHub", None) == hub, standalones),
+                                        key=lambda obj: obj.priority))
+            actionClasses.append(hub)
+            actionClasses.extend(sorted(filter(lambda obj: getattr(obj, "postForHub", None) == hub, standalones),
+                                        key=lambda obj: obj.priority))
+
+        # Instantiate all hubs and their pre/post standalone spokes, passing
+        # the arguments defining our spoke API and setting up continue/quit
+        # signal handlers.
+        self._actions = []
+        for klass in actionClasses:
+            obj = klass(data)
+            obj.populate()
+
+            if not obj.showable:
+                continue
+
+            obj.register_event_cb("continue", self._on_continue_clicked)
+            obj.register_event_cb("quit", self._on_quit_clicked)
+
+            self._actions.append(obj)
+
+        # If we set these values on the very first window shown, they will get
+        # propagated to later ones.
+        self._actions[0].setup()
+
+        self._actions[0].window.set_beta(not isFinal)
+        self._actions[0].window.set_property("distribution", _("%s %s INSTALLATION") % (productName, productVersion))
 
     def run(self):
-        self._ui.run()
+        from gi.repository import Gtk
+
+        self._actions[0].window.show_all()
+        Gtk.main()
+
+    def _on_continue_clicked(self):
+        # If we're on the last screen, clicking Continue is the same as clicking Quit.
+        if len(self._actions) == 1:
+            self._on_quit_clicked()
+            return
+
+        # If the current action wants us to jump to an arbitrary point ahead,
+        # look for where that is now.
+        if self._actions[0].skipTo:
+            found = False
+            for ndx in range(1, len(self._actions)):
+                if self._actions[ndx].__class__.__name__ == self._actions[0].skipTo:
+                    found = True
+                    break
+
+            # If we found the point in question, compose a new actions list
+            # consisting of the current action, the one to jump to, and all
+            # the ones after.  That means the rest of the code below doesn't
+            # have to change.
+            if found:
+                self._actions = [self._actions[0]] + self._actions[ndx:]
+
+        self._actions[1].setup()
+        self._actions[1].window.set_beta(self._actions[0].window.get_beta())
+        self._actions[1].window.set_property("distribution", self._actions[0].window.get_property("distribution"))
+
+        # Do this last.  Setting up curAction could take a while, and we want
+        # to leave something on the screen while we work.
+        self._actions[1].window.show_all()
+        self._actions[0].window.hide()
+        self._actions.pop(0)
+
+    def _on_quit_clicked(self):
+        from gi.repository import Gtk
+        Gtk.main_quit()
 
 class UIObject(object):
     """This is the base class from which all other UI classes are derived.  It
