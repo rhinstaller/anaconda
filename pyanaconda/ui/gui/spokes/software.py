c8342ae7b49f3b92ebdcc056227c7e2dd0174fc9
Chris Lumens <chris@bangmoney.org>
2011-12-09 14:02:26 -0500

    Add the beginnings of the software selection spoke.
    For now, I've just dumped hardcoded desktop and add-on choices into the UI,
    similar to how the welcome spoke works with language selection.  You also
    can't add any addons with the button.  However, the rest of the UI should
    be functional if boring.

---
+++ pyanaconda/ui/gui/spokes/software.py
@@ -0,0 +1,95 @@
+# Software selection spoke classes
+#
+# Copyright (C) 2011  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+# Red Hat Author(s): Chris Lumens <clumens@redhat.com>
+#
+
+from gi.repository import Gtk
+
+from pyanaconda.ui.gui.spokes import NormalSpoke
+from pyanaconda.ui.gui.categories.software import SoftwareCategory
+
+__all__ = ["SoftwareSelectionSpoke"]
+
+class SoftwareSelectionSpoke(NormalSpoke):
+    builderObjects = ["addonStore", "desktopStore", "softwareWindow"]
+    mainWidgetName = "softwareWindow"
+    uiFile = "spokes/software.ui"
+
+    category = SoftwareCategory
+
+    icon = "package-x-generic-symbolic"
+    title = "SOFTWARE SELECTION"
+
+    def apply(self):
+        pass
+
+    @property
+    def completed(self):
+        return self._get_selected_desktop() is not None
+
+    @property
+    def status(self):
+        row = self._get_selected_desktop()
+        if not row:
+            return "Nothing selected"
+
+        return row[2]
+
+    def populate(self):
+        NormalSpoke.populate(self)
+
+        self._desktopStore = self.builder.get_object("desktopStore")
+        self._addSelection(self._desktopStore, "Desktop", "The default Fedora desktop.")
+        self._addSelection(self._desktopStore, "KDE Plasma Desktop", "A complete, modern desktop built using KDE Plasma.")
+        self._addSelection(self._desktopStore, "LXDE Desktop", "A light, fast, less resource-hungry desktop.")
+        self._addSelection(self._desktopStore, "XFCE Desktop", "A complete, well-integrated desktop.")
+
+        self._addonStore = self.builder.get_object("addonStore")
+        self._addSelection(self._addonStore, "Security", "Security analysis tools.")
+        self._addSelection(self._addonStore, "Games", "A perfect showcase of the best games available in Fedora.")
+        self._addSelection(self._addonStore, "Electronic Lab", "Fedora's high-end hardware design and simulation platform.")
+        self._addSelection(self._addonStore, "Design Suite", "Open creativity.")
+
+    def setup(self):
+        NormalSpoke.setup(self)
+
+    def _addSelection(self, store, name, description):
+        store.append([False, "<b>%s</b>\n%s" % (name, description), name])
+
+    # Returns the row in the store corresponding to what's selected on the
+    # left hand panel, or None if nothing's selected.
+    def _get_selected_desktop(self):
+        desktopView = self.builder.get_object("desktopView")
+        (store, itr) = desktopView.get_selection().get_selected()
+        if not itr:
+            return None
+
+        return self._desktopStore[itr]
+
+    # Signal handlers
+    def on_row_toggled(self, renderer, path):
+        self._addonStore[path][0] = not self._addonStore[path][0]
+
+    def on_custom_clicked(self, button):
+        # FIXME: does nothing for now
+        pass
+
+    def on_back_clicked(self, window):
+        self.window.hide()
+        Gtk.main_quit()
