5d79c05fb891ec9c78f4504ed126defbc1a60595
Chris Lumens <clumens@redhat.com>
2012-01-15 13:40:01 -0500

    Skip loader's language and keyboard screens in the new UI.
    This is a lame minimal effort patch, because loader's just going to
    go away pretty soon anyway.

--- loader/loader.c
+++ loader/loader.c
@@ -1334,16 +1334,8 @@ static void doLoaderMain(struct loaderData_s *loaderData,
     i = 0;
     step = STEP_LANG;
 
-    /* Before anything, check if we're installing from CD/DVD.  If so, we can
-     * skip right over most of these steps.  Note that askmethod or repo= will
-     * override this check.
-     */
-    if (!FL_ASKMETHOD(flags) && !loaderData->instRepo && findInstallCD(loaderData) == LOADER_OK) {
-        logMessage(DEBUGLVL, "Found installation media, so skipping lang and kbd");
-        queryCDMediaCheck(loaderData->instRepo);
-        skipLangKbd = 1;
-        flags |= LOADER_FLAGS_NOPASS;
-    }
+    skipLangKbd = 1;
+    flags |= LOADER_FLAGS_NOPASS;
 
     while (step != STEP_DONE) {
         logMessage(DEBUGLVL, "in doLoaderMain, step = %s", step_names[step]);
