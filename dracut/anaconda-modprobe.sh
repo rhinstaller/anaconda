18cf04a0faad854149abe1555e23fb03f710ba1f
Brian C. Lane <bcl@redhat.com>
2012-03-26 15:50:59 -0700

    load modules needed by Anaconda (#804522)
    Anaconda expects a minimum set of modules to be loaded, things
    like raid1, etc.

---
+++ dracut/anaconda-modprobe.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# load modules needed by anaconda
+ARCH=$(uname -m)
+KERNEL=$(uname -r)
+
+MODULE_LIST="cramfs squashfs iscsi_tcp"
+
+SCSI_MODULES=/lib/modules/$KERNEL/kernel/drivers/scsi/device_handler/
+for m in $SCSI_MODULES/*.ko; do
+    MODULE_LIST+="`basename $m .ko` "
+done
+
+if [ "$ARCH" != "s390" -a "$ARCH" != "s390x" ]; then
+    MODULE_LIST+="floppy edd iscsi_ibft"
+fi
+
+if [ "$ARCH" = "ppc" ]; then
+    MODULE_LIST+="spufs"
+fi
+
+MODULE_LIST+="raid0 raid1 raid5 raid6 raid456 raid10 linear dm-mod dm-zero \
+              dm-mirror dm-snapshot dm-multipath dm-round-robin dm-crypt cbc sha256 lrw xts"
+
+for m in $MODULE_LIST; do
+    modprobe $m
+done
+
