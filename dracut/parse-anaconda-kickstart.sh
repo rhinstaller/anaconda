638ae3d070c1c200be863d6c5d61322e28f33d28
Will Woods <wwoods@redhat.com>
2012-03-27 13:47:29 -0400

    make ks=file:... parse kickstart earlier (#806931)
    ks=file:... was parsing the kickstart *after* the 'cmdline' hook
    finished, so the new arguments weren't being used.
    
    Move the parsing into the 'cmdline' hook so the new arguments can get
    used by the rest of dracut.

--- dracut/parse-anaconda-kickstart.sh
+++ dracut/parse-anaconda-kickstart.sh
@@ -2,7 +2,7 @@
 # parse-anaconda-kickstart.sh: handle kickstart settings
 
 # no need to do this twice
-[ -f /tmp/ks.cfg ] && return
+[ -f /tmp/ks.cfg.done ] && return
 
 # inst.ks: provide a "URI" for the kickstart file
 kickstart="$(getarg ks= inst.ks=)"
@@ -12,8 +12,23 @@ fi
 # no root? the kickstart will probably tell us what our root device is.
 [ "$kickstart" ] && [ -z "$root" ] && root="anaconda-kickstart"
 
-case "$kickstart" in
-    http:*|https:*|ftp:*|nfs:*|nfs4:*) set_neednet ;;
+case "${kickstart%%:*}" in
+    http|https|ftp|nfs|nfs4) # network kickstart? set "neednet"!
+        set_neednet
+    ;;
+    file|path) # "file:<path>" - "path:<path>" is accepted but deprecated
+        splitsep ":" "$kickstart" kstype kspath
+        if [ -f "$kspath" ]; then
+            info "anaconda: parsing kickstart $kspath"
+            cp $kspath /tmp/ks.cfg
+            parse_kickstart /tmp/ks.cfg
+            [ "$root" = "anaconda-kickstart" ] && root=""
+            > /tmp/ks.cfg.done
+        else
+            warn "inst.ks='$kickstart'"
+            warn "can't find $kspath!"
+        fi
+    ;;
 esac
 export kickstart
 
