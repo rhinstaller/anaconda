ec828ce94065b7c3d4c1ab1c78dd9e203b3abb45
David Lehman <dlehman@redhat.com>
2012-05-01 09:19:45 -0500

    Show lvm vg free space as a whole number in the partition gui.
    Related: rhbz#734128

--- iw/partition_gui.py
+++ iw/partition_gui.py
@@ -998,7 +998,7 @@ class PartitionWindow(InstallWindow):
                 if vg.freeSpace > 0:
                     iter = self.tree.append(vgparent)
                     self.tree[iter]['Device'] = _("Free")
-                    self.tree[iter]['Size (MB)'] = vg.freeSpace
+                    self.tree[iter]['Size (MB)'] = "%.0f" % vg.freeSpace
                     self.tree[iter]['PyObject'] = None
                     self.tree[iter]['Mount Point'] = ""
                     self.tree[iter]['IsLeaf'] = True
