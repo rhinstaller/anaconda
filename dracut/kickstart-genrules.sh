665f01f85ce85653b5d37af19ad87e7f0064bd7d
Will Woods <wwoods@redhat.com>
2012-06-08 19:22:22 -0400

    fix ks=cdrom (#828589)
    We weren't handling the "empty $ksdev" case, which (as it turns out) is
    legit for 'ks=cdrom'.
    
    Note that the old option was "ks=cdrom[:<path>]". Rather than trying
    to cleverly detect the difference between a device and path, we're
    making it "cdrom:<dev>:<path>", which matches the "hd:.." argument.
    
    Thus if you want to specify a path for the kickstart but not the CDROM
    device, do: ks=cdrom::<path>
    
    <path> defaults to '/ks.cfg', as always.

--- dracut/kickstart-genrules.sh
+++ dracut/kickstart-genrules.sh
@@ -6,16 +6,17 @@ case "${kickstart%%:*}" in
         # handled by fetch-kickstart-net in the online hook
         wait_for_kickstart
     ;;
-    cdrom|hd|bd) # cdrom:<dev>, hd:<dev>:<path>, bd:<dev>:<path>
+    cdrom|hd|bd) # cdrom, cdrom:<dev>:<path>, hd:<dev>:<path>, bd:<dev>:<path>
         splitsep ":" "$kickstart" kstype ksdev kspath
+        [ "$kstype" = "cdrom" ] && [ -z "$ksdev" ] && ksdev="/dev/cdrom"
         ksdev=$(disk_to_dev_path $ksdev)
         if [ "$kstype" = "bd" ]; then # TODO FIXME: no biospart support yet
             warn "inst.ks='$kickstart'"
             warn "can't get kickstart: biospart isn't supported yet"
             ksdev=""
         else
-            when_diskdev_appears $ksdev \
-                fetch-kickstart-disk \$env{DEVNAME} $kspath
+            when_diskdev_appears "$ksdev" \
+                fetch-kickstart-disk \$env{DEVNAME} "$kspath"
             wait_for_kickstart
         fi
     ;;
