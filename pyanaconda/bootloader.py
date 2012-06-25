733830486b139924164b807b722d7464c38ed88b
Chris Lumens <clumens@redhat.com>
2012-06-25 13:41:12 -0400

    Can't import * inside a function.

--- pyanaconda/bootloader.py
+++ pyanaconda/bootloader.py
@@ -2147,7 +2147,7 @@ def writeBootLoader(storage, payload):
         image. We only have to add images for the non-default kernels and
         adjust the default to reflect whatever the default variant is.
     """
-    from pyanaconda.errors import *
+    from pyanaconda.errors import BootLoaderError
 
     stage1_device = storage.bootloader.stage1_device
     log.info("bootloader stage1 target device is %s" % stage1_device.name)
