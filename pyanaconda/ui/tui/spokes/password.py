203566434fdecde7dff3811cf2c8eafd4ae43fa1
Martin Sivak <msivak@redhat.com>
2012-08-09 10:40:43 +0200

    Add translations to Password Spoke

--- pyanaconda/ui/tui/spokes/password.py
+++ pyanaconda/ui/tui/spokes/password.py
@@ -3,7 +3,7 @@ from pyanaconda.ui.tui.simpleline import TextWidget
 import getpass
 
 class PasswordSpoke(NormalTUISpoke):
-    title = "Set root password"
+    title = _("Set root password")
     category = "password"
 
     def __init__(self, app, ksdata, storage, payload, instclass):
@@ -17,24 +17,24 @@ class PasswordSpoke(NormalTUISpoke):
     @property
     def status(self):
         if self._password is None:
-            return "Password is not set."
+            return _("Password is not set.")
         else:
-            return "Password is set."
+            return _("Password is set.")
 
     def refresh(self, args = None):
         NormalTUISpoke.refresh(self, args)
 
-        self._window += [TextWidget("Please select new root password. You will have to type it twice."), ""]
+        self._window += [TextWidget(_("Please select new root password. You will have to type it twice.")), ""]
 
         return True
 
     def prompt(self, args = None):
         """Overriden prompt as password typing is special."""
-        p1 = getpass.getpass("Password: ")
-        p2 = getpass.getpass("Password (confirm): ")
+        p1 = getpass.getpass(_("Password: "))
+        p2 = getpass.getpass(_("Password (confirm): "))
 
         if p1 != p2:
-            print "Passwords do not match!"
+            print _("Passwords do not match!")
         else:
             self._password = p1
             self.apply()
