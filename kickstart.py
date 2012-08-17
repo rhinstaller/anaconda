67d49f13c8065a5e6bad50b155e1bf67a21e78e3
Vratislav Podzimek <vpodzime@redhat.com>
2012-08-17 14:21:59 +0200

    Try to use both udev_resolve_devspec(pv) and pv as device name
    udev_resolve_devspec(pv) returns None if pv is has no
    --onpart=/dev/disk/by... option. In such cases use pv name.
    
    Resolves: rhbz#809640

--- kickstart.py
+++ kickstart.py
@@ -1106,7 +1106,8 @@ class VolGroupData(commands.volgroup.F16_VolGroupData):
         for pv in self.physvols:
             # if pv is using --onpart, use original device
             pv = anaconda.id.ksdata.onPart.get(pv, pv)
-            dev = devicetree.getDeviceByName(udev_resolve_devspec(pv))
+            dev_name = udev_resolve_devspec(pv) or pv
+            dev = devicetree.getDeviceByName(dev_name)
             if dev and dev.format.type == "luks":
                 try:
                     dev = devicetree.getChildren(dev)[0]
