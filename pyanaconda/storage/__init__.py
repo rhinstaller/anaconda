1ec91e85763abfa2c772653b502e284042810397
David Lehman <dlehman@redhat.com>
2012-09-12 17:49:05 -0500

    Write storage configs after payload install for live installs. (#856836)
    For yum payloads we write storage config before installing the payload
    so %post scripts have access to the configuration files if needed. For
    live installs, we have to write the config after installing the payload
    since installing the payload would overwrite parts of the storage
    configuration (notably /etc/fstab).
    (cherry picked from commit d10d55e9b8cd9e39086387fee7b87981701ad33b)

--- pyanaconda/storage/__init__.py
+++ pyanaconda/storage/__init__.py
@@ -214,7 +214,6 @@ def turnOnFilesystems(storage):
         storage.mountFilesystems(raiseErrors=False,
                                  readOnly=False,
                                  skipRoot=False)
-        storage.write()
         writeEscrowPackets(storage)
     else:
         if upgrade_migrate:
