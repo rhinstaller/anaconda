31bf37afdfa86bad22a8fda910f9284c0361993a
Chris Lumens <clumens@redhat.com>
2012-10-01 13:33:28 -0400

    Use a better starting value for required space than 0.

--- pyanaconda/packaging/yumpayload.py
+++ pyanaconda/packaging/yumpayload.py
@@ -136,7 +136,10 @@ class YumPayload(PackagePayload):
 
         self.install_device = None
 
-        self._space_required = Size(bytes=0)
+        # This value comes from a default install of the x86_64 Fedora 18.  It
+        # is meant as a best first guess only.  Once package metadata is
+        # available we can use that as a better value.
+        self._space_required = Size(spec="2800 MB")
 
         self._groups = None
         self._packages = []
