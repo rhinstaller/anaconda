a16e05c516e14a29bde5df3a18fc1f2fde634e50
David Lehman <dlehman@redhat.com>
2012-10-04 16:40:42 -0500

    Handle all logical/extended partitions in unusedDevices.
    Don't just handle those that are part of a Root.

--- pyanaconda/storage/__init__.py
+++ pyanaconda/storage/__init__.py
@@ -490,13 +490,14 @@ class Storage(object):
 
                 used_devices.extend(device.ancestors)
 
-                if getattr(device, "isLogical", False):
-                    extended = device.disk.format.extendedPartition.path
-                    used_devices.append(self.devicetree.getDeviceByPath(extended))
-
         for new in [d for d in self.devicetree.leaves if not d.exists]:
             if new in self.swaps or getattr(new.format, "mountpoint", None):
                 used_devices.extend(new.ancestors)
+
+        for device in self.partitions:
+            if getattr(device, "isLogical", False):
+                extended = device.disk.format.extendedPartition.path
+                used_devices.append(self.devicetree.getDeviceByPath(extended))
 
         used = set(used_devices)
         _all = set(self.devices)
