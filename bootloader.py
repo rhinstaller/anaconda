faba4e0266a5cfa62b87e98bdbbc36bd45e3aa6b
Peter Jones <pjones@redhat.com>
2012-10-15 15:26:25 -0400

    Too much C code recently leads to extra semi-colons.

--- bootloader.py
+++ bootloader.py
@@ -227,9 +227,9 @@ def writeBootloader(anaconda):
     f.write("# DEFAULTKERNEL specifies the default kernel package type\n")
     f.write("DEFAULTKERNEL=%s\n" %(defkern,))
     if anaconda.id.bootloader.trusted_boot:
-        f.write("# HYPERVISOR specifies the default multiboot kernel\n");
-        f.write("HYPERVISOR=/boot/tboot.gz\n");
-        f.write("HYPERVISOR_ARGS=logging=vga,serial,memory\n");
+        f.write("# HYPERVISOR specifies the default multiboot kernel\n")
+        f.write("HYPERVISOR=/boot/tboot.gz\n")
+        f.write("HYPERVISOR_ARGS=logging=vga,serial,memory\n")
     f.close()
 
     dosync()
