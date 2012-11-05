rhinstaller/anaconda

commit 33673a5c0dc6f38c59264743ab2a765777d12f1f

Modify behavior when leaving the reclaim storage dialog (#864128, #867770, #868903).
I was calling back into the continue method directly and then attempting to
display a dialog from there, which looks like was causing problems with the
main loop.  Instead, just emit the proper signal and let dialogs get called
how they're supposed to.

This makes the following behavior change:  When you click "Reclaim space"
on the reclaim dialog, you will go back to the hub instead of back to one of
the three installation options dialogs.  If you didn't do enough, you will
have to go back into the storage spoke and take further action.

pyanaconda/ui/gui/spokes/storage.py
@@ -709,7 +709,7 @@ class StorageSpoke(NormalSpoke, StorageChecker):
                 # resizeDialog handles okay/cancel on its own, so we can throw
                 # out the return value.
                 self.run_lightbox_dialog(resizeDialog)
-                self.on_continue_clicked(button)
+                self.window.emit("button-clicked")
                 return
 
             self.on_back_clicked(self.window)

commit 7a8143b4eca5c286ae3d7abfbe9873767c765b95

Prompt for encryption passphrase in reclaim path. (#869391)
This got broken by commit 33673a5c0. Previously after reclaim we would
run through on_continue_clicked again and presumably go through the
"continue" path, which is where we'd prompt for the passphrase.

pyanaconda/ui/gui/spokes/storage.py
@@ -715,6 +715,14 @@ class StorageSpoke(NormalSpoke, StorageChecker):
             if dialog.custom:
                 self.skipTo = "CustomPartitioningSpoke"
             else:
+                if self.encrypted:
+                    dialog = PassphraseDialog(self.data)
+                    rc = self.run_lightbox_dialog(dialog)
+                    if rc == 0:
+                        return
+
+                    self.passphrase = dialog.passphrase
+
                 self.apply()
 
                 resizeDialog = ResizeDialog(self.data, self.storage, self.payload)