rhinstaller/anaconda

commit 70e339b86b0d4a000e0e62253703ac4ca8618b74

Do not list some layouts twice (#882526)
If some layout is related to two (or more) languages, we get it twice
while iterating over languages and their related layouts. Since we use
a mapping from a layout name (e.g. 'us') to decription ('English (English (US))')
the result is that some items are listed twice in the list of all available
layouts. This needs some further fixing, but I'm leaving this for the related
post-F18 UI changes (see #859465 for more information).

pyanaconda/keyboard.py
@@ -374,10 +374,14 @@ class XklWrapper(object):
             name = item_str(item.name)
             description = item_str(item.description)
 
-        if dest:
-            self.name_to_show_str[name] = "%s (%s)" % (dest.encode("utf-8"),
-                                                       description.encode("utf-8"))
-        self._variants_list.append(_Layout(name, description))
+        #if this layout has already been added for some other language,
+        #do not add it again (would result in duplicates in our lists)
+        if name not in self.name_to_show_str:
+            if dest:
+                self.name_to_show_str[name] = "%s (%s)" % (dest.encode("utf-8"),
+                                                    description.encode("utf-8"))
+
+            self._variants_list.append(_Layout(name, description))
 
     def _get_language_variants(self, c_reg, item, user_data=None):
         #helper "global" variable

commit a02d93edf30c56d381b3a5ebe5f545cfd066ebbc

Fix getting country layout variants
Commit 70e339b86b0d4a000e0e62253703ac4ca8618b74 introduced the bug
resulting in almost empty dictionary of country layout variants.
Iteration over countries uses the same _get_variant method as iteration
over languages, but comes afterwards. The check added in
70e339b86b0d4a000e0e62253703ac4ca8618b74 fails for almost every layout
that is processed in iteration over countries.

pyanaconda/keyboard.py
@@ -356,7 +356,7 @@ class XklWrapper(object):
         #'grp' means that we want layout (group) switching options
         self.configreg.foreach_option('grp', self._get_switch_option, None)
 
-    def _get_variant(self, c_reg, item, subitem, dest):
+    def _get_variant(self, c_reg, item, subitem, lang):
         if subitem:
             name = item_str(item.name) + " (" + item_str(subitem.name) + ")"
             description = item_str(subitem.description)
@@ -366,11 +366,13 @@ class XklWrapper(object):
 
         #if this layout has already been added for some other language,
         #do not add it again (would result in duplicates in our lists)
-        if name not in self.name_to_show_str:
-            if dest:
-                self.name_to_show_str[name] = "%s (%s)" % (dest.encode("utf-8"),
+        if lang and name not in self.name_to_show_str:
+            self.name_to_show_str[name] = "%s (%s)" % (lang.encode("utf-8"),
                                                     description.encode("utf-8"))
-
+            self._variants_list.append(_Layout(name, description))
+        elif lang is None:
+            #if lang is None, we are iterating over countries not languages
+            #and should just append the layout to the list
             self._variants_list.append(_Layout(name, description))
 
     def _get_language_variants(self, c_reg, item, user_data=None):