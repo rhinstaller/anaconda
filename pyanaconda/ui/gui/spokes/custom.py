f17bc055b5661230b995b8ea82e68af2de5e9a6e
Chris Lumens <clumens@redhat.com>
2013-01-07 11:27:16 -0500

    Do not lightbox the Add Mountpoint dialog (#875291).
    For some reason, lightboxing this dialog means if you go back into the custom
    partitioning screen, all other dialogs will be displayed hidden but still
    take focus.  I can't figure out what's going on, so just disabling this
    lightbox for now seems a reasonable workaround.

--- pyanaconda/ui/gui/spokes/custom.py
+++ pyanaconda/ui/gui/spokes/custom.py
@@ -1758,14 +1758,13 @@ class CustomPartitioningSpoke(NormalSpoke, StorageChecker):
 
         dialog = AddDialog(self.data,
                            mountpoints=self.__storage.mountpoints.keys())
-        with enlightbox(self.window, dialog.window):
-            dialog.refresh()
-            rc = dialog.run()
+        dialog.refresh()
+        rc = dialog.run()
 
-            if rc != 1:
-                # user cancel
-                dialog.window.destroy()
-                return
+        if rc != 1:
+            # user cancel
+            dialog.window.destroy()
+            return
 
         # create a device of the default type, using any disks, with an
         # appropriate fstype and mountpoint
