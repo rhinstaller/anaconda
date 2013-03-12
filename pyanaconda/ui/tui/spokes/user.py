19f1858d8b22ea381d5dba92428846c7cfafb9ef
Martin Sivak <msivak@redhat.com>
2013-03-12 14:39:01 +0100

    Add user creation spoke to TUI

---
+++ pyanaconda/ui/tui/spokes/user.py
@@ -0,0 +1,101 @@
+# Root password text spoke
+#
+# Copyright (C) 2013  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+# Red Hat Author(s): Martin Sivak <msivak@redhat.com>
+#
+
+from pyanaconda.ui.tui.spokes import EditTUISpoke
+from pyanaconda.ui.tui.spokes import EditTUISpokeEntry as Entry
+from pyanaconda.ui.common import FirstbootSpokeMixIn
+from pyanaconda.ui.tui.simpleline import TextWidget
+from pyanaconda.ui.tui import YesNoDialog
+from pyanaconda.users import guess_username
+
+import re
+import gettext
+
+_ = lambda x: gettext.ldgettext("anaconda", x)
+
+__all__ = ["UserSpoke"]
+
+class UserSpoke(FirstbootSpokeMixIn, EditTUISpoke):
+    title = _("Create user")
+    category = "password"
+
+    edit_fields = [
+        Entry("Create user", "_create", EditTUISpoke.CHECK, True),
+        Entry("Fullname", "gecos", re.compile("^[^:]*$"), lambda self,args: args._create),
+        Entry("Username", "name", re.compile("^[a-z0-9_]+$"), lambda self,args: args._create),
+        Entry("Use password", "_use_password", EditTUISpoke.CHECK, lambda self,args: args._create),
+        Entry("Password", "password", EditTUISpoke.PASSWORD, lambda self,args: args._use_password and args._create),
+        Entry("Administrator", "_admin", EditTUISpoke.CHECK, lambda self,args: args._create),
+        Entry("Groups", "_groups", re.compile("^([a-z0-9_]+)?(, ?([a-z0-9_]+))*$"), lambda self,args: args._create)
+        ]
+
+    def __init__(self, app, data, storage, payload, instclass):
+        FirstbootSpokeMixIn.__init__(self)
+        EditTUISpoke.__init__(self, app, data, storage, payload, instclass)
+
+        if self.data.user.userList:
+            self.args = self.data.user.userList[0]
+            self.args._create = True
+        else:
+            self.args = self.data.UserData()
+            self.args._create = False
+
+        self.args._use_password = self.args.isCrypted or self.args.password
+
+    def refresh(self, args = None):
+        self.args._admin = "wheel" in self.args.groups
+        self.args._groups = ", ".join(self.args.groups)
+        return EditTUISpoke.refresh(self, args)
+
+    @property
+    def completed(self):
+        return self.data.user.userList
+
+    @property
+    def mandatory(self):
+        return True
+
+    @property
+    def status(self):
+        if len(self.data.user.userList) == 0:
+            return _("No user will be created")
+        elif "wheel" in self.data.user.userList[0].groups:
+            return _("Administrator %s will be created") % self.data.user.userList[0].name
+        else:
+            return _("User %s will be created") % self.data.user.userList[0].name
+
+    def apply(self):
+        if self.args.gecos and not self.args.name:
+            self.args.name = guess_username(self.args.gecos)
+
+        self.args.groups = [g.strip() for g in self.args._groups.split(",") if g]
+
+        # Add or remove the user from wheel group
+        if self.args._admin and "wheel" not in self.args.groups:
+            self.args.groups.append("wheel")
+        elif not self.args._admin and "wheel" in self.args.groups:
+            self.args.groups.remove("wheel")
+
+        # Add or remove the user from userlist as needed
+        if self.args._create and (self.args not in self.data.user.userList):
+            self.data.user.userList.append(self.args)
+        elif (not self.args._create) and (self.args in self.data.user.userList):
+            self.data.user.userList.remove(self.args)
