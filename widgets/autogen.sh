5cd1181d516b08bf6eb17043bc51448642f9e1c9
David Shea <dshea@redhat.com>
2013-08-16 11:46:00 -0400

    Fix widgets autotools generation.
    Remove the old gettext.h symlink if present. Add --foreign to the
    automake options in configure.ac so that things work better if someone
    tries to run autoreconf.

--- widgets/autogen.sh
+++ widgets/autogen.sh
@@ -7,4 +7,7 @@ autoconf
 autoheader --force
 automake --foreign --add-missing --copy
 rm -rf autom4te.cache
+
+# Remove the old symlink if present
+if [ -h src/gettext.h ] ; then rm src/gettext.h ; fi
 cp -f /usr/share/gettext/gettext.h src
