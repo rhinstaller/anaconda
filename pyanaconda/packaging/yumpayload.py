5844860fd1ff6effd2e141612c7b6afbbe0a57a2
Brian C. Lane <bcl@redhat.com>
2013-09-05 11:36:56 -0700

    Only ignore missing packages entries (#983316)
    Only ignore missing packages listed in %packages, do not ignore missing
    dependencies -- the install will not work if we do. Also add some
    debugging output from buildTransaction.
    
    Resolves: rhbz#983316

--- pyanaconda/packaging/yumpayload.py
+++ pyanaconda/packaging/yumpayload.py
@@ -1230,6 +1230,7 @@ reposdir=%s
             # check dependencies
             log.info("checking dependencies")
             (code, msgs) = self._yum.buildTransaction(unfinished_transactions_check=False)
+            log.debug("buildTransaction = (%s, %s)" % (code, msgs))
             self._removeTxSaveFile()
             if code == 0:
                 # empty transaction?
@@ -1237,8 +1238,6 @@ reposdir=%s
             elif code == 2:
                 # success
                 log.debug("success")
-            elif self.data.packages.handleMissing == KS_MISSING_IGNORE:
-                log.debug("ignoring missing due to ks config")
             else:
                 for msg in msgs:
                     log.warning(msg)
