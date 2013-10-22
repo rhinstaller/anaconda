939e0310bfa5bd1e17f41419492187759e3d7ef4
Radek Vykydal <rvykydal@redhat.com>
2013-10-22 14:26:26 +0200

    network: add team support to kickstart (#1003591)
    Resolves: rhbz#1003591

--- pyanaconda/kickstart.py
+++ pyanaconda/kickstart.py
@@ -874,7 +874,7 @@ class Logging(commands.logging.FC6_Logging):
                 remote_server = "%s:%s" %(self.host, self.port)
             logger.updateRemote(remote_server)
 
-class Network(commands.network.F19_Network):
+class Network(commands.network.F20_Network):
     def execute(self, storage, ksdata, instClass):
         network.write_network_config(storage, ksdata, instClass, ROOT_PATH)
 
