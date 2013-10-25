34029d143b2e3d9c500c8f4963a5f067812b35a0
David Lehman <dlehman@redhat.com>
2013-10-25 17:15:13 -0500

    Make sure to actually set the autopart flag when needed. (#1023584)
    (cherry picked from commit 568e72a8176720fdac37282ac154648ce969e3ef)

--- pyanaconda/ui/gui/spokes/storage.py
+++ pyanaconda/ui/gui/spokes/storage.py
@@ -865,6 +865,10 @@ class StorageSpoke(NormalSpoke, StorageChecker):
                     # User pressed cancel on the reclaim dialog, so don't leave
                     # the storage spoke.
                     return
+
+                # if the user did not press cancel that means they want to
+                # proceed with autopart
+                self.autopart = True
             elif dialog.continue_response == dialog.RESPONSE_CONTINUE_CUSTOM:
                 self.autopart = False
                 self.skipTo = "CustomPartitioningSpoke"
@@ -886,6 +890,10 @@ class StorageSpoke(NormalSpoke, StorageChecker):
                 # User pressed cancel on the reclaim dialog, so don't leave
                 # the storage spoke.
                 return
+
+            # if the user did not press cancel that means they want to
+            # proceed with autopart
+            self.autopart = True
         elif rc == dialog.RESPONSE_QUIT:
             raise SystemExit("user-selected exit")
         elif rc == dialog.RESPONSE_CUSTOM:
