70483a8dfa1dbf898643c937d26bb038f485c370
Will Woods <wwoods@redhat.com>
2013-10-25 16:17:57 -0400

    Only eject CDROM devices we're actually using (#966495)
    Rather than ejecting *every optical drive* on the system, we should only
    eject those devices that are actually being used by the installer.
    
    So, check each cdrom device to see if it's mounted somewhere, and eject
    it if so.
    
    Resolves: rhbz#966495

--- pyanaconda/iutil.py
+++ pyanaconda/iutil.py
@@ -757,3 +757,10 @@ class DataHolder(dict):
 
     def copy(self):
         return DataHolder(**dict.copy(self))
+
+def get_mount_paths(devnode):
+    '''given a device node, return a list of all active mountpoints.'''
+    devno = os.stat(devnode).st_rdev
+    majmin = "%d:%d" % (os.major(devno),os.minor(devno))
+    mountinfo = (line.split() for line in open("/proc/self/mountinfo"))
+    return [info[4] for info in mountinfo if info[2] == majmin]
