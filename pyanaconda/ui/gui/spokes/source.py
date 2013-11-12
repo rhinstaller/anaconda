17dd3c88ad8ba8e8fdfd90ee6e9df12571b1bbbb
Brian C. Lane <bcl@redhat.com>
2013-11-11 17:12:06 -0800

    use deepcopy on ksdata method (#1024509)
    Once the method was changed it was blowing up while trying to clean up
    the iso usage. This also resets the iso state and button when switching
    to another method.
    
    Resolves: rhbz#1024509

--- pyanaconda/ui/gui/spokes/source.py
+++ pyanaconda/ui/gui/spokes/source.py
@@ -312,7 +312,7 @@ class SourceSpoke(NormalSpoke):
         """
         import copy
 
-        old_source = copy.copy(self.data.method)
+        old_source = copy.deepcopy(self.data.method)
 
         if self._autodetectButton.get_active():
             if not self._cdrom:
@@ -415,6 +415,10 @@ class SourceSpoke(NormalSpoke):
         # If the user moved from an HDISO method to some other, we need to
         # clear the protected bit on that device.
         if old_source.method == "harddrive" and old_source.partition:
+            self._currentIsoFile = None
+            self._isoChooserButton.set_label(self._origIsoChooserButton)
+            self._isoChooserButton.set_use_underline(True)
+
             if old_source.partition in self.storage.config.protectedDevSpecs:
                 self.storage.config.protectedDevSpecs.remove(old_source.partition)
 
@@ -537,6 +541,7 @@ class SourceSpoke(NormalSpoke):
         self._urlEntry = self.builder.get_object("urlEntry")
         self._protocolComboBox = self.builder.get_object("protocolComboBox")
         self._isoChooserButton = self.builder.get_object("isoChooserButton")
+        self._origIsoChooserButton = self._isoChooserButton.get_label()
 
         self._mirrorlistCheckbox = self.builder.get_object("mirrorlistCheckbox")
 
