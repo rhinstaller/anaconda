d087dcd12bad1c3912db959e9d7559634b50a815
David Lehman <dlehman@redhat.com>
2013-12-03 10:43:47 -0600

    Disallow /boot on btrfs subvolume until grubby supports it. (#1052446)
    (cherry picked from commit 91dbd1befc5713f6810c1edb1d5c4c1645d4b505)

--- pyanaconda/bootloader.py
+++ pyanaconda/bootloader.py
@@ -1374,10 +1374,9 @@ class GRUB2(GRUB):
     terminal_type = "console"
 
     # requirements for boot devices
-    stage2_device_types = ["partition", "mdarray", "btrfs volume",
-                           "btrfs subvolume"]
-    stage2_raid_levels = [mdraid.RAID0, mdraid.RAID1, mdraid.RAID4,
-                          mdraid.RAID5, mdraid.RAID6, mdraid.RAID10]
+    stage2_device_types = ["partition", "mdarray"]
+    stage2_raid_levels = [raid.RAID0, raid.RAID1, raid.RAID4,
+                          raid.RAID5, raid.RAID6, raid.RAID10]
     stage2_raid_metadata = ["0", "0.90", "1.0", "1.2"]
 
     @property
