2a8208c97e35f8287912a86bc593fe27b36a5e92
Samantha N. Bueno <sbueno+anaconda@redhat.com>
2013-12-16 09:45:03 -0500

    Fix up username checking regex a bit.
    Users shouldn't be able to create usernames with capital letters.

--- tests/regex_tests/username_test.py
+++ tests/regex_tests/username_test.py
@@ -65,7 +65,6 @@ class UsernameRegexTestCase(unittest.TestCase):
         """Test a list of possible username values."""
         good_tests = [
                 'gburdell',
-                'GBurdell',
                 'gburdell$',
                 'g_burdell',
                 '_burdell',
@@ -90,6 +89,7 @@ class UsernameRegexTestCase(unittest.TestCase):
                 ' gburdell',
                 ':gburdell',
                 'root',
+                'GBurdell',
                 '$',
                 '-'
                 ]
