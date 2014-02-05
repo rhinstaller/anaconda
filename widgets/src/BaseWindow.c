c8ad51dcf5f0c5dc823f8166d3dcc132bced2ebf
Máirín Duffy <duffy@redhat.com>
2014-02-05 17:46:14 -0500

    Minor aesthetic cleanups (#1045250).
    Some minor aesthetic updates to help enhance branding:
    - Upped up the padding on the top navbar since it was a bit tight.
    - Scaled the sidebar down just slightly to increase real estate
      for content.

--- widgets/src/BaseWindow.c
+++ widgets/src/BaseWindow.c
@@ -229,9 +229,10 @@ static void anaconda_base_window_init(AnacondaBaseWindow *win) {
     win->priv->nav_area = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(win->priv->nav_area), FALSE);
     gtk_grid_set_column_homogeneous(GTK_GRID(win->priv->nav_area), FALSE);
-    gtk_widget_set_margin_start(win->priv->nav_area, 6);
-    gtk_widget_set_margin_end(win->priv->nav_area, 6);
-    gtk_widget_set_margin_top(win->priv->nav_area, 6);
+    gtk_widget_set_margin_start(win->priv->nav_area, 18);
+    gtk_widget_set_margin_end(win->priv->nav_area, 18);
+    gtk_widget_set_margin_top(win->priv->nav_area, 12);
+    gtk_widget_set_margin_bottom(win->priv->nav_area, 6);
 
     gtk_container_add(GTK_CONTAINER(win->priv->nav_box), win->priv->nav_area);
 
