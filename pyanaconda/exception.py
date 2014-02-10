a51ccac4e191c43eb92ed536fc516491b60676af
Samantha N. Bueno <sbueno+anaconda@redhat.com>
2014-02-10 13:18:13 -0500

    Once again fix cmdline error handling. (#1360223)
    Raise cmdline error if a user attempts to install a missing pkg
    during non-interactive ks install.
    
    This also makes a minimal change to the original CmdlineError str
    which was displayed.
    
    Backport of lost patch.
    
    Resolves: rhbz#1360223
    (based on commit e8afc79f034a5c73d828df8948f56837440aa758)

--- pyanaconda/exception.py
+++ pyanaconda/exception.py
@@ -140,17 +140,19 @@ class AnacondaExceptionHandler(ExceptionHandler):
                     log.debug("In the main thread, running exception handler")
                     if issubclass(ty, CmdlineError) or not self._interactive:
                         if issubclass(ty, CmdlineError):
-                            cmdline_error_msg = _("\nThe installation was stopped due to "
-                                                  "incomplete spokes detected while running "
-                                                  "in non-interactive cmdline mode. Since there "
-                                                  "cannot be any questions in cmdline mode, "
-                                                  "edit your kickstart file and retry "
-                                                  "installation.\nThe exact error message is: "
-                                                  "\n\n%s.\n\nThe installer will now terminate.") % str(value)
+                            cmdline_error_msg = _("\nThe installation was stopped due to an "
+                                                  "error which occurred while running in "
+                                                  "non-interactive cmdline mode. Since there "
+                                                  "cannot be any questions in cmdline mode, edit "
+                                                  "your kickstart file and retry installation. "
+                                                  "\nThe exact error message is: \n\n%s. \n\nThe "
+                                                  "installer will now terminate.") % str(value)
                         else:
-                            cmdline_error_msg = _("\nRunning in cmdline mode, no interactive debugging "
-                                                  "allowed.\nThe exact error message is: "
-                                                  "\n\n%s.\n\nThe installer will now terminate.") % str(value)
+                            cmdline_error_msg = _("\nRunning in cmdline mode, no interactive "
+                                                  "debugging allowed.\n"
+                                                  "The exact error message is: "
+                                                  "\n\n%s.\n\nThe installer will now terminate.") \
+                                                % str(value)
 
                         # since there is no UI in cmdline mode and it is completely
                         # non-interactive, we can't show a message window asking the user
