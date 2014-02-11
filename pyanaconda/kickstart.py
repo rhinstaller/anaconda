2ca2684d273930d3d347335e1e6c768a59a07684
Brian C. Lane <bcl@redhat.com>
2014-02-11 07:25:57 -0800

    kickstart user accounts should be locked by default (#1063554)
    Commit 18a8f661635884e7861bc14f200455e470bebb0b changed createUser so
    that an account could be created with an empty password (see bug
    --password or --lock argument being created with no password, which is
    not what was intended.
    
    The impact was limited to local accounts, ssh blocks access to accounts
    with empty passwords by default. But another local user could access one
    of these accounts by using su.
    
    This patch fixes user creation so that when accounts are created from a
    kickstart and their password argument is blank (the default from
    pykickstart is "") it will switch it to None so that createUser will
    lock the passwordless account.
    
    Resolves: rhbz#1063554

--- pyanaconda/kickstart.py
+++ pyanaconda/kickstart.py
@@ -1435,7 +1435,14 @@ class User(commands.user.F19_User):
         for usr in self.userList:
             kwargs = usr.__dict__
             kwargs.update({"algo": algo, "root": ROOT_PATH})
-            users.createUser(usr.name, **kwargs)
+
+            # If the user password came from a kickstart and it is blank we
+            # need to make sure the account is locked, not created with an
+            # empty password.
+            if ksdata.user.seen and kwargs.get("password", "") == "":
+                kwargs["password"] = None
+            if not users.createUser(usr.name, **kwargs):
+                log.error("User %s already exists, not creating." % usr.name)
 
 class VolGroup(commands.volgroup.F20_VolGroup):
     def execute(self, storage, ksdata, instClass):
