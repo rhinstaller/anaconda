7d5a1ec1d4b1aa06330e65e72955bcbceb33be3b
Vratislav Podzimek <vpodzime@redhat.com>
2014-02-17 09:01:36 +0100

    Firstboot is deprecated and gone on Fedora 20 and anything newer
    There only is the Initial Setup now.

--- pyanaconda/kickstart.py
+++ pyanaconda/kickstart.py
@@ -606,9 +606,8 @@ class Firstboot(commands.firstboot.FC3_Firstboot):
             f = open(ROOT_PATH + "/etc/reconfigSys", "w+")
             f.close()
 
-        iutil.execWithRedirect("systemctl", [action, "firstboot-graphical.service",
-                                                     "initial-setup-graphical.service",
-                                                     "initial-setup-text.service"],
+        iutil.execWithRedirect("systemctl", [action, "initial-setup-graphical.service",
+                                             "initial-setup-text.service"],
                                root=ROOT_PATH)
 
 class Group(commands.group.F12_Group):
