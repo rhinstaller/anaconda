4e65f3d3175d31b3fb3f20a8e93687f75dfed968
mulhern <amulhern@redhat.com>
2014-02-24 08:21:11 -0500

    Remove redundant _setCurrentFreeSpace() call (#1043763)
    Related: rhbz#1043763
    
    _setCurrentFreeSpace() is called in  _updateSpaceDisplay() which is invoked on
    the next line.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- pyanaconda/ui/gui/spokes/custom.py
+++ pyanaconda/ui/gui/spokes/custom.py
@@ -912,8 +912,6 @@ class CustomPartitioningSpoke(NormalSpoke, StorageChecker):
         self.passphrase = self.data.autopart.passphrase
         self._reset_storage()
         self._do_refresh()
-        # update our free space number based on Storage
-        self._setCurrentFreeSpace()
 
         self._updateSpaceDisplay()
         self._applyButton.set_sensitive(False)
