rhinstaller/anaconda

commit ea61ee97601b3aa3f49c2163d0b86c48ef6a3e31

Deal with a couple more "except Exception" lines.
Most I got rid of because they were unnecessary, but one I've left but disabled
the warning on because we really don't want anaconda-yum to crash ever.

pyanaconda/kickstart.py
@@ -23,6 +23,7 @@ from blivet.deviceaction import ActionCreateFormat, ActionDestroyFormat, ActionR
 from blivet.devices import LUKSDevice
 from blivet.devicelibs.lvm import getPossiblePhysicalExtents
 from blivet.devicelibs import swap
+from blivet.errors import SizeParamsError
 from blivet.formats import getFormat
 from blivet.partitioning import doPartitioning
 from blivet.partitioning import growLVM
@@ -702,7 +703,7 @@ class LogVolData(commands.logvol.F20_LogVolData):
 
         try:
             size = Size(spec="%d MiB" % self.size)
-        except Exception:
+        except (ValueError, SizeParamsError):
             raise KickstartValueError(formatErrorMsg(self.lineno, msg="The size %s is not valid." % self.size))
 
         if self.mountpoint == "swap":
@@ -844,7 +845,7 @@ class LogVolData(commands.logvol.F20_LogVolData):
             if self.maxSizeMB:
                 try:
                     maxsize = Size(spec="%d MiB" % self.maxSizeMB)
-                except Exception:
+                except (ValueError, SizeParamsError):
                     raise KickstartValueError(formatErrorMsg(self.lineno, msg="The maximum size %s is not valid." % self.maxSizeMB))
             else:
                 maxsize = None
@@ -934,7 +935,7 @@ class PartitionData(commands.partition.F18_PartData):
 
         try:
             size = Size(spec="%d MiB" % self.size)
-        except Exception:
+        except (ValueError, SizeParamsError):
             raise KickstartValueError(formatErrorMsg(self.lineno, msg="The size %s is not valid." % self.size))
 
         if self.onbiosdisk != "":
@@ -1091,7 +1092,7 @@ class PartitionData(commands.partition.F18_PartData):
         if self.maxSizeMB:
             try:
                 maxsize = Size(spec="%d MiB" % self.maxSizeMB)
-            except Exception:
+            except (ValueError, SizeParamsError):
                 raise KickstartValueError(formatErrorMsg(self.lineno, msg="The maximum size %s is not valid." % self.maxSizeMB))
         else:
             maxsize = None

scripts/anaconda-yum
@@ -156,6 +156,7 @@ def run_yum_transaction(release, arch, yum_conf, install_root, ts_file, script_l
             logfile.close()
     except YumBaseError as e:
         print("ERROR: transaction error: %s" % e)
+    # pylint: disable-msg=W0703
     except Exception as e:
         print("ERROR: unexpected error: %s" % e)
     finally:
@@ -254,7 +255,7 @@ class RPMCallback(object):
 
         try:
             repo = self.yb.repos.getRepo(txmbr.po.repoid)
-        except Exception as e:
+        except yum.Errors.RepoError as e:
             print("ERROR: getRepo failed: %s" % e)
             raise Exception("rpmcallback getRepo failed")
 

commit 0147694d2d6f8454523ec50f23de889b0f7e82d8

Don't traceback when no size is given in kickstart (#1067707).
There are various circumstances where a size may not be given - recommended swap
size, grow, and the like.  We need to ignore these situations and let the
methods we call deal with the default value.  This bug was caused by
ea61ee97601b3aa3f49c2163d0b86c48ef6a3e31.  I've added a test case for it.

pyanaconda/kickstart.py
@@ -710,6 +710,8 @@ class LogVolData(commands.logvol.F20_LogVolData):
             size = Size(spec="%d MiB" % self.size)
         except (ValueError, SizeParamsError):
             raise KickstartValueError(formatErrorMsg(self.lineno, msg="The size %s is not valid." % self.size))
+        except TypeError:
+            pass
 
         if self.mountpoint == "swap":
             ty = "swap"
@@ -852,6 +854,8 @@ class LogVolData(commands.logvol.F20_LogVolData):
                     maxsize = Size(spec="%d MiB" % self.maxSizeMB)
                 except (ValueError, SizeParamsError):
                     raise KickstartValueError(formatErrorMsg(self.lineno, msg="The maximum size %s is not valid." % self.maxSizeMB))
+                except TypeError:
+                    pass
             else:
                 maxsize = None
 
@@ -942,6 +946,8 @@ class PartitionData(commands.partition.F18_PartData):
             size = Size(spec="%d MiB" % self.size)
         except (ValueError, SizeParamsError):
             raise KickstartValueError(formatErrorMsg(self.lineno, msg="The size %s is not valid." % self.size))
+        except TypeError:
+            pass
 
         if self.onbiosdisk != "":
             for (disk, biosdisk) in storage.eddDict.iteritems():
@@ -1096,6 +1102,8 @@ class PartitionData(commands.partition.F18_PartData):
                 maxsize = Size(spec="%d MiB" % self.maxSizeMB)
             except (ValueError, SizeParamsError):
                 raise KickstartValueError(formatErrorMsg(self.lineno, msg="The maximum size %s is not valid." % self.maxSizeMB))
+            except TypeError:
+                pass
         else:
             maxsize = None
 

tests/storage/cases/__init__.py
@@ -119,7 +119,7 @@ class TestCaseComponent(object):
     def __init__(self):
         """Create a new TestCaseComponent instance.  This __init__ method should
            typically do very little.  However, subclasses must be sure to set
-           self.disksToCreate.  This attribute is a list of (disk name, size in MB)
+           self.disksToCreate.  This attribute is a list of (disk name, blivet.Size)
            tuples that will be used in this test.  Disks given in this list will
            be automatically created by setupDisks and destroyed by tearDownDisks.
         """

tests/storage/cases/bz1067707.py
@@ -1,0 +1,45 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Chris Lumens <clumens@redhat.com>
+
+from . import TestCase, TestCaseComponent
+from blivet.size import Size
+
+class SwapWithRecommendedSizeComponent(TestCaseComponent):
+    name = "SwapWithRecommendedSize"
+
+    def __init__(self, *args, **kwargs):
+        TestCaseComponent.__init__(self, *args, **kwargs)
+        self.disksToCreate = [("anatest-disk1", Size(spec="1GiB"))]
+
+    @property
+    def ks(self):
+        return """
+zerombr
+clearpart --all --initlabel
+part swap --recommended
+"""
+
+class BZ1067707_TestCase(TestCase):
+    components = [SwapWithRecommendedSizeComponent]
+    name = "1067707"
+    desc = """Partition lines do not necessarily require a size to be
+specified.  There are plenty of reasons why a user might not do this:  putting
+a filesystem on a given pre-existing partition, giving a resize or maxsize
+option, or (in this case) asking for the recommended swap size.
+"""

tests/storage/run_storage_tests.py
@@ -8,7 +8,10 @@ if os.geteuid() != 0:
     os._exit(77)
 
 from cases.bz1014545 import BZ1014545_TestCase
-tc = BZ1014545_TestCase()
-failures = tc.run()
+from cases.bz1067707 import BZ1067707_TestCase
+
+for tc in [BZ1014545_TestCase(),
+           BZ1067707_TestCase()]:
+    failures = tc.run()
 
 os._exit(failures)