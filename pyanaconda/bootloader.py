1cb6810001bffb15196c5f327dcde8629219caa7
Samantha N. Bueno <sbueno+anaconda@redhat.com>
2014-03-11 10:31:26 -0400

    Don't create bootloader entries for kdump initrd and kernel. (#1036086)
    As this is not necessary, don't try to do it.
    
    Resolves: rhbz#1036086

--- pyanaconda/bootloader.py
+++ pyanaconda/bootloader.py
@@ -2102,6 +2102,10 @@ class ZIPL(BootLoader):
 
     def write_config_images(self, config):
         for image in self.images:
+            if "kdump" in (image.initrd or image.kernel):
+                # no need to create bootloader entries for kdump
+                continue
+
             args = Arguments()
             if image.initrd:
                 initrd_line = "\tramdisk=%s/%s\n" % (self.boot_dir,
