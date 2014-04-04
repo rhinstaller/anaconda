820c4ad6d76a71ca580bb2a3b990d4c41b4bb826
Colin Walters <walters@verbum.org>
2014-04-04 23:49:51 +0000

    gui/spokes/software: Enable iff payload is PackagePayload
    This should be the equivalent of the livecd test.
    
    Resolves: rhbz#1113535
    Port of rpmostreepayload patches from master
    commit 63e5d9cb577428519d61a279c3b53442877f9c51

--- pyanaconda/ui/gui/spokes/software.py
+++ pyanaconda/ui/gui/spokes/software.py
@@ -24,7 +24,7 @@ from gi.repository import Gdk, Gtk
 
 from pyanaconda.flags import flags
 from pyanaconda.i18n import _, N_
-from pyanaconda.packaging import MetadataError
+from pyanaconda.packaging import MetadataError, PackagePayload
 from pyanaconda.threads import threadMgr, AnacondaThread
 from pyanaconda import constants
 
@@ -164,7 +164,7 @@ class SoftwareSelectionSpoke(NormalSpoke):
 
     @property
     def showable(self):
-        return not flags.livecdInstall and not self.data.method.method == "liveimg"
+        return isinstance(self.payload, PackagePayload)
 
     @property
     def status(self):
