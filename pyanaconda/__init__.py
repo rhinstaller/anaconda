2a99e198c40b22fd2f3bf3988902b8d02817ea82
Colin Walters <walters@verbum.org>
2014-04-30 19:26:37 -0400

    Add RPMOSTreePayload
    This is a new backend that uses ostree to install.  However, it's
    named RPMOSTree because a future iteration will use rpm-ostree which
    will support a hybrid of tree + RPM packages.
    
    Resolves: rhbz#1113535
    Port of rpmostreepayload patches from master
    commit 131cbcec9c113dd5f66d14d440297d50b2900e01

--- pyanaconda/__init__.py
+++ pyanaconda/__init__.py
@@ -124,7 +124,10 @@ class Anaconda(object):
             if not klass:
                 from flags import flags
 
-                if flags.livecdInstall:
+                if self.ksdata.ostreesetup.seen:
+                    from pyanaconda.packaging.rpmostreepayload import RPMOSTreePayload
+                    klass = RPMOSTreePayload
+                elif flags.livecdInstall:
                     from pyanaconda.packaging.livepayload import LiveImagePayload
                     klass = LiveImagePayload
                 elif self.ksdata.method.method == "liveimg":
