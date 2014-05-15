6187aa0d074816b055a0f13d1174662e8c601d76
mulhern <amulhern@redhat.com>
2014-05-15 16:25:29 -0400

    Make rescue_mode part of flags, hence more publicly available (#1090009)
    Related: rhbz#1090009
    
    In particular, blivet can make use of it when updating from anaconda
    flags.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- pyanaconda/anaconda.py
+++ pyanaconda/anaconda.py
@@ -63,7 +63,6 @@ class Anaconda(object):
         self.proxyUsername = None
         self.proxyPassword = None
         self.reIPLMessage = None
-        self.rescue = False
         self.rescue_mount = True
         self.rootParts = None
 
