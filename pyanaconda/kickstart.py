0bdee770a0afd101a3666143247ebd34aa6e7ba5
Brian C. Lane <bcl@redhat.com>
2014-06-27 17:23:17 -0700

    Add autopart --fstype support (#1112697)
    This allows the user to override the default filesystem when using
    autopart by specifying autopart --fstype=xfs for example.
    
    Resolves: rhbz#1112697

--- pyanaconda/kickstart.py
+++ pyanaconda/kickstart.py
@@ -236,13 +236,31 @@ class Authconfig(commands.authconfig.FC3_Authconfig):
         except OSError as msg:
             log.error("Error running /usr/sbin/authconfig %s: %s" %  (args, msg))
 
-class AutoPart(commands.autopart.F20_AutoPart):
+class AutoPart(commands.autopart.RHEL7_AutoPart):
+    def parse(self, args):
+        retval = commands.autopart.RHEL7_AutoPart.parse(self, args)
+
+        if self.fstype:
+            fmt = blivet.formats.getFormat(self.fstype)
+            if not fmt or fmt.type is None:
+                raise KickstartValueError(formatErrorMsg(self.lineno,
+                        msg=_("autopart fstype of %s is invalid.") % self.fstype))
+
+        return retval
+
     def execute(self, storage, ksdata, instClass):
         from blivet.partitioning import doAutoPartition
         from blivet.partitioning import sanityCheck
 
         if not self.autopart:
             return
+
+        if self.fstype:
+            try:
+                storage.setDefaultFSType(self.fstype)
+            except ValueError:
+                raise KickstartValueError(formatErrorMsg(self.lineno,
+                        msg=_("Settings default fstype to %s failed.") % self.fstype))
 
         # sets up default autopartitioning.  use clearpart separately
         # if you want it
