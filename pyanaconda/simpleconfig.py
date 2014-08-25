8a01dc259414e48dd737889c702a3ec9e4ad4765
David Shea <dshea@redhat.com>
2014-08-25 14:02:28 -0400

    Update our copy of the GPL
    The FSF moved like ten years ago. The new copy also messes with the
    whitespace and replaces "Library" with "Lesser" in reference to the
    LGPL. Replace the two boilerplate comments with the old address with
    comments copied from newer files.

--- pyanaconda/simpleconfig.py
+++ pyanaconda/simpleconfig.py
@@ -1,19 +1,26 @@
 #
 # simpleconifg.py - representation of a simple configuration file (sh-like)
 #
-# Matt Wilson <msw@redhat.com>
-# Jeremy Katz <katzj@redhat.com>
-# Will Woods <wwoods@redhat.com>
-# Brian C. Lane <bcl@redhat.com>
+# Copyright (C) 1999-2014 Red Hat, Inc.
 #
-# Copyright 1999-2012 Red Hat, Inc.
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
 #
-# This software may be freely redistributed under the terms of the GNU
-# library public license.
-#
-# You should have received a copy of the GNU Library Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# Author(s): Matt Wilson <msw@redhat.com>
+#            Jeremy Katz <katzj@redhat.com>
+#            Will Woods <wwoods@redhat.com>
+#            Brian C. Lane <bcl@redhat.com>
 #
 import os
 import shutil
