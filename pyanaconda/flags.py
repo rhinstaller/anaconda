48878fa1432350a88d639fcf4e0f149426e1ddb3
Gene Czarcinski <gczarcinski@ec.rr.com>
2014-09-17 15:19:13 -0400

    Add nombr to anaconda to suppress updating MBR (#886502)
    This patch adds the boot parameter "nombr" to suppress updating the MBR while
    still completely installing grub2 and creating the grub.cfg file.  The actual
    suppression adds "--grub-setup=/bin/true" to the execution of
    "/sbin/grub-install"
    
    Kickstart, regular, and live installs are supported.

--- pyanaconda/flags.py
+++ pyanaconda/flags.py
@@ -61,6 +61,7 @@ class Flags(object):
         self.askmethod = False
         self.eject = True
         self.extlinux = False
+        self.nombr = False
         self.gpt = False
         self.leavebootorder = False
         self.testing = False
@@ -79,7 +80,7 @@ class Flags(object):
 
     def read_cmdline(self):
         for f in ("selinux", "debug", "leavebootorder", "testing", "extlinux",
-                  "gpt", "dnf", "noefi"):
+                  "nombr", "gpt", "dnf", "noefi"):
             self.set_cmdline_bool(f)
 
         if not selinux.is_selinux_enabled():
