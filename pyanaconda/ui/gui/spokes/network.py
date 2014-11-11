rhinstaller/anaconda

commit 7a9a7de0d6da52d504c70b012cfbe86285d1de83

network: add support for vlan tag in iBFT (#804511)
Resolves: rhbz#804511

Implementing the feature, NM now creates read-only temporary in-memory
connection for iBFT configurations. Don't show the connection in installer GUI.

pyanaconda/ui/gui/spokes/network.py
@@ -426,6 +426,9 @@ class NetworkControlBox(GObject.GObject):
             log.debug("network: GUI, connection %s found", uuid)
             if self.dev_cfg(uuid=uuid):
                 continue
+            if setting["connection"]["read-only"]:
+                log.debug("network: GUI, not adding read-only connection %s", uuid)
+                continue
             dev_cfg = DeviceConfiguration(con_uuid=uuid)
             if dev_cfg.device_type in self.supported_device_types:
                 # Configs for ethernet has been already added,
@@ -670,6 +673,10 @@ class NetworkControlBox(GObject.GObject):
             return
 
         try:
+            read_only = nm.nm_device_setting_value(device.get_iface(), "connection", "read-only")
+            if read_only:
+                log.debug("network: not adding read-only connection for device %s", device.get_iface())
+                return
             con_uuid = nm.nm_device_setting_value(device.get_iface(), "connection", "uuid")
             dev_cfg = self.dev_cfg(uuid=con_uuid)
         except nm.UnknownDeviceError as e:

pyanaconda/nm.py
@@ -509,6 +509,10 @@ def _device_settings(name):
         settings = _find_settings(name, 'bond', 'interface-name')
     elif devtype == NetworkManager.DeviceType.VLAN:
         settings = _find_settings(name, 'vlan', 'interface-name')
+        if not settings:
+            # connections generated by NM from iBFT
+            parent, _sep, vlanid = name.partition(".")
+            settings = _find_settings(int(vlanid), 'vlan', 'id')
     else:
         settings = _find_settings(name, 'connection', 'interface-name')
         if not settings:

commit b020000fd496ed6aea23a1bc4ceee4444d4155e4

Don't traceback if connection does not have read-only setting (#1158919)
Resolves: rhbz#1158919

caused by commit 7a9a7de0d6da52d504c70b012cfbe86285d1de83

pyanaconda/ui/gui/spokes/network.py
@@ -430,7 +430,7 @@ class NetworkControlBox(GObject.GObject):
             log.debug("network: GUI, connection %s found", uuid)
             if self.dev_cfg(uuid=uuid):
                 continue
-            if setting["connection"]["read-only"]:
+            if setting["connection"].get("read-only", False):
                 log.debug("network: GUI, not adding read-only connection %s", uuid)
                 continue
             dev_cfg = DeviceConfiguration(con_uuid=uuid)