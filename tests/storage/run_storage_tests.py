cebd7d4a0571b3f80b366a6f2c585ec18615e4f3
Chris Lumens <clumens@redhat.com>
2014-11-20 11:30:15 -0500

    Find storage test cases automatically.

--- tests/storage/run_storage_tests.py
+++ tests/storage/run_storage_tests.py
@@ -2,23 +2,26 @@
 
 import os, sys
 
+from pyanaconda.ui.common import collect
+
 if os.geteuid() != 0:
     sys.stderr.write("You must be root to run the storage tests; skipping.\n")
     # This return code tells the automake test driver that this test was skipped.
     os._exit(77)
 
-from cases.bz1014545 import BZ1014545_TestCase
-from cases.bz1067707 import BZ1067707_TestCase
-from cases.reuse import PartitionReuse_TestCase, LVMReuse_TestCase, BTRFSReuse_TestCase, ThinpReuse_TestCase
+if "top_srcdir" not in os.environ:
+    sys.stderr.write("$top_srcdir must be defined in the test environment\n")
+    # This return code tells the automake test driver that the test setup failed
+    sys.exit(99)
 
 failures = 0
 
-for tc in [PartitionReuse_TestCase(),
-           LVMReuse_TestCase(),
-           BTRFSReuse_TestCase(),
-           ThinpReuse_TestCase(),
-           BZ1014545_TestCase(),
-           BZ1067707_TestCase()]:
-    failures += tc.run()
+classes = collect("cases.%s",
+                  os.path.abspath(os.path.join(os.environ["top_srcdir"], "tests/storage/cases/")),
+                  lambda obj: getattr(obj, "desc", None) is not None)
+
+for tc in classes:
+    obj = tc()
+    failures += obj.run()
 
 os._exit(failures)
