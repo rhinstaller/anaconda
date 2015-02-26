60154cd29af771f80a77ddbf0695c2635d39241d
Alexander Todorov <atodorov@redhat.com>
2015-02-26 06:39:54 -0500

    backport pylint updates from master

--- tests/pylint/pylint-one.sh
+++ tests/pylint/pylint-one.sh
@@ -12,20 +12,20 @@ if grep -q '# pylint: skip-file' $1; then
     exit 0
 fi
 
-file_suffix="$(eval echo \$$#|sed s?/?_?g)"
-
 pylint_output="$(pylint \
     --msg-template='{msg_id}:{line:3d},{column}: {obj}: {msg}' \
     -r n --disable=C,R --rcfile=/dev/null \
     --dummy-variables-rgx=_ \
     --ignored-classes=DefaultInstall,Popen,QueueFactory,TransactionSet \
     --defining-attr-methods=__init__,_grabObjects,initialize,reset,start,setUp \
-    --load-plugins=intl,preconf,markup \
+    --load-plugins=intl,preconf,markup,eintr,pointless-override,environ \
+    --init-import=y \
     --init-hook=\
 'import gi.overrides, os;
 gi.overrides.__path__[0:0] = (os.environ["ANACONDA_WIDGETS_OVERRIDES"].split(":") if "ANACONDA_WIDGETS_OVERRIDES" in os.environ else [])' \
     $DISABLED_WARN_OPTIONS \
     $DISABLED_ERR_OPTIONS \
+    $EXTRA_OPTIONS \
     $NON_STRICT_OPTIONS "$@" 2>&1 | \
     egrep -v -f "$FALSE_POSITIVES" \
     )"
@@ -33,6 +33,8 @@ gi.overrides.__path__[0:0] = (os.environ["ANACONDA_WIDGETS_OVERRIDES"].split(":"
 if [ -n "$(echo "$pylint_output" | fgrep -v '************* Module ')" ]; then
     # Replace the Module line with the actual filename
     pylint_output="$(echo "$pylint_output" | sed "s|\* Module .*|* Module $(eval echo \$$#)|")"
-    echo "$pylint_output" > pylint-out_$file_suffix
-    touch "pylint-$file_suffix-failed"
+    echo "$pylint_output"
+    exit 1
+else
+    exit 0
 fi
