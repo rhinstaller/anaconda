42b45776ee73bf293fa013ae9608ca508d6a3d7c
Brian C. Lane <bcl@redhat.com>
2015-05-29 17:49:55 -0700

    Add platform specific group selection (#884385)
    Virtualization platforms would like to have their packages installed
    automatically when running inside them. This uses systemd-detect-virt to
    identify the platform and creates a group id to be selected by prefixing
    it with "platform-" so that, for example, running on kvm will result in
    a group named "platform-kvm" being selected if it exists.
    
    Resolves: rhbz#884385
    
    (cherry picked from commit 06f531eb3a2d31e5a622581f099fb93be780e193)
    
    Related: rhbz#1196721

--- pyanaconda/iutil.py
+++ pyanaconda/iutil.py
@@ -937,3 +937,22 @@ def ipmi_report(event):
     execWithCapture("ipmitool", ["sel", "add", path])
 
     os.remove(path)
+
+def get_platform_groupid():
+    """ Return a platform group id string
+
+        This runs systemd-detect-virt and if the result is not 'none' it
+        prefixes the lower case result with "platform-" for use as a group id.
+
+        :returns: Empty string or a group id for the detected platform
+        :rtype: str
+    """
+    try:
+        platform = execWithCapture("systemd-detect-virt", []).strip()
+    except (IOError, AttributeError):
+        return ""
+
+    if platform == "none":
+        return ""
+
+    return "platform-" + platform.lower()
