8fcd91d227f3a0ed688cf1914387ab66b05955d5
Chris Lumens <clumens@redhat.com>
2015-06-08 11:48:10 -0400

    Fix disk argument passing to virt-cat in the ostree test.

--- tests/kickstart_tests/basic-ostree.sh
+++ tests/kickstart_tests/basic-ostree.sh
@@ -35,20 +35,21 @@ prepare() {
 
 validate() {
     disksdir=$1
-    args=$(for d in ${disksdir}/*img; do echo -drive file=${d}; done)
+    qemuArgs=$(for d in ${disksdir}/*img; do echo -drive file=${d}; done)
+    virtCatArgs=$(for d in ${disksdir}/*img; do echo -a ${d}; done)
 
     # Now attempt to boot the resulting VM and see if the install
     # actually worked.  The VM will shut itself down so there's no
     # need to worry with that here.
     timeout 5m /usr/bin/qemu-kvm -m 2048 \
                                  -smp 2 \
-                                 ${args} \
+                                 ${qemuArgs} \
                                  -vnc localhost:3
 
     # There should be a /root/RESULT file with results in it.  Check
     # its contents and decide whether the test finally succeeded or
     # not.
-    result=$(virt-cat ${args} -m /dev/mapper/fedora-root /ostree/deploy/fedora-atomic/var/roothome/RESULT)
+    result=$(virt-cat ${virtCatArgs} -m /dev/mapper/fedora-root /ostree/deploy/fedora-atomic/var/roothome/RESULT)
     if [[ $? != 0 ]]; then
         status=1
         echo '*** /root/RESULT does not exist in VM image.'
