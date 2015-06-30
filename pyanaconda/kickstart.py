1b8f716b8ccb689309c366b3779eaf59dc6beffd
Brian C. Lane <bcl@redhat.com>
2015-06-30 15:27:16 -0700

    Add RAID swaps to /etc/fstab (#1234469)
    swaps needs to be explicitly added to fstab now, it was working for
    partitions and logvol but not RAID. This adds RAID support.
    
    Resolves: rhbz#1234469

--- pyanaconda/kickstart.py
+++ pyanaconda/kickstart.py
@@ -1463,6 +1463,8 @@ class RaidData(commands.raid.F18_RaidData):
 
             dev.format.mountpoint = self.mountpoint
             dev.format.mountopts = self.fsopts
+            if ty == "swap":
+                storage.addFstabSwap(dev)
             return
 
         # Get a list of all the RAID members.
@@ -1517,6 +1519,8 @@ class RaidData(commands.raid.F18_RaidData):
 
             removeExistingFormat(device, storage)
             devicetree.registerAction(ActionCreateFormat(device, kwargs["fmt"]))
+            if ty == "swap":
+                storage.addFstabSwap(device)
         else:
             if devicename and devicename in (a.name for a in storage.mdarrays):
                 raise KickstartValueError(formatErrorMsg(self.lineno,
@@ -1537,6 +1541,8 @@ class RaidData(commands.raid.F18_RaidData):
                 raise KickstartValueError(formatErrorMsg(self.lineno, msg=str(e)))
 
             storage.createDevice(request)
+            if ty == "swap":
+                storage.addFstabSwap(request)
 
         if self.encrypted:
             if self.passphrase and not storage.encryptionPassphrase:
