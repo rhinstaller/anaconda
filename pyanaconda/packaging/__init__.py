rhinstaller/anaconda

commit 3e8009986134d1743615204a0e265a5696bf9567

Move the big block of late storage writing out of install.py.
We're trying to reduce install.py to a simpler, more straightfoward format
where it's just a list of things that need to be done.  We can get rid of
some of the conditional checking by moving this code into payloads and
having those payloads just provide blank overridden methods.

pyanaconda/packaging/__init__.py
@@ -69,6 +69,7 @@ from blivet.errors import StorageError
 import blivet.util
 import blivet.arch
 from blivet.platform import platform
+from blivet import setSysroot
 
 from pyanaconda.product import productName, productVersion
 USER_AGENT = "%s (anaconda)/%s" %(productName, productVersion)
@@ -597,6 +598,45 @@ class Payload(object):
         #   kickstart should handle this before we get here
 
         self._copyDriverDiskFiles()
+
+    def writeStorageEarly(self):
+        """Some packaging payloads require that the storage configuration be
+           written out before doing installation.  Right now, this is basically
+           just the dnfpayload.  Payloads should only implement one of these
+           methods by overriding the unneeded one with a pass.
+        """
+        if self.data.method.method != "liveimg" and not flags.dirInstall:
+            self.storage.write()
+
+    def writeStorageLate(self):
+        """Some packaging payloads require that the storage configuration be
+           written out after doing installation.  Right now, this is basically
+           every payload except for dnf.  Payloads should only implement one of
+           these methods by overriding the unneeded one with a pass.
+        """
+        if self.data.method.method == "liveimg" and not flags.dirInstall:
+            if iutil.getSysroot() != iutil.getTargetPhysicalRoot():
+                setSysroot(iutil.getTargetPhysicalRoot(), iutil.getSysroot())
+
+                # Now that we have the FS layout in the target, umount
+                # things that were in the legacy sysroot, and put them in
+                # the target root, except for the physical /.  First,
+                # unmount all target filesystems.
+                self.storage.umountFilesystems()
+
+                # Explicitly mount the root on the physical sysroot
+                rootmnt = storage.mountpoints.get('/')
+                rootmnt.setup()
+                rootmnt.format.setup(options=rootmnt.format.options, chroot=iutil.getTargetPhysicalRoot())
+
+                self.prepareMountTargets(self.storage)
+
+                # Everything else goes in the target root, including /boot
+                # since the bootloader code will expect to find /boot
+                # inside the chroot.
+                self.storage.mountFilesystems(skipRoot=True)
+
+            self.storage.write()
 
 # Inherit abstract methods from Payload
 # pylint: disable=abstract-method

pyanaconda/packaging/livepayload.py
@@ -539,3 +539,6 @@ class LiveImageKSPayload(LiveImagePayload):
             # Strip out vmlinuz- from the names
             return sorted((n.split("/")[-1][8:] for n in names if "boot/vmlinuz-" in n),
                     key=functools.cmp_to_key(versionCmp))
+
+    def writeStorageEarly(self):
+        pass

pyanaconda/install.py
@@ -37,7 +37,6 @@ from pyanaconda.ui.lib.entropy import wait_for_entropy
 from pyanaconda.kickstart import runPostScripts, runPreInstallScripts
 from pyanaconda.kexec import setup_kexec
 import logging
-import blivet
 log = logging.getLogger("anaconda")
 
 def _writeKS(ksdata):
@@ -200,10 +199,7 @@ def doInstall(storage, payload, ksdata, instClass):
                                                             wait_for_entropy=entropy_wait_clbk)
 
     turnOnFilesystems(storage, mountOnly=flags.flags.dirInstall, callbacks=callbacks_reg)
-    write_storage_late = (flags.flags.livecdInstall or ksdata.ostreesetup.seen
-                          or ksdata.method.method == "liveimg")
-    if not write_storage_late and not flags.flags.dirInstall:
-        storage.write()
+    payload.writeStorageEarly()
 
     # Run %pre-install scripts with the filesystem mounted and no packages
     with progress_report(_("Running pre-installation scripts")):
@@ -244,29 +240,7 @@ def doInstall(storage, payload, ksdata, instClass):
     payload.preInstall(packages=packages, groups=payload.languageGroups())
     payload.install()
 
-    if write_storage_late and not flags.flags.dirInstall:
-        if iutil.getSysroot() != iutil.getTargetPhysicalRoot():
-            blivet.setSysroot(iutil.getTargetPhysicalRoot(),
-                              iutil.getSysroot())
-
-            # Now that we have the FS layout in the target, umount
-            # things that were in the legacy sysroot, and put them in
-            # the target root, except for the physical /.  First,
-            # unmount all target filesystems.
-            storage.umountFilesystems()
-
-            # Explicitly mount the root on the physical sysroot
-            rootmnt = storage.mountpoints.get('/')
-            rootmnt.setup()
-            rootmnt.format.setup(options=rootmnt.format.options, chroot=iutil.getTargetPhysicalRoot())
-
-            payload.prepareMountTargets(storage)
-
-            # Everything else goes in the target root, including /boot
-            # since the bootloader code will expect to find /boot
-            # inside the chroot.
-            storage.mountFilesystems(skipRoot=True)
-        storage.write()
+    payload.writeStorageLate()
 
     # Do bootloader.
     if willInstallBootloader:

pyanaconda/packaging/rpmostreepayload.py
@@ -299,3 +299,6 @@ class RPMOSTreePayload(ArchivePayload):
         set_kargs_args.extend(self.storage.bootloader.boot_args)
         set_kargs_args.append("root=" + self.storage.rootDevice.fstabSpec)
         self._safeExecWithRedirect("ostree", set_kargs_args, root=iutil.getSysroot())
+
+    def writeStorageEarly(self):
+        pass

commit b5ede2f1baf7167b834e1222b993471bb10c4a2b

fix storage writing for live and ostree installs (#1236937)
3e800998 broke storage writing in two cases: regular live
installs and ostree installs. The old write_storage_late check
checked for flags.livecdInstall and ksdata.ostreesetup.seen as
well as ksdata.method.method == "liveimg". The new check in
writeStorageLate() dropped those two, only keeping the method
check.

Since the idea here is that payloads choose which writeStorage
method to use and override the other with a pass, let's change
the conditionals in the writeStorage methods to only check for
dirinstall, and tweak the payloads to match the behaviour we
want: LiveImagePayload, LiveImageKSPayload and RPMOSTreePayload
choose writeStorageLate(), DNFPayload chooses
writeStorageEarly(). Note - LiveImageKSPayload inherits from
LiveImagePayload. I left TarPayload alone since it's incomplete,
but I guess it'd want Late.

pyanaconda/packaging/__init__.py
@@ -605,7 +605,7 @@ class Payload(object):
            just the dnfpayload.  Payloads should only implement one of these
            methods by overriding the unneeded one with a pass.
         """
-        if self.data.method.method != "liveimg" and not flags.dirInstall:
+        if not flags.dirInstall:
             self.storage.write()
 
     def writeStorageLate(self):
@@ -614,7 +614,7 @@ class Payload(object):
            every payload except for dnf.  Payloads should only implement one of
            these methods by overriding the unneeded one with a pass.
         """
-        if self.data.method.method == "liveimg" and not flags.dirInstall:
+        if not flags.dirInstall:
             if iutil.getSysroot() != iutil.getTargetPhysicalRoot():
                 setSysroot(iutil.getTargetPhysicalRoot(), iutil.getSysroot())
 

pyanaconda/packaging/dnfpayload.py
@@ -848,3 +848,6 @@ class DNFPayload(packaging.PackagePayload):
                 log.error(e)
 
         super(DNFPayload, self).postInstall()
+
+    def writeStorageLate(self):
+        pass

pyanaconda/packaging/livepayload.py
@@ -201,6 +201,9 @@ class LiveImagePayload(ImagePayload):
     def kernelVersionList(self):
         return self._kernelVersionList
 
+    def writeStorageEarly(self):
+        pass
+
 class DownloadProgress(object):
     """ Provide methods for download progress reporting."""
 
@@ -539,6 +542,3 @@ class LiveImageKSPayload(LiveImagePayload):
             # Strip out vmlinuz- from the names
             return sorted((n.split("/")[-1][8:] for n in names if "boot/vmlinuz-" in n),
                     key=functools.cmp_to_key(versionCmp))
-
-    def writeStorageEarly(self):
-        pass