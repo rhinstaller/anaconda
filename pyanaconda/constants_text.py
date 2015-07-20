33c9fdf8acaadf3b6589c90d1f7e8712606affc2
Samantha N. Bueno <sbueno+anaconda@redhat.com>
2015-07-20 12:55:57 -0400

    Get rid of unnecessary constants in constants_text. (#965985)
    Without ncurses, we don't need to keep these around anymore for the
    new text mode.

--- pyanaconda/constants_text.py
+++ pyanaconda/constants_text.py
@@ -1,7 +1,7 @@
 #
 # constants_text.py: text mode constants
 #
-# Copyright (C) 2000, 2001, 2002  Red Hat, Inc.  All rights reserved.
+# Copyright (C) 2015  Red Hat, Inc.  All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,38 +20,6 @@
 # pylint: disable=wildcard-import
 from pyanaconda.constants import *
 from pyanaconda.i18n import _, N_
-
-class Translator:
-    """A simple class to facilitate on-the-fly translation for newt buttons"""
-    def __init__(self, button, check):
-        self.button = button
-        self.check = check
-
-    def __getitem__(self, which):
-        if which == 0:
-            return _(self.button)
-        elif which == 1:
-            return self.check
-        raise IndexError
-
-    def __len__(self):
-        return 2
-
-TEXT_OK_STR = N_("OK")
-TEXT_OK_CHECK = "ok"
-TEXT_OK_BUTTON = Translator(TEXT_OK_STR, TEXT_OK_CHECK)
-
-TEXT_CANCEL_STR = N_("Cancel")
-TEXT_CANCEL_CHECK = "cancel"
-TEXT_CANCEL_BUTTON = Translator(TEXT_CANCEL_STR, TEXT_CANCEL_CHECK)
-
-TEXT_YES_STR = N_("Yes")
-TEXT_YES_CHECK = "yes"
-TEXT_YES_BUTTON = Translator(TEXT_YES_STR, TEXT_YES_CHECK)
-
-TEXT_NO_STR = N_("No")
-TEXT_NO_CHECK = "no"
-TEXT_NO_BUTTON = Translator(TEXT_NO_STR, TEXT_NO_CHECK)
 
 # Make the return calls from the UIScreen input() function more clear
 INPUT_PROCESSED = None
