bc6378b2fed442e2f5a5d723ada9f87baed1e835
Will Woods <wwoods@redhat.com>
2015-07-21 16:53:26 -0400

    Wait forever for kickstarts on CDROM (#1168902)
    Commit 5d016ef added support for doing cdrom-swapping while doing
    "inst.ks=cdrom[:...]". But it turns out that if you're slow to swap the
    discs, we'll hit dracut's retry timeout and it'll drop to the emergency
    shell.
    
    So, in response to User Requests, we now disable the timeout entirely
    when using "inst.ks=cdrom[:...]"; dracut will wait forever for the
    kickstart and/or installer media, and if anything goes wrong you're just
    stuck and you'll need to reset.
    
    Resolves: rhbz#1168902

--- dracut/kickstart-genrules.sh
+++ dracut/kickstart-genrules.sh
@@ -19,6 +19,12 @@ case "${kickstart%%:*}" in
             when_diskdev_appears "$ksdev" \
                 fetch-kickstart-disk \$env{DEVNAME} "$kspath"
         fi
+        # "cdrom:" also means "wait forever for kickstart" because rhbz#1168902
+        if [ "$kstype" = "cdrom" ]; then
+            # if we reset main_loop to 0 every loop, we never hit the timeout.
+            # (see dracut's dracut-initqueue.sh for details on the mainloop)
+            echo "main_loop=0" > "$hookdir/initqueue/ks-cdrom-wait-forever.sh"
+        fi
         wait_for_kickstart
     ;;
     bd) # bd:<dev>:<path> - biospart (TODO... if anyone uses this anymore)
