69f65f6429e12353bbeca249e1487f744ae322c8
Martin Kolman <mkolman@redhat.com>
2015-07-29 11:48:40 +0200

    Disable a Pylint false-positive (#1234896)
    We make sure the given variable is callabale by means that
    Pylint can't verify.
    
    Related: rhbz#1234896

--- pyanaconda/ui/gui/__init__.py
+++ pyanaconda/ui/gui/__init__.py
@@ -283,6 +283,7 @@ class GUIObject(common.UIObject):
         self._doPostAutostep()
 
         # run the autostep-done callback (if any)
+        # pylint: disable=not-callable
         if self.autostepDoneCallback:
             self.autostepDoneCallback(self)
 
