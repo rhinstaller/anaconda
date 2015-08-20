2a102e23676eba71855094e279c767b461097d97
Chris Lumens <clumens@redhat.com>
2015-08-20 10:55:52 -0400

    Do a better job reporting failures from kickstart_tests.
    When the failure message is a line from anaconda's logs, it starts with
    a timestamp.  Those include colons, so because I am splitting on colons
    we get an unhelpful explanation like "03".  Thus, just grab the first
    bunch of characters from the log line and display that instead.  It
    won't give the complete reason (which could be very long) but it is a
    whole lot more helpful than just a number.

--- tests/kickstart_tests/run_report.sh
+++ tests/kickstart_tests/run_report.sh
@@ -2,7 +2,7 @@
 
 BEGIN {
     printf("\n\n\n%-30s | %-10s | %s\n", "TEST", "RESULT", "EXPLANATION");
-    printf("-------------------------------+------------+---------------------------------------------\n");
+    printf("-------------------------------+------------+--------------------------------------------------------\n");
     FS=":"
 }
 /^RESULT:/ { if ($4 == "Test timed out.") {
@@ -11,9 +11,12 @@ BEGIN {
              } else if (match($0, "Traceback")) {
                  result = "FAILED";
                  explanation = "Traceback";
+             } else if (match($0, "SUCCESS")) {
+                 result = $3;
+                 explanation = "";
              } else {
                  result = $3;
-                 explanation = $4;
+                 explanation = substr($0, index($0, $4), 55);
              }
 
              printf("%-30s | %-10s | %s\n", $2, result, explanation);
