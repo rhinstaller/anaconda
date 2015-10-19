e34dcec487f71fcf7d2a3e92556399f1ba754e7f
Chris Lumens <clumens@redhat.com>
2015-10-19 13:42:39 -0400

    Fix a long line in kickstart_tests/functions.sh.

--- tests/kickstart_tests/functions.sh
+++ tests/kickstart_tests/functions.sh
@@ -10,7 +10,8 @@
 # Public License for more details.  You should have received a copy of the
 # GNU General Public License along with this program; if not, write to the
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the # source code or documentation are not subject to the GNU General Public
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
 # License and may only be used or replicated with the express permission of
 # Red Hat, Inc.
 #
