rhinstaller/anaconda

commit 1e1f8587f9f8a793f18be20bd52b03dc89605eed

network: assume --device=link as default also for ks on hd (#1085310)
Ie when network is not activated in dracut.

Related: rhbz#1085310

If not overriden by ksdevice.

pyanaconda/network.py
@@ -1183,9 +1183,8 @@ def get_device_name(network_data):
     :param network_data: A pykickstart NetworkData object
     :returns: a string naming a physical device, or "" meaning none matched
     :rtype: str
-
     """
-    ksspec = network_data.device or flags.cmdline.get('ksdevice') or ""
+    ksspec = network_data.device or flags.cmdline.get('ksdevice') or "link"
     dev_name = ks_spec_to_device_name(ksspec)
     if not dev_name:
         return ""

commit 958711c218d67cd90228845e99d021abff4ddefc

network: Don't set --device link default for hostname only network cmd (#1272274)
network --hostname=blah

used in %pre section could override a device configuration with dhcp which is a
regression caused by commit 1e1f8587f9f8a793f18be20bd52b03dc89605eed

Resolves: rhbz#1272274

pyanaconda/kickstart.py
@@ -1096,6 +1096,20 @@ class Network(commands.network.F24_Network):
         commands.network.F24_Network.__init__(self, *args, **kwargs)
         self.packages = []
 
+    def parse(self, args):
+        nd = commands.network.F24_Network.parse(self, args)
+        setting_only_hostname = nd.hostname and len(args) <= 2
+        if not setting_only_hostname:
+            if not nd.device:
+                ksdevice = flags.cmdline.get('ksdevice')
+                if ksdevice:
+                    log.info('network: setting %s from ksdevice for missing kickstart --device', ksdevice)
+                    nd.device = ksdevice
+                else:
+                    log.info('network: setting "link" for missing --device specification in kickstart')
+                    nd.device = "link"
+        return nd
+
     def setup(self):
         if network.is_using_team_device():
             self.packages = ["teamd"]

pyanaconda/network.py
@@ -1184,7 +1184,7 @@ def get_device_name(network_data):
     :returns: a string naming a physical device, or "" meaning none matched
     :rtype: str
     """
-    ksspec = network_data.device or flags.cmdline.get('ksdevice') or "link"
+    ksspec = network_data.device or ""
     dev_name = ks_spec_to_device_name(ksspec)
     if not dev_name:
         return ""