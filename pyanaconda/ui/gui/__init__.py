e1d3097990214e12ef9e7ba55c15cfc2600ab0a5
Jiri Konecny <jkonecny@redhat.com>
2016-02-02 08:48:38 +0100

    Increase GI version required of AnacondaWidgets
    Anaconda GI now requires the AnacondaWidgets in version 3.3.
    
    It's required for the multiselection feature in the Custom spoke.

--- pyanaconda/ui/gui/__init__.py
+++ pyanaconda/ui/gui/__init__.py
@@ -26,7 +26,7 @@ from contextlib import contextmanager
 import gi
 gi.require_version("Gdk", "3.0")
 gi.require_version("Gtk", "3.0")
-gi.require_version("AnacondaWidgets", "3.0")
+gi.require_version("AnacondaWidgets", "3.3")
 gi.require_version("Keybinder", "3.0")
 gi.require_version("GdkPixbuf", "2.0")
 gi.require_version("GLib", "2.0")
