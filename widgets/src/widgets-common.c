c25430fc39eaf0349a51d975201b210c31e91162
David Shea <dshea@redhat.com>
2016-02-19 15:29:35 -0500

    Use CSS to style the internal widgets.
    Provide names for the stylable internal widgets, and use CSS instead of
    Pango attributes and markup to set the style attributes. This allows the
    appearance of the anaconda widgets to be more easily modified by
    product-specific stylesheets.
    
    The new widget names are only applied to the leaf widgets (labels and
    buttons and such) since making the containers mess-with-able sounds like
    trouble, and the type and existence of the containers could change if we
    decide to switch to gadget types.
    
    As part of the changes for moving things to CSS, what looks like the
    intent of anaconda_mountpoint_selector_toggle_background (unselected
    selectors are gray, selected selectors are black) actually works now,
    and the incomplete state of AnacondaSpokeSelector is now tied to the
    inconsistent widget flag.
    
    The CSS data is stored in regular CSS files that get compiled into
    libAnacondaWidgets.so, since that seemed less gross than a bunch of
    multi-line C strings.

--- widgets/src/widgets-common.c
+++ widgets/src/widgets-common.c
@@ -21,5 +21,48 @@
  */
 
 #include "config.h"
-
 #include "widgets-common.h"
+
+#include <gtk/gtk.h>
+
+/**
+ * anaconda_widget_apply_stylesheet:
+ * @widget: The widget to apply the style data to.
+ * @name: The name of the widget's stylesheet.
+ *
+ * Apply CSS data to a widget's #GtkStyleContext. The data will not affect any
+ * other widgets, including children of this widget.
+ *
+ * The CSS data lives in the resource bundle, the advantage of which is that
+ * the stylesheet is just a normal stylesheet in the source tree with normal
+ * syntax highlighting and no weird C string stuff or anything. The
+ * disadvantage is that the stylesheet can only be applied to one widget at a
+ * time so there's a bunch of tiny little stylesheets in the resources
+ * directory, but that's the world we live in.
+ *
+ * The stylesheet should live in the resources/ directory in the source tree
+ * and will be fetched as /org/fedoraproject/anaconda/widgets/<name>.css.
+ *
+ * The stylesheet is added to the style context at one less than
+ * #GTK_STYLE_PROVIDER_PRIORITY_APPLICATION so that the style will not
+ * overridden by a sloppy wildcard in a theme somewhere, but it will be
+ * overridden by the application-level stylesheet, which may include
+ * product-specific customizations.
+ *
+ */
+void anaconda_widget_apply_stylesheet(GtkWidget *widget, const gchar *name)
+{
+    GtkCssProvider *style_provider;
+    GtkStyleContext *style_context;
+    gchar *resource_path;
+
+    resource_path = g_strdup_printf("/org/fedoraproject/anaconda/widgets/%s.css", name);
+
+    style_provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_resource(style_provider, resource_path);
+    g_free(resource_path);
+
+    style_context = gtk_widget_get_style_context(widget);
+    gtk_style_context_add_provider(style_context, GTK_STYLE_PROVIDER(style_provider),
+            GTK_STYLE_PROVIDER_PRIORITY_APPLICATION - 1);
+}
