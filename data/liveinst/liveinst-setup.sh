dd9e80b6cc276cbb826cb671503803f85ec8bb49
David Shea <dshea@redhat.com>
2016-03-02 15:25:31 -0500

    Remove some history from the liveinst setup. (#1313098)
    /.livecd-configured was renamed in 2007, and I don't think we
    particularly care what happens on OLPC anymore. The remaining
    conditional did not handle lmc-created live media and is redundant now
    anyway.
    
    (cherry picked from commit e053e22682d122618a83124ab83b0b180fdebdd9)

--- data/liveinst/liveinst-setup.sh
+++ data/liveinst/liveinst-setup.sh
@@ -10,13 +10,5 @@ fi
 # Also lets us run (with the X11 backend) on Wayland
 [ -x /usr/bin/xhost ] && xhost +si:localuser:root > /dev/null 2>&1
 
-# don't run on geode (olpc)
-if [ `grep -c Geode /proc/cpuinfo` -eq 0 ]; then
-  if [ -b /dev/mapper/live-osimg-min ]; then
-    test -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs && source ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs
-    cp /usr/share/applications/liveinst.desktop "${XDG_DESKTOP_DIR:-$HOME/Desktop}"
-  elif [ -f /.livecd-configured ]; then  # FIXME: old way... this should go away
-    test -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs && source ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs
-    cp /usr/share/applications/liveinst.desktop "${XDG_DESKTOP_DIR:-$HOME/Desktop}"
-  fi
-fi
+test -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs && source ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs
+cp /usr/share/applications/liveinst.desktop "${XDG_DESKTOP_DIR:-$HOME/Desktop}"
