74867cf94717b05cfa18b1891cd2de9ab729dfb8
Karel Valek <kvalek@redhat.com>
2016-03-02 11:26:15 +0100

    Added PROGRESS_REPORT for retry messages
    Resolves: rhbz#1255094
    
    Signed-off-by: Karel Valek <kvalek@redhat.com>

--- pyanaconda/packaging/yumpayload.py
+++ pyanaconda/packaging/yumpayload.py
@@ -1446,6 +1446,7 @@ reposdir=%s
         progress_map = {
             "PROGRESS_PREP"    : _("Preparing transaction from installation source"),
             "PROGRESS_INSTALL" : _("Installing"),
+            "PROGRESS_RETRY"   : _(""),
             "PROGRESS_POST"    : _("Performing post-installation setup tasks")
         }
 
