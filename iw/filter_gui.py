dc32cf98f1516bda4dc0e7735bccf2bb84fdc262
Samantha N. Bueno <sbueno+anaconda@redhat.com>
2016-03-22 08:50:34 -0400

    Revert "Refactor variable name (#914637)"
    This reverts commit 9a912f8cda76ad73c7dc59af3102748c4cee0dcc.
    
    Resolves: rhbz#1319697

--- iw/filter_gui.py
+++ iw/filter_gui.py
@@ -47,12 +47,6 @@ _ = lambda x: gettext.ldgettext("anaconda", x)
 import logging
 log = logging.getLogger("anaconda")
 
-# Define constants that will be used within the FilterWindow class
-# for the variable device_data
-UDEV_DATA = 0
-VISIBLE_IN_UI = 1
-ACTIVE_IN_UI = 2
-DEVICE_MUTABILITY = 3
 DEVICE_COL = 4
 MODEL_COL = 5
 CAPACITY_COL = 6
@@ -134,7 +128,7 @@ class Callbacks(object):
         self.sizeLabel = self.xml.get_widget("sizeLabel")
         self.sizeLabel.connect("realize", self.update)
 
-    def addToUI(self, device_data):
+    def addToUI(self, tuple):
         pass
 
     def deviceToggled(self, set, device):
@@ -209,12 +203,12 @@ class MPathCallbacks(FilteredCallbacks):
         self.interconnectEntry.connect("changed", lambda entry: self.model.get_model().refilter())
         self.IDEntry.connect("changed", lambda entry: self.model.get_model().refilter())
 
-    def addToUI(self, device_data):
-        if not device_data[VENDOR_COL] in self._vendors:
-            self._vendors.append(device_data[VENDOR_COL])
+    def addToUI(self, tuple):
+        if not tuple[VENDOR_COL] in self._vendors:
+            self._vendors.append(tuple[VENDOR_COL])
 
-        if not device_data[INTERCONNECT_COL] in self._interconnects:
-            self._interconnects.append(device_data[INTERCONNECT_COL])
+        if not tuple[INTERCONNECT_COL] in self._interconnects:
+            self._interconnects.append(tuple[INTERCONNECT_COL])
 
     def isMember(self, info):
         return info and isMultipath(info)
@@ -713,17 +707,17 @@ class FilterWindow(InstallWindow):
                 return
 
     def populate(self, nonraids, mpaths, raids, activeByDefault=False):
-        def _addDeviceData(device_data):
+        def _addTuple(tuple):
             global totalDevices, totalSize
             global selectedDevices, selectedSize
             added = False
 
-            self.store.append(None, device_data)
+            self.store.append(None, tuple)
 
             for pg in self.pages:
-                if pg.cb.isMember(device_data[UDEV_DATA]):
+                if pg.cb.isMember(tuple[0]):
                     added = True
-                    pg.cb.addToUI(device_data)
+                    pg.cb.addToUI(tuple)
 
             # Only update the size label if this device was added to any pages.
             # This prevents situations where we're only displaying the basic
@@ -731,11 +725,11 @@ class FilterWindow(InstallWindow):
             # in the store that cannot be seen.
             if added:
                 totalDevices += 1
-                totalSize += device_data[UDEV_DATA]["XXX_SIZE"]
+                totalSize += tuple[0]["XXX_SIZE"]
 
-                if device_data[ACTIVE_IN_UI]:
+                if tuple[2]:
                     selectedDevices += 1
-                    selectedSize += device_data[UDEV_DATA]["XXX_SIZE"]
+                    selectedSize += tuple[0]["XXX_SIZE"]
 
         def _isProtected(info):
             protectedNames = map(udev_resolve_devspec, self.anaconda.protected)
@@ -791,11 +785,11 @@ class FilterWindow(InstallWindow):
             else:
                 ident = udev_device_get_wwid(d)
 
-            device_data = (d, True, _active(d), _isProtected(d), name,
+            tuple = (d, True, _active(d), _isProtected(d), name,
                      partedDevice.model, long(d["XXX_SIZE"]),
                      udev_device_get_vendor(d), udev_device_get_bus(d),
                      udev_device_get_serial(d), ident, "", "", "", "")
-            _addDeviceData(device_data)
+            _addTuple(tuple)
 
         if raids and flags.dmraid:
             # isw/imsm is handled by mdadm -- not dmraid/pyblock
@@ -836,10 +830,10 @@ class FilterWindow(InstallWindow):
                         "sysfs_path": sysfs_path}
 
                 model = "BIOS RAID set (%s)" % rs.rs.set_type
-                device_data = (data, True, _active(data), _isProtected(data), rs.name,
+                tuple = (data, True, _active(data), _isProtected(data), rs.name,
                          model, long(size), "", "", "", "",
                          "\n".join(members), "", "", "")
-                _addDeviceData(device_data)
+                _addTuple(tuple)
 
             # handle imsm/isw separately without pyblock/dmraid
             mduuids = set()
@@ -911,7 +905,7 @@ class FilterWindow(InstallWindow):
             # dict as that would break NameCache matching
             data = mpath[0].copy()
             data["name"] = udev_device_get_multipath_name(mpath[0])
-            device_data = (data, True, _active(data), _isProtected(data),
+            tuple = (data, True, _active(data), _isProtected(data),
                      udev_device_get_multipath_name(mpath[0]), model,
                      long(mpath[0]["XXX_SIZE"]),
                      udev_device_get_vendor(mpath[0]),
@@ -919,7 +913,7 @@ class FilterWindow(InstallWindow):
                      udev_device_get_serial(mpath[0]),
                      udev_device_get_wwid(mpath[0]),
                      paths, "", "", "")
-            _addDeviceData(device_data)
+            _addTuple(tuple)
 
     def split_list(self, pred, lst):
         pos = []
