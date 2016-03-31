af0cfb2eab968586e859a835480ebd6f6239444b
David Lehman <dlehman@redhat.com>
2016-03-31 12:14:57 -0400

    Reset invalid disk selection before proceeding.
    (cherry picked from commit e9d82e38bb11ab2e3a06e76c1f98eefeae2cf919)
    
    Resolves: rhbz#1265330

--- pyanaconda/ui/gui/spokes/storage.py
+++ pyanaconda/ui/gui/spokes/storage.py
@@ -833,6 +833,7 @@ class StorageSpoke(NormalSpoke, StorageChecker):
             # The disk selection has to make sense before we can proceed.
             self.set_error(_("There was a problem with your disk selection. "
                              "Click here for details."))
+            self._unhide_disks()
             return
 
         # hide/unhide disks as requested
