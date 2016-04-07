9c217b5ce3450a2dfa3e1b9a4f6b64acc9d3f9c0
Vojtech Trefny <vtrefny@redhat.com>
2016-04-07 12:36:12 +0200

    Fix adding offload iSCSI devices

--- pyanaconda/ui/gui/spokes/filter.py
+++ pyanaconda/ui/gui/spokes/filter.py
@@ -179,7 +179,7 @@ class SearchPage(FilterPage):
 
         ports = []
         for disk in disks:
-            if hasattr(disk, "node"):
+            if hasattr(disk, "node") and disk.node is not None:
                 ports.append(str(disk.node.port))
 
         self.setupCombo(self._portCombo, ports)
@@ -339,7 +339,7 @@ class OtherPage(FilterPage):
             paths = [d.name for d in disk.parents]
             selected = disk.name in selectedNames
 
-            if hasattr(disk, "node"):
+            if hasattr(disk, "node") and disk.node is not None:
                 port = str(disk.node.port)
                 lun = str(disk.node.tpgt)
             else:
