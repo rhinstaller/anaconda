2a4ff6a09d5d8bd4aa09c2b9a9d2edc50accc801
Chris Lumens <clumens@redhat.com>
2016-04-11 13:53:10 -0400

    Split gui test running out into its own script.
    This brings run_gui_tests.sh back, but in much reduced form.  It can't
    be run out of nosetests_root.sh due to the exec.  Also, running it on
    its own means this long-lived process can get its own job in jenkins and
    not hold up PRs.

---
+++ tests/run_gui_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! rpm -q python3-nose-testconfig &> /dev/null; then
+    echo "python3-nose-testconfig is not available; exiting."
+    exit 99
+fi
+
+export LC_ALL=C # translations confuse Dogtail
+
+exec ${srcdir}/nosetests.sh -s --nologcapture --process-timeout 1200 \
+    --tc=resultsdir:$(readlink -f autogui-results-$(date +"%Y%m%d_%H%M%S")) \
+    ${srcdir}/gui/test_*.py
