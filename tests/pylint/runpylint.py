0d9fda62b7e8e4f8b4c478eb70d3eabdaaca02d2
Brian C. Lane <bcl@redhat.com>
2016-04-18 17:23:36 -0700

    Ignore some E1101 no-member errors when running pylint
    Some of these are due to a lack of introspection of the module from
    pylint (eg. BlockDev) and the GLib.GError one is possibly a pylint bug
    since it is supposed to work with GLib.
    
    For now we ignore tham as specifically as possible so that real E1101
    errors will be easy to find.

--- tests/pylint/runpylint.py
+++ tests/pylint/runpylint.py
@@ -14,7 +14,18 @@ class AnacondaLintConfig(PocketLintConfig):
 
                                 # XXX: These are temporary until dogtail and koji have python3 versions.
                                 FalsePositive(r"^E0401.*: Unable to import 'dogtail.*'$"),
-                                FalsePositive(r"^E0401.*: Unable to import 'koji'$")
+                                FalsePositive(r"^E0401.*: Unable to import 'koji'$"),
+                                FalsePositive(r"^E1101.*: Instance of 'GError' has no 'message' member"),
+                                FalsePositive(r"^E1101.*: FedoraGeoIPProvider._refresh: Instance of 'LookupDict' has no 'ok' member"),
+                                FalsePositive(r"^E1101.*: HostipGeoIPProvider._refresh: Instance of 'LookupDict' has no 'ok' member"),
+                                FalsePositive(r"^E1101.*: Geocoder._reverse_geocode_nominatim: Instance of 'LookupDict' has no 'ok' member"),
+                                # TODO: BlockDev introspection needs to be added to pylint to handle these
+                                FalsePositive(r"E1101.*: Instance of 'int' has no 'dasd_needs_format' member"),
+                                FalsePositive(r"E1101.*: Instance of 'int' has no 'dasd_format' member"),
+                                FalsePositive(r"E1101.*: Instance of 'int' has no 'sanitize_dev_input' member"),
+                                FalsePositive(r"E1101.*: Instance of 'int' has no 'dasd_online' member"),
+                                FalsePositive(r"E1101.*: Instance of 'int' has no 'zfcp_sanitize_wwpn_input' member"),
+                                FalsePositive(r"E1101.*: Instance of 'int' has no 'zfcp_sanitize_lun_input' member"),
                               ]
 
     @property
