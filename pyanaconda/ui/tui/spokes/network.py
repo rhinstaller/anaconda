2373bfafe5e328cab4605c6ec59936d705722818
Radek Vykydal <rvykydal@redhat.com>
2016-05-27 15:39:45 +0200

    network: add support for --no-activate kickstart opton (#1277975)
    Resolves: rhbz#1277975
    
    This will allow to override special default of first network command
    which is --activate.

--- pyanaconda/ui/tui/spokes/network.py
+++ pyanaconda/ui/tui/spokes/network.py
@@ -257,12 +257,17 @@ class NetworkSpoke(FirstbootSpokeMixIn, EditTUISpoke):
         hostname = self.data.network.hostname
 
         self.data.network.network = []
-        for name in nm.nm_devices():
+        for i, name in enumerate(nm.nm_devices()):
             nd = network.ksdata_from_ifcfg(name)
             if not nd:
                 continue
             if name in nm.nm_activated_devices():
                 nd.activate = True
+            else:
+                # First network command defaults to --activate so we must
+                # use --no-activate explicitly to prevent the default
+                if i == 0:
+                    nd.activate = False
             self.data.network.network.append(nd)
 
         (valid, error) = network.sanityCheckHostname(self.hostname_dialog.value)
