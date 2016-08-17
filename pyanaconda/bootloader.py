2288ef45ff729f5e24de1a193f28c3ada490d904
Phil Dibowitz <phil@ipom.com>
2016-08-16 21:09:08 -0700

    efiboot: stderr= is not an option to efibootmgr

--- pyanaconda/bootloader.py
+++ pyanaconda/bootloader.py
@@ -1799,7 +1799,7 @@ class EFIGRUB1(EFIBase, GRUB):
 
             eg: HD(1,800,64000,faacb4ef-e361-455e-bd97-ca33632550c3)
         """
-        buf = self.efibootmgr("-v", stderr="/dev/tty5", capture=True)
+        buf = self.efibootmgr("-v", capture=True)
         matches = re.search(productName + r'\s+(HD\(.+?\))', buf)
         if matches and matches.groups():
             return matches.group(1)
