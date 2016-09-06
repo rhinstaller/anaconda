e0e86f4d349974d71bcf933609ae1742daa054e0
Martin Kolman <mkolman@redhat.com>
2016-09-06 16:30:10 +0200

    Improved driver disk copying (#1269915)
    Turns out that just changing flags for the cp command won't do
    as function needs to support both single file driver disk RPM copy
    as well as copying all driver disk RPMs from a given folder.
    
    So let's drop the cp command all together and replace it with Python shutil
    based equivalent. As far as I can tell it seams that there should be
    no sub-folders in a driver disk RPM repo, so don't copy them
    but just log a warning instead.
    
    Also improve the copy-RPM unit tests to cover more scenarios and
    add add more logging in the stage2 driver disk package handling code.
    
    Related: rhbz#1269915

--- dracut/driver_updates.py
+++ dracut/driver_updates.py
@@ -73,6 +73,7 @@ import fnmatch
 # because it breaks sometimes.
 if os.isatty(0):
     import readline # pylint:disable=unused-import
+import shutil
 
 from contextlib import contextmanager
 from logging.handlers import SysLogHandler
@@ -263,7 +264,23 @@ def save_repo(repo, target="/run/install"):
     """copy a repo to the place where the installer will look for it later."""
     newdir = mkdir_seq(os.path.join(target, "DD-"))
     log.debug("save_repo: copying %s to %s", repo, newdir)
-    subprocess.call(["cp", "-arT", repo, newdir])
+    # repo can be two sorts of stuff:
+    # - a path to directory containing rpm files
+    # -> in this case copy it's contents to target
+    # - a path to an RPM file
+    # -> in this case copy the file to destination
+    if os.path.isfile(repo):
+        shutil.copy2(repo, newdir)
+    elif os.path.isdir(repo):
+        for item in os.listdir(repo):
+            item_path = os.path.join(repo, item)
+            if os.path.isfile(item_path):
+                log.debug("copying %s to %s", item_path, newdir)
+                shutil.copy2(item_path, newdir)
+            else:
+                log.warning("DD repo content not a file: %s", item_path)
+    else:
+        log.error("ERROR: DD repository needs to be a file file or a directory: %s", repo)
     return newdir
 
 def extract_drivers(drivers=None, repos=None, outdir="/updates",
