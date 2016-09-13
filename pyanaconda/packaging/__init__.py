rhinstaller/anaconda

commit 136961544060bd3891af981bbae5bc969117d08a

Fix needsNetwork testing only additional repositories (#1358788)
Now the needsNetwork method is testing also main repository.

Related: rhbz#1358788

pyanaconda/packaging/__init__.py
@@ -219,16 +219,38 @@ class Payload(object):
         urls = [repo.baseurl]
         if repo.mirrorlist:
             urls.extend(repo.mirrorlist)
+        return self._sourceNeedsNetwork(urls)
+
+    def _sourceNeedsNetwork(self, sources):
+        """ Return True if the source requires network.
+
+            :param sources: Source paths for testing
+            :type sources: list
+            :returns: True if any source requires network
+        """
         network_protocols = ["http:", "ftp:", "nfs:", "nfsiso:"]
-        for url in urls:
-            if any(url.startswith(p) for p in network_protocols):
+        for s in sources:
+            if any(s.startswith(p) for p in network_protocols):
+                log.debug("Source %s needs network for installation", s)
                 return True
 
+        log.debug("Source doesn't require network for installation")
         return False
 
     @property
     def needsNetwork(self):
-        return any(self._repoNeedsNetwork(r) for r in self.data.repo.dataList())
+        url = ""
+        if self.data.method.method == "nfs":
+            # NFS is always on network
+            return True
+        elif self.data.method.method == "url":
+            if self.data.url.url:
+                url = self.data.url.url
+            else:
+                url = self.data.url.mirrorlist
+
+        return (self._sourceNeedsNetwork([url]) or
+                any(self._repoNeedsNetwork(repo) for repo in self.data.repo.dataList()))
 
     def _resetMethod(self):
         self.data.method.method = ""

commit e7edf2692d7634ecb54554c39b8d6d4c1b18cfb6

Fix traceback when payload have None as url (#1371494)
It's a regression from commit 136961544060bd3891af981bbae5bc969117d08a

Resolves: rhbz#1371494

pyanaconda/packaging/__init__.py
@@ -230,7 +230,7 @@ class Payload(object):
         """
         network_protocols = ["http:", "ftp:", "nfs:", "nfsiso:"]
         for s in sources:
-            if any(s.startswith(p) for p in network_protocols):
+            if s and any(s.startswith(p) for p in network_protocols):
                 log.debug("Source %s needs network for installation", s)
                 return True
 