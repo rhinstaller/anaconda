c810d671d05a05bf2020cb57399a52cae5d55daa
Pat Riehecky <riehecky@fnal.gov>
2016-10-07 11:48:07 -0500

    Echoing 4de0ec44bdf0f68545bb55bb5fea00464b65fcab May as well include the SL file

---
+++ pyanaconda/installclasses/scientific.py
@@ -0,0 +1,40 @@
+#
+# scientific.py
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from pyanaconda.installclasses.rhel import RHELBaseInstallClass
+from pyanaconda.product import productName
+
+class ScientificBaseInstallClass(RHELBaseInstallClass):
+    '''
+        Scientific Linux is a Free RHEL rebuild.
+        In general it should install mostly like RHEL.
+
+        These few items are different between them.
+    '''
+    name = "Scientific Linux"
+
+    hidden = not productName.startswith("Scientific") # pylint: disable=no-member
+
+    installUpdates = True
+
+    efi_dir = "sl"
+
+    help_placeholder = "SLPlaceholder.html"
+    help_placeholder_with_links = "SLPlaceholder.html"
+
+    def __init__(self):
+        RHELBaseInstallClass.__init__(self)
