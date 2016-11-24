e245055139a19386ef01412c1390aef3ad123494
Martin Kolman <mkolman@redhat.com>
2016-11-24 17:38:17 +0100

    Add a signal/slot implementation
    The Signal class provides a signal/slot implementation that
    can be used to make code less tightly coupled and more modular.
    
    Lets say that we have a class that needs to call a callback once
    something happens. The class doesn't care about what the callback does,
    if there is one or if there are multiple callbacks.
    
    With this signal/slot implementation it simply instantiates the Signal
    class and exports it as part of it's API:
    
    self.task_complete = Signal()
    
    Once the event of interest happens, it calls the emit method of the signal
    (in this case with an argument):
    
    self.task_complete.emit(result)
    
    Other classes that care about the given event can connect their callbacks like this:
    
    def _task_done_cb(self, task_result):
        log.info("Task %s completed." % task_result.name)
    
    foo.task_complete.connect(self._task_done_cb)
    
    Many callback can be connected to a single signal.
    To disconnect a callback from a signal just pass the callback to the disconnect() method
    and it will be disconnected from the signal:
    
    foo_task_complete.disconnect(self._task_done_cb)
    
    It is also possible to clear all callback from a signal with the clear() method:
    
    foo.task_complete.clear()

---
+++ pyanaconda/isignal.py
@@ -0,0 +1,63 @@
+""" A signal/slot implementation
+
+File:    isignal.py
+Author:  Thiago Marcos P. Santos
+Author:  Christopher S. Case
+Author:  David H. Bronke
+Created: August 28, 2008
+Updated: December 12, 2011
+License: MIT
+
+"""
+import inspect
+from weakref import WeakKeyDictionary
+
+
+class Signal(object):
+    def __init__(self):
+        # The original implementation used WeakSet to store functions,
+        # but that causes lambdas without any other reference to be
+        # garbage collected. So we use a normal set to avoid that.
+        self._functions = set()
+        self._methods = WeakKeyDictionary()
+
+    # The original implementation used __call__, so one would just call the signal itself:
+    #
+    # my_signal("foo")
+    #
+    # This has been changed to the emit() method to both be more consistent with how signals/slots
+    # work in Qt & GTK and to make it more easily apparent that a signal is being triggered.
+    # The correct way to trigger a signal is therefore:
+    #
+    # my_signal.emit("foo")
+    def emit(self, *args, **kargs):
+        # Call handler functions
+        for func in self._functions:
+            func(*args, **kargs)
+
+        # Call handler methods
+        for obj, funcs in self._methods.items():
+            for func in funcs:
+                func(obj, *args, **kargs)
+
+    def connect(self, slot):
+        if inspect.ismethod(slot):
+            if slot.__self__ not in self._methods:
+                self._methods[slot.__self__] = set()
+
+            self._methods[slot.__self__].add(slot.__func__)
+
+        else:
+            self._functions.add(slot)
+
+    def disconnect(self, slot):
+        if inspect.ismethod(slot):
+            if slot.__self__ in self._methods:
+                self._methods[slot.__self__].remove(slot.__func__)
+        else:
+            if slot in self._functions:
+                self._functions.remove(slot)
+
+    def clear(self):
+        self._functions.clear()
+        self._methods.clear()
---
+++ tests/pyanaconda_tests/signal_test.py
@@ -0,0 +1,156 @@
+#
+# Martin Kolman <mkolman@redhat.com>
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use, modify,
+# copy, or redistribute it subject to the terms and conditions of the GNU
+# General Public License v.2.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  Any Red Hat
+# trademarks that are incorporated in the source code or documentation are not
+# subject to the GNU General Public License and may only be used or replicated
+# with the express permission of Red Hat, Inc.
+#
+# Test the Python-based signal and slot implementation.
+#
+
+import unittest
+
+from pyanaconda.isignal import Signal
+
+class FooClass(object):
+    def __init__(self):
+        self._var = None
+
+    @property
+    def var(self):
+        return self._var
+
+    def set_var(self, value):
+        self._var = value
+
+class SignalTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.var = None
+
+    def method_test(self):
+        """Test if a method can be correctly connected to a signal."""
+        signal = Signal()
+        foo = FooClass()
+        self.assertIsNone(foo.var)
+        # connect the signal
+        signal.connect(foo.set_var)
+        # trigger the signal
+        signal.emit("bar")
+        # check if the callback triggered correctly
+        self.assertEqual(foo.var, "bar")
+        # try to trigger the signal again
+        signal.emit("baz")
+        self.assertEqual(foo.var, "baz")
+        # now try to disconnect the signal
+        signal.disconnect(foo.set_var)
+        # check that calling the signal again
+        # no longer triggers the callback
+        signal.emit("anaconda")
+        self.assertEqual(foo.var, "baz")
+
+    def function_test(self):
+        """Test if a local function can be correctly connected to a signal."""
+
+        # create a local function
+        def set_var(value):
+            self.var = value
+
+        signal = Signal()
+        self.assertIsNone(self.var)
+        # connect the signal
+        signal.connect(set_var)
+        # trigger the signal
+        signal.emit("bar")
+        # check if the callback triggered correctly
+        self.assertEqual(self.var, "bar")
+        # try to trigger the signal again
+        signal.emit("baz")
+        self.assertEqual(self.var, "baz")
+        # now try to disconnect the signal
+        signal.disconnect(set_var)
+        # check that calling the signal again
+        # no longer triggers the callback
+        signal.emit("anaconda")
+        self.assertEqual(self.var, "baz")
+
+    def lambda_test(self):
+        """Test if a lambda can be correctly connected to a signal."""
+        foo = FooClass()
+        signal = Signal()
+        self.assertIsNone(foo.var)
+        # connect the signal
+        # pylint: disable=unnecessary-lambda
+        lambda_instance = lambda x: foo.set_var(x)
+        signal.connect(lambda_instance)
+        # trigger the signal
+        signal.emit("bar")
+        # check if the callback triggered correctly
+        self.assertEqual(foo.var, "bar")
+        # try to trigger the signal again
+        signal.emit("baz")
+        self.assertEqual(foo.var, "baz")
+        # now try to disconnect the signal
+        signal.disconnect(lambda_instance)
+        # check that calling the signal again
+        # no longer triggers the callback
+        signal.emit("anaconda")
+        self.assertEqual(foo.var, "baz")
+
+    def clear_test(self):
+        """Test if the clear() method correctly clears any connected callbacks."""
+        def set_var(value):
+            self.var = value
+
+        signal = Signal()
+        foo = FooClass()
+        lambda_foo = FooClass()
+        self.assertIsNone(foo.var)
+        self.assertIsNone(lambda_foo.var)
+        self.assertIsNone(self.var)
+        # connect the callbacks
+        signal.connect(set_var)
+        signal.connect(foo.set_var)
+        # pylint: disable=unnecessary-lambda
+        signal.connect(lambda x: lambda_foo.set_var(x))
+        # trigger the signal
+        signal.emit("bar")
+        # check that the callbacks were triggered
+        self.assertEqual(self.var, "bar")
+        self.assertEqual(foo.var, "bar")
+        self.assertEqual(lambda_foo.var, "bar")
+        # clear the callbacks
+        signal.clear()
+        # trigger the signal again
+        signal.emit("anaconda")
+        # check that the callbacks were not triggered
+        self.assertEqual(self.var, "bar")
+        self.assertEqual(foo.var, "bar")
+        self.assertEqual(lambda_foo.var, "bar")
+
+    def signal_chain_test(self):
+        """Check if signals can be chained together."""
+        foo = FooClass()
+        self.assertIsNone(foo.var)
+        signal1 = Signal()
+        signal1.connect(foo.set_var)
+        signal2 = Signal()
+        signal2.connect(signal1.emit)
+        signal3 = Signal()
+        signal3.connect(signal2.emit)
+        # trigger the chain
+        signal3.emit("bar")
+        # check if the initial callback was triggered
+        self.assertEqual(foo.var, "bar")
