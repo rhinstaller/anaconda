98a1f0f88015111bceacabae006af53880d74238
Vendula Poncova <vponcova@redhat.com>
2017-02-03 13:22:22 +0100

    Enable multiple inst.ks urls (#1391724)
    If there are defined multiple inst.ks urls in boot options, we enable
    to try the urls sequentially one by one until we successfully fetch
    the kickstart file. All defined locations must be urls (only http,
    https or ftp), otherwise we will try only the last location.
    
    Resolves: rhbz#1391724

--- dracut/parse-anaconda-kickstart.sh
+++ dracut/parse-anaconda-kickstart.sh
@@ -12,8 +12,23 @@ fi
 # no root? the kickstart will probably tell us what our root device is.
 [ "$kickstart" ] && [ -z "$root" ] && root="anaconda-kickstart"
 
+# Clean.
+rm -f /tmp/ks_urls
+
 case "${kickstart%%:*}" in
     http|https|ftp|nfs|nfs4) # network kickstart? set "neednet"!
+        # Save inst.ks urls to the file /tmp/ks_urls.
+        # If all given inst.ks locations are urls, we will
+        # enable multiple fetching. Otherwise, we will try
+        # only the last location, if it is an url.
+        locations="$(getargs ks= inst.ks=)"
+
+        if are_urls "$locations"; then
+            echo "$locations" > /tmp/ks_urls
+        elif are_urls "$kickstart"; then
+            echo "$kickstart" > /tmp/ks_urls
+        fi
+
         set_neednet
     ;;
     file|path) # "file:<path>" - "path:<path>" is accepted but deprecated
