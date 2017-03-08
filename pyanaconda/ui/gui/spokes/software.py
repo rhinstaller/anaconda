012fe32eff1d3291a1f36a24f5a906f12ba01141
Jiri Konecny <jkonecny@redhat.com>
2017-03-08 11:46:53 +0100

    Fix partial kickstart software selection in GUI (#1404158)
    When groups are selected in %packages section in a kickstart file, mark
    these groups in the Software spoke. However if the user visits the
    Software spoke the settings from kickstart (environment & groups) will
    be replaced by what the user selects in the spoke. Individual package
    selection from kickstart should be unaffected.
    
    Kickstart file without user interaction is the only option how to
    install groups and packages which are not inside of the selected
    environment.
    
    Related: rhbz#1404158
    
    (Cherry picked from 59bf385a9250c883f4acb81140fefe6d4f62e82b)

--- pyanaconda/ui/gui/spokes/software.py
+++ pyanaconda/ui/gui/spokes/software.py
@@ -25,7 +25,7 @@ from gi.repository import Gtk, Pango
 
 from pyanaconda.flags import flags
 from pyanaconda.i18n import _, C_, CN_
-from pyanaconda.payload import PackagePayload, payloadMgr, NoSuchGroup
+from pyanaconda.payload import PackagePayload, payloadMgr, NoSuchGroup, PayloadError
 from pyanaconda.threads import threadMgr, AnacondaThread
 from pyanaconda import constants, iutil
 
@@ -111,6 +111,7 @@ class SoftwareSelectionSpoke(NormalSpoke):
         self._fakeRadio = Gtk.RadioButton(group=None)
         self._fakeRadio.set_active(True)
 
+
     # Payload event handlers
     def _downloading_package_md(self):
         # Reset the error state from previous payloads
@@ -177,13 +178,16 @@ class SoftwareSelectionSpoke(NormalSpoke):
         hubQ.send_message(self.__class__.__name__, payloadMgr.error)
 
     def _apply(self):
+        # Environment needs to be set during a GUI installation, but is not required
+        # for a kickstart install (even partial)
         if not self.environment:
+            log.debug("Environment is not set, skip user packages settings")
             return
 
         # NOTE: This block is skipped for kickstart where addons and _origAddons will
         # both be [], preventing it from wiping out the kickstart's package selection
         addons = self._get_selected_addons()
-        if set(addons) != set(self._origAddons):
+        if not self._kickstarted and set(addons) != set(self._origAddons):
             self._selectFlag = False
             self.payload.data.packages.packageList = []
             self.payload.data.packages.groupList = []
@@ -236,7 +240,7 @@ class SoftwareSelectionSpoke(NormalSpoke):
                 return self.environment_valid
             # if we don't have environment we need to at least have the %packages
             # section in kickstart
-            elif flags.automatedInstall and self.data.packages.seen:
+            elif self._kickstarted:
                 return True
             # no environment and no %packages section -> manual intervention is needed
             else:
@@ -291,7 +295,7 @@ class SoftwareSelectionSpoke(NormalSpoke):
 
         # kickstart installation
         if flags.automatedInstall:
-            if self.data.packages.seen:
+            if self._kickstarted:
                 # %packages section is present in kickstart but environment is not set
                 if self.environment is None:
                     return _("Custom software selected")
@@ -322,6 +326,19 @@ class SoftwareSelectionSpoke(NormalSpoke):
 
     def _initialize(self):
         threadMgr.wait(constants.THREAD_PAYLOAD)
+        # Select groups which should be selected by kickstart
+        try:
+            for group in self.payload.selectedGroupsIDs():
+                if self.environment and self.payload.environmentOptionIsDefault(self.environment, group):
+                    self._addonStates[group] = self._ADDON_DEFAULT
+                else:
+                    self._addonStates[group] = self._ADDON_SELECTED
+        except PayloadError as e:
+            # Group translation is not supported
+            log.warning(e)
+            # It's better to have all or nothing selected from kickstart
+            self._addonStates = {}
+
         if not self._kickstarted:
             # having done all the slow downloading, we need to do the first refresh
             # of the UI here so there's an environment selected by default.  This
