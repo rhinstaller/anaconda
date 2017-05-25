rhinstaller/anaconda

commit 62e95b9b070be62921cce4809de0dd4cb048ad31

Make it possible to use a custom getpass()
Make it possible to use a custom getpass() implementation.
The getpass() function provided by the Python standard library
is hardcoded to always expect input from /dev/tty.
This is unfortunately not enough for the Initial Setup multi-tty
usecase, where password input has to be supported from arbitrary
consoles. So make it possible to use a custom getpass() implementation
that does not have this limitation and can run on an arbitrary console.

pyanaconda/ui/tui/simpleline/base.py
@@ -87,6 +87,7 @@ class App(object):
         self._width = width
         self.quit_question = yes_or_no_question
         self.quit_message = quit_message or N_(u"Do you really want to quit?")
+        self._custom_getpass = None
 
         # async control queue
         if queue_instance:
@@ -128,6 +129,17 @@ class App(object):
             self._handlers[event] = []
         self._handlers[event].append((callback, data))
 
+    @property
+    def simpleline_getpass(self):
+        if self._custom_getpass is None:
+            return getpass.getpass
+        else:
+            return self._custom_getpass
+
+    @simpleline_getpass.setter
+    def simpleline_getpass(self, new_getpass):
+        self._custom_getpass = new_getpass
+
     def _thread_input(self, queue_instance, prompt, hidden):
         """This method is responsible for interruptible user input.
 
@@ -145,7 +157,7 @@ class App(object):
         """
 
         if hidden:
-            data = getpass.getpass(prompt)
+            data = self.simpleline_getpass(prompt)
         else:
             widget = TextWidget(str(prompt))
             widget.render(self.width)

pyanaconda/ui/tui/spokes/askvnc.py
@@ -126,6 +126,7 @@ class VNCPassSpoke(NormalTUISpoke):
         else:
             self._message = _("Please provide VNC password (must be six to eight characters long).\n"
                               "You will have to type it twice. Leave blank for no password")
+        self._app = app
 
     @property
     def indirect(self):
@@ -143,8 +144,8 @@ class VNCPassSpoke(NormalTUISpoke):
 
     def prompt(self, args=None):
         """Override prompt as password typing is special."""
-        p1 = getpass.getpass(_("Password: "))
-        p2 = getpass.getpass(_("Password (confirm): "))
+        p1 = self._app.simpleline_getpass(_("Password: "))
+        p2 = self._app.simpleline_getpass(_("Password (confirm): "))
 
         if p1 != p2:
             print(_("Passwords do not match!"))

commit e6247f042182828a18a46a68c55288f45eb33f3f

Fix pylint errors
Pylint errors introduced by commit:
62e95b9b070be62921cce4809de0dd4cb048ad31

pyanaconda/ui/tui/simpleline/base.py
@@ -157,7 +157,7 @@ class App(object):
         """
 
         if hidden:
-            data = self.simpleline_getpass(prompt)
+            data = self.simpleline_getpass = prompt
         else:
             widget = TextWidget(str(prompt))
             widget.render(self.width)

pyanaconda/ui/tui/spokes/askvnc.py
@@ -27,7 +27,6 @@ from pyanaconda.ui.communication import hubQ
 from pyanaconda.ui.tui import exception_msg_handler
 from pyanaconda.iutil import execWithRedirect, ipmi_abort
 from pyanaconda.flags import can_touch_runtime_system
-import getpass
 import sys
 
 def exception_msg_handler_and_exit(event, data):