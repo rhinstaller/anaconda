7f7f05a702fee8e75b66a2e566a7e76304f468d7
Dusty Mabe <dusty@dustymabe.com>
2017-07-14 09:51:21 -0400

    rpmostreepayload: try to verify local ostree repo cache
    This is a crude way of trying to make sure the repo is actually
    an ostree repo before we pass it as a local cache to ostree.
    
    Filed an RFE to have this better handled in ostree upstream:
    https://github.com/ostreedev/ostree/issues/1005

--- pyanaconda/payload/rpmostreepayload.py
+++ pyanaconda/payload/rpmostreepayload.py
@@ -177,7 +177,7 @@ class RPMOSTreePayload(ArchivePayload):
         # and the second by <https://github.com/projectatomic/rpm-ostree-toolbox/>
         if OSTree.check_version(2017, 8):
             for path in ['/ostree/repo', '/install/ostree/repo']:
-                if os.path.isdir(path):
+                if os.path.isdir(path + '/objects'):
                     pull_opts['localcache-repos'] = GLib.Variant('as', [path])
                     break
 
