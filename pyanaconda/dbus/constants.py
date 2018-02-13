f59633eae4a3be2eb782f3138d514328d0a0559e
Vendula Poncova <vponcova@redhat.com>
2018-02-13 15:55:47 +0100

    Remove constants with DBus names and paths
    The constants with DBus names and paths from pyanaconda.dbus.constants
    can be removed, because they were replaced with objects from
    pyanaconda.dbus.objects.

--- pyanaconda/dbus/constants.py
+++ pyanaconda/dbus/constants.py
@@ -1,5 +1,5 @@
 #
-# Anaconda DBUS constants
+# DBus constants.
 #
 # Copyright (C) 2017  Red Hat, Inc.  All rights reserved.
 #
@@ -17,65 +17,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from pydbus.auto_names import auto_object_path
-
-# main Anaconda DBUS namespace
-ANACONDA_DBUS_NAMESPACE = "org.fedoraproject.Anaconda"
-
-# DBUS namespace for modules
-DBUS_MODULE_NAMESPACE = "{}.Modules".format(ANACONDA_DBUS_NAMESPACE)
-
-# DBUS namespace for addons
-DBUS_ADDON_NAMESPACE = "{}.Addons".format(ANACONDA_DBUS_NAMESPACE)
-
-# BOSS
-DBUS_BOSS_NAME = "{}.Boss".format(ANACONDA_DBUS_NAMESPACE)
-DBUS_BOSS_PATH = auto_object_path(DBUS_BOSS_NAME)
-
-DBUS_BOSS_INSTALLATION_NAME = "{}.Installation".format(DBUS_BOSS_NAME)
-DBUS_BOSS_INSTALLATION_PATH = auto_object_path(DBUS_BOSS_INSTALLATION_NAME)
-
-# Temporary interface for anaconda
-DBUS_BOSS_ANACONDA_NAME = "{}.Anaconda".format(DBUS_BOSS_NAME)
-
-# Anaconda DBUS modules
-MODULE_FOO_NAME = "{}.Foo".format(DBUS_MODULE_NAMESPACE)
-MODULE_FOO_PATH = auto_object_path(MODULE_FOO_NAME)
-
-MODULE_BAR_NAME = "{}.Bar".format(DBUS_MODULE_NAMESPACE)
-MODULE_BAR_PATH = auto_object_path(MODULE_BAR_NAME)
-
-MODULE_TIMEZONE_NAME = "{}.Timezone".format(DBUS_MODULE_NAMESPACE)
-MODULE_TIMEZONE_PATH = auto_object_path(MODULE_TIMEZONE_NAME)
-
-# Addons (likely for testing purposes only)
-ADDON_BAZ_NAME = "{}.Baz".format(DBUS_ADDON_NAMESPACE)
-ADDON_BAZ_PATH = auto_object_path(ADDON_BAZ_NAME)
-
-# list of all expected Anaconda services
-ANACONDA_SERVICES = [MODULE_FOO_NAME,
-                     MODULE_BAR_NAME,
-                     MODULE_TIMEZONE_NAME]
-
-# Task interface name
-DBUS_TASK_NAME = "{}.Task".format(ANACONDA_DBUS_NAMESPACE)
-
-# list of all expected Anaconda DBUS modules
-ANACONDA_MODULES = [(MODULE_FOO_NAME, MODULE_FOO_PATH),
-                    (MODULE_BAR_NAME, MODULE_BAR_PATH),
-                    (MODULE_TIMEZONE_NAME, MODULE_TIMEZONE_PATH)]
+# no flags are set
+DBUS_FLAG_NONE = 0
 
 # status codes
 DBUS_START_REPLY_SUCCESS = 1
-
-# no flags are set
-DBUS_FLAG_NONE = 0
 
 # NOTE: There is no DBUS_START_REPLY_FAILURE or something similar,
 #       as there is a separate field for error reporting.
 #       For more information see the DBUS docs:
 #       https://dbus.freedesktop.org/doc/dbus-specification.html#bus-messages-start-service-by-name
-
-
-
-
