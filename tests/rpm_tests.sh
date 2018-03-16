087f8b52c92a7e23d927a44915baabb562f29914
Jiri Konecny <jkonecny@redhat.com>
2018-03-16 17:01:42 +0100

    Support running just chosen rpm test

--- tests/rpm_tests.sh
+++ tests/rpm_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-set -- "${top_srcdir}"/tests/rpm_tests
+# If no tests were selected by user or makefile, select all of them
+if [ $# -eq 0 ] && [ -z $RPM_TESTS_ARGS ]; then
+    set -- "${top_srcdir}"/tests/rpm_tests
+fi
 
-exec nosetests-3 -v --exclude=logpicker -a \!acceptance,\!slow "$@"
+exec nosetests-3 -v --exclude=logpicker -a \!acceptance,\!slow $RPM_TESTS_ARGS "$@"
