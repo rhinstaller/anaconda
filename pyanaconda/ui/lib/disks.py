993cbcf6e2e496b339f76d6326fc2ab73a6272f2
Vojtech Trefny <vtrefny@redhat.com>
2018-03-21 12:37:52 +0100

    Fix 'isDisk' property name (#1558906)
    It's 'is_disk' since Blivet 2.0

--- pyanaconda/ui/lib/disks.py
+++ pyanaconda/ui/lib/disks.py
@@ -90,7 +90,7 @@ def applyDiskSelection(storage, data, use_names):
     for disk in (d for d in storage.disks if d.name in onlyuse):
         onlyuse.extend(d.name for d in disk.ancestors
                        if d.name not in onlyuse
-                       and d.isDisk)
+                       and d.is_disk)
 
     data.ignoredisk.onlyuse = onlyuse
     data.clearpart.drives = use_names[:]
