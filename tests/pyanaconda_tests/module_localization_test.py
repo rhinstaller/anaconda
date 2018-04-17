rhinstaller/anaconda

commit 99849ad3c44537ea071b5ae0bbcd78c39c375f3a

localization module: don't use Kickstarted so another command can be added

anaconda.py
@@ -584,13 +584,9 @@ if __name__ == "__main__":
     # If the language was set on the command line, copy that to kickstart
     if opts.lang:
         localization_proxy.SetLanguage(opts.lang)
-        localization_proxy.SetKickstarted(True)
 
     # Setup the locale environment
-    if localization_proxy.Kickstarted:
-        locale_option = localization_proxy.Language
-    else:
-        locale_option = None
+    locale_option = localization_proxy.Language or None
     localization.setup_locale_environment(locale_option, text_mode=anaconda.tui_mode)
 
     # Now that LANG is set, do something with it

pyanaconda/ui/gui/spokes/welcome.py
@@ -115,8 +115,8 @@ class WelcomeLanguageSpoke(LangLocaleHandler, StandaloneSpoke):
         if flags.flags.singlelang:
             return True
 
-        if flags.flags.automatedInstall and self._l12_module.proxy.Kickstarted:
-            return bool(self._l12_module.proxy.Language)
+        if flags.flags.automatedInstall and self._l12_module.proxy.Language:
+            return True
         else:
             return False
 
@@ -154,7 +154,7 @@ class WelcomeLanguageSpoke(LangLocaleHandler, StandaloneSpoke):
 
         # bootopts and kickstart have priority over geoip
         language = self._l12_module.proxy.Language
-        if language and self._l12_module.proxy.Kickstarted:
+        if language:
             locales = [language]
         else:
             locales = localization.get_territory_locales(territory) or [DEFAULT_LANG]

commit 3f418054cc736b42d75b0eb706de0a7ea9242e33

localization: use LanguageKickstarted module property (#1568119)
Fixes regression caused by
commit 99849ad3c44537ea071b5ae0bbcd78c39c375f3a

anaconda.py
@@ -593,9 +593,13 @@ if __name__ == "__main__":
     # If the language was set on the command line, copy that to kickstart
     if opts.lang:
         localization_proxy.SetLanguage(opts.lang)
+        localization_proxy.SetLanguageKickstarted(True)
 
     # Setup the locale environment
-    locale_option = localization_proxy.Language or None
+    if localization_proxy.LanguageKickstarted:
+        locale_option = localization_proxy.Language
+    else:
+        locale_option = None
     localization.setup_locale_environment(locale_option, text_mode=anaconda.tui_mode)
 
     # Now that LANG is set, do something with it

pyanaconda/modules/localization/localization_interface.py
@@ -83,6 +83,14 @@ class LocalizationInterface(KickstartModuleInterface):
         """
         return self.implementation.language_seen
 
+    @emits_properties_changed
+    def SetLanguageKickstarted(self, language_seen: Bool):
+        """Set if language should be considered as coming from kickstart
+
+        :param bool language_seen: if language should be considered as coming from kickstart
+        """
+        self.implementation.set_language_seen(language_seen)
+
     # TODO MOD - remove this when we get logic for inferring what we are
     # getting and the other option value (localed proxy) into the module?
     @property

pyanaconda/ui/gui/spokes/welcome.py
@@ -111,10 +111,8 @@ class WelcomeLanguageSpoke(LangLocaleHandler, StandaloneSpoke):
         if flags.flags.singlelang:
             return True
 
-        if flags.flags.automatedInstall and self._l12_module.proxy.Language:
-            return True
-        else:
-            return False
+        if flags.flags.automatedInstall and self._l12_module.proxy.LanguageKickstarted:
+            return bool(self._l12_module.proxy.Language)
 
     def _row_is_separator(self, model, itr, *args):
         return model[itr][3]
@@ -150,7 +148,7 @@ class WelcomeLanguageSpoke(LangLocaleHandler, StandaloneSpoke):
 
         # bootopts and kickstart have priority over geoip
         language = self._l12_module.proxy.Language
-        if language:
+        if language and self._l12_module.proxy.LanguageKickstarted:
             locales = [language]
         else:
             locales = localization.get_territory_locales(territory) or [DEFAULT_LANG]

tests/pyanaconda_tests/module_localization_test.py
@@ -117,6 +117,12 @@ class LocalizationInterfaceTestCase(unittest.TestCase):
         self.localization_interface.ReadKickstart(ks_in)
         self.assertEqual(self.localization_interface.LanguageKickstarted, True)
 
+    def set_language_kickstarted_test(self):
+        """Test SetLanguageKickstart."""
+        self.localization_interface.SetLanguageKickstarted(True)
+        self.assertEqual(self.localization_interface.LanguageKickstarted, True)
+        self.callback.assert_called_once_with(LOCALIZATION.interface_name, {'LanguageKickstarted': True}, [])
+
     def _test_kickstart(self, ks_in, ks_out):
         check_kickstart_interface(self, self.localization_interface, ks_in, ks_out)
 