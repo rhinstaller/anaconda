30de9c3dff878ab574921acb34e5629f0e5239c4
Vendula Poncova <vponcova@redhat.com>
2018-06-14 16:06:18 +0200

    Use the zFCP discovery task in UI
    The UI should run a remote task to discover zFCP devices.

--- pyanaconda/ui/gui/spokes/advstorage/zfcp.py
+++ pyanaconda/ui/gui/spokes/advstorage/zfcp.py
@@ -16,22 +16,15 @@
 # License and may only be used or replicated with the express permission of
 # Red Hat, Inc.
 #
-
-import gi
-gi.require_version("BlockDev", "2.0")
-
-from gi.repository import BlockDev as blockdev
-
-from blivet import zfcp
-from pyanaconda.ui.gui import GUIObject
-from pyanaconda.core.async_utils import async_action_nowait
-from pyanaconda.core.timer import Timer
+from pyanaconda.modules.common.constants.objects import ZFCP
+from pyanaconda.modules.common.constants.services import STORAGE
+from pyanaconda.modules.common.errors.configuration import StorageDiscoveryError
+from pyanaconda.modules.common.task import async_run_task
 from pyanaconda.storage_utils import try_populate_devicetree
-from pyanaconda.core.regexes import DASD_DEVICE_NUMBER, ZFCP_WWPN_NUMBER, ZFCP_LUN_NUMBER
-from pyanaconda.threading import threadMgr, AnacondaThread
-from pyanaconda.core import constants
+from pyanaconda.ui.gui import GUIObject
 
 __all__ = ["ZFCPDialog"]
+
 
 class ZFCPDialog(GUIObject):
     """ Gtk dialog which allows users to manually add zFCP devices without
@@ -46,27 +39,21 @@ class ZFCPDialog(GUIObject):
 
     def __init__(self, data, storage):
         super().__init__(data)
-        self.storage = storage
-        self.zfcp = zfcp.zFCP()
-
-        self._discoveryError = None
-
+        self._storage = storage
         self._update_devicetree = False
+        self._zfcp_proxy = STORAGE.get_proxy(ZFCP)
 
         # grab all of the ui objects
         self._configureGrid = self.builder.get_object("configureGrid")
         self._conditionNotebook = self.builder.get_object("conditionNotebook")
-
         self._startButton = self.builder.get_object("startButton")
         self._okButton = self.builder.get_object("okButton")
         self._cancelButton = self.builder.get_object("cancelButton")
         self._retryButton = self.builder.get_object("retryButton")
         self._errorLabel = self.builder.get_object("deviceErrorLabel")
-
         self._deviceEntry = self.builder.get_object("deviceEntry")
         self._wwpnEntry = self.builder.get_object("wwpnEntry")
         self._lunEntry = self.builder.get_object("lunEntry")
-
         self._spinner = self.builder.get_object("waitSpinner")
 
     def refresh(self):
@@ -80,7 +67,7 @@ class ZFCPDialog(GUIObject):
         # We need to call this to get the device nodes to show up
         # in our devicetree.
         if self._update_devicetree:
-            try_populate_devicetree(self.storage.devicetree)
+            try_populate_devicetree(self._storage.devicetree)
         return rc
 
     def _set_configure_sensitive(self, sensitivity):
@@ -100,93 +87,41 @@ class ZFCPDialog(GUIObject):
         self._set_configure_sensitive(False)
         self._conditionNotebook.set_current_page(1)
 
-        # Initialize.
-        config_error = None
-        device = None
-        wwpn = None
-        lun = None
-
         # Get the input.
         device_name = self._deviceEntry.get_text().strip()
         wwpn_name = self._wwpnEntry.get_text().strip()
         lun_name = self._lunEntry.get_text().strip()
 
-        # Check the input.
-        if not DASD_DEVICE_NUMBER.match(device_name):
-            config_error = "Incorrect format of the given device number."
-        elif not ZFCP_WWPN_NUMBER.match(wwpn_name):
-            config_error = "Incorrect format of the given WWPN number."
-        elif not ZFCP_LUN_NUMBER.match(lun_name):
-            config_error = "Incorrect format of the given LUN number."
-        else:
-            try:
-                # Get the full ids.
-                device = blockdev.s390.sanitize_dev_input(device_name)
-                wwpn = blockdev.s390.zfcp_sanitize_wwpn_input(wwpn_name)
-                lun = blockdev.s390.zfcp_sanitize_lun_input(lun_name)
-            except (blockdev.S390Error, ValueError) as err:
-                config_error = str(err)
+        # Get the discovery task.
+        task_path = self._zfcp_proxy.DiscoverWithTask(device_name, wwpn_name, lun_name)
+        task_proxy = STORAGE.get_proxy(task_path)
 
-        # Process the configuration error.
-        if config_error:
-            self._errorLabel.set_text(config_error)
-            self._conditionNotebook.set_current_page(2)
-            self._set_configure_sensitive(True)
-            self._cancelButton.set_sensitive(True)
         # Start the discovery.
-        else:
-            # Discover.
-            self._spinner.start()
-            threadMgr.add(AnacondaThread(name=constants.THREAD_ZFCP_DISCOVER,
-                                         target=self._discover,
-                                         args=(device, wwpn, lun)))
+        async_run_task(task_proxy, self.process_result)
+        self._spinner.start()
 
-            # Periodically call the check till it is done.
-            Timer().timeout_msec(250, self._check_discover)
+    def process_result(self, task_proxy):
+        """Process the result of the task.
 
-    @async_action_nowait
-    def _check_discover(self, *args):
-        """ After the zFCP discover thread runs, check to see whether a valid
-            device was discovered. Display an error message if not.
-
-            If the discover is not done, return True to indicate that the check
-            has to be run again, otherwise check the discovery and return False.
+        :param task_proxy: a task proxy
         """
-        # Discovery is not done, return True.
-        if threadMgr.get(constants.THREAD_ZFCP_DISCOVER):
-            return True
-
-        # Discovery has finished, run the check.
+        # Stop the spinner.
         self._spinner.stop()
-
-        if self._discoveryError:
-            # Failure, display a message and leave the user on the dialog so
-            # they can try again (or cancel)
-            self._errorLabel.set_text(self._discoveryError)
-            self._discoveryError = None
-
-            self._conditionNotebook.set_current_page(2)
-            self._set_configure_sensitive(True)
-        else:
-            # Great success. Just return to the advanced storage window and let the
-            # UI update with the newly-added device
-            self.window.response(1)
-            return False
-
         self._cancelButton.set_sensitive(True)
-        # Discovery and the check have finished, return False.
-        return False
 
-    def _discover(self, *args):
-        """ Given the configuration options from a user, attempt to discover
-            a zFCP device. This includes searching black-listed devices.
-        """
-        # attempt to add the device
         try:
-            self.zfcp.add_fcp(args[0], args[1], args[2])
+            # Finish the task
+            task_proxy.Finish()
+        except StorageDiscoveryError as e:
+            # Discovery has failed, show the error.
+            self._errorLabel.set_text(str(e))
+            self._set_configure_sensitive(True)
+            self._conditionNotebook.set_current_page(2)
+        else:
+            # Discovery succeeded.
             self._update_devicetree = True
-        except ValueError as e:
-            self._discoveryError = str(e)
+            self._okButton.set_sensitive(True)
+            self._conditionNotebook.set_current_page(3)
 
     def on_entry_activated(self, entry, user_data=None):
         # When an entry is activated, press the discover or retry button
