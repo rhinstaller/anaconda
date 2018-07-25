d01dd17c1406307307b0b37c6c06df4b796d84a2
Jiri Konecny <jkonecny@redhat.com>
2018-07-25 12:17:43 +0200

    Add BaseOS between default base repositories
    Related: rhbz#1610806

--- pyanaconda/payload/__init__.py
+++ pyanaconda/payload/__init__.py
@@ -1010,7 +1010,10 @@ class ArchivePayload(ImagePayload):
 class PackagePayload(Payload):
     """A PackagePayload installs a set of packages onto the target system."""
 
-    DEFAULT_REPOS = [productName.split('-')[0].lower(), "fedora-modular-server", "rawhide"]  # pylint: disable=no-member
+    DEFAULT_REPOS = [productName.split('-')[0].lower(),
+                     "fedora-modular-server",
+                     "rawhide",
+                     "BaseOS"]  # pylint: disable=no-member
 
     def __init__(self, data):
         if self.__class__ is PackagePayload:
