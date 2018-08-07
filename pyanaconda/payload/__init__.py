6961ae9b21951e076aed961ac950160e605a4000
Jiri Konecny <jkonecny@redhat.com>
2018-08-07 11:47:30 +0200

    Separate _find_and_mount_iso from _setup_media
    The _find_and_mount_iso can be used in a more general way. This prepares
    ground for the inst.addrepo hard drive support.
    
    Related: rhbz#1595415

--- pyanaconda/payload/__init__.py
+++ pyanaconda/payload/__init__.py
@@ -1166,43 +1166,54 @@ class PackagePayload(Payload):
     def _setupMedia(self, device):
         method = self.data.method
         if method.method == "harddrive":
-            self._setupDevice(device, mountpoint=ISO_DIR)
-
-            # check for ISO images in the newly mounted dir
-            path = ISO_DIR
-            if method.dir:
-                path = os.path.normpath("%s/%s" % (path, method.dir))
-
-            # XXX it would be nice to streamline this when we're just setting
-            #     things back up after storage activation instead of having to
-            #     pretend we don't already know which ISO image we're going to
-            #     use
-            image = findFirstIsoImage(path)
-            if not image:
-                device.teardown(recursive=True)
-                raise PayloadSetupError("failed to find valid iso image")
-
-            if path.endswith(".iso"):
-                path = os.path.dirname(path)
-
-            # this could already be set up the first time through
-            if not os.path.ismount(INSTALL_TREE):
-                # mount the ISO on a loop
-                image = os.path.normpath("%s/%s" % (path, image))
-                mountImage(image, INSTALL_TREE)
-
-            if not method.dir.endswith(".iso"):
-                method.dir = os.path.normpath("%s/%s" % (method.dir,
-                                                         os.path.basename(image)))
-                while method.dir.startswith("/"):
-                    # riduculous
-                    method.dir = method.dir[1:]
+            method.dir = self._find_and_mount_iso(device, ISO_DIR, method.dir, INSTALL_TREE)
         # Check to see if the device is already mounted, in which case
         # we don't need to mount it again
         elif method.method == "cdrom" and blivet.util.get_mount_paths(device.path):
             return
         else:
             device.format.setup(mountpoint=INSTALL_TREE)
+
+    def _find_and_mount_iso(self, device, device_mount_dir, iso_path, iso_mount_dir):
+        """Find and mount installation source from ISO on device.
+
+        Return changed path to the iso to save looking for iso in the future call.
+        """
+        self._setupDevice(device, mountpoint=device_mount_dir)
+
+        # check for ISO images in the newly mounted dir
+        path = device_mount_dir
+        if iso_path:
+            path = os.path.normpath("%s/%s" % (path, iso_path))
+
+        # XXX it would be nice to streamline this when we're just setting
+        #     things back up after storage activation instead of having to
+        #     pretend we don't already know which ISO image we're going to
+        #     use
+        image = findFirstIsoImage(path)
+        if not image:
+            device.teardown(recursive=True)
+            raise PayloadSetupError("failed to find valid iso image")
+
+        if path.endswith(".iso"):
+            path = os.path.dirname(path)
+
+        # this could already be set up the first time through
+        if not os.path.ismount(iso_mount_dir):
+            # mount the ISO on a loop
+            image = os.path.normpath("%s/%s" % (path, image))
+            mountImage(image, iso_mount_dir)
+
+        if not iso_path.endswith(".iso"):
+            result_path = os.path.normpath("%s/%s" % (iso_path,
+                                                      os.path.basename(image)))
+            while result_path.startswith("/"):
+                # riduculous
+                result_path = result_path[1:]
+
+            return result_path
+
+        return iso_path
 
     def _setupInstallDevice(self, storage, checkmount):
         # XXX FIXME: does this need to handle whatever was set up by dracut?
