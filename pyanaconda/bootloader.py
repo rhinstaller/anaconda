461ad1570e337dd8d0ea7726a3dea48dd864f064
Radek Vykydal <rvykydal@redhat.com>
2018-09-17 15:15:05 +0200

    prefixdevname: pass net.ifnames.prefix option to installed system
    Resolves: rhbz#1615997

--- pyanaconda/bootloader.py
+++ pyanaconda/bootloader.py
@@ -233,7 +233,7 @@ class BootLoader(object):
                             "noht", "acpi", "video", "pci", "nodmraid",
                             "nompath", "nomodeset", "noiswmd", "fips",
                             "selinux", "biosdevname", "ipv6.disable",
-                            "net.ifnames"]
+                            "net.ifnames", "net.ifnames.prefix"]
     preserve_args = []
 
     _trusted_boot = False
