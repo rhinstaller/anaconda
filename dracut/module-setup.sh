57cfb5cb200c596519cef605986c40f600604de9
Radek Vykydal <rvykydal@redhat.com>
2018-10-04 10:56:49 +0200

    Add option to set kernel.hung_task_timeout_secs option
    Adds installer boot option
    
    inst.kernel.hung_task_timeout_secs
    
    which sets
    
    kernel.hung_task_timoeut_secs sysctl option
    
    Used for VM guests in kickstart tests.
    
    Resolves: rhbz#1633549

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -46,6 +46,7 @@ install() {
     inst_hook pre-pivot 50 "$moddir/anaconda-copy-s390ccwconf.sh"
     inst_hook pre-pivot 90 "$moddir/anaconda-copy-dhclient.sh"
     inst_hook pre-pivot 91 "$moddir/anaconda-copy-prefixdevname.sh"
+    inst_hook pre-pivot 95 "$moddir/anaconda-set-kernel-hung-timeout.sh"
     inst_hook pre-pivot 99 "$moddir/save-initramfs.sh"
     inst_hook pre-shutdown 50 "$moddir/anaconda-pre-shutdown.sh"
     # kickstart parsing, WOOOO
