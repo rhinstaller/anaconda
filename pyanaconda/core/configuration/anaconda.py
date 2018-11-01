23f1029b8170a4399df7c430313106afb8324676
Vendula Poncova <vponcova@redhat.com>
2018-11-01 14:25:45 +0100

    Configure the installation system
    For now, the installation system can be specified in the Anaconda
    configuration file. The supported types of the system are BOOT_ISO,
    LIVE_OS and UNKNOWN. The type will determine what the installer
    is allowed to do with the current system.
    
    This is a temporary solution. The type will be replaced with a list
    of rules. The rules will be modified by configuration snippets, that
    will be installed by packages specific for each type of the system.

--- pyanaconda/core/configuration/anaconda.py
+++ pyanaconda/core/configuration/anaconda.py
@@ -72,6 +72,40 @@ class AnacondaSection(Section):
         return self._get_option("kickstart_modules").split()
 
 
+class SystemType(Enum):
+    """The type of the installation system."""
+    BOOT_ISO = "BOOT_ISO"
+    LIVE_OS = "LIVE_OS"
+    UNKNOWN = "UNKNOWN"
+
+
+class InstallationSystem(Section):
+    """The Installation System section."""
+
+    @property
+    def _type(self):
+        """Type of the installation system.
+
+        FIXME: This is a temporary solution.
+        """
+        return self._get_option("type", SystemType)
+
+    @property
+    def _is_boot_iso(self):
+        """Are we running in the boot.iso?"""
+        return self._type is SystemType.BOOT_ISO
+
+    @property
+    def _is_live_os(self):
+        """Are we running in the live OS?"""
+        return self._type is SystemType.LIVE_OS
+
+    @property
+    def _is_unknown(self):
+        """Are we running in the unknown OS?"""
+        return self._type is SystemType.UNKNOWN
+
+
 class ServicesSection(Section):
     """The Services section."""
 
@@ -195,6 +229,7 @@ class AnacondaConfiguration(object):
         self._parser = create_parser()
 
         self._anaconda = AnacondaSection("Anaconda", self.get_parser())
+        self._system = InstallationSystem("Installation System", self.get_parser())
         self._target = InstallationTarget("Installation Target", self.get_parser())
         self._storage = StorageSection("Storage", self.get_parser())
         self._services = ServicesSection("Services", self.get_parser())
@@ -203,6 +238,11 @@ class AnacondaConfiguration(object):
     def anaconda(self):
         """The Anaconda section."""
         return self._anaconda
+
+    @property
+    def system(self):
+        """The Installation System section."""
+        return self._system
 
     @property
     def target(self):
@@ -291,6 +331,15 @@ class AnacondaConfiguration(object):
         self.storage._set_option("gpt", opts.gpt)
         self.storage._set_option("multipath_friendly_names", opts.multipath_friendly_names)
 
+        # Set the type of the installation system.
+        if opts.liveinst:
+            self.system._set_option("type", SystemType.LIVE_OS.value)
+        elif opts.images or opts.dirinstall:
+            self.system._set_option("type", SystemType.UNKNOWN.value)
+        else:
+            self.system._set_option("type", SystemType.BOOT_ISO.value)
+
+        # Set the type of the installation target.
         if opts.images:
             # The image installation is requested.
             self.target._set_option("type", TargetType.IMAGE.value)
