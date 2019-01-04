dba01875b9c4218cfb0f43bbbe182f425b5c8a07
Vendula Poncova <vponcova@redhat.com>
2019-01-04 18:20:07 +0100

    Remove the unused attribute orig_fstab
    The attribute is never used.

--- pyanaconda/storage/osinstall.py
+++ pyanaconda/storage/osinstall.py
@@ -390,7 +390,6 @@ class FSSet(object):
         self.devicetree = devicetree
         self.crypt_tab = None
         self.blkid_tab = None
-        self.orig_fstab = None
         self.active = False
         self._dev = None
         self._devpts = None
@@ -619,9 +618,6 @@ class FSSet(object):
             log.debug("parsing %s", path)
 
             lines = f.readlines()
-
-            # save the original file
-            self.orig_fstab = ''.join(lines)
 
             for line in lines:
 
