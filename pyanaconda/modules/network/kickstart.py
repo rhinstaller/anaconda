569b08ebf66cce7f7ac12ea6b6f393a7fd01460e
Radek Vykydal <rvykydal@redhat.com>
2019-02-04 17:21:47 +0100

    network module: update docstrings and style of kickstart.py

--- pyanaconda/modules/network/kickstart.py
+++ pyanaconda/modules/network/kickstart.py
@@ -21,6 +21,7 @@ from pyanaconda.core.kickstart import VERSION, KickstartSpecification, commands 
 
 DEFAULT_DEVICE_SPECIFICATION = "link"
 
+
 class Network(COMMANDS.Network):
     def parse(self, args):
         hostname_only_command = is_hostname_only_network_args(args)
@@ -49,6 +50,7 @@ class NetworkKickstartSpecification(KickstartSpecification):
 
 # TODO force moving hostname data into separate line?
 def update_network_hostname_data(network_data_list, hostname_data):
+    """Apply hostname value to kickstart network data."""
     hostname_found = False
     for nd in network_data_list:
         if nd.hostname:
@@ -57,7 +59,9 @@ def update_network_hostname_data(network_data_list, hostname_data):
     if not hostname_found:
         network_data_list.append(hostname_data)
 
+
 def update_network_data_with_default_device(network_data_list, device_specification):
+    """Apply default --device value to kickstart network data."""
     updated = False
     for nd in network_data_list:
         if not nd.device and not is_hostname_only_network_data(nd):
@@ -65,7 +69,15 @@ def update_network_data_with_default_device(network_data_list, device_specificat
             updated = True
     return updated
 
+
 def update_first_network_command_activate_value(network_data_list):
+    """Applies the historical default to the first network command.
+
+    For the first network command the device is activated by default.  To
+    override it --no-activate option has to be used. For following network
+    commands the devices are not activated by default.
+
+    """
     if network_data_list:
         nd = network_data_list[0]
         if not is_hostname_only_network_data(nd):
@@ -74,12 +86,15 @@ def update_first_network_command_activate_value(network_data_list):
                 return True
     return False
 
+
 def is_hostname_only_network_args(args):
     return (len(args) == 1 and args[0].startswith("--hostname") or
             len(args) == 2 and "--hostname" in args)
 
+
 def is_hostname_only_network_data(network_data):
     return network_data.bootProto == ""
+
 
 def default_ks_vlan_interface_name(parent, vlanid):
     return "%s.%s" % (parent, vlanid)
