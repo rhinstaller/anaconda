rhinstaller/anaconda

commit 70cf5a4c485c576323c52a8f6388ff32ec62e73d

payload: remove ImagePayload abstract class
This abstract class didn't implemented anything so there was no reason
to have it.

pyanaconda/payload/livepayload.py
@@ -39,7 +39,7 @@ from pyanaconda.core.configuration.anaconda import conf
 from pyanaconda.core.util import ProxyString, ProxyStringError
 from pyanaconda.core import util
 from pyanaconda.core.i18n import _
-from pyanaconda.payload import ImagePayload, versionCmp
+from pyanaconda.payload import Payload, versionCmp
 from pyanaconda.payload.errors import PayloadSetupError, PayloadInstallError
 from pyanaconda.threading import threadMgr, AnacondaThread
 from pyanaconda.errors import errorHandler, ERROR_RAISE
@@ -55,7 +55,7 @@ from pyanaconda.anaconda_loggers import get_packaging_logger
 log = get_packaging_logger()
 
 
-class LiveImagePayload(ImagePayload):
+class LiveImagePayload(Payload):
     """ A LivePayload copies the source image onto the target system. """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -327,7 +327,7 @@ class LiveImageKSPayload(LiveImagePayload):
         """ Check the availability and size of the image.
         """
         # This is on purpose, we don't want to call LiveImagePayload's setup method.
-        ImagePayload.setup(self, storage)
+        super().setup(self, storage)
 
         if self.data.method.url.startswith("file://"):
             error = self._setup_file_image()
@@ -343,7 +343,7 @@ class LiveImageKSPayload(LiveImagePayload):
 
     def unsetup(self):
         # Skip LiveImagePayload's unsetup method
-        ImagePayload.unsetup(self)
+        super().unsetup(self)
 
     def _preInstall_url_image(self):
         """ Download the image using Requests with progress reporting"""

pyanaconda/payload/__init__.py
@@ -686,18 +686,6 @@ class Payload(object):
             log.info("Some of the requirements were not applied.")
 
 
-# Inherit abstract methods from Payload
-# pylint: disable=abstract-method
-class ImagePayload(Payload):
-    """An ImagePayload installs an OS image to the target system."""
-
-    def __init__(self, data):
-        if self.__class__ is ImagePayload:
-            raise TypeError("ImagePayload is an abstract class")
-
-        super().__init__(data)
-
-
 class PackagePayload(Payload):
     """A PackagePayload installs a set of packages onto the target system."""
 

commit 62b631ed9602cda6118c838dcb9d7b13fc41154b

Fix live payload error introduced by clean-up (#1685258)
It was introduced by 70cf5a4c485c576323c52a8f6388ff32ec62e73d commit.

Resolves: rhbz#1685258

Reported-by: Brian Lane <bcl@redhat.com>

pyanaconda/payload/livepayload.py
@@ -328,7 +328,8 @@ class LiveImageKSPayload(LiveImagePayload):
         """ Check the availability and size of the image.
         """
         # This is on purpose, we don't want to call LiveImagePayload's setup method.
-        super().setup(self, storage)
+        # FIXME: this should be solved on a inheritance level not like this
+        Payload.setup(self, storage)
 
         if self.data.method.url.startswith("file://"):
             error = self._setup_file_image()
@@ -344,7 +345,8 @@ class LiveImageKSPayload(LiveImagePayload):
 
     def unsetup(self):
         # Skip LiveImagePayload's unsetup method
-        super().unsetup(self)
+        # FIXME: this should be solved on a inheritance level not like this
+        Payload.unsetup(self)
 
     def _pre_install_url_image(self):
         """ Download the image using Requests with progress reporting"""