1c45e7b3af8de8934e25b69b5f86cdbff6c96ffb
Vendula Poncova <vponcova@redhat.com>
2019-04-05 13:53:27 +0200

    Handle bytes and strings from RPM (#1693766)
    RPM can return bytes or strings, so let's use the function
    decode_bytes that can handle both and always returns a string.
    
    Resolves: rhbz#1693766

--- pyanaconda/payload/__init__.py
+++ pyanaconda/payload/__init__.py
@@ -36,7 +36,7 @@ from pyanaconda import isys
 from pyanaconda.image import findFirstIsoImage
 from pyanaconda.image import mountImage
 from pyanaconda.image import opticalInstallMedia, verifyMedia, verify_valid_installtree
-from pyanaconda.core.util import ProxyString, ProxyStringError
+from pyanaconda.core.util import ProxyString, ProxyStringError, decode_bytes
 from pyanaconda.core.regexes import VERSION_DIGITS
 from pyanaconda.payload.errors import PayloadError, PayloadSetupError, NoSuchGroup
 from pyanaconda.payload import utils as payload_utils
@@ -726,7 +726,7 @@ class PackagePayload(Payload, metaclass=ABCMeta):
         ts = rpm.TransactionSet(util.getSysroot())
         mi = ts.dbMatch('providename', 'kernel')
         for hdr in mi:
-            unicode_fnames = (f.decode("utf-8") for f in hdr.filenames)
+            unicode_fnames = (decode_bytes(f) for f in hdr.filenames)
             # Find all /boot/vmlinuz- files and strip off vmlinuz-
             files.extend((f.split("/")[-1][8:] for f in unicode_fnames
                          if fnmatch(f, "/boot/vmlinuz-*") or
