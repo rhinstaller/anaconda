c6e8e9585bd636503bd3b56e12891680a4525780
Jiri Konecny <jkonecny@redhat.com>
2019-06-04 14:24:47 +0200

    Implement payload RequirementsModule interface

--- pyanaconda/modules/payload/requirements_interface.py
+++ pyanaconda/modules/payload/requirements_interface.py
@@ -19,11 +19,65 @@
 #
 
 from pyanaconda.dbus.interface import dbus_interface
+from pyanaconda.dbus.typing import *  # pylint: disable=wildcard-import
 
 from pyanaconda.modules.common.constants.objects import REQUIREMENTS
 from pyanaconda.modules.common.base import ModuleInterfaceTemplate
+from pyanaconda.modules.common.structures.payload import Requirement
 
 
 @dbus_interface(REQUIREMENTS.interface_name)
 class RequirementsInterface(ModuleInterfaceTemplate):
     """DBus interface for Requirements payload module."""
+
+    def AddPackages(self, package_ids: List[Str], reason: Str, strong: Bool):
+        """Add packages required for the reason.
+
+        If a package is already required, the new reason will be
+        added and the strength of the requirement will be updated.
+
+        :param package_ids: names of packages to be added
+        :param reason: description of reason for adding the packages
+        :param strong: is the requirement strong (ie is not satisfying it fatal?)
+        """
+        self.implementation.add_packages(package_ids, reason, strong)
+
+    def AddGroups(self, group_ids: List[Str], reason: Str, strong: Bool):
+        """Add groups required for the reason.
+
+        If a group is already required, the new reason will be
+        added and the strength of the requirement will be updated.
+
+        :param group_ids: ids of groups to be added
+        :param reason: descripiton of reason for adding the groups
+        :param strong: is the requirement strong
+        """
+        self.implementation.add_groups(group_ids, reason, strong)
+
+    @property
+    def Packages(self) -> List[Structure]:
+        """List of package requirements.
+
+        return: list of package requirements
+        rtype: list of Requirement
+        """
+        requirements = self.implementation.packages
+        return Requirement.to_structure_list(requirements)
+
+    @property
+    def Groups(self) -> List[Structure]:
+        """List of group requirements.
+
+        return: list of group requirements
+        rtype: list of Requirement
+        """
+        requirements = self.implementation.groups
+        return Requirement.to_structure_list(requirements)
+
+    @property
+    def Empty(self) -> Bool:
+        """Are requirements empty?
+
+        return: True if there are no requirements, else False
+        """
+        return self.implementation.empty
