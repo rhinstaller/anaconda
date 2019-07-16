07913d30bac5423ff8bd70167950815b56f6bc8a
Jiri Konecny <jkonecny@redhat.com>
2019-07-16 18:25:37 +0200

    Add tests for new payload handlers solution
    Test handlers creation with the new API and generating of the kickstart
    with no handler set.

--- tests/nosetests/pyanaconda_tests/module_payload_test.py
+++ tests/nosetests/pyanaconda_tests/module_payload_test.py
@@ -17,13 +17,15 @@
 #
 # Red Hat Author(s): Jiri Konecny <jkonecny@redhat.com>
 #
-import unittest
+from unittest import TestCase
+from mock import patch
 
 from pyanaconda.modules.payload.payload_interface import PayloadInterface
 from pyanaconda.modules.payload.payload import PayloadModule
+from pyanaconda.modules.common.constants.objects import PAYLOAD_DEFAULT, LIVE_OS_HANDLER
 
 
-class PayloadInterfaceTestCase(unittest.TestCase):
+class PayloadInterfaceTestCase(TestCase):
 
     def setUp(self):
         """Set up the payload module."""
@@ -35,3 +37,39 @@ class PayloadInterfaceTestCase(unittest.TestCase):
         self.assertEqual(self.payload_interface.KickstartCommands, ['liveimg'])
         self.assertEqual(self.payload_interface.KickstartSections, ["packages"])
         self.assertEqual(self.payload_interface.KickstartAddons, [])
+
+    def no_handler_set_test(self):
+        """Test empty string is returned when no handler is set."""
+        self.assertEqual(self.payload_interface.GetActiveHandlerPath(), "")
+
+    def generate_kickstart_without_handler_test(self):
+        """Test kickstart parsing without handler set."""
+        self.assertEqual(self.payload_interface.GenerateKickstart(), "")
+
+    @patch('pyanaconda.dbus.DBus.publish_object')
+    def create_dnf_handler_test(self, publisher):
+        """Test creation and publishing of the DNF handler module."""
+        self.payload_interface.CreateDNFHandler()
+        self.assertEqual(self.payload_interface.GetActiveHandlerPath(),
+                         PAYLOAD_DEFAULT.object_path)
+        # here the publisher is called twice because the Packages section is also published
+        self.assertEqual(publisher.call_count, 2)
+
+    @patch('pyanaconda.dbus.DBus.publish_object')
+    def create_live_os_handler_test(self, publisher):
+        """Test creation and publishing of the Live OS handler module."""
+        self.payload_interface.CreateLiveOSHandler()
+        self.assertEqual(self.payload_interface.GetActiveHandlerPath(),
+                         LIVE_OS_HANDLER.object_path)
+        publisher.assert_called_once()
+
+    @patch('pyanaconda.dbus.DBus.publish_object')
+    def create_multiple_handlers_test(self, publisher):
+        """Test creating two handlers."""
+        self.payload_interface.CreateDNFHandler()
+        self.payload_interface.CreateLiveOSHandler()
+
+        # The last one should win
+        self.assertEqual(self.payload_interface.GetActiveHandlerPath(),
+                         LIVE_OS_HANDLER.object_path)
+        self.assertEqual(publisher.call_count, 3)
