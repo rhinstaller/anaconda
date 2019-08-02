7caaedc35a6fdc33912db497c7d8a68159b3344d
Martin Kolman <mkolman@redhat.com>
2019-08-02 18:08:46 +0200

    Add DBUS Tasks for realm discovery & joining a realm

--- pyanaconda/modules/security/installation.py
+++ pyanaconda/modules/security/installation.py
@@ -16,6 +16,9 @@
 # Red Hat, Inc.
 #
 import os
+import copy
+
+from pyanaconda.core import util
 
 from pyanaconda.simpleconfig import SimpleConfigFile
 
@@ -25,7 +28,9 @@ from pyanaconda.modules.security.constants import SELinuxMode
 from pyanaconda.anaconda_loggers import get_module_logger
 log = get_module_logger(__name__)
 
-__all__ = ["ConfigureSELinuxTask"]
+__all__ = ["ConfigureSELinuxTask", "RealmDiscoverTask", "RealmJoinTask"]
+
+REALM_TOOL_NAME = "realm"
 
 
 class ConfigureSELinuxTask(Task):
@@ -72,3 +77,137 @@ class ConfigureSELinuxTask(Task):
             selinux_cfg.write()
         except IOError as msg:
             log.error("SELinux configuration failed: %s", msg)
+
+
+class RealmDiscoverTask(Task):
+    """Task for discovering information about a realm we intend to join (if any).
+
+    Based on results from this task we might attempt to join the realm via a separate
+    task later on.
+    """
+
+    def __init__(self, sysroot, realm_data):
+        """Create a new realm discovery task.
+
+        :param str sysroot: a path to the root of the target system
+        :param realm_data: realm data holder
+        """
+        super().__init__()
+        self._sysroot = sysroot
+        self._realm_data = realm_data
+
+    @property
+    def name(self):
+        return "Discover information about a realm"
+
+    def _parse_realm_data(self, output):
+        """Parse data from realm tool output.
+
+        First line is the realm name, and following lines are data
+        formatted as a simple key value store:
+
+        "name: value"
+
+        We only care about the keys called "required-package"
+        and ignore the rest.
+
+        :param str output: output of the realm tool
+        :return: a tuple reporting discovery success and package requirements
+        :rtype: (bool, list(str))
+        """
+        required_packages = ["realmd"]
+        discovered_realm_data = None
+
+        lines = output.split("\n")
+        if lines:
+            discovered_realm_data = lines.pop(0).strip()
+            log.info("Realm discovered: %s", discovered_realm_data)
+            for line in lines:
+                parts = line.split(":", 1)
+                if len(parts) == 2 and parts[0].strip() == "required-package":
+                    package_spec = parts[1].strip()
+                    # "" is not a valid package specification
+                    if package_spec:
+                        required_packages.append(package_spec)
+
+            log.info("Realm %s needs packages %s",
+                      discovered_realm_data, ", ".join(required_packages))
+        return bool(discovered_realm_data), required_packages
+
+    def run(self):
+        if not self._realm_data.name:
+            log.debug("No realm name set, skipping realm discovery.")
+            return self._realm_data
+
+        output = ""
+
+        try:
+            argv = ["discover", "--verbose"] + self._realm_data.discover_options + [self._realm_data.name]
+            output = util.execWithCapture(REALM_TOOL_NAME, argv, filter_stderr=True)
+        except OSError:
+            # TODO: A lousy way of propagating what will usually be
+            # 'no such realm'
+            # The error message is logged by util
+            return self._realm_data
+
+        realm_discovered, required_packages = self._parse_realm_data(output)
+
+        # set the result in the realm data holder and return it
+        self._realm_data.discovered = realm_discovered
+        self._realm_data.required_packages = required_packages
+        return self._realm_data
+
+
+class RealmJoinTask(Task):
+    """Task for joining a realm we have discovered (if any)."""
+
+    def __init__(self, sysroot, realm_data):
+        """Create a new realm discovery task.
+
+        :param str sysroot: a path to the root of the target system
+        :param realm_data: realm data holder
+        """
+        super().__init__()
+        self._sysroot = sysroot
+        # We do a deep copy of the realm data holder to avoid
+        # changes to the data in the Task from changing the backing
+        # data structure in the module, without triggering the changed signal.
+        # This also works the other way around, preventing changes
+        # to the data structure in the module influencing the task after
+        # it has been instantiated.
+        self._realm_data = copy.deepcopy(realm_data)
+
+    @property
+    def name(self):
+        return "Join a realm"
+
+    def set_realm_data(self, realm_data):
+        """Set a new version of realm data to the task."""
+        log.debug("Setting new realm data for realm join task: %s", realm_data)
+        self._realm_data = realm_data
+
+    def run(self):
+        if not self._realm_data.discovered:
+            log.debug("No realm has been discovered, so not joining any realm.")
+            return
+
+        for arg in self._realm_data.join_options:
+            if arg.startswith("--no-password") or arg.startswith("--one-time-password"):
+                pw_args = []
+                break
+        else:
+            # no explicit password argument, using implicit --no-password
+            pw_args = ["--no-password"]
+
+        argv = ["join", "--install", self._sysroot, "--verbose"] + pw_args + self._realm_data.join_options
+        rc = -1
+        try:
+            rc = util.execWithRedirect(REALM_TOOL_NAME, argv)
+        except OSError:
+            log.exception("Realm %s join failed with exception.", self._realm_data.name)
+            pass
+
+        if rc == 0:
+            log.info("Joined realm %s", self._realm_data.name)
+        else:
+            log.info("Joining realm %s failed", self._realm_data.name)
