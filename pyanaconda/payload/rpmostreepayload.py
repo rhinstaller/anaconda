e4d87f651cb97724f85639cce1bc86e56bf88b70
Jiri Konecny <jkonecny@redhat.com>
2019-08-20 14:36:43 +0200

    Replace remote for installed refs in rpmpayload (#1734970)
    Use the new API and replace the remote before it will be removed. We can
    remove remote when it's still used.
    
    Related: rhbz#1734970

--- pyanaconda/payload/rpmostreepayload.py
+++ pyanaconda/payload/rpmostreepayload.py
@@ -323,8 +323,9 @@ class RPMOSTreePayload(Payload):
                     util.ipmi_abort(scripts=self.data.scripts)
                     sys.exit(1)
 
-            self._flatpak_payload.remove_remote(FlatpakPayload.LOCAL_REMOTE_NAME)
             self._flatpak_payload.add_remote("fedora", "oci+https://registry.fedoraproject.org")
+            self._flatpak_payload.replace_installed_refs_remote("fedora")
+            self._flatpak_payload.remove_remote(FlatpakPayload.LOCAL_REMOTE_NAME)
 
         mainctx.pop_thread_default()
 
