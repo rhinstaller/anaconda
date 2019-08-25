c5e42f725509e077f0cc5bb9f42cd7c751042d31
Vendula Poncova <vponcova@redhat.com>
2019-08-25 19:31:23 +0200

    Don't use DBus error registration from pydbus
    Move the code for the error registration to pyanaconda.

--- pyanaconda/dbus/error.py
+++ pyanaconda/dbus/error.py
@@ -16,10 +16,113 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from pydbus.error import map_error, map_by_default
 from pyanaconda.dbus.namespace import get_dbus_name
 
-__all__ = ['dbus_error', 'dbus_error_by_default']
+import gi
+gi.require_version("Gio", "2.0")
+gi.require_version("GLib", "2.0")
+from gi.repository import Gio, GLib
+
+__all__ = ['dbus_error', 'dbus_error_by_default', "GLibErrorHandler", "ErrorRegister"]
+
+
+class ErrorRegister(object):
+    """Class for mapping exceptions to DBus errors."""
+
+    def __init__(self):
+        self._default_class = None
+        self._default_namespace = "not.known.Error"
+        self._map = dict()
+        self._reversed_map = dict()
+
+    def set_default_namespace(self, namespace):
+        """Set the namespace for names of unknown errors."""
+        self._default_namespace = namespace
+
+    def set_default_exception(self, exception_cls):
+        """Set the exception class as a default."""
+        self._default_class = exception_cls
+
+    def map_exception_to_name(self, exception_cls, name):
+        """Map the exception class to a DBus name."""
+        self._map[name] = exception_cls
+        self._reversed_map[exception_cls] = name
+
+    def get_error_name(self, exception_cls):
+        """Get the DBus name of the exception."""
+        if exception_cls in self._reversed_map:
+            return self._reversed_map.get(exception_cls)
+
+        if self._default_namespace:
+            return "{}.{}".format(self._default_namespace, exception_cls.__name__)
+
+        return exception_cls.__name__
+
+    def get_exception_class(self, name):
+        """Get the exception class mapped to the DBus name."""
+        return self._map.get(name, self._default_class)
+
+
+class GLibErrorHandler(object):
+    """Class for handling DBus errors based on GLib."""
+
+    # Register of DBus errors.
+    register = ErrorRegister()
+
+    @classmethod
+    def handle_server_error(cls, server, invocation, e):
+        """Handle a local exception on the server side."""
+        server.set_call_error(
+            invocation,
+            cls.register.get_error_name(type(e)),
+            str(e)
+        )
+
+    @classmethod
+    def handle_client_error(cls, client, e):
+        """Handle a remote DBus error on the client side."""
+        raise cls._get_exception(e)
+
+    @classmethod
+    def _get_exception(cls, e):
+        """Get an exception for the remote DBus error."""
+        if not isinstance(e, GLib.Error):
+            return e
+
+        if not Gio.DBusError.is_remote_error(e):
+            return e
+
+        name = Gio.DBusError.get_remote_error(e)
+        if not cls._is_name_registered(name):
+            return e
+
+        message = cls._get_exception_message(name, e.message)
+        return cls._create_exception(name, message, e.domain, e.code)
+
+    @classmethod
+    def _is_name_registered(cls, name):
+        """Is there an exception for the given DBus name?"""
+        return cls.register.get_exception_class(name) is not None
+
+    @staticmethod
+    def _get_exception_message(name, message):
+        """Transform the message of the exception."""
+        prefix = "{}:{}: ".format("GDBus.Error", name)
+
+        if message.startswith(prefix):
+            return message[len(prefix):]
+
+        return message
+
+    @classmethod
+    def _create_exception(cls, name, message, domain, code):
+        """Create an exception from the given parameters."""
+        exception_cls = cls.register.get_exception_class(name)
+        exception = exception_cls(message)
+        exception.dbus_name = name
+        exception.dbus_domain = domain
+        exception.dbus_code = code
+        return exception
 
 
 def dbus_error(error_name, namespace):
@@ -43,3 +146,18 @@ def dbus_error_by_default(cls):
     :return: a decorated class
     """
     return map_by_default(cls)
+
+
+def map_error(error_name, error_handler=GLibErrorHandler):
+    """Map decorated exception class to a DBus error."""
+    def decorated(cls):
+        error_handler.register.map_exception_to_name(cls, error_name)
+        return cls
+
+    return decorated
+
+
+def map_by_default(cls, error_handler=GLibErrorHandler):
+    """Map decorated exception class to all unknown DBus errors."""
+    error_handler.register.set_default_exception(cls)
+    return cls
