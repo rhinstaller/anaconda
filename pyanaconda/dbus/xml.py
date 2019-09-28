82257e7351a61207ed60225e45db0328d04a19ab
Vendula Poncova <vponcova@redhat.com>
2019-09-28 21:38:41 +0200

    Add support for parsing the XML specification
    Create a new class XMLParser that provides a support for parsing
    the XML specification. Move some methods from XMLGenerator to this
    class.

--- pyanaconda/dbus/xml.py
+++ pyanaconda/dbus/xml.py
@@ -25,46 +25,59 @@
 from xml.etree import ElementTree
 from xml.dom import minidom
 
+__all__ = ["XMLParser", "XMLGenerator"]
 
-class XMLGenerator(object):
-    """Class for creating XML and XML elements."""
 
-    @staticmethod
-    def element_to_xml(element):
-        """Return XML of the element."""
-        return ElementTree.tostring(element, method="xml", encoding="unicode")
+class XMLParser(object):
+    """Class for parsing XML."""
 
     @staticmethod
     def xml_to_element(xml):
         return ElementTree.fromstring(xml)
 
     @staticmethod
-    def prettify_xml(xml):
-        """Return pretty printed XML."""
-        # Remove newlines and extra whitespaces,
-        xml_line = "".join([line.strip() for line in xml.splitlines()])
-
-        # Generate pretty xml.
-        return minidom.parseString(xml_line).toprettyxml(indent="  ")
+    def is_member(member_node):
+        return member_node.tag in ("method", "signal", "property")
 
     @staticmethod
-    def add_child(parent_element, child_element):
-        """Append the child element to the parent element."""
-        parent_element.append(child_element)
+    def is_interface(member_node):
+        return member_node.tag == "interface"
 
     @staticmethod
-    def add_comment(element, comment):
-        element.append(ElementTree.Comment(text=comment))
+    def is_signal(member_node):
+        return member_node.tag == "signal"
 
     @staticmethod
-    def get_node_element():
-        """Create a node element called node."""
-        return ElementTree.Element("node")
+    def is_method(member_node):
+        return member_node.tag == "method"
 
     @staticmethod
-    def get_interface_element(name):
-        """Create an interface element."""
-        return ElementTree.Element("interface", {"name": name})
+    def is_property(member_node):
+        return member_node.tag == "property"
+
+    @staticmethod
+    def is_parameter(member_node):
+        return member_node.tag == "arg"
+
+    @staticmethod
+    def has_name(node, node_name):
+        return node.attrib.get("name", "") == node_name
+
+    @staticmethod
+    def get_name(node):
+        return node.attrib["name"]
+
+    @staticmethod
+    def get_type(node):
+        return node.attrib["type"]
+
+    @staticmethod
+    def get_access(node):
+        return node.attrib["access"]
+
+    @staticmethod
+    def get_direction(node):
+        return node.attrib["direction"]
 
     @staticmethod
     def get_interfaces_from_node(node_element):
@@ -86,13 +99,50 @@ class XMLGenerator(object):
 
         return properties
 
+
+class XMLGenerator(XMLParser):
+    """Class for generating XML."""
+
     @staticmethod
-    def get_signal_element(name):
+    def element_to_xml(element):
+        """Return XML of the element."""
+        return ElementTree.tostring(element, method="xml", encoding="unicode")
+
+    @staticmethod
+    def prettify_xml(xml):
+        """Return pretty printed XML."""
+        # Remove newlines and extra whitespaces,
+        xml_line = "".join([line.strip() for line in xml.splitlines()])
+
+        # Generate pretty xml.
+        return minidom.parseString(xml_line).toprettyxml(indent="  ")
+
+    @staticmethod
+    def add_child(parent_element, child_element):
+        """Append the child element to the parent element."""
+        parent_element.append(child_element)
+
+    @staticmethod
+    def add_comment(element, comment):
+        element.append(ElementTree.Comment(text=comment))
+
+    @staticmethod
+    def create_node():
+        """Create a node element called node."""
+        return ElementTree.Element("node")
+
+    @staticmethod
+    def create_interface(name):
+        """Create an interface element."""
+        return ElementTree.Element("interface", {"name": name})
+
+    @staticmethod
+    def create_signal(name):
         """Create a signal element."""
         return ElementTree.Element("signal", {"name": name})
 
     @staticmethod
-    def get_method_element(name):
+    def create_method(name):
         """Create a method element."""
         return ElementTree.Element("method", {"name": name})
 
@@ -117,11 +167,3 @@ class XMLGenerator(object):
             "access": access
         }
         return ElementTree.Element(tag, attr)
-
-    @staticmethod
-    def is_member(member_node):
-        return member_node.tag in ("method", "signal", "property")
-
-    @staticmethod
-    def has_name(node, node_name):
-        return node.attrib.get("name", "") == node_name
