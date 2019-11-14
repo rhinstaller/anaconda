269084b83ff073f72d9374d796bbfffb1d80e195
Radek Vykydal <rvykydal@redhat.com>
2019-11-14 14:44:11 +0100

    keyboard: fix getting options from LocaledWrapper

--- pyanaconda/modules/localization/localed.py
+++ pyanaconda/modules/localization/localed.py
@@ -113,8 +113,8 @@ class LocaledWrapper(object):
                       "X11Options property")
             return ""
 
-        # returned GVariant is unpacked to a tuple with a single element
-        return options[0]
+        # returned GVariant is unpacked to a tuple with options
+        return options
 
     def set_keymap(self, keymap, convert=False):
         """
