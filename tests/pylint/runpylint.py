28fea64189dd83a0e9ab616c8fdcc9e4ea651861
Jiri Konecny <jkonecny@redhat.com>
2020-01-23 10:44:53 +0100

    Adapt runpylint to use pocketlint replacement
    Add pylint configuration file and move as much as possible to this
    configuration. That means from now on we will use official pylint
    configuration file instead of middleware pocketlint script. This will
    give us much more freedom in the configuration.
    
    Anaconda specific configuration were moved to runpylint.py script so it
    won't be bothering us anymore for other projects as in pocketlint case.
    
    This configuration should reflect pocketlint state.

--- tests/pylint/runpylint.py
+++ tests/pylint/runpylint.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 
+import atexit
+import shutil
 import sys
+import tempfile
 import time
+import os
 
 from os import path
-from pocketlint import FalsePositive, PocketLintConfig, PocketLinter
+
+from censorship import CensorshipConfig, CensorshipLinter, FalsePositive
 
 TIME_LOG = "pylint-time.log"
 
 
-class AnacondaLintConfig(PocketLintConfig):
+class AnacondaLintConfig(CensorshipConfig):
     def __init__(self):
-        PocketLintConfig.__init__(self)
+        super().__init__()
 
-        self.falsePositives = [
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        self.pylintrc_path = os.path.join(current_dir, "pylintrc")
+
+        self.false_positives = [
                                 FalsePositive(r"^E1101.*: Instance of 'KickstartSpecificationHandler' has no '.*' member$"),
                                 FalsePositive(r"^E1101.*: Instance of 'GError' has no 'message' member"),
                                 FalsePositive(r"^E1101.*: FedoraGeoIPProvider._refresh: Instance of 'LookupDict' has no 'ok' member"),
@@ -34,20 +43,86 @@ class AnacondaLintConfig(PocketLintConfig):
                                 FalsePositive(r"E1101.*: Instance of 'int' has no 'dasd_is_ldl' member"),
                               ]
 
-        # This will solve problems with C python extensions
-        self.loadAllExtensions = True
+    def _files(self):
+        srcdir = os.environ.get("top_srcdir", os.getcwd())
+
+        retval = self._get_py_paths(srcdir)
+
+        return retval
+
+    def _get_py_paths(self, directory):
+        retval = []
+
+        for (root, dirnames, files) in os.walk(directory):
+
+            # skip scanning of already added python modules
+            skip = False
+            for i in retval:
+                if root.startswith(i):
+                    skip = True
+                    break
+
+            if skip:
+                continue
+
+            if "__init__.py" in files:
+                retval.append(root)
+                continue
+
+            for f in files:
+                try:
+                    with open(root + "/" + f) as fo:
+                        lines = fo.readlines()
+                except UnicodeDecodeError:
+                    # If we couldn't open this file, just skip it.  It wasn't
+                    # going to be valid python anyway.
+                    continue
+
+                # Test any file that either ends in .py or contains #!/usr/bin/python
+                # in the first line.
+                if f.endswith(".py") or (lines and str(lines[0]).startswith("#!/usr/bin/python")):
+                    retval.append(root + "/" + f)
+
+        return retval
 
     @property
-    def extraArgs(self):
-        return ["--init-import", "y"]
+    def check_paths(self):
+        return self._files()
 
-    @property
-    def initHook(self):
-        return """'import gi.overrides, os; gi.overrides.__path__[0:0] = (os.environ["ANACONDA_WIDGETS_OVERRIDES"].split(":") if "ANACONDA_WIDGETS_OVERRIDES" in os.environ else [])'"""
 
-    @property
-    def ignoreNames(self):
-        return {"translation-canary"}
+def _setup_environment(self):
+    # We need top_builddir to be set so we know where to put the pylint analysis
+    # stuff.  Usually this will be set up if we are run via "make test" but if
+    # not, hope that we are at least being run out of the right directory.
+    builddir = os.environ.get("top_builddir", os.getcwd())
+
+    # XDG_RUNTIME_DIR is "required" to be set, so make one up in case something
+    # actually tries to do something with it.
+    if "XDG_RUNTIME_DIR" not in os.environ:
+        d = tempfile.mkdtemp()
+        os.environ["XDG_RUNTIME_DIR"] = d
+        atexit.register(self._del_xdg_runtime_dir)
+
+    # Unset TERM so that things that use readline don't output terminal garbage.
+    if "TERM" in os.environ:
+        os.environ.pop("TERM")
+
+    # Don't try to connect to the accessibility socket.
+    os.environ["NO_AT_BRIDGE"] = "1"
+
+    # Force the GDK backend to X11.  Otherwise if no display can be found, Gdk
+    # tries every backend type, which includes "broadway", which prints an error
+    # and keeps changing the content of said error.
+    os.environ["GDK_BACKEND"] = "x11"
+
+    # Save analysis data in the pylint directory.
+    os.environ["PYLINTHOME"] = builddir + "/tests/pylint/.pylint.d"
+    if not os.path.exists(os.environ["PYLINTHOME"]):
+        os.mkdir(os.environ["PYLINTHOME"])
+
+
+def _del_xdg_runtime_dir(self):
+    shutil.rmtree(os.environ["XDG_RUNTIME_DIR"])
 
 
 def _get_timelog_path():
@@ -69,7 +144,7 @@ def save_end_time():
 
 if __name__ == "__main__":
     conf = AnacondaLintConfig()
-    linter = PocketLinter(conf)
+    linter = CensorshipLinter(conf)
     save_start_time()
     rc = linter.run()
     save_end_time()
