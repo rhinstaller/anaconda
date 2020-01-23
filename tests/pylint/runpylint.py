eadc2198533c5e9e7d2f09819c4194e0dc57b7e7
Jiri Konecny <jkonecny@redhat.com>
2020-01-23 13:45:02 +0100

    Fix GError message False positive
    You can avoid using message attribute by str(GError). We are using that
    on plenty of places so I "hope" it works correctly :D.

--- tests/pylint/runpylint.py
+++ tests/pylint/runpylint.py
@@ -24,7 +24,6 @@ class AnacondaLintConfig(CensorshipConfig):
 
         self.false_positives = [
                                 FalsePositive(r"^E1101.*: Instance of 'KickstartSpecificationHandler' has no '.*' member$"),
-                                FalsePositive(r"^E1101.*: Instance of 'GError' has no 'message' member"),
                                 FalsePositive(r"^E1101.*: FedoraGeoIPProvider._refresh: Instance of 'LookupDict' has no 'ok' member"),
                                 FalsePositive(r"^E1101.*: HostipGeoIPProvider._refresh: Instance of 'LookupDict' has no 'ok' member"),
                                 FalsePositive(r"^E1101.*: Geocoder._reverse_geocode_nominatim: Instance of 'LookupDict' has no 'ok' member"),
