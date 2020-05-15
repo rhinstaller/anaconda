523ed3efe3713c1d8e450f19d4697a8ad745f94b
Jiri Konecny <jkonecny@redhat.com>
2020-05-15 18:13:00 +0200

    Switch call of pip3 to python3 -m pip
    This is the best way how to avoid problem with different naming on
    different distributions.

--- scripts/testing/setup-mock-test-env.py
+++ scripts/testing/setup-mock-test-env.py
@@ -370,7 +370,9 @@ def install_pip_packages_to_mock(mock_command, packages):
     cmd = _prepare_command(mock_command)
 
     cmd = _run_cmd_in_chroot(cmd)
-    cmd.append('pip3 install --install-option="--install-scripts=/usr/bin" {}'.format(packages))
+    cmd.append(
+        'python3 -m pip install --install-option="--install-scripts=/usr/bin" {}'.format(packages)
+    )
 
     _check_subprocess(cmd, "Can't install packages via pip to mock.")
 
