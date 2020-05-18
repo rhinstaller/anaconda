6fc5f48c728be50de900915222520dcb880e1064
Jiri Konecny <jkonecny@redhat.com>
2020-05-18 18:20:15 +0200

    Add payload property to SourceSwitchHandler
    It will work as a shortcut.

--- pyanaconda/ui/helpers.py
+++ pyanaconda/ui/helpers.py
@@ -83,6 +83,10 @@ class SourceSwitchHandler(object, metaclass=ABCMeta):
     def storage(self):
         pass
 
+    @abstractproperty
+    def payload(self):
+        pass
+
     def __init__(self):
         self._device = None
         self._current_iso_path = None
