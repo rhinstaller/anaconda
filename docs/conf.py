b82a9e0a556bdc3ba850919ccb6be377c57acce3
Jiri Konecny <jkonecny@redhat.com>
2020-05-18 11:46:57 +0200

    Remove sphinx test dependency
    We can remove the whole package as test dependency with just one line of
    pylint disable. This will also help switching to RHEL where are not all
    dependencies for sphinx in the build root.

--- docs/conf.py
+++ docs/conf.py
@@ -316,7 +316,7 @@ intersphinx_mapping = {'https://docs.python.org/3': None}
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
+    import sphinx_rtd_theme  # pylint: disable=import-error
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
