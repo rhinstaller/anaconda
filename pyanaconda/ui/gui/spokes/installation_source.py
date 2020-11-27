rhinstaller/anaconda

commit 4a2b8f26f82cb084951d9bc784abd362f4474b02

Improve updates repo configuration in GUI (#1670471)
Instead of the previous "do not install updates" checkbox use a
radio button group with install updates/do not install updates options.
This also gets rid of the very long line of text that was next to the
old checkbox.

Also fix a two UI element glitches:
- update configuration elements being sensitive for certain sources
  that don't support them
- HTTP URL options checkbox not being available for closest mirror after
  switching to a non-HTTP remote installation source and back

Resolves: rhbz#1670471

pyanaconda/ui/gui/spokes/installation_source.py
@@ -709,7 +709,7 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler):
         self._url_type_combo_box = self.builder.get_object("urlTypeComboBox")
         self._url_type_label = self.builder.get_object("urlTypeLabel")
 
-        self._no_updates_checkbox = self.builder.get_object("noUpdatesCheckbox")
+        self._updates_radio_button = self.builder.get_object("updatesRadioButton")
 
         self._verify_iso_button = self.builder.get_object("verifyIsoButton")
 
@@ -1002,7 +1002,7 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler):
                 self.data.method.method = None
                 self._proxy_url = self.data.method.proxy
 
-        self._setup_no_updates()
+        self._setup_updates()
 
         # Setup the addon repos
         self._reset_repo_store()
@@ -1066,7 +1066,7 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler):
         else:
             self._url_type_combo_box.set_active_id(URL_TYPE_URL)
 
-    def _setup_no_updates(self):
+    def _setup_updates(self):
         """ Setup the state of the No Updates checkbox.
 
             If closest mirror is not selected, check it.
@@ -1074,18 +1074,20 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler):
             uncheck it.
         """
         self._updates_box.set_sensitive(self._mirror_active())
-        active = not self._mirror_active() or not self.payload.is_repo_enabled("updates")
-        self._no_updates_checkbox.set_active(active)
+        active = self._mirror_active() or self.payload.is_repo_enabled("updates")
+        self._updates_radio_button.set_active(active)
 
     @property
     def showable(self):
         return isinstance(self.payload, PackagePayload)
 
     def _mirror_active(self):
-        return self._protocol_combo_box.get_active_id() == PROTOCOL_MIRROR
+        return self._protocol_combo_box.get_active_id() == PROTOCOL_MIRROR and \
+            self._network_button.get_active()
+
 
     def _http_active(self):
-        return self._protocol_combo_box.get_active_id() in [PROTOCOL_HTTP, PROTOCOL_HTTPS]
+        return self._protocol_combo_box.get_active_id() in [PROTOCOL_HTTP, PROTOCOL_HTTPS, PROTOCOL_MIRROR]
 
     def _ftp_active(self):
         return self._protocol_combo_box.get_active_id() == PROTOCOL_FTP
@@ -1248,7 +1250,7 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler):
         if relatedBox:
             relatedBox.set_sensitive(enabled)
 
-        self._setup_no_updates()
+        self._setup_updates()
 
     def on_back_clicked(self, button):
         """If any input validation checks failed, keep the user on the screen.
@@ -1371,13 +1373,13 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler):
         self._url_entry.set_sensitive(self._http_active() or self._ftp_active() or
                                       self._nfs_active())
 
-        # Only allow thse widgets to be shown if it makes sense for the
+        # Only allow these widgets to be shown if it makes sense for the
         # the currently selected protocol.
         self._proxy_button.set_sensitive(self._http_active() or self._mirror_active())
         self._nfs_opts_box.set_visible(self._nfs_active())
         self._url_type_combo_box.set_visible(self._http_active())
         self._url_type_label.set_visible(self._http_active())
-        self._setup_no_updates()
+        self._setup_updates()
 
         # Any changes to the protocol combo box also need to update the checks.
         # Emitting the urlEntry 'changed' signal will see if the entered URL
@@ -1626,9 +1628,9 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler):
     def _on_urlEtry_changed(self, editable):
         self._remove_url_prefix(editable, self._protocol_combo_box, self.on_urlEntry_changed)
 
-    def on_noUpdatesCheckbox_toggled(self, *args):
+    def on_updatesRadioButton_toggled(self, button):
         """Toggle the enable state of the updates repo."""
-        if self._no_updates_checkbox.get_active():
+        if self._updates_radio_button.get_active():
             self.payload.set_updates_enabled(False)
         else:
             self.payload.set_updates_enabled(True)

pyanaconda/ui/gui/spokes/installation_source.glade
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!-- Generated with glade 3.19.0 -->
+<!-- Generated with glade 3.22.1 -->
 <interface>
   <requires lib="gtk+" version="3.0"/>
   <requires lib="AnacondaWidgets" version="1.0"/>
@@ -918,30 +918,38 @@
                                   </packing>
                                 </child>
                                 <child>
-                                  <object class="GtkCheckButton" id="noUpdatesCheckbox">
-                                    <property name="visible">True</property>
+                                  <object class="GtkRadioButton" id="updatesRadioButton">
+                                    <property name="label" translatable="yes" context="GUI|Software Source">Install the _latest available software updates</property>
                                     <property name="can_focus">True</property>
                                     <property name="receives_default">False</property>
                                     <property name="margin_left">12</property>
-                                    <property name="margin_bottom">12</property>
                                     <property name="use_underline">True</property>
                                     <property name="xalign">0</property>
                                     <property name="draw_indicator">True</property>
-                                    <signal name="toggled" handler="on_noUpdatesCheckbox_toggled" swapped="no"/>
-                                    <child>
-                                      <object class="GtkLabel" id="label10">
-                                        <property name="visible">True</property>
-                                        <property name="can_focus">False</property>
-                                        <property name="label" translatable="yes" context="GUI|Software Source">Don't install the latest available software _updates. Install the default versions provided by the installation source above.</property>
-                                        <property name="use_underline">True</property>
-                                        <property name="wrap">True</property>
-                                      </object>
-                                    </child>
+                                    <signal name="toggled" handler="on_updatesRadioButton_toggled" swapped="no"/>
                                   </object>
                                   <packing>
                                     <property name="expand">False</property>
                                     <property name="fill">True</property>
                                     <property name="position">1</property>
+                                  </packing>
+                                </child>
+                                <child>
+                                  <object class="GtkRadioButton" id="noUpdatesRadioButton">
+                                    <property name="label" translatable="yes" context="GUI|Software Source">Install the _default versions provided by the installation source (above) only</property>
+                                    <property name="can_focus">True</property>
+                                    <property name="receives_default">False</property>
+                                    <property name="margin_left">12</property>
+                                    <property name="use_underline">True</property>
+                                    <property name="xalign">0</property>
+                                    <property name="active">True</property>
+                                    <property name="draw_indicator">True</property>
+                                    <property name="group">updatesRadioButton</property>
+                                  </object>
+                                  <packing>
+                                    <property name="expand">False</property>
+                                    <property name="fill">True</property>
+                                    <property name="position">2</property>
                                   </packing>
                                 </child>
                               </object>

commit 98e011f1f8af900ed6f65432ad7466973d44735a

Fix the logic for enabling latest updates
Fix bugs introduced in the commit 4a2b8f2. The negation of the logic wasn't
applied correctly. Without the fix, updates repositories are disabled when the
updates are enabled.

pyanaconda/ui/gui/spokes/installation_source.py
@@ -1085,7 +1085,7 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler, SourceSwitchHandler):
             uncheck it.
         """
         self._updates_box.set_sensitive(self._mirror_active())
-        active = self._mirror_active() or self.payload.is_repo_enabled("updates")
+        active = self._mirror_active() and self.payload.is_repo_enabled("updates")
         self._updates_radio_button.set_active(active)
 
     @property
@@ -1646,10 +1646,8 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler, SourceSwitchHandler):
 
     def on_updatesRadioButton_toggled(self, button):
         """Toggle the enable state of the updates repo."""
-        if self._updates_radio_button.get_active():
-            self.payload.set_updates_enabled(False)
-        else:
-            self.payload.set_updates_enabled(True)
+        active = self._updates_radio_button.get_active()
+        self.payload.set_updates_enabled(active)
 
         # Refresh the metadata using the new set of repos
         self._updates_change = True