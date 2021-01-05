e1b522fc6427c960c0c463b70b21f45eb44c5225
Amy Parker <enbyamy@gmail.com>
2021-01-04 19:37:29 -0800

    Correct PEP8 on commit
    This fixes the PEP8 issues on commit 65d8859 from Epsilqn:master.

--- tests/pylint/runpylint.py
+++ tests/pylint/runpylint.py
@@ -77,7 +77,8 @@ class AnacondaLintConfig(CensorshipConfig):
 
                 # Test any file that either ends in .py or contains #!/usr/bin/env python
                 # in the first line.
-                if f.endswith(".py") or (lines and str(lines[0]).startswith("#!/usr/bin/env python")):
+                if f.endswith(".py") or 
+                (lines and str(lines[0]).startswith("#!/usr/bin/env python")):
                     retval.append(root + "/" + f)
 
         return retval
