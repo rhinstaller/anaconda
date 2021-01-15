rhinstaller/anaconda

commit ef0b5e98dc8cdb46aa80f3210825db366143aab6

Don't run spokes that don't support non-package payloads
Make sure that the installation source and software selection spokes don't run
at all for non-package payloads. Define the should_run method instead of the
showable property to prevent the initialization of the spokes.

pyanaconda/ui/gui/spokes/installation_source.py
@@ -40,6 +40,7 @@ from pyanaconda.payload.image import find_optical_install_media, find_potential_
 from pyanaconda.core.payload import ProxyString, ProxyStringError, parse_nfs_url, create_nfs_url
 from pyanaconda.core.util import cmp_obj_attrs, id_generator
 from pyanaconda.ui.communication import hubQ
+from pyanaconda.ui.context import context
 from pyanaconda.ui.helpers import InputCheck, InputCheckHandler, SourceSwitchHandler
 from pyanaconda.ui.lib.subscription import switch_source
 from pyanaconda.ui.gui import GUIObject
@@ -405,6 +406,11 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler, SourceSwitchHandler):
 
     icon = "media-optical-symbolic"
     title = CN_("GUI|Spoke", "_Installation Source")
+
+    @classmethod
+    def should_run(cls, environment, data):
+        """Don't run for any non-package payload."""
+        return context.payload.type == PAYLOAD_TYPE_DNF
 
     def __init__(self, *args, **kwargs):
         NormalSpoke.__init__(self, *args, **kwargs)
@@ -1082,10 +1088,6 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler, SourceSwitchHandler):
         self._updates_box.set_sensitive(self._mirror_active())
         active = self._mirror_active() and self.payload.is_repo_enabled("updates")
         self._updates_radio_button.set_active(active)
-
-    @property
-    def showable(self):
-        return self.payload.type == PAYLOAD_TYPE_DNF
 
     def _mirror_active(self):
         return self._protocol_combo_box.get_active_id() == PROTOCOL_MIRROR and \

pyanaconda/ui/gui/spokes/software_selection.py
@@ -30,6 +30,7 @@ from pyanaconda.threading import threadMgr, AnacondaThread
 from pyanaconda.core import util, constants
 
 from pyanaconda.ui.communication import hubQ
+from pyanaconda.ui.context import context
 from pyanaconda.ui.gui.spokes import NormalSpoke
 from pyanaconda.ui.gui.spokes.lib.detailederror import DetailedErrorDialog
 from pyanaconda.ui.gui.utils import blockedHandler, escape_markup
@@ -73,6 +74,11 @@ class SoftwareSelectionSpoke(NormalSpoke):
     _ADDON_SELECTED = 1
     # user de-selected
     _ADDON_DESELECTED = 2
+
+    @classmethod
+    def should_run(cls, environment, data):
+        """Don't run for any non-package payload."""
+        return context.payload.type == PAYLOAD_TYPE_DNF
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -247,10 +253,6 @@ class SoftwareSelectionSpoke(NormalSpoke):
                     not threadMgr.get(constants.THREAD_PAYLOAD) and
                     not threadMgr.get(constants.THREAD_CHECK_SOFTWARE) and
                     self.payload.base_repo is not None)
-
-    @property
-    def showable(self):
-        return self.payload.type == PAYLOAD_TYPE_DNF
 
     @property
     def status(self):

pyanaconda/ui/tui/spokes/installation_source.py
@@ -24,6 +24,7 @@ from pyanaconda.modules.common.constants.services import STORAGE
 from pyanaconda.modules.common.structures.payload import RepoConfigurationData
 from pyanaconda.payload.utils import get_device_path
 from pyanaconda.ui.categories.software import SoftwareCategory
+from pyanaconda.ui.context import context
 from pyanaconda.ui.tui.spokes import NormalTUISpoke
 from pyanaconda.ui.tui.tuiobject import Dialog
 from pyanaconda.threading import threadMgr, AnacondaThread
@@ -66,6 +67,11 @@ class SourceSpoke(NormalTUISpoke, SourceSwitchHandler):
 
     SET_NETWORK_INSTALL_MODE = "network_install"
 
+    @classmethod
+    def should_run(cls, environment, data):
+        """Don't run for any non-package payload."""
+        return context.payload.type == PAYLOAD_TYPE_DNF
+
     def __init__(self, data, storage, payload):
         NormalTUISpoke.__init__(self, data, storage, payload)
         SourceSwitchHandler.__init__(self)
@@ -98,10 +104,6 @@ class SourceSpoke(NormalTUISpoke, SourceSwitchHandler):
 
     def _payload_error(self):
         self._error = True
-
-    @property
-    def showable(self):
-        return self.payload.type == PAYLOAD_TYPE_DNF
 
     @property
     def status(self):

pyanaconda/ui/tui/spokes/software_selection.py
@@ -18,6 +18,7 @@
 #
 from pyanaconda.flags import flags
 from pyanaconda.ui.categories.software import SoftwareCategory
+from pyanaconda.ui.context import context
 from pyanaconda.ui.tui.spokes import NormalTUISpoke
 from pyanaconda.threading import threadMgr, AnacondaThread
 from pyanaconda.payload.manager import payloadMgr, PayloadState
@@ -47,6 +48,11 @@ class SoftwareSpoke(NormalTUISpoke):
     """
     helpFile = "SoftwareSpoke.txt"
     category = SoftwareCategory
+
+    @classmethod
+    def should_run(cls, environment, data):
+        """Don't run for any non-package payload."""
+        return context.payload.type == PAYLOAD_TYPE_DNF
 
     def __init__(self, data, storage, payload):
         super().__init__(data, storage, payload)
@@ -133,10 +139,6 @@ class SoftwareSpoke(NormalTUISpoke):
                 addons.extend(addons_list)
 
         return addons
-
-    @property
-    def showable(self):
-        return self.payload.type == PAYLOAD_TYPE_DNF
 
     @property
     def status(self):

commit c2f45193ea64235bcf166414524fbdd9ee8c2429

Skip payload-related spokes in the initial setup (#1915541)
The initial setup doesn't install payload. It was broken by the commit ef0b5e9.

Resolves: rhbz#1915541

pyanaconda/ui/gui/spokes/installation_source.py
@@ -410,6 +410,9 @@ class SourceSpoke(NormalSpoke, GUISpokeInputCheckHandler, SourceSwitchHandler):
     @classmethod
     def should_run(cls, environment, data):
         """Don't run for any non-package payload."""
+        if not NormalSpoke.should_run(environment, data):
+            return False
+
         return context.payload.type == PAYLOAD_TYPE_DNF
 
     def __init__(self, *args, **kwargs):

pyanaconda/ui/gui/spokes/language_support.py
@@ -65,6 +65,9 @@ class LangsupportSpoke(NormalSpoke, LangLocaleHandler):
     @classmethod
     def should_run(cls, environment, data):
         """Don't show the language support spoke on live media."""
+        if not NormalSpoke.should_run(environment, data):
+            return False
+
         return context.payload.type not in PAYLOAD_LIVE_TYPES
 
     def __init__(self, *args, **kwargs):

pyanaconda/ui/gui/spokes/software_selection.py
@@ -78,6 +78,9 @@ class SoftwareSelectionSpoke(NormalSpoke):
     @classmethod
     def should_run(cls, environment, data):
         """Don't run for any non-package payload."""
+        if not NormalSpoke.should_run(environment, data):
+            return False
+
         return context.payload.type == PAYLOAD_TYPE_DNF
 
     def __init__(self, *args, **kwargs):

pyanaconda/ui/tui/spokes/installation_source.py
@@ -70,6 +70,9 @@ class SourceSpoke(NormalTUISpoke, SourceSwitchHandler):
     @classmethod
     def should_run(cls, environment, data):
         """Don't run for any non-package payload."""
+        if not NormalTUISpoke.should_run(environment, data):
+            return False
+
         return context.payload.type == PAYLOAD_TYPE_DNF
 
     def __init__(self, data, storage, payload):

pyanaconda/ui/tui/spokes/software_selection.py
@@ -52,6 +52,9 @@ class SoftwareSpoke(NormalTUISpoke):
     @classmethod
     def should_run(cls, environment, data):
         """Don't run for any non-package payload."""
+        if not NormalTUISpoke.should_run(environment, data):
+            return False
+
         return context.payload.type == PAYLOAD_TYPE_DNF
 
     def __init__(self, data, storage, payload):