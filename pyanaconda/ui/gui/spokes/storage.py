rhinstaller/anaconda

commit 2e305f85702b3fecb0b8cac276ae7c88f65bad90

Don't run spokes that don't support dir installations
Make sure that the storage spoke doesn't run at all on dir installations.
Define the should_run method instead of the showable property to prevent
the initialization of the spoke.

pyanaconda/ui/gui/spokes/storage.py
@@ -82,6 +82,11 @@ class StorageSpoke(NormalSpoke, StorageCheckHandler):
     icon = "drive-harddisk-symbolic"
     title = CN_("GUI|Spoke", "Installation _Destination")
 
+    @classmethod
+    def should_run(cls, environment, data):
+        """Don't run the storage spoke on dir installations."""
+        return not conf.target.is_directory
+
     def __init__(self, *args, **kwargs):
         StorageCheckHandler.__init__(self)
         NormalSpoke.__init__(self, *args, **kwargs)
@@ -253,10 +258,6 @@ class StorageSpoke(NormalSpoke, StorageCheckHandler):
             and not threadMgr.get(constants.THREAD_STORAGE) \
             and not threadMgr.get(constants.THREAD_DASDFMT) \
             and not threadMgr.get(constants.THREAD_EXECUTE_STORAGE)
-
-    @property
-    def showable(self):
-        return not conf.target.is_directory
 
     @property
     def status(self):

pyanaconda/ui/tui/spokes/storage.py
@@ -76,6 +76,11 @@ class StorageSpoke(NormalTUISpoke):
     helpFile = "StorageSpoke.txt"
     category = SystemCategory
 
+    @classmethod
+    def should_run(cls, environment, data):
+        """Don't run the storage spoke on dir installations."""
+        return not conf.target.is_directory
+
     def __init__(self, data, storage, payload):
         super().__init__(data, storage, payload)
         self.title = N_("Installation Destination")
@@ -111,10 +116,6 @@ class StorageSpoke(NormalTUISpoke):
     @property
     def mandatory(self):
         return True
-
-    @property
-    def showable(self):
-        return not conf.target.is_directory
 
     @property
     def status(self):

commit 5e91307ec13d8b73134c80c2cc5a76b4378c7dac

Skip storage-related spokes in the initial setup (#1918048)
The initial setup doesn't configure storage. It was broken by the commit 2e305f8.

Resolves: rhbz#1918048

pyanaconda/ui/gui/spokes/storage.py
@@ -85,6 +85,9 @@ class StorageSpoke(NormalSpoke, StorageCheckHandler):
     @classmethod
     def should_run(cls, environment, data):
         """Don't run the storage spoke on dir installations."""
+        if not NormalSpoke.should_run(environment, data):
+            return False
+
         return not conf.target.is_directory
 
     def __init__(self, *args, **kwargs):

pyanaconda/ui/tui/spokes/storage.py
@@ -79,6 +79,9 @@ class StorageSpoke(NormalTUISpoke):
     @classmethod
     def should_run(cls, environment, data):
         """Don't run the storage spoke on dir installations."""
+        if not NormalTUISpoke.should_run(environment, data):
+            return False
+
         return not conf.target.is_directory
 
     def __init__(self, data, storage, payload):