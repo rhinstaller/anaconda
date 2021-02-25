rhinstaller/anaconda

commit 6b80b7b33c312faa69c22e4bdfe758f186d32786

Fix the should_run methods of the network spoke
The network spoke should use the same logic for its visibility in TUI and GUI.

pyanaconda/ui/gui/spokes/network.py
@@ -1466,6 +1466,14 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalSpoke):
 
     category = SystemCategory
 
+    @classmethod
+    def should_run(cls, environment, data):
+        """Should the spoke run?"""
+        if not FirstbootSpokeMixIn.should_run(environment, data):
+            return False
+
+        return conf.system.can_configure_network
+
     def __init__(self, *args, **kwargs):
         NormalSpoke.__init__(self, *args, **kwargs)
         self.networking_changed = False

pyanaconda/ui/tui/spokes/network.py
@@ -224,6 +224,10 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalTUISpoke):
 
     @classmethod
     def should_run(cls, environment, data):
+        """Should the spoke run?"""
+        if not FirstbootSpokeMixIn.should_run(environment, data):
+            return False
+
         return conf.system.can_configure_network
 
     def initialize(self):

commit 89fb55d22f64eda26d18c735dfd59ee99b5191c2

The network spoke should be visible in live spins (#1932961)
It should be always possible to configure the hostname for the target system
in the graphical network spoke, unless the spoke was specifically disabled in
the Anaconda configuration files.

This regression was caused by the commit 6b80b7b.

Resolves: rhbz#1932961

pyanaconda/ui/gui/spokes/network.py
@@ -1472,7 +1472,8 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalSpoke):
         if not FirstbootSpokeMixIn.should_run(environment, data):
             return False
 
-        return conf.system.can_configure_network
+        # Always allow to configure the hostname for the target system.
+        return True
 
     def __init__(self, *args, **kwargs):
         NormalSpoke.__init__(self, *args, **kwargs)