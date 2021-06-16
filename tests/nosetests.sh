0bf0aa72928e60d007377e7db9094f228e132921
Jiri Konecny <jkonecny@redhat.com>
2021-06-16 12:36:22 +0200

    Run our unit tests with unittest python3 framework
    We need to migrate away from nosetests because they are not compatible with
    python 3.10. Let's finally do the step.

--- tests/nosetests.sh
+++ tests/nosetests.sh
@@ -2,13 +2,13 @@
 
 # If $top_srcdir has not been set by automake, import the test environment
 if [ -z "$top_srcdir" ]; then
-    top_srcdir="$(dirname "$0")/.."
+    top_srcdir="$(realpath $(dirname "$0")/..)"
     . ${top_srcdir}/tests/testenv.sh
 fi
 
 # If no tests were selected by user or makefile, select all of them
 if [ $# -eq 0 ] && [ -z "$NOSE_TESTS_ARGS" ]; then
-    set -- "${top_srcdir}"/tests/nosetests/*_tests
+    set -- $top_srcdir/tests/nosetests
 fi
 
-exec python3 -m nose -v --exclude=logpicker -a \!acceptance,\!slow $NOSE_TESTS_ARGS "$@"
+exec python3 -m unittest discover -t $top_srcdir -v -b -s $@ $NOSE_TESTS_ARGS
