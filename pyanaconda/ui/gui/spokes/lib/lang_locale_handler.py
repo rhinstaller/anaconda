473dfa35a415930d16c0f7204bd9c65f1db9d8ca
Vladimir Slavik <vslavik@redhat.com>
2021-07-01 14:11:39 +0200

    Reorder imports to reduce linter warnings

--- pyanaconda/ui/gui/spokes/lib/lang_locale_handler.py
+++ pyanaconda/ui/gui/spokes/lib/lang_locale_handler.py
@@ -22,17 +22,17 @@ screens handling languages or locales configuration.
 
 """
 
-import gi
-gi.require_version("Gtk", "3.0")
-gi.require_version("Pango", "1.0")
-gi.require_version("GdkPixbuf", "2.0")
-
 from abc import ABCMeta, abstractmethod
-from gi.repository import Gtk, Pango, GdkPixbuf
 
 from pyanaconda import localization
 from pyanaconda.core.util import strip_accents
 from pyanaconda.ui.gui.utils import set_treeview_selection, timed_action, override_cell_property
+
+import gi
+gi.require_version("Gtk", "3.0")
+gi.require_version("Pango", "1.0")
+gi.require_version("GdkPixbuf", "2.0")
+from gi.repository import Gtk, Pango, GdkPixbuf
 
 
 class LangLocaleHandler(object, metaclass=ABCMeta):
