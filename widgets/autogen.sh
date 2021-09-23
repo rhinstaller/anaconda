ef039f56bbd878133af40d230e1e8fc6f40a4ca7
Pavel Raiskup <praiskup@redhat.com>
2021-09-23 08:04:10 +0200

    Use the recommended autoreconf command

--- widgets/autogen.sh
+++ widgets/autogen.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -e
-[ -d m4 ] || mkdir m4
-libtoolize --copy --force
-aclocal -I m4
-autoconf
-autoheader --force
-automake --foreign --add-missing --copy
-rm -rf autom4te.cache
+
+autoreconf -vfi
 
 # Remove the old symlink if present
 if [ -h src/gettext.h ] ; then rm src/gettext.h ; fi
