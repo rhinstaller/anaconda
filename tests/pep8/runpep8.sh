5e4ae89b1dd8fea44a5bef590b3ab1626bc7b31c
Vladimir Slavik <vslavik@redhat.com>
2021-11-01 15:48:48 +0100

    Fix all Shellcheck warnings in tests

--- tests/pep8/runpep8.sh
+++ tests/pep8/runpep8.sh
@@ -5,14 +5,13 @@ if [ -z "$top_srcdir" ]; then
     top_srcdir="$(dirname "$0")/../.."
 fi
 
-. ${top_srcdir}/tests/testenv.sh
-. ${top_srcdir}/tests/pep8/config.target
+. "${top_srcdir}/tests/testenv.sh"
+. "${top_srcdir}/tests/pep8/config.target"
 
 pep8_command=""
 for cmd in pep8 pycodestyle pycodestyle-3
 do
-    which ${cmd} >/dev/null 2>&1
-    if [[ $? -eq 0 ]]; then
+    if which ${cmd} >/dev/null 2>&1 ; then
         pep8_command=${cmd}
     fi
 done
@@ -30,4 +29,4 @@ do
     PEP8_TARGET_PATHS+=" ${top_srcdir}/${target}"
 done
 
-${pep8_command} --config ${top_srcdir}/tests/pep8/config ${PEP8_TARGET_PATHS}
+${pep8_command} --config "${top_srcdir}/tests/pep8/config" "${PEP8_TARGET_PATHS}"
