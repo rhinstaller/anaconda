cd18bc55f9a01040a26afc9e3f0214bd98c3061a
Vladimir Slavik <vslavik@redhat.com>
2021-11-22 12:20:27 +0100

    Check for substring in POSIX compatible way
    Makes ShellCHeck happy & conforms to the hashbang expectations.
    
    Another alternative is:
     #!/bin/bash
     (...)
     shellcheck disable=SC2076

--- dracut/anaconda-copy-prefixdevname.sh
+++ dracut/anaconda-copy-prefixdevname.sh
@@ -2,7 +2,7 @@
 # Copy over persistent network device names to anaconda environment
 
 CMDLINE=$(cat /proc/cmdline)
-if [[ ${CMDLINE} =~ "net.ifnames.prefix=" ]]; then
+if [ -z "${CMDLINE##*net.ifnames.prefix=*}" ]; then
   mkdir -p /run/initramfs/state/etc/systemd/network
   cp /etc/systemd/network/*-net-ifnames-prefix* /run/initramfs/state/etc/systemd/network
 fi
