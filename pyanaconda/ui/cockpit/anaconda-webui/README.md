d36bc105de51445efbececcd2266167f46d45484
Katerina Koukiou <kkoukiou@redhat.com>
2021-12-03 11:54:08 +0100

    Introduce webui plugin base code

---
+++ pyanaconda/ui/cockpit/anaconda-webui/README.md
@@ -0,0 +1,29 @@
+# Anaconda Web UI
+
+# Getting and building the source
+
+Make sure you have `npm` available (usually from your distribution package).
+These commands check out the source and build it into the `dist/` directory:
+
+```
+git clone https://github.com/rhinstaller/anaconda.git
+cd anaconda/pyanaconda/ui/cockpit/anaconda-webui
+make
+```
+
+# Running eslint
+
+Anaconda Web UI uses [ESLint](https://eslint.org/) to automatically check
+JavaScript code style in `.js` and `.jsx` files.
+
+The linter is executed within every build as a webpack preloader.
+
+For developer convenience, the ESLint can be started explicitly by:
+
+    $ npm run eslint
+
+Violations of some rules can be fixed automatically by:
+
+    $ npm run eslint:fix
+
+Rules configuration can be found in the `.eslintrc.json` file.
---
+++ pyanaconda/ui/cockpit/anaconda-webui/webpack.config.js
@@ -0,0 +1,149 @@
+const path = require("path");
+
+const copy = require("copy-webpack-plugin");
+const extract = require("mini-css-extract-plugin");
+const TerserJSPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+const CockpitPoPlugin = require("./src/lib/cockpit-po-plugin");
+
+// HACK: OpenSSL 3 does not support md4 any more, but webpack hardcodes it all over the place: https://github.com/webpack/webpack/issues/13572
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+
+const webpack = require("webpack");
+
+/* A standard nodejs and webpack pattern */
+const production = process.env.NODE_ENV === 'production';
+
+// Non-JS files which are copied verbatim to dist/
+const copy_files = [
+    "./src/index.html",
+    "./src/manifest.json",
+];
+
+const plugins = [
+    new copy({ patterns: copy_files }),
+    new extract({filename: "[name].css"}),
+    new ESLintPlugin({ extensions: ["js", "jsx"] }),
+    new CockpitPoPlugin(),
+];
+
+/* Only minimize when in production mode */
+if (production) {
+    plugins.unshift(new CompressionPlugin({
+        test: /\.(js|html|css)$/,
+        deleteOriginalAssets: true
+    }));
+}
+
+module.exports = {
+    mode: production ? 'production' : 'development',
+    resolve: {
+        modules: [ "node_modules", path.resolve(__dirname, 'src/lib') ],
+        alias: { 'font-awesome': 'font-awesome-sass/assets/stylesheets' },
+    },
+    resolveLoader: {
+        modules: [ "node_modules", path.resolve(__dirname, 'src/lib') ],
+    },
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+    entry: {
+        index: "./src/index.js",
+    },
+    // cockpit.js gets included via <script>, everything else should be bundled
+    externals: { "cockpit": "cockpit" },
+    devtool: "source-map",
+    stats: "errors-warnings",
+
+    optimization: {
+        minimize: production,
+        minimizer: [
+            new TerserJSPlugin({
+                extractComments: {
+                    condition: true,
+                    filename: `[file].LICENSE.txt?query=[query]&filebase=[base]`,
+                    banner(licenseFile) {
+                        return `License information can be found in ${licenseFile}`;
+                    },
+                },
+            }),
+            new CssMinimizerPlugin()
+        ],
+    },
+
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                use: "babel-loader",
+                test: /\.(js|jsx)$/
+            },
+            /* HACK: remove unwanted fonts from PatternFly's css */
+            {
+                test: /patternfly-4-cockpit.scss$/,
+                use: [
+                    extract.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            url: false,
+                        },
+                    },
+                    {
+                        loader: 'string-replace-loader',
+                        options: {
+                            multiple: [
+                                {
+                                    search: /src:url\("patternfly-icons-fake-path\/pficon[^}]*/g,
+                                    replace: 'src:url("../base1/fonts/patternfly.woff") format("woff");',
+                                },
+                                {
+                                    search: /@font-face[^}]*patternfly-fonts-fake-path[^}]*}/g,
+                                    replace: '',
+                                },
+                            ]
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: !production,
+                            sassOptions: {
+                                outputStyle: production ? 'compressed' : undefined,
+                            },
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.s?css$/,
+                exclude: /patternfly-4-cockpit.scss/,
+                use: [
+                    extract.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            url: false
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: !production,
+                            sassOptions: {
+                                outputStyle: production ? 'compressed' : undefined,
+                            },
+                        },
+                    },
+                ]
+            },
+        ]
+    },
+    plugins: plugins
+}
