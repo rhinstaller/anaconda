84a71650a5e7eac7c26666c3d67bee11e804696a
Vladimir Slavik <vslavik@redhat.com>
2021-12-10 15:25:45 +0100

    Remove the root= kwarg of execInSysroot
    It is not used by anything anymore.

--- pyanaconda/core/util.py
+++ pyanaconda/core/util.py
@@ -365,18 +365,14 @@ def _run_program(argv, root='/', stdin=None, stdout=None, env_prune=None, log_ou
     return (proc.returncode, output_string)
 
 
-def execInSysroot(command, argv, stdin=None, root=None):
+def execInSysroot(command, argv, stdin=None):
     """ Run an external program in the target root.
         :param command: The command to run
         :param argv: The argument list
         :param stdin: The file object to read stdin from.
-        :param root: The directory to chroot to before running the command.
         :return: The return code of the command
     """
-    if root is None:
-        root = conf.target.system_root
-
-    return execWithRedirect(command, argv, stdin=stdin, root=root)
+    return execWithRedirect(command, argv, stdin=stdin, root=conf.target.system_root)
 
 
 def execWithRedirect(command, argv, stdin=None, stdout=None,
