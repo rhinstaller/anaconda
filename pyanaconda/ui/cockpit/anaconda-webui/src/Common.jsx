e8ced2e368c1dfb756f68e4a2435e9441dae56bb
Katerina Koukiou <kkoukiou@redhat.com>
2022-01-06 18:36:25 +0100

    webui: Introduce template react components for all configuration subpages
    Also start implementing the language selection intro menu and the root
    password subpage which can be now used as a code reference.
    
    react/prop-types rule was disabled in eslint, as I find that PropTypes
    add a lot of code and little help.
    
    Lastly introduce a new test file for the root account subpage, and since
    now we have more than one (2!!) test files, let's use the
    test/common/run-test wrapper for running the tests in CI.

---
+++ pyanaconda/ui/cockpit/anaconda-webui/src/Common.jsx
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2022 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with This program; If not, see <http://www.gnu.org/licenses/>.
+ */
+import React, { createContext } from 'react';
+
+import {
+    Button,
+    Level,
+    PageSection, PageSectionVariants,
+    Title,
+} from '@patternfly/react-core';
+
+import { usePageLocation } from 'hooks';
+
+export const AddressContext = createContext('');
+
+export const Header = ({ done, title }) => {
+    const { path } = usePageLocation();
+    const pageId = path[0];
+
+    return (
+        <PageSection variant={pageId === 'summary' ? PageSectionVariants.light : PageSectionVariants.darker}>
+            <Level hasGutter>
+                {pageId !== 'summary' &&
+                <Button
+                  id='header-done-btn'
+                  variant='primary'
+                  onClick={done}>
+                    Done
+                </Button>}
+                <Title headingLevel='h1' size='2xl'>
+                    {title || 'Subpage description'}
+                </Title>
+                <Button
+                  id={'help-btn-' + pageId}
+                  variant='secondary'
+                  onClick={() => console.log('I am on pageId ' + (pageId || 'summary'))}>
+                    Help
+                </Button>
+            </Level>
+        </PageSection>
+    );
+};
--- pyanaconda/ui/cockpit/anaconda-webui/src/app.jsx
+++ pyanaconda/ui/cockpit/anaconda-webui/src/app.jsx
@@ -1,76 +1,45 @@
 /*
- * This file is part of Cockpit.
- *
  * Copyright (C) 2021 Red Hat, Inc.
  *
- * Cockpit is free software; you can redistribute it and/or modify it
+ * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * Cockpit is distributed in the hope that it will be useful, but
+ * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ * along with This program; If not, see <http://www.gnu.org/licenses/>.
  */
-
 import cockpit from 'cockpit';
 import React, { useEffect, useState } from 'react';
+
 import {
-    Card, CardBody, CardTitle,
-    DescriptionList, DescriptionListGroup, DescriptionListTerm, DescriptionListDescription,
-    Switch,
+    Page,
 } from '@patternfly/react-core';
 
-import { useEvent, useObject } from 'hooks';
+import { InstallationLanguage } from './InstallationLanguage.jsx';
+import { Summary } from './Summary.jsx';
+import { AddressContext } from './Common.jsx';
+
+import { usePageLocation } from 'hooks';
 
 export const Application = () => {
-    const [timezoneProps, setTimezoneProps] = useState();
     const [address, setAddress] = useState();
+    const { path } = usePageLocation();
 
-    const timezoneProxy = useObject(() => {
-        const client = cockpit.dbus('org.fedoraproject.Anaconda.Modules.Timezone', { superuser: 'try', bus: 'none', address });
-        const proxy = client.proxy(
-            'org.fedoraproject.Anaconda.Modules.Timezone',
-            '/org/fedoraproject/Anaconda/Modules/Timezone',
-        );
-        setTimezoneProps(proxy.data);
-
-        return proxy;
-    }, null, [address]);
-
-    useEvent(timezoneProxy, 'changed', (event, data) => setTimezoneProps(data));
     useEffect(() => cockpit.file('/run/anaconda/bus.address').watch(setAddress), []);
 
     return (
-        <Card>
-            <CardTitle>Anaconda Web UI</CardTitle>
-            <CardBody>
-                {timezoneProps &&
-                <DescriptionList isHorizontal>
-                    {Object.keys(timezoneProps).map(prop => {
-                        return (
-                            <DescriptionListGroup key={prop}>
-                                <DescriptionListTerm>{prop}</DescriptionListTerm>
-                                <DescriptionListDescription>
-                                    {prop === 'NTPEnabled'
-                                        ? <Switch
-                                            id='switch-ntp-enabled'
-                                            isChecked={timezoneProps[prop]}
-                                            label='On'
-                                            labelOff='Off'
-                                            onChange={enabled => timezoneProxy.SetNTPEnabled(enabled)}
-                                        />
-                                        : timezoneProps[prop].toString()}
-                                </DescriptionListDescription>
-                            </DescriptionListGroup>
-                        );
-                    })}
-                </DescriptionList>}
-            </CardBody>
-        </Card>
+        <Page>
+            {!path.length > 0 && <InstallationLanguage onSelectLang={() => cockpit.location.go(['summary'])} />}
+            {path.length > 0 &&
+            <AddressContext.Provider value={address}>
+                <Summary />
+            </AddressContext.Provider>}
+        </Page>
     );
 };
