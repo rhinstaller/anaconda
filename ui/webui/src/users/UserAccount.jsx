6a47055697520f7c1acb182c41af575d8a0787c7
Vladimír Slávik <vslavik@redhat.com>
2022-01-13 18:39:36 +0100

    Basic functionality for user

--- ui/webui/src/users/UserAccount.jsx
+++ ui/webui/src/users/UserAccount.jsx
@@ -15,18 +15,67 @@
  * along with This program; If not, see <http://www.gnu.org/licenses/>.
  */
 import cockpit from "cockpit";
-import React from "react";
+import React, { useState, useContext, /* useEffect */ } from "react";
 
 import {
+    Form,
+    FormGroup,
+    TextInput,
     PageSection
 } from "@patternfly/react-core";
 
-import { Header } from "../Common.jsx";
+import { AddressContext, Header } from "../Common.jsx";
 
 const _ = cockpit.gettext;
 
+import { useEvent, useObject } from "hooks";
+
 export const UserAccount = () => {
+    const [name, setName] = useState();
+    const [gecos, setGecos] = useState();
+    const address = useContext(AddressContext);
+
+    const usersProxy = useObject(() => {
+        const client = cockpit.dbus("org.fedoraproject.Anaconda.Modules.Users", { superuser: "try", bus: "none", address });
+        const proxy = client.proxy(
+            "org.fedoraproject.Anaconda.Modules.Users",
+            "/org/fedoraproject/Anaconda/Modules/Users",
+        );
+
+        return proxy;
+    }, null, [address]);
+
+    const onUsernameChange = (value) => {
+        // console.log("onUsernameChange: " + value);
+        setName(value);
+    };
+
+    const onGecosChange = (value) => {
+        // console.log("onGecosChange: " + value);
+        setGecos(value);
+    };
+
+    useEvent(usersProxy, "changed", (event, data) => {
+        // setUiSomething(data.DBusProxySomething);
+    });
+
     const onDoneClicked = () => {
+        const user = {
+            gecos: cockpit.variant("s", gecos),
+            name: cockpit.variant("s", name),
+            uid: cockpit.variant("u", 0),
+            groups: cockpit.variant("as", ["wheel"]),
+            "uid-mode": cockpit.variant("s", "ID_MODE_USE_DEFAULT"),
+            gid: cockpit.variant("u", 0),
+            "gid-mode": cockpit.variant("s", "ID_MODE_USE_DEFAULT"),
+            homedir: cockpit.variant("s", ""),
+            password: cockpit.variant("s", ""),
+            "is-crypted": cockpit.variant("b", true),
+            lock: cockpit.variant("b", false),
+            shell: cockpit.variant("s", ""),
+        };
+        console.log("onDoneClicked, user: " + user);
+        usersProxy.SetUsers([user]);
         cockpit.location.go(["summary"]);
     };
 
@@ -37,7 +86,14 @@ export const UserAccount = () => {
               title={_("Create user")}
             />
             <PageSection>
-                Not implemented
+                <Form isHorizontal>
+                    <FormGroup label={_("Real name")}>
+                        <TextInput id="gecos" onChange={onGecosChange} placeholder="John Doe" />
+                    </FormGroup>
+                    <FormGroup label={_("User name")}>
+                        <TextInput id="name" onChange={onUsernameChange} placeholder="jdoe" />
+                    </FormGroup>
+                </Form>
             </PageSection>
         </>
     );
