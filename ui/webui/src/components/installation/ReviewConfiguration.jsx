baffda69765d7c5ba9624c331da4af4cb201a9ce
Martin Kolman <mkolman@redhat.com>
2022-03-28 20:17:14 +0200

    webui: Add Review screen implementation
    Add implementation of the Review screen based on latest mockups.
    
    The Review screen shows system configuration just before the
    installation is started. All the data on the screen is "live" and
    fetched over DBus from the Anaconda backend.
    
    There is also a "selected disks will be overwritten" checkbox that
    needs to be checked before the installation can be started.

--- ui/webui/src/components/installation/ReviewConfiguration.jsx
+++ ui/webui/src/components/installation/ReviewConfiguration.jsx
@@ -14,16 +14,125 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with This program; If not, see <http://www.gnu.org/licenses/>.
  */
-import React from "react";
+import cockpit from "cockpit";
+import React, { useEffect, useState } from "react";
 
 import {
-    Form
+    Checkbox, Title,
+    DataList, DataListItem,
+    DataListItemRow, DataListItemCells,
+    DataListCell,
+    DescriptionList, DescriptionListGroup,
+    DescriptionListTerm, DescriptionListDescription,
+    Flex, Popover,
 } from "@patternfly/react-core";
 
+import {
+    getSelectedDisks, getDeviceData,
+} from "../../apis/storage.js";
+
+import {
+    getLanguage, getLanguageData,
+} from "../../apis/localization.js";
+
+import { HelpIcon } from "@patternfly/react-icons";
+
+const _ = cockpit.gettext;
+
 export const ReviewConfiguration = () => {
+    const [deviceData, setDeviceData] = useState({});
+    const [selectedDisks, setSelectedDisks] = useState();
+    const [systemLanguage, setSystemLanguage] = useState();
+
+    useEffect(() => {
+        getLanguage()
+                .then(res => {
+                    getLanguageData({ lang: res }).then(res => {
+                        setSystemLanguage(res["native-name"].v);
+                    }, console.error);
+                }, console.error);
+        getSelectedDisks()
+                .then(res => {
+                    setSelectedDisks(res);
+                    // get detailed data for the selected disks
+                    res.forEach(disk => {
+                        getDeviceData({ disk })
+                                .then(res => {
+                                    setDeviceData(d => ({ ...d, [disk]: res[0] }));
+                                }, console.error);
+                    });
+                }, console.error);
+    }, []);
+
+    // handle case of disks not (yet) loaded
+    if (!selectedDisks || !systemLanguage) {
+        return null;
+    }
+
     return (
-        <Form isHorizontal>
-            Imagine configured values for review here.
-        </Form>
+        <>
+            <DescriptionList isHorizontal>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>
+                        {_("Language")}
+                    </DescriptionListTerm>
+                    <DescriptionListDescription id="installation-review-target-system-language">
+                        {systemLanguage}
+                    </DescriptionListDescription>
+                </DescriptionListGroup>
+            </DescriptionList>
+            <Title headingLevel="h3" size="xl">
+                {_("Installation destination")}
+            </Title>
+            <DataList>
+                {selectedDisks.map(selectedDisk => (
+                    <DataListItem key={selectedDisk}>
+                        <DataListItemRow>
+                            <DataListItemCells
+                              dataListCells={[
+                                  <DataListCell key={selectedDisk} id={"installation-review-disk-label-" + selectedDisk}>
+                                      {_("Local standard disk")}
+                                  </DataListCell>,
+                                  <DataListCell key={"description-" + selectedDisk} id={"installation-review-disk-description-" + selectedDisk}>
+                                      {deviceData && deviceData[selectedDisk] && deviceData[selectedDisk].description.v + " (" + selectedDisks + ")"}
+                                  </DataListCell>,
+                                  <DataListCell key={"size-" + selectedDisk} id={"installation-review-disk-size-" + selectedDisk}>
+                                      {cockpit.format_bytes(deviceData && deviceData[selectedDisk] && deviceData[selectedDisk].size.v) + " " + _("total")}
+                                  </DataListCell>
+                              ]}
+                            />
+                        </DataListItemRow>
+                    </DataListItem>
+                ))}
+            </DataList>
+        </>
+    );
+};
+
+export const ReviewConfigurationFooter = ({ isEraseChecked, setIsEraseChecked }) => {
+    return (
+        <Flex alignItems={{ default: "alignItemsCenter" }}>
+            <Checkbox
+              label={_("Erase disks to install")}
+              isChecked={isEraseChecked}
+              onChange={setIsEraseChecked}
+              id="installation-review-disk-erase-confirm"
+            />
+            <Popover
+              headerContent={
+                  _("All data on the listed disks will be erased")
+              }
+              bodyContent={
+                  _("Therefore it is recommended to only proceed if all data on these disks is either backed up or not important.")
+              }
+            >
+                <button
+                  type="button"
+                  className="pf-c-form__group-label-help"
+                >
+                    <HelpIcon />
+                </button>
+            </Popover>
+        </Flex>
     );
 };
