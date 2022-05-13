b444cc5bc4e246df332477042a4358030c16fd3d
Vladimir Slavik <vslavik@redhat.com>
2022-05-13 12:01:14 +0200

    Don't fall back to default timezone in welcome spoke
    The removed code was never called, because time zone value is always set to
    something. The default value is initialized in TimezoneService.
    
    We also don't have any reports of this being a problem, so let's remove it.

--- pyanaconda/ui/gui/spokes/welcome.py
+++ pyanaconda/ui/gui/spokes/welcome.py
@@ -118,7 +118,6 @@ class WelcomeLanguageSpoke(StandaloneSpoke, LangLocaleHandler):
         if flags.flags.automatedInstall and geoloc_result.is_empty():
             return
 
-        loc_timezones = localization.get_locale_timezones(self._l12_module.Language)
         if geoloc_result.timezone:
             # (the geolocation module makes sure that the returned timezone is
             # either a valid timezone or empty string)
@@ -131,11 +130,6 @@ class WelcomeLanguageSpoke(StandaloneSpoke, LangLocaleHandler):
             # enter the Date & Time spoke to acknowledge the timezone detected
             # by geolocation before continuing the installation.
             self._tz_module.Kickstarted = True
-        elif loc_timezones and not self._tz_module.Timezone:
-            # no data is provided by Geolocation, try to get timezone from the
-            # current language
-            log.info("geolocation not finished in time, using default timezone")
-            self._tz_module.Timezone = loc_timezones[0]
 
     @property
     def completed(self):
