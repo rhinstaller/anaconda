a68063e36d9057051f27b3a228fd01b896d2f6b8
Vendula Poncova <vponcova@redhat.com>
2022-06-03 13:37:14 +0200

    Remove unused pylint false positives (#infra)
    The FedoraGeoIPProvider and HostipGeoIPProvider classes were removed.

--- tests/pylint/runpylint.py
+++ tests/pylint/runpylint.py
@@ -24,8 +24,6 @@ class AnacondaLintConfig(CensorshipConfig):
 
         self.false_positives = [
             FalsePositive(r"^E1101.*: Instance of 'KickstartSpecificationHandler' has no '.*' member$"),
-            FalsePositive(r"^E1101.*: FedoraGeoIPProvider._refresh: Instance of 'LookupDict' has no 'ok' member"),
-            FalsePositive(r"^E1101.*: HostipGeoIPProvider._refresh: Instance of 'LookupDict' has no 'ok' member"),
             FalsePositive(r"^E1101.*: Method 'PropertiesChanged' has no 'connect' member"),
 
             # TODO: BlockDev introspection needs to be added to pylint to handle these
