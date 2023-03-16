e4b49ba403b44b9bbf753dcb55424910b8aad078
Vendula Poncova <vponcova@redhat.com>
2023-03-16 18:19:22 +0100

    Simplify the NFS source
    Use a new support for sources that provide access to a repository.
    The goal is to make this source usable by additional repositories.

--- pyanaconda/modules/payloads/source/nfs/nfs.py
+++ pyanaconda/modules/payloads/source/nfs/nfs.py
@@ -19,35 +19,32 @@
 #
 import os.path
 
+from pyanaconda.anaconda_loggers import get_module_logger
+from pyanaconda.core.constants import URL_TYPE_BASEURL
 from pyanaconda.core.i18n import _
 from pyanaconda.core.payload import create_nfs_url, parse_nfs_url
-from pyanaconda.core.signal import Signal
+from pyanaconda.modules.common.errors.general import InvalidValueError
 from pyanaconda.modules.common.structures.payload import RepoConfigurationData
 from pyanaconda.modules.payloads.constants import SourceType, SourceState
-from pyanaconda.modules.payloads.source.nfs.nfs_interface import NFSSourceInterface
-from pyanaconda.modules.payloads.source.nfs.initialization import SetUpNFSSourceTask
 from pyanaconda.modules.payloads.source.mount_tasks import TearDownMountTask
-from pyanaconda.modules.payloads.source.source_base import PayloadSourceBase, RPMSourceMixin
+from pyanaconda.modules.payloads.source.nfs.initialization import SetUpNFSSourceTask, \
+    SetUpNFSSourceResult
+from pyanaconda.modules.payloads.source.source_base import PayloadSourceBase, RPMSourceMixin, \
+    RepositorySourceMixin
+from pyanaconda.modules.payloads.source.source_base_interface import RepositorySourceInterface
 from pyanaconda.modules.payloads.source.utils import MountPointGenerator
 
-from pyanaconda.anaconda_loggers import get_module_logger
 log = get_module_logger(__name__)
 
 __all__ = ["NFSSourceModule"]
 
 
-class NFSSourceModule(PayloadSourceBase, RPMSourceMixin):
-    """The NFS source module.
-
-    TODO: Merge code from HardDriveSourceModule and this one.
-    TODO: Add install_tree_path property.
-    """
+class NFSSourceModule(PayloadSourceBase, RepositorySourceMixin, RPMSourceMixin):
+    """The NFS source module."""
 
     def __init__(self):
+        """Create a new source."""
         super().__init__()
-        self._url = ""
-        self.url_changed = Signal()
-        self._install_tree_path = ""
         self._device_mount = MountPointGenerator.generate_mount_point(
             self.type.value.lower() + "-device"
         )
@@ -55,12 +52,9 @@ class NFSSourceModule(PayloadSourceBase, RPMSourceMixin):
             self.type.value.lower() + "-iso"
         )
 
-    def __repr__(self):
-        return "Source(type='NFS', url='{}')".format(self.url)
-
     def for_publication(self):
         """Return a DBus representation."""
-        return NFSSourceInterface(self)
+        return RepositorySourceInterface(self)
 
     @property
     def type(self):
@@ -70,7 +64,9 @@ class NFSSourceModule(PayloadSourceBase, RPMSourceMixin):
     @property
     def description(self):
         """Get description of this source."""
-        return _("NFS server {}").format(self.url)
+        nfs = parse_nfs_url(self.configuration.url)
+        url = "{}:{}".format(nfs.host, nfs.path)
+        return _("NFS server {}").format(url)
 
     @property
     def network_required(self):
@@ -91,58 +87,43 @@ class NFSSourceModule(PayloadSourceBase, RPMSourceMixin):
 
     def get_state(self):
         """Get state of this source."""
-        res = os.path.ismount(self._device_mount) and bool(self._install_tree_path)
-        return SourceState.from_bool(res)
+        return SourceState.from_bool(
+            os.path.ismount(self._device_mount)
+            and bool(self._repository)
+        )
 
     def process_kickstart(self, data):
         """Process the kickstart data."""
-        nfs_url = create_nfs_url(data.nfs.server, data.nfs.dir, data.nfs.opts)
-        self.set_url(nfs_url)
+        configuration = RepoConfigurationData()
+        configuration.url = create_nfs_url(
+            data.nfs.server,
+            data.nfs.dir,
+            data.nfs.opts
+        )
+        self.set_configuration(configuration)
 
     def setup_kickstart(self, data):
         """Setup the kickstart data."""
-        (opts, host, path) = parse_nfs_url(self.url)
+        opts, host, path = parse_nfs_url(self.configuration.url)
 
         data.nfs.server = host
         data.nfs.dir = path
         data.nfs.opts = opts
         data.nfs.seen = True
 
-    def generate_repo_configuration(self):
-        """Generate RepoConfigurationData structure."""
-        return RepoConfigurationData.from_directory(self.install_tree_path)
+    def _validate_configuration(self, configuration):
+        """Validate the specified source configuration."""
+        if not configuration.url.startswith("nfs:"):
+            raise InvalidValueError(
+                "Invalid protocol of a NFS source: '{}'"
+                "".format(configuration.url)
+            )
 
-    @property
-    def url(self):
-        """URL for mounting.
-
-        Combines server address, path, and options.
-
-        :rtype: str
-        """
-        return self._url
-
-    def set_url(self, url):
-        """Set all NFS values with a valid URL.
-
-        Fires all signals.
-
-        :param url: URL
-        :type url: str
-        """
-        self._url = url
-        self.url_changed.emit()
-        log.debug("NFS URL is set to %s", self._url)
-
-    @property
-    def install_tree_path(self):
-        """Path to the install tree.
-
-        Read only, and available only after the setup task finishes successfully.
-
-        :rtype: str
-        """
-        return self._install_tree_path
+        if configuration.type != URL_TYPE_BASEURL:
+            raise InvalidValueError(
+                "Invalid URL type of a NFS source: '{}'"
+                "".format(configuration.type)
+            )
 
     def set_up_with_tasks(self):
         """Set up the installation source for installation.
@@ -150,9 +131,19 @@ class NFSSourceModule(PayloadSourceBase, RPMSourceMixin):
         :return: list of tasks required for the source setup
         :rtype: [Task]
         """
-        task = SetUpNFSSourceTask(self._device_mount, self._iso_mount, self._url)
-        task.succeeded_signal.connect(lambda: self._handle_setup_task_result(task))
+        task = SetUpNFSSourceTask(
+            self.configuration,
+            self._device_mount,
+            self._iso_mount,
+        )
+        task.succeeded_signal.connect(
+            lambda: self._on_set_up_succeeded(task.get_result())
+        )
         return [task]
+
+    def _on_set_up_succeeded(self, result: SetUpNFSSourceResult):
+        """Update the generated repository configuration."""
+        self._set_repository(result.repository)
 
     def tear_down_with_tasks(self):
         """Tear down the installation source.
@@ -166,7 +157,10 @@ class NFSSourceModule(PayloadSourceBase, RPMSourceMixin):
         ]
         return tasks
 
-    def _handle_setup_task_result(self, task):
-        result = task.get_result()
-        self._install_tree_path = result
-        log.debug("NFS install tree path is set to '%s'", self._install_tree_path)
+    def generate_repo_configuration(self):
+        """Generate RepoConfigurationData structure."""
+        return self.repository
+
+    def __repr__(self):
+        """Generate a string representation."""
+        return "Source(type='NFS', url='{}')".format(self.configuration.url)
