e38ec06277870bc5a90d8c105d7b399ebafa7b4d
Vladimír Slávik <vslavik@redhat.com>
2023-03-20 17:08:27 +0100

    Update ks imports and tests for new commands
    The pykickstart commands are still unused, but tests won't work unless
    the commands are properly plugged in.

--- pyanaconda/core/kickstart/commands.py
+++ pyanaconda/core/kickstart/commands.py
@@ -53,7 +53,8 @@ from pykickstart.commands.mount import F27_Mount as Mount
 from pykickstart.commands.network import F27_Network as Network
 from pykickstart.commands.nfs import FC6_NFS as NFS
 from pykickstart.commands.nvdimm import F28_Nvdimm as Nvdimm
-from pykickstart.commands.ostreesetup import F21_OSTreeSetup as OSTreeSetup
+from pykickstart.commands.ostreecontainer import F38_OSTreeContainer as OSTreeContainer
+from pykickstart.commands.ostreesetup import F38_OSTreeSetup as OSTreeSetup
 from pykickstart.commands.partition import F34_Partition as Partition
 from pykickstart.commands.raid import F29_Raid as Raid
 from pykickstart.commands.realm import F19_Realm as Realm
