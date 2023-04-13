49cf4eb8e2f1a10afdc5f20e0aaa2d24f843d711
Jelle van der Waa <jvanderwaa@redhat.com>
2023-04-13 12:15:11 +0200

    WebUI: introduce a new MachineCase subclass for VirtInstallMachine
    Avoid overriding the machine_class everywhere by introducing a
    VirtInstallMachineCase which also unifies the sys.path modifications
    into one place. As side-effect the helpers class is now pre-pended to
    sys.path so having python-installer installed no longer breaks running
    run-tests.

---
+++ ui/webui/test/anacondalib.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#
+# Copyright (C) 2023 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+
+# import Cockpit's machinery for test VMs and its browser test API
+TEST_DIR = os.path.dirname(__file__)
+sys.path.append(os.path.join(TEST_DIR, "common"))
+sys.path.insert(0, os.path.join(TEST_DIR, "helpers"))
+sys.path.append(os.path.join(os.path.dirname(TEST_DIR), "bots/machine"))
+
+from machine_install import VirtInstallMachine
+from testlib import MachineCase  # pylint: disable=import-error
+
+
+class VirtInstallMachineCase(MachineCase):
+    MachineCase.machine_class = VirtInstallMachine
