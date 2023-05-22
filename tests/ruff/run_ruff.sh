982029f49b07d808b29f39d705ce81380a7d09eb
Vladimír Slávik <vslavik@redhat.com>
2023-05-22 16:46:35 +0200

    Add ruff, a very fast linter
    It completes in a few seconds, as opposed to pylint's quarter of an hour.
    However, it is also hardly as thorough and correct.

---
+++ tests/ruff/run_ruff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# If $top_srcdir has not been set by automake, detect it
+if [ -z "$top_srcdir" ]; then
+    top_srcdir="$(realpath "$(dirname "$0")/../..")"
+fi
+
+ruff --version
+
+ruff check \
+  --config "$top_srcdir/tests/ruff/ruff.toml" \
+  "$top_srcdir/pyanaconda/" \
+  "$top_srcdir/tests/" \
+  "$top_srcdir/scripts/" \
+  "$top_srcdir/ui/webui/test/"
