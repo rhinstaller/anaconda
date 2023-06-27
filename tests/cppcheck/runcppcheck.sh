33508dd96ae8842e74d25d1b7f6c6dc0ff85b423
Jiri Konecny <jkonecny@redhat.com>
2023-06-27 11:17:21 +0200

    Inline testlib.sh file
    This was used only on one place so there is no need to have a separate
    library file.

--- tests/cppcheck/runcppcheck.sh
+++ tests/cppcheck/runcppcheck.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
+# Print a list of files to test on stdout
+# Takes filter arguments identical to the find utility, for example
+# findtestfiles -name '*.py'. Note that pruning directories will not
+# work since find is passed a list of filenames as the path arguments.
+findtestfiles()
+{
+    # If the test is being run from a git work tree, use a list of all files
+    # known to git
+    # shellcheck disable=SC2154
+    if [ -d "${top_srcdir}/.git" ]; then
+        findpath=$(git ls-files -c "${top_srcdir}")
+    # Otherwise list everything under $top_srcdir
+    else
+        findpath="${top_srcdir} -type f"
+    fi
+
+    # shellcheck disable=SC2086
+    find $findpath "$@"
+}
+
 # If $top_srcdir has not been set by automake, import the test environment
 if [ -z "$top_srcdir" ]; then
     top_srcdir="$(dirname "$0")/../.."
     . "${top_srcdir}/tests/testenv.sh"
 fi
-
-. "${top_srcdir}/tests/lib/testlib.sh"
 
 if ! type cppcheck > /dev/null 2>&1 ; then
     echo "SKIP - cppcheck must be installed to run it."
