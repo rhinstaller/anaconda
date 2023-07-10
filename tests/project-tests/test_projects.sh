0418eccdc7abbd8abb54ca61809192c420d886f9
Vladimír Slávik <vslavik@redhat.com>
2023-07-10 17:32:12 +0200

    Squashed 'translation-canary/' changes from 3bc2ad68a8..3a5b84bc96
    3a5b84bc96 Merge pull request #7 from VladimirSlavik/master-python-3.12
    f80a7809f0 Update translation canary test loader for python 3.12
    d6a4098517 Merge pull request #6 from VladimirSlavik/master-shellcheck-fixes
    2baf53f1cd Fix ShellCheck issues in translation_canary
    4d4e65b89e Merge pull request #5 from vojtechtrefny/master_merge-custom-changes
    9b236dd5dc Add pylint-log to .gitignore
    ca3af6872c Update list of projects to test after move from Zanata to Weblate
    977bad1f73 Ignore fallback ITS rule warning from gettext
    051b2ed5f3 Use "raise from" to link exceptions
    ea40f61a39 Remove Zanata from translation canary
    be9b933b14 Fix pylint errors in translation canary
    fccbb1bf38 Switch the license to LGPL
    
    git-subtree-dir: translation-canary
    git-subtree-split: 3a5b84bc96d66a8231a69f99de6200c541532a07

--- tests/project-tests/test_projects.sh
+++ tests/project-tests/test_projects.sh
@@ -3,17 +3,17 @@
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions of
-# the GNU General Public License v.2, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY expressed or implied, including the implied warranties of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-# Public License for more details.  You should have received a copy of the
-# GNU General Public License along with this program; if not, write to the
-# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
-# source code or documentation are not subject to the GNU General Public
-# License and may only be used or replicated with the express permission of
-# Red Hat, Inc.
+# the GNU Lesser General Public License v.2, or (at your option) any later
+# version. This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY expressed or implied, including the implied
+# warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU Lesser General Public License for more details.  You should have
+# received a copy of the GNU Lesser General Public License along with this
+# program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.  Any Red Hat trademarks
+# that are incorporated in the source code or documentation are not subject
+# to the GNU Lesser General Public License and may only be used or
+# replicated with the express permission of Red Hat, Inc.
 #
 # Red Hat Author(s): David Shea <dshea@redhat.com>
 
@@ -25,28 +25,20 @@ if [ $# -ne 1 ]; then
 fi
 
 status=0
-while read project_name branch ; do
-    echo "Testing $project_name:$branch"
+while read -r project_name l10n_git ; do
+    echo "Testing $project_name"
 
-    podir="$(mktemp -d ${project_name}-${branch}.XXXXXX)"
+    podir="$(mktemp -d "${project_name}".XXXXXX)"
 
-    # The zanata output only matters if something goes wrong
-    zanata_output="$(zanata pull --project-type gettext --project-id "$project_name" --project-version "$branch" --transdir "$podir" --url https://fedora.zanata.org/ 2>&1)"
-    if [ $? -ne 0 ]; then
-        echo "Zanata pull failed for $project_name:$branch"
-        echo "$zanata_output"
-        status=1
-        rm -rf "$podir"
-        continue
-    fi
+    # Download translations
+    git clone --depth 1 -- "$l10n_git" "$podir"
 
     # Ignore the percent-translated warnings
-    python3 -W ignore -m translation_canary.translated "$podir"
-    if [ $? -ne 0 ]; then
-        echo "Canary test failed for $project_name:$branch"
+    if ! python3 -W ignore -m translation_canary.translated "$podir"; then
+        echo "Canary test failed for $project_name"
         status=1
     else
-        echo "Success: $project_name:$branch"
+        echo "Success: $project_name"
     fi
 
     rm -rf "$podir"
--- translation_canary/translated/__init__.py
+++ translation_canary/translated/__init__.py
@@ -4,17 +4,17 @@
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions of
-# the GNU General Public License v.2, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY expressed or implied, including the implied warranties of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-# Public License for more details.  You should have received a copy of the
-# GNU General Public License along with this program; if not, write to the
-# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
-# source code or documentation are not subject to the GNU General Public
-# License and may only be used or replicated with the express permission of
-# Red Hat, Inc.
+# the GNU Lesser General Public License v.2, or (at your option) any later
+# version. This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY expressed or implied, including the implied
+# warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU Lesser General Public License for more details.  You should have
+# received a copy of the GNU Lesser General Public License along with this
+# program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.  Any Red Hat trademarks
+# that are incorporated in the source code or documentation are not subject
+# to the GNU Lesser General Public License and may only be used or
+# replicated with the express permission of Red Hat, Inc.
 #
 # Red Hat Author(s): David Shea <dshea@redhat.com>
 
@@ -34,13 +34,16 @@ _tests = []
 
 # Gather tests from this directory
 import pkgutil
+import importlib
+
 for finder, mod_name, _ispkg in pkgutil.iter_modules(__path__):
     # Skip __main__
     if mod_name == "__main__":
         continue
 
     # Load the module
-    module = finder.find_module(mod_name).load_module(mod_name)
+    full_name = "{}.{}".format(__name__, mod_name)
+    module = importlib.import_module(full_name)
 
     # Look for attributes that start with 'test_' and add them to the test list
     for attrname, attr in module.__dict__.items():
