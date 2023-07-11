964d3ad141bb53450f50e985ab78c0942075324e
Vladimír Slávik <vslavik@redhat.com>
2023-07-11 15:48:13 +0200

    Squashed 'translation-canary/' changes from 3bc2ad68a8..5bb81253b4
    5bb81253b4 Merge pull request #8 from VladimirSlavik/master-readme-subtree
    b8491107cf Add subtree usage to readme
    3a5b84bc96 Merge pull request #7 from VladimirSlavik/master-python-3.12
    f80a7809f0 Update translation canary test loader for python 3.12
    d6a4098517 Merge pull request #6 from VladimirSlavik/master-shellcheck-fixes
    2baf53f1cd Fix ShellCheck issues in translation_canary
    4d4e65b89e Merge pull request #5 from vojtechtrefny/master_merge-custom-changes
    9b236dd5dc Add pylint-log to .gitignore
    ca3af6872c Update list of projects to test after move from Zanata to Weblate
    977bad1f73 Ignore fallback ITS rule warning from gettext
    051b2ed5f3 Use "raise from" to link exceptions
    ea40f61a39 Remove Zanata from translation canary
    be9b933b14 Fix pylint errors in translation canary
    fccbb1bf38 Switch the license to LGPL
    
    git-subtree-dir: translation-canary
    git-subtree-split: 5bb81253b44b1facfacfe649067401c91cd08d9f

--- translation_canary/translated/test_markup.py
+++ translation_canary/translated/test_markup.py
@@ -7,17 +7,17 @@
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions of
-# the GNU General Public License v.2, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY expressed or implied, including the implied warranties of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-# Public License for more details.  You should have received a copy of the
-# GNU General Public License along with this program; if not, write to the
-# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
-# source code or documentation are not subject to the GNU General Public
-# License and may only be used or replicated with the express permission of
-# Red Hat, Inc.
+# the GNU Lesser General Public License v.2, or (at your option) any later
+# version. This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY expressed or implied, including the implied
+# warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU Lesser General Public License for more details.  You should have
+# received a copy of the GNU Lesser General Public License along with this
+# program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.  Any Red Hat trademarks
+# that are incorporated in the source code or documentation are not subject
+# to the GNU Lesser General Public License and may only be used or
+# replicated with the express permission of Red Hat, Inc.
 #
 # Red Hat Author(s): David Shea <dshea@redhat.com>
 
@@ -29,6 +29,7 @@ except ImportError:
 
 from pocketlint.pangocheck import is_markup, markup_match
 import xml.etree.ElementTree as ET
+
 
 def test_markup(pofile):
     po = polib.pofile(pofile)
@@ -46,18 +47,23 @@ def test_markup(pofile):
                 try:
                     # pylint: disable=unescaped-markup
                     ET.fromstring('<markup>%s</markup>' % msgstr)
-                except ET.ParseError:
+                except ET.ParseError as e:
                     if entry.msgid_plural:
-                        raise AssertionError("Invalid markup translation for %d translation of msgid %s\n%s" %
-                                (plural_id, entry.msgid, msgstr))
+                        raise AssertionError(
+                            "Invalid markup translation for {} translation of msgid {}\n{}".
+                            format(plural_id, entry.msgid, msgstr)) from e
                     else:
-                        raise AssertionError("Invalid markup translation for msgid %s\n%s" %
-                                (entry.msgid, msgstr))
+                        raise AssertionError(
+                            "Invalid markup translation for msgid {}\n{}".
+                            format(entry.msgid, msgstr)) from e
 
                 # Check if the markup has the same number and kind of tags
                 if not markup_match(entry.msgid, msgstr):
                     if entry.msgid_plural:
-                        raise AssertionError("Markup does not match for %d translation of msgid %s\n%s" %
-                                (plural_id, entry.msgid, msgstr))
+                        raise AssertionError(
+                            "Markup does not match for {} translation of msgid {}\n{}".
+                            format(plural_id, entry.msgid, msgstr))
                     else:
-                        raise AssertionError("Markup does not match for msgid %s\n%s" % (entry.msgid, msgstr))
+                        raise AssertionError(
+                            "Markup does not match for msgid {}\n{}".
+                            format(entry.msgid, msgstr))
