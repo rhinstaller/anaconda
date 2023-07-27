b6b3af9465802d9c3b681aa5f1795623b6d33c8e
Vladimír Slávik <vslavik@redhat.com>
2023-07-27 14:29:47 +0200

    make: Add l10n config file with SHA variable, use
    Note that getting only the desired SHA "downloaded" is complicated due to git
    and GitHub choices. Cloning fetches and checks out both at once, which is what
    the original code did. However, that is allowed only for named branches and
    tags.
    
    It is possible to configure the git *server* to allow this for arbitrary SHA,
    but it is neither default nor recommended, so GitHub does not allow this.
    
    So the actual series of calls is:
    - clone the repo = fetch only one ref, actually does not matter which one
    - fetch only the one commit that we want
    - check out that commit
    
    (cherry picked from commit 70d7814e6eccc1142298e08fb2c18f55c7c15d37)

---
+++ po/l10n-config.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2023  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# What to pull from the l10n repo when getting translations
+# This supports anything that git can use, but is intended to be a SHA of a commit that works.
+GIT_L10N_SHA ?= master
