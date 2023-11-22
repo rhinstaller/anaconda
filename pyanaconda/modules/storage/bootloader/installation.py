6a2da1f5fccc15226aff06046f26a81124e1e559
Vladimír Slávik <vslavik@redhat.com>
2023-11-22 13:35:07 +0100

    bootloader: Detect bootupd and skip regular install
    (cherry picked from commit 0d42d2fac55d2355e98334de08494dc0eb3d5a0b)
    
    Related: RHEL-2250

--- pyanaconda/modules/storage/bootloader/installation.py
+++ pyanaconda/modules/storage/bootloader/installation.py
@@ -20,6 +20,7 @@
 from blivet import arch
 from blivet.devices import BTRFSDevice
 from pyanaconda.core.constants import PAYLOAD_TYPE_RPM_OSTREE, PAYLOAD_LIVE_TYPES
+from pyanaconda.modules.payloads.payload.rpm_ostree.util import have_bootupd
 from pyanaconda.modules.storage.bootloader import BootLoaderError
 
 from pyanaconda.core.util import execInSysroot
@@ -108,11 +109,13 @@ class ConfigureBootloaderTask(Task):
 class InstallBootloaderTask(Task):
     """Installation task for the bootloader."""
 
-    def __init__(self, storage, mode):
+    def __init__(self, storage, mode, payload_type, sysroot):
         """Create a new task."""
         super().__init__()
         self._storage = storage
         self._mode = mode
+        self._payload_type = payload_type
+        self._sysroot = sysroot
 
     @property
     def name(self):
@@ -133,6 +136,10 @@ class InstallBootloaderTask(Task):
 
         if self._mode == BootloaderMode.SKIPPED:
             log.debug("The bootloader installation is skipped.")
+            return
+
+        if self._payload_type == PAYLOAD_TYPE_RPM_OSTREE and have_bootupd(self._sysroot):
+            log.debug("Will not install regular bootloader for ostree with bootupd")
             return
 
         try:
@@ -249,7 +256,9 @@ class FixBTRFSBootloaderTask(Task):
 
         InstallBootloaderTask(
             self._storage,
-            self._mode
+            self._mode,
+            self._payload_type,
+            self._sysroot
         ).run()
 
 
