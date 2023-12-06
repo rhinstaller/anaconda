6e18b20a20cb04098bdf0e820361378048db840d
Colin Walters <walters@verbum.org>
2023-12-06 10:58:56 -0500

    bootupd: Use --write-uuid
    This is an even newer behavior that takes over handling
    of the "UUID stamp files", which we want in general instead of
    using the static labels.
    
    Note `--write-uuid` implies `--with-static-configs`.
    
    This should fix this use case:
    
    ```
    clearpart --all --initlabel --disklabel=gpt
    reqpart --add-boot
    part / --grow --fstype xfs
    ```
    
    Whereas right now we require:
    
    ```
    clearpart --all --initlabel --disklabel=gpt
    reqpart
    part /boot --size=1000  --fstype=ext4 --label=boot
    part / --grow --fstype xfs
    ```
    
    Specifically the `--label=boot`.
    
    (cherry picked from commit 7b091defdda1dccb229a62188739d23b815a6b3f)
    
    Related: RHEL-2250

--- pyanaconda/modules/payloads/payload/rpm_ostree/installation.py
+++ pyanaconda/modules/payloads/payload/rpm_ostree/installation.py
@@ -437,7 +437,7 @@ class ConfigureBootloader(Task):
                 "backend",
                 "install",
                 "--auto",
-                "--with-static-configs",
+                "--write-uuid",
                 "--device",
                 dev_data.path,
                 "/",
