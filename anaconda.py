3ce535159b510a8887093f3dbf111d754a3b2c8e
Martin Kolman <mkolman@redhat.com>
2024-03-11 17:27:20 +0100

    Add RDP boot options & deprecate VNC boot options
    Add boot options for use with Remote Desktop Protocol,
    used to guide the installation remotely using the GUI.
    
    Also deprecate the VNC boot options.
    
    Resolves: RHEL-38407

--- anaconda.py
+++ anaconda.py
@@ -162,6 +162,20 @@ def setup_environment():
         os.environ["EDITOR"] = "/usr/bin/nano"
 
 
+def warn_on_deprecated_options(opts, log):
+    """Check if deprecated options have been used & log a warning."""
+
+    if opts.vnc:
+        log.warning("The vnc option has been deprecated, use the rdp and "
+                    "related options instead.")
+    if opts.vncconnect:
+        log.warning("The vncconnect option has been deprecated, use the rdp and "
+                    "related options instead.")
+    if opts.vncpassword:
+        log.warning("The vncpassword option has been deprecated, use the rdp and "
+                    "related options instead.")
+
+
 if __name__ == "__main__":
     # check if the CLI help is requested and return it at once,
     # without importing random stuff and spamming stdout
@@ -248,6 +262,9 @@ if __name__ == "__main__":
     if removed_no_inst_args:
         stdout_log.warning("All Anaconda kernel boot arguments are now required to use "
                            "'inst.' prefix!")
+
+    # log warning when deprecated options are used
+    warn_on_deprecated_options(opts, log)
 
     # print errors encountered during boot
     startup_utils.print_dracut_errors(stdout_log)
