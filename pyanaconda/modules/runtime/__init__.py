3ab96861a4c213db1cba56a816bf881c7204853d
Adam Kaňkovský <akankovs@redhat.com>
2024-03-26 10:24:36 +0100

    Dbus interface for runtime commands

---
+++ pyanaconda/modules/runtime/__init__.py
@@ -0,0 +1,3 @@
+from pyanaconda.modules.runtime.runtime_commands import RuntimeCommandsModule
+
+__all__ = ["RuntimeCommandsModule"]
--- pyanaconda/modules/runtime/runtime_interface.py
+++ pyanaconda/modules/runtime/runtime_interface.py
@@ -18,6 +18,7 @@
 # Red Hat, Inc.
 #
 from dasbus.server.interface import dbus_interface
+from dasbus.server.property import emits_properties_changed
 
 from pyanaconda.modules.common.constants.services import RUNTIME
 from pyanaconda.modules.common.base import KickstartModuleInterface
@@ -28,4 +29,97 @@ __all__ = ["RuntimeInterface"]
 @dbus_interface(RUNTIME.interface_name)
 class RuntimeInterface(KickstartModuleInterface):
     """DBus interface for the Runtime module."""
-    pass
+
+    def connect_signals(self):
+        """Connect the signals for runtime command module properties."""
+        super().connect_signals()
+        self.watch_property("LoggingHost", self.implementation.logging_host_changed)
+        self.watch_property("LoggingPort", self.implementation.logging_port_changed)
+        self.watch_property("Rescue", self.implementation.rescue_changed)
+        self.watch_property("RescueNoMount", self.implementation.rescue_nomount_changed)
+        self.watch_property("RescueRoMount", self.implementation.rescue_romount_changed)
+        self.watch_property("EULAAgreed", self.implementation.eula_agreed_changed)
+
+    @property
+    def LoggingHost(self) -> Str:
+        """The logging host for the installation process."""
+        return self.implementation.logging_host
+
+    @LoggingHost.setter
+    @emits_properties_changed
+    def LoggingHost(self, host: Str):
+        """Set the logging host for the installation process.
+
+        :param host: The host address as a string.
+        """
+        self.implementation.set_logging_host(host)
+
+    @property
+    def LoggingPort(self) -> Str:
+        """The logging port number."""
+        return self.implementation.logging_port
+
+    @LoggingPort.setter
+    @emits_properties_changed
+    def LoggingPort(self, port: Str):
+        """Set the logging port for the installation process.
+
+        :param port: The port number as a string.
+        """
+        self.implementation.set_logging_port(port)
+
+    @property
+    def Rescue(self) -> Bool:
+        """Flag indicating whether rescue mode is enabled."""
+        return self.implementation.rescue
+
+    @Rescue.setter
+    @emits_properties_changed
+    def Rescue(self, rescue: Bool):
+        """Enable or disable rescue mode.
+
+        :param rescue: A boolean value indicating rescue mode status.
+        """
+        self.implementation.set_rescue(rescue)
+
+    @property
+    def RescueNoMount(self) -> Bool:
+        """Flag for disabling mount in rescue mode."""
+        return self.implementation.rescue_nomount
+
+    @RescueNoMount.setter
+    @emits_properties_changed
+    def RescueNoMount(self, nomount: Bool):
+        """Set the nomount flag for rescue mode.
+
+        :param nomount: A boolean value.
+        """
+        self.implementation.set_rescue_nomount(nomount)
+
+    @property
+    def RescueRoMount(self) -> Bool:
+        """Flag for read-only mount in rescue mode."""
+        return self.implementation.rescue_romount
+
+    @RescueRoMount.setter
+    @emits_properties_changed
+    def RescueRoMount(self, romount: Bool):
+        """Enable or disable read-only mount in rescue mode.
+
+        :param romount: A boolean value.
+        """
+        self.implementation.set_rescue_romount(romount)
+
+    @property
+    def EULAAgreed(self) -> Bool:
+        """Flag indicating whether EULA was agreed to."""
+        return self.implementation.eula_agreed
+
+    @EULAAgreed.setter
+    @emits_properties_changed
+    def EULAAgreed(self, agreed: Bool):
+        """Set the EULA agreement flag.
+
+        :param agreed: A boolean value.
+        """
+        self.implementation.set_eula_agreed(agreed)
