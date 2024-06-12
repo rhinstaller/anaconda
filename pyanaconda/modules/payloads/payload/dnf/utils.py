ab84e4fd84d98dd360f82c3d96f56a33dce1a731
Pavla Kratochvilova <pkratoch@redhat.com>
2024-06-12 14:54:14 +0200

    Update TransactionItemState_ERROR import to libdnf5

--- pyanaconda/modules/payloads/payload/dnf/utils.py
+++ pyanaconda/modules/payloads/payload/dnf/utils.py
@@ -19,7 +19,7 @@ import fnmatch
 import hashlib
 
 import rpm
-from libdnf.transaction import TransactionItemState_ERROR
+from libdnf5.transaction import TransactionItemState_ERROR
 
 from pyanaconda.anaconda_loggers import get_module_logger
 from pyanaconda.core.configuration.anaconda import conf
