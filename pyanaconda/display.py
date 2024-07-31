a21ceb4e2b3f6795a9fb7fe2702bc27eb3216a8e
Martin Kolman <mkolman@redhat.com>
2024-07-31 14:30:38 +0200

    Check if text mode was actually requested by kickstart (#2293672)
    Provide information about text mode being requested via DBus property
    set at kickstart parsing time & check this property when deciding if
    the VNC question should be shown.
    
    Also cleanup the code a bit and fix a typo affecting vnc command parsing.
    
    Resolves: rhbz#2293672

--- pyanaconda/display.py
+++ pyanaconda/display.py
@@ -155,15 +155,20 @@ def check_vnc_can_be_started(anaconda):
                               (blivet.util.total_memory(), min_gui_ram))
         vnc_startup_possible = False
 
-    # disable VNC question if text mode is requested and this is a ks install
-    if anaconda.tui_mode and flags.automatedInstall:
-        error_messages.append("Not asking for VNC because of an automated install")
-        vnc_startup_possible = False
-
-    # disable VNC question if we were explicitly asked for text in kickstart
-    if anaconda.display_mode == constants.DisplayModes.TUI:
-        error_messages.append("Not asking for VNC because text mode was explicitly asked for in kickstart")
-        vnc_startup_possible = False
+    # if running in text mode, we might sometimes skip showing the VNC question
+    if anaconda.tui_mode:
+        # disable VNC question if we were explicitly asked for text mode in kickstart
+        ui_proxy = RUNTIME.get_proxy(USER_INTERFACE)
+        if ui_proxy.DisplayModeTextKickstarted:
+            error_messages.append(
+                "Not asking for VNC because text mode was explicitly asked for in kickstart"
+            )
+            vnc_startup_possible = False
+        # disable VNC question if text mode is requested and this is an automated kickstart
+        # installation
+        elif flags.automatedInstall:
+            error_messages.append("Not asking for VNC because of an automated install")
+            vnc_startup_possible = False
 
     # disable VNC question if we don't have network
     network_proxy = NETWORK.get_proxy()
