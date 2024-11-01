2fd98d3579a08f7324b998b32445b0c35bce12ff
Owen W. Taylor <otaylor@fishsoup.net>
2024-11-01 09:41:42 -0400

    Extract get_downloader_for_repo_configuration() utility function
    Extract the code for creating a "downloader" (prepared session.get()
    method) from the tree_info file - we'll use this when querying
    and downloading Flatpak repositories from remote sources as well.

--- pyanaconda/modules/payloads/base/utils.py
+++ pyanaconda/modules/payloads/base/utils.py
@@ -17,8 +17,13 @@
 # License and may only be used or replicated with the express permission of
 # Red Hat, Inc.
 #
+from functools import partial
+
 from pyanaconda.anaconda_loggers import get_module_logger
-from pyanaconda.core.payload import rpm_version_key
+from pyanaconda.core.configuration.anaconda import conf
+from pyanaconda.core.constants import NETWORK_CONNECTION_TIMEOUT, USER_AGENT
+from pyanaconda.core.payload import ProxyString, ProxyStringError, rpm_version_key
+from pyanaconda.modules.common.structures.payload import RepoConfigurationData
 
 log = get_module_logger(__name__)
 
@@ -26,3 +31,53 @@ log = get_module_logger(__name__)
 def sort_kernel_version_list(kernel_version_list):
     """Sort the given kernel version list."""
     kernel_version_list.sort(key=rpm_version_key)
+
+
+def get_downloader_for_repo_configuration(session, data: RepoConfigurationData):
+    """Get a configured session.get method.
+
+    :return: a partial function
+    """
+    # Prepare the SSL configuration.
+    ssl_enabled = conf.payload.verify_ssl and data.ssl_verification_enabled
+
+    # ssl_verify can be:
+    #   - the path to a cert file
+    #   - True, to use the system's certificates
+    #   - False, to not verify
+    ssl_verify = data.ssl_configuration.ca_cert_path or ssl_enabled
+
+    # ssl_cert can be:
+    #   - a tuple of paths to a client cert file and a client key file
+    #   - None
+    ssl_client_cert = data.ssl_configuration.client_cert_path or None
+    ssl_client_key = data.ssl_configuration.client_key_path or None
+    ssl_cert = (ssl_client_cert, ssl_client_key) if ssl_client_cert else None
+
+    # Prepare the proxy configuration.
+    proxy_url = data.proxy or None
+    proxies = {}
+
+    if proxy_url:
+        try:
+            proxy = ProxyString(proxy_url)
+            proxies = {
+                "http": proxy.url,
+                "https": proxy.url,
+                "ftp": proxy.url
+            }
+        except ProxyStringError as e:
+            log.debug("Failed to parse the proxy '%s': %s", proxy_url, e)
+
+    # Prepare headers.
+    headers = {"user-agent": USER_AGENT}
+
+    # Return a partial function.
+    return partial(
+        session.get,
+        headers=headers,
+        proxies=proxies,
+        verify=ssl_verify,
+        cert=ssl_cert,
+        timeout=NETWORK_CONNECTION_TIMEOUT
+    )
