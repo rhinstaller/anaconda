df929ff91434c4b13489b2b9f8d3a35bc89398f1
Owen W. Taylor <otaylor@fishsoup.net>
2024-12-17 13:11:41 -0500

    Extract pick_download_location(), calculate_required_space() utilities
    We'll need the functionality of picking a download location and
    calculating required space for Flatpak installation as well as for
    DNF installation, so move the utility functions into
    payloads/base/utils.py.
    
    Resolves: RHEL-75804

--- pyanaconda/modules/payloads/base/utils.py
+++ pyanaconda/modules/payloads/base/utils.py
@@ -17,12 +17,19 @@
 # License and may only be used or replicated with the express permission of
 # Red Hat, Inc.
 #
+import os
 from functools import partial
+
+from blivet.size import Size
 
 from pyanaconda.anaconda_loggers import get_module_logger
 from pyanaconda.core.configuration.anaconda import conf
 from pyanaconda.core.constants import NETWORK_CONNECTION_TIMEOUT, USER_AGENT
+from pyanaconda.core.path import join_paths
 from pyanaconda.core.payload import ProxyString, ProxyStringError, rpm_version_key
+from pyanaconda.core.util import execWithCapture
+from pyanaconda.modules.common.constants.objects import DEVICE_TREE
+from pyanaconda.modules.common.constants.services import STORAGE
 from pyanaconda.modules.common.structures.payload import RepoConfigurationData
 
 log = get_module_logger(__name__)
@@ -81,3 +88,187 @@ def get_downloader_for_repo_configuration(session, data: RepoConfigurationData):
         cert=ssl_cert,
         timeout=NETWORK_CONNECTION_TIMEOUT
     )
+
+
+def get_free_space_map(current=True, scheduled=False):
+    """Get the available file system disk space.
+
+    :param bool current: use information about current mount points
+    :param bool scheduled: use information about scheduled mount points
+    :return: a dictionary of mount points and their available space
+    """
+    mount_points = {}
+
+    if scheduled:
+        mount_points.update(_get_scheduled_free_space_map())
+
+    if current:
+        mount_points.update(_get_current_free_space_map())
+
+    return mount_points
+
+
+def _get_current_free_space_map():
+    """Get the available file system disk space of the current system.
+
+    :return: a dictionary of mount points and their available space
+    """
+    mapping = {}
+
+    # Generate the dictionary of mount points and sizes.
+    output = execWithCapture('df', ['--output=target,avail'])
+    lines = output.rstrip().splitlines()
+
+    for line in lines:
+        key, val = line.rsplit(maxsplit=1)
+
+        if not key.startswith('/'):
+            continue
+
+        mapping[key] = Size(int(val) * 1024)
+
+    # Add /var/tmp/ if this is a directory or image installation.
+    if not conf.target.is_hardware:
+        var_tmp = os.statvfs("/var/tmp")
+        mapping["/var/tmp"] = Size(var_tmp.f_frsize * var_tmp.f_bfree)
+
+    return mapping
+
+
+def _get_scheduled_free_space_map():
+    """Get the available file system disk space of the scheduled system.
+
+    :return: a dictionary of mount points and their available space
+    """
+    device_tree = STORAGE.get_proxy(DEVICE_TREE)
+    mount_points = {}
+
+    for mount_point in device_tree.GetMountPoints():
+        # we can ignore swap
+        if not mount_point.startswith('/'):
+            continue
+
+        free_space = Size(
+            device_tree.GetFileSystemFreeSpace([mount_point])
+        )
+        mount_point = os.path.normpath(
+            conf.target.system_root + mount_point
+        )
+        mount_points[mount_point] = free_space
+
+    return mount_points
+
+
+def _pick_mount_points(mount_points, download_size, install_size):
+    """Pick mount points for the package installation.
+
+    :return: a set of sufficient mount points
+    """
+    suitable = {
+        '/var/tmp',
+        conf.target.system_root,
+        join_paths(conf.target.system_root, 'home'),
+        join_paths(conf.target.system_root, 'tmp'),
+        join_paths(conf.target.system_root, 'var'),
+    }
+
+    sufficient = set()
+
+    for mount_point, size in mount_points.items():
+        # Ignore mount points that are not suitable.
+        if mount_point not in suitable:
+            continue
+
+        if size >= (download_size + install_size):
+            log.debug("Considering %s (%s) for download and install.", mount_point, size)
+            sufficient.add(mount_point)
+
+        elif size >= download_size and not mount_point.startswith(conf.target.system_root):
+            log.debug("Considering %s (%s) for download.", mount_point, size)
+            sufficient.add(mount_point)
+
+    return sufficient
+
+
+def _get_biggest_mount_point(mount_points, sufficient):
+    """Get the biggest sufficient mount point.
+
+    :return: a mount point or None
+    """
+    return max(sufficient, default=None, key=mount_points.get)
+
+
+def pick_download_location(download_size, install_size, cache_dir_suffix):
+    """Pick a download location
+
+    :param dnf_manager: the DNF manager
+    :return: a path to the download location
+    """
+    mount_points = get_free_space_map()
+
+    # Try to find mount points that are sufficient for download and install.
+    sufficient = _pick_mount_points(
+        mount_points,
+        download_size,
+        install_size
+    )
+
+    # Or find mount points that are sufficient only for download.
+    if not sufficient:
+        sufficient = _pick_mount_points(
+            mount_points,
+            download_size,
+            install_size=0
+        )
+
+    # Ignore the system root if there are other mount points.
+    if len(sufficient) > 1:
+        sufficient.discard(conf.target.system_root)
+
+    if not sufficient:
+        raise RuntimeError(
+            "Not enough disk space to download the "
+            "packages; size {}.".format(download_size)
+        )
+
+    # Choose the biggest sufficient mount point.
+    mount_point = _get_biggest_mount_point(mount_points, sufficient)
+
+    log.info("Mount point %s picked as download location", mount_point)
+    location = join_paths(mount_point, cache_dir_suffix)
+
+    return location
+
+
+def calculate_required_space(download_size, installation_size):
+    """Calculate the space required for the installation.
+
+    This takes into account whether the download location is part of the installed
+    system or not.
+
+    :param Size download_size: the download size
+    :param Size installation_size: the installation size
+    :return Size: the required space
+    """
+    mount_points = get_free_space_map(scheduled=True)
+
+    # Find sufficient mount points.
+    sufficient = _pick_mount_points(
+        mount_points,
+        download_size,
+        installation_size
+    )
+
+    # Choose the biggest sufficient mount point.
+    mount_point = _get_biggest_mount_point(mount_points, sufficient)
+
+    if not mount_point or mount_point.startswith(conf.target.system_root):
+        log.debug("The install and download space is required.")
+        required_space = installation_size + download_size
+    else:
+        log.debug("Use the %s mount point for the %s download.", mount_point, download_size)
+        log.debug("Only the install space is required.")
+        required_space = installation_size
+
+    log.debug("The package installation requires %s.", required_space)
+    return required_space
--- tests/unit_tests/pyanaconda_tests/modules/payloads/payload/test_module_payload_dnf_utils.py
+++ tests/unit_tests/pyanaconda_tests/modules/payloads/payload/test_module_payload_dnf_utils.py
@@ -16,36 +16,25 @@
 # Red Hat, Inc.
 #
 import unittest
-from textwrap import dedent
 from unittest.mock import Mock, patch
-
-import pytest
-from blivet.size import Size
 
 from pyanaconda.core.constants import (
     GROUP_PACKAGE_TYPES_ALL,
     GROUP_PACKAGE_TYPES_REQUIRED,
 )
-from pyanaconda.modules.common.constants.objects import DEVICE_TREE
-from pyanaconda.modules.common.constants.services import STORAGE
 from pyanaconda.modules.common.structures.packages import PackagesSelectionData
 from pyanaconda.modules.common.structures.payload import RepoConfigurationData
 from pyanaconda.modules.payloads.payload.dnf.dnf_manager import DNFManager
 from pyanaconda.modules.payloads.payload.dnf.utils import (
-    _pick_mount_points,
-    calculate_required_space,
     collect_installation_devices,
-    get_free_space_map,
     get_installation_specs,
     get_kernel_package,
     get_kernel_version_list,
     get_product_release_version,
-    pick_download_location,
 )
 from pyanaconda.modules.payloads.source.cdrom.cdrom import CdromSourceModule
 from pyanaconda.modules.payloads.source.harddrive.harddrive import HardDriveSourceModule
 from pyanaconda.modules.payloads.source.url.url import URLSourceModule
-from tests.unit_tests.pyanaconda_tests import patch_dbus_get_proxy_with_cache
 
 
 class DNFUtilsPackagesTestCase(unittest.TestCase):
@@ -204,225 +193,6 @@ class DNFUtilsPackagesTestCase(unittest.TestCase):
             '7.8.16-200.fc32.x86_64',
             '8.8.18-200.fc32.x86_64',
         ]
-
-    @patch("pyanaconda.modules.payloads.payload.dnf.utils.execWithCapture")
-    def test_get_free_space(self, exec_mock):
-        """Test the get_free_space function."""
-        output = """
-        Mounted on        Avail
-        /dev                100
-        /dev/shm            200
-        /run                300
-        /                   400
-        /tmp                500
-        /boot               600
-        /home               700
-        /boot/efi           800
-        """
-        exec_mock.return_value = dedent(output).strip()
-
-        assert get_free_space_map() == {
-            '/dev': Size("100 KiB"),
-            '/dev/shm': Size("200 KiB"),
-            '/run': Size("300 KiB"),
-            '/': Size("400 KiB"),
-            '/tmp': Size("500 KiB"),
-            '/boot': Size("600 KiB"),
-            '/home': Size("700 KiB"),
-            '/boot/efi': Size("800 KiB"),
-        }
-
-    @patch("os.statvfs")
-    @patch("pyanaconda.modules.payloads.payload.dnf.utils.conf")
-    @patch("pyanaconda.modules.payloads.payload.dnf.utils.execWithCapture")
-    def test_get_free_space_image(self, exec_mock, conf_mock, statvfs_mock):
-        """Test the get_free_space function."""
-        output = """
-        Mounted on        Avail
-        /                   100
-        /boot               200
-        """
-        exec_mock.return_value = dedent(output).strip()
-        conf_mock.target.is_hardware = False
-        statvfs_mock.return_value = Mock(f_frsize=1024, f_bfree=300)
-
-        assert get_free_space_map() == {
-            '/': Size("100 KiB"),
-            '/boot': Size("200 KiB"),
-            '/var/tmp': Size("300 KiB"),
-        }
-
-    def test_pick_mount_points(self):
-        """Test the _pick_mount_points function."""
-        mount_points = {
-            "/": Size("1 G"),
-            "/home": Size("1 G"),
-            "/var/tmp": Size("1 G"),
-            "/mnt/sysroot": Size("1 G"),
-            "/mnt/sysroot/home": Size("1 G"),
-            "/mnt/sysroot/tmp": Size("1 G"),
-            "/mnt/sysroot/var": Size("1 G"),
-            "/mnt/sysroot/usr": Size("1 G"),
-
-        }
-
-        # All mount points are big enough.
-        # Choose all suitable mount points.
-        sufficient = _pick_mount_points(
-            mount_points,
-            download_size=Size("0.5 G"),
-            install_size=Size("0.5 G")
-        )
-        assert sufficient == {
-            "/var/tmp",
-            "/mnt/sysroot",
-            "/mnt/sysroot/home",
-            "/mnt/sysroot/tmp",
-            "/mnt/sysroot/var"
-        }
-
-        # No mount point is big enough for installation.
-        # Choose non-sysroot mount points for download.
-        sufficient = _pick_mount_points(
-            mount_points,
-            download_size=Size("0.5 G"),
-            install_size=Size("1.5 G")
-        )
-        assert sufficient == {
-            "/var/tmp",
-        }
-
-        # No mount point is big enough for installation or download.
-        sufficient = _pick_mount_points(
-            mount_points,
-            download_size=Size("1.5 G"),
-            install_size=Size("1.5 G")
-        )
-        assert sufficient == set()
-
-    @patch("pyanaconda.modules.payloads.payload.dnf.utils.get_free_space_map")
-    def test_pick_download_location(self, free_space_getter):
-        """Test the pick_download_location function."""
-        download_size = Size(100)
-        installation_size = Size(200)
-        total_size = Size(300)
-
-        dnf_manager = Mock()
-        dnf_manager.get_download_size.return_value = download_size
-        dnf_manager.get_installation_size.return_value = installation_size
-
-        # Found mount points for download and install.
-        # Don't use /mnt/sysroot if possible.
-        free_space_getter.return_value = {
-            "/var/tmp": download_size,
-            "/mnt/sysroot": total_size,
-        }
-
-        path = pick_download_location(dnf_manager)
-        assert path == "/var/tmp/dnf.package.cache"
-
-        # Found mount points only for download.
-        # Use the biggest mount point.
-        free_space_getter.return_value = {
-            "/mnt/sysroot/tmp": download_size + 1,
-            "/mnt/sysroot/home": download_size,
-        }
-
-        path = pick_download_location(dnf_manager)
-        assert path == "/mnt/sysroot/tmp/dnf.package.cache"
-
-        # No mount point to use.
-        # Fail with an exception.
-        free_space_getter.return_value = {}
-
-        with pytest.raises(RuntimeError) as cm:
-            pick_download_location(dnf_manager)
-
-        msg = "Not enough disk space to download the packages; size 100 B."
-        assert str(cm.value) == msg
-
-    @patch("pyanaconda.modules.payloads.payload.dnf.utils.execWithCapture")
-    @patch_dbus_get_proxy_with_cache
-    def test_get_combined_free_space(self, proxy_getter, exec_mock):
-        """Test the get_free_space function with the combined options."""
-        output = """
-        Mounted on        Avail
-        /                   100
-        /tmp                200
-        """
-        exec_mock.return_value = dedent(output).strip()
-
-        mount_points = {
-            '/': Size("300 KiB"),
-            '/boot': Size("400 KiB"),
-        }
-
-        def get_mount_points():
-            return list(mount_points.keys())
-
-        def get_free_space(paths):
-            return sum(map(mount_points.get, paths))
-
-        device_tree = STORAGE.get_proxy(DEVICE_TREE)
-        device_tree.GetMountPoints.side_effect = get_mount_points
-        device_tree.GetFileSystemFreeSpace.side_effect = get_free_space
-
-        assert get_free_space_map(current=True, scheduled=False) == {
-            '/': Size("100 KiB"),
-            '/tmp': Size("200 KiB"),
-        }
-
-        assert get_free_space_map(current=False, scheduled=True) == {
-            '/mnt/sysroot': Size("300 KiB"),
-            '/mnt/sysroot/boot': Size("400 KiB"),
-        }
-
-        assert get_free_space_map(current=True, scheduled=True) == {
-            '/': Size("100 KiB"),
-            '/tmp': Size("200 KiB"),
-            '/mnt/sysroot': Size("300 KiB"),
-            '/mnt/sysroot/boot': Size("400 KiB"),
-        }
-
-        assert get_free_space_map(current=False, scheduled=False) == {}
-
-    @patch("pyanaconda.modules.payloads.payload.dnf.utils.get_free_space_map")
-    def test_calculate_required_space(self, free_space_getter):
-        """Test the calculate_required_space function."""
-        download_size = Size(100)
-        installation_size = Size(200)
-        total_size = Size(300)
-
-        dnf_manager = Mock()
-        dnf_manager.get_download_size.return_value = download_size
-        dnf_manager.get_installation_size.return_value = installation_size
-
-        # No mount point to use.
-        # The total size is required.
-        free_space_getter.return_value = {}
-        assert calculate_required_space(dnf_manager) == total_size
-
-        # Found a mount point for download and install.
-        # The total size is required.
-        free_space_getter.return_value = {
-            "/mnt/sysroot/home": total_size
-        }
-        assert calculate_required_space(dnf_manager) == total_size
-
-        # Found a mount point for download.
-        # The installation size is required.
-        free_space_getter.return_value = {
-            "/var/tmp": download_size
-        }
-        assert calculate_required_space(dnf_manager) == installation_size
-
-        # The biggest mount point can be used for download and install.
-        # The total size is required.
-        free_space_getter.return_value = {
-            "/var/tmp": download_size,
-            "/mnt/sysroot": total_size
-        }
-        assert calculate_required_space(dnf_manager) == total_size
 
     def test_collect_installation_devices(self):
         """Test the collect_installation_devices function."""
