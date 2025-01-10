120fd8c35c84193e82985a6771eede6186177789
Jiri Konecny <jkonecny@redhat.com>
2025-01-10 17:37:11 +0100

    Split LocaledWrapper code for compositor
    Let's use inheritance to show which code is used for installation and
    what for compositor control. It increases the readability and improve
    modifications for the future use. To make it correct it is split into
    Base class and two specific classes. That way it is crystal clear what
    is used for what and there should not be constant fighting between
    different usages and side effects.
    
    As a side effect this change will fix a potential issue when installed
    system which doesn't selected `options` during the installation will
    use options from the running system. This is harder to achieve state but
    it could happen.

--- tests/unit_tests/pyanaconda_tests/modules/localization/test_localed_wrapper.py
+++ tests/unit_tests/pyanaconda_tests/modules/localization/test_localed_wrapper.py
@@ -20,7 +20,7 @@ from unittest.mock import Mock, patch
 
 from pyanaconda.core.glib import Variant
 from pyanaconda.core.signal import Signal
-from pyanaconda.modules.localization.localed import LocaledWrapper
+from pyanaconda.modules.localization.localed import CompositorLocaledWrapper, LocaledWrapper
 
 
 class LocaledWrapperTestCase(unittest.TestCase):
@@ -68,7 +68,6 @@ class LocaledWrapperTestCase(unittest.TestCase):
             "cz"
         assert localed_wrapper.layouts_variants == \
             ["cz (qwerty)", "fi", "us (euro)", "fr"]
-        assert localed_wrapper.current_layout_variant == "cz (qwerty)"
         assert localed_wrapper.options == \
             ["grp:alt_shift_toggle", "grp:ctrl_alt_toggle"]
 
@@ -79,7 +78,6 @@ class LocaledWrapperTestCase(unittest.TestCase):
         assert localed_wrapper.keymap == ""
         assert localed_wrapper.options == []
         assert localed_wrapper.layouts_variants == []
-        assert localed_wrapper.current_layout_variant == ""
 
     @patch("pyanaconda.modules.localization.localed.SystemBus")
     @patch("pyanaconda.modules.localization.localed.LOCALED")
@@ -118,13 +116,110 @@ class LocaledWrapperTestCase(unittest.TestCase):
         localed_wrapper.set_and_convert_layouts(["us-altgr-intl"])
         localed_wrapper.convert_layouts(["us-altgr-intl"])
 
-        # verify that user defined list doesn't change
-        localed_wrapper._user_layouts_variants = []
-        localed_wrapper.set_keymap("cz")
-        localed_wrapper.convert_keymap("cz")
-        localed_wrapper.set_and_convert_keymap("cz")
-        assert localed_wrapper._user_layouts_variants == []
-        # only set_layouts should change user defined layouts
+        # test set_layout on proxy with options
+        mocked_localed_proxy.SetX11Keyboard.reset_mock()
+        localed_wrapper.set_layouts(["cz (qwerty)", "us"])
+        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
+            "cz,us",
+            "pc105",  # hardcoded
+            "qwerty,",
+            "",
+            False,
+            False
+        )
+
+        # test set_layout on proxy with options not set explicitly (None)
+        mocked_localed_proxy.SetX11Keyboard.reset_mock()
+        localed_wrapper.set_layouts(["cz (qwerty)", "us"], options=None)
+        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
+            "cz,us",
+            "pc105",  # hardcoded
+            "qwerty,",
+            "",
+            False,
+            False
+        )
+
+        mocked_localed_proxy.SetX11Keyboard.reset_mock()
+        localed_wrapper.set_layouts(["us"], "", True)
+        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
+            "us",
+            "pc105",  # hardcoded
+            "",
+            "",  # empty options will remove existing options
+            True,
+            False
+        )
+
+    @patch("pyanaconda.modules.localization.localed.SystemBus")
+    def test_localed_wrapper_no_systembus(self, mocked_system_bus):
+        """Test LocaledWrapper in environment without system bus.
+
+        Which is also the environment of our tests.
+        """
+        # Emulates mock environment
+        mocked_system_bus.check_connection.return_value = False
+        localed_wrapper = LocaledWrapper()
+        self._guarded_localed_wrapper_calls_check(localed_wrapper)
+
+
+class CompositorLocaledWrapperTestCase(LocaledWrapperTestCase):
+
+    @patch("pyanaconda.modules.localization.localed.SystemBus")
+    @patch("pyanaconda.modules.localization.localed.LOCALED")
+    @patch("pyanaconda.modules.localization.localed.conf")
+    def test_compositor_localed_wrapper_properties(
+        self, mocked_conf, mocked_localed_service, mocked_system_bus
+    ):
+        """Test conversion of return values from Localed service to CompositorLocaledWraper."""
+        mocked_system_bus.check_connection.return_value = True
+        mocked_conf.system.provides_system_bus = True
+        mocked_localed_proxy = Mock()
+        mocked_localed_service.get_proxy.return_value = mocked_localed_proxy
+        localed_wrapper = CompositorLocaledWrapper()
+        mocked_localed_proxy.VConsoleKeymap = "cz"
+        mocked_localed_proxy.X11Layout = "cz,fi,us,fr"
+        mocked_localed_proxy.X11Variant = "qwerty,,euro"
+        mocked_localed_proxy.X11Options = "grp:alt_shift_toggle,grp:ctrl_alt_toggle"
+        assert localed_wrapper.layouts_variants == \
+            ["cz (qwerty)", "fi", "us (euro)", "fr"]
+        assert localed_wrapper.current_layout_variant == "cz (qwerty)"
+        assert localed_wrapper.options == \
+            ["grp:alt_shift_toggle", "grp:ctrl_alt_toggle"]
+
+        mocked_localed_proxy.VConsoleKeymap = ""
+        mocked_localed_proxy.X11Layout = ""
+        mocked_localed_proxy.X11Variant = ""
+        mocked_localed_proxy.X11Options = ""
+        assert localed_wrapper.options == []
+        assert localed_wrapper.layouts_variants == []
+        assert localed_wrapper.current_layout_variant == ""
+
+    @patch("pyanaconda.modules.localization.localed.SystemBus")
+    @patch("pyanaconda.modules.localization.localed.LOCALED")
+    @patch("pyanaconda.modules.localization.localed.conf")
+    def test_compositor_localed_wrapper_safe_calls(
+        self, mocked_conf, mocked_localed_service, mocked_system_bus
+    ):
+        """Test calling CopmositorLocaledWrapper with invalid values does not raise exception."""
+        mocked_system_bus.check_connection.return_value = True
+        mocked_conf.system.provides_system_bus = True
+        mocked_localed_proxy = Mock()
+        mocked_localed_service.get_proxy.return_value = mocked_localed_proxy
+        mocked_localed_proxy.VConsoleKeymap = "cz"
+        mocked_localed_proxy.X11Layout = "cz,fi,us,fr"
+        mocked_localed_proxy.X11Variant = "qwerty,,euro"
+        mocked_localed_proxy.X11Options = "grp:alt_shift_toggle,grp:ctrl_alt_toggle"
+        localed_wrapper = CompositorLocaledWrapper()
+        # valid values
+        localed_wrapper.set_layouts(["cz (qwerty)", "us (euro)"],
+                                    options="grp:alt_shift_toggle",
+                                    convert=True)
+        # invalid values
+        # rhbz#1843379
+        localed_wrapper.set_layouts(["us-altgr-intl"])
+
+        # set_layouts should change user defined layouts
         localed_wrapper.set_layouts(["cz", "us (euro)"])
         assert localed_wrapper._user_layouts_variants == ["cz", "us (euro)"]
 
@@ -164,111 +259,12 @@ class LocaledWrapperTestCase(unittest.TestCase):
         )
 
     @patch("pyanaconda.modules.localization.localed.SystemBus")
-    def test_localed_wrapper_no_systembus(self, mocked_system_bus):
-        """Test LocaledWrapper in environment without system bus.
-
-        Which is also the environment of our tests.
-        """
-        # Emulates mock environment
-        mocked_system_bus.check_connection.return_value = False
-        localed_wrapper = LocaledWrapper()
-        self._guarded_localed_wrapper_calls_check(localed_wrapper)
-
-    @patch("pyanaconda.modules.localization.localed.SystemBus")
     @patch("pyanaconda.modules.localization.localed.LOCALED")
     @patch("pyanaconda.modules.localization.localed.conf")
-    def test_localed_wrapper_set_current_layout(self, mocked_conf,
-                                                mocked_localed_service,
-                                                mocked_system_bus):
-        """Test LocaledWrapper method to set current layout to compositor.
-
-        Verify that the layout to be set is moved to the first place.
-        """
-        mocked_system_bus.check_connection.return_value = True
-        mocked_conf.system.provides_system_bus = True
-        mocked_localed_proxy = Mock()
-        mocked_localed_service.get_proxy.return_value = mocked_localed_proxy
-        mocked_localed_proxy.X11Layout = "cz,fi,us,fr"
-        mocked_localed_proxy.X11Variant = "qwerty,,euro"
-        mocked_localed_proxy.X11Options = ""
-        localed_wrapper = LocaledWrapper()
-        user_defined = ["cz (qwerty)", "fi", "us (euro)", "fr"]
-
-        # check if layout is correctly set
-        localed_wrapper._user_layouts_variants = user_defined
-        localed_wrapper.set_current_layout("fi")
-        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
-        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
-            "fi,us,fr,cz",
-            "pc105",  # hardcoded
-            ",euro,,qwerty",
-            "",
-            False,
-            False
-        )
-
-        # check if layout is correctly set including variant
-        mocked_localed_proxy.SetX11Keyboard.reset_mock()
-        localed_wrapper._user_layouts_variants = user_defined
-
-        assert localed_wrapper.set_current_layout("us (euro)") is True
-        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
-        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
-            "us,fr,cz,fi",
-            "pc105",  # hardcoded
-            "euro,,qwerty,",
-            "",
-            False,
-            False
-        )
-
-        # check when we are selecting non-existing layout
-        mocked_localed_proxy.SetX11Keyboard.reset_mock()
-        mocked_localed_proxy.X11Layout = "fi"
-        mocked_localed_proxy.X11Variant = ""
-        mocked_localed_proxy.X11Options = ""
-        localed_wrapper._user_layouts_variants = user_defined
-
-        assert localed_wrapper.set_current_layout("cz") is False
-        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
-        mocked_localed_proxy.SetX11Keyboard.assert_not_called()
-
-        # check when the layout set is empty
-        mocked_localed_proxy.SetX11Keyboard.reset_mock()
-        mocked_localed_proxy.X11Layout = ""
-        mocked_localed_proxy.X11Variant = ""
-        localed_wrapper._user_layouts_variants = user_defined
-
-        assert localed_wrapper.set_current_layout("fr") is True
-        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
-        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
-            "fr,cz,fi,us",
-            "pc105",  # hardcoded
-            ",qwerty,,euro",
-            "",
-            False,
-            False
-        )
-
-        # can't set layout when we don't have user defined set
-        mocked_localed_proxy.SetX11Keyboard.reset_mock()
-        mocked_localed_proxy.X11Layout = "cz, us"
-        mocked_localed_proxy.X11Variant = ""
-        mocked_localed_proxy.X11Options = ""
-        user_defined = []
-        localed_wrapper._user_layouts_variants = user_defined
-
-        assert localed_wrapper.set_current_layout("cz (qwerty)") is False
-        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
-        mocked_localed_proxy.SetX11Keyboard.assert_not_called()
-
-    @patch("pyanaconda.modules.localization.localed.SystemBus")
-    @patch("pyanaconda.modules.localization.localed.LOCALED")
-    @patch("pyanaconda.modules.localization.localed.conf")
-    def test_localed_wrapper_set_next_layout(self, mocked_conf,
-                                             mocked_localed_service,
-                                             mocked_system_bus):
-        """Test LocaledWrapper method to set current layout to compositor.
+    def test_compositor_localed_wrapper_set_next_layout(
+        self, mocked_conf, mocked_localed_service, mocked_system_bus
+    ):
+        """Test CompositorLocaledWrapper method to set current layout to compositor.
 
         Verify that we are selecting next layout to what is currently set in compositor.
         Because setting current layout changing the ordering we have to decide next layout based
@@ -282,7 +278,7 @@ class LocaledWrapperTestCase(unittest.TestCase):
         mocked_localed_proxy.X11Layout = "cz,fi,us,fr"
         mocked_localed_proxy.X11Variant = "qwerty,,euro"
         mocked_localed_proxy.X11Options = ""
-        localed_wrapper = LocaledWrapper()
+        localed_wrapper = CompositorLocaledWrapper()
 
         # test switch to next layout
         user_defined = ["cz (qwerty)", "fi", "us (euro)", "fr"]
@@ -392,10 +388,98 @@ class LocaledWrapperTestCase(unittest.TestCase):
     @patch("pyanaconda.modules.localization.localed.SystemBus")
     @patch("pyanaconda.modules.localization.localed.LOCALED")
     @patch("pyanaconda.modules.localization.localed.conf")
-    def test_localed_wrapper_signals(self, mocked_conf,
+    def test_compositor_localed_wrapper_set_current_layout(
+        self, mocked_conf, mocked_localed_service, mocked_system_bus
+    ):
+        """Test CompositorLocaledWrapper method to set current layout to compositor.
+
+        Verify that the layout to be set is moved to the first place.
+        """
+        mocked_system_bus.check_connection.return_value = True
+        mocked_conf.system.provides_system_bus = True
+        mocked_localed_proxy = Mock()
+        mocked_localed_service.get_proxy.return_value = mocked_localed_proxy
+        mocked_localed_proxy.X11Layout = "cz,fi,us,fr"
+        mocked_localed_proxy.X11Variant = "qwerty,,euro"
+        mocked_localed_proxy.X11Options = ""
+        localed_wrapper = CompositorLocaledWrapper()
+        user_defined = ["cz (qwerty)", "fi", "us (euro)", "fr"]
+
+        # check if layout is correctly set
+        localed_wrapper._user_layouts_variants = user_defined
+        localed_wrapper.set_current_layout("fi")
+        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
+        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
+            "fi,us,fr,cz",
+            "pc105",  # hardcoded
+            ",euro,,qwerty",
+            "",
+            False,
+            False
+        )
+
+        # check if layout is correctly set including variant
+        mocked_localed_proxy.SetX11Keyboard.reset_mock()
+        localed_wrapper._user_layouts_variants = user_defined
+
+        assert localed_wrapper.set_current_layout("us (euro)") is True
+        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
+        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
+            "us,fr,cz,fi",
+            "pc105",  # hardcoded
+            "euro,,qwerty,",
+            "",
+            False,
+            False
+        )
+
+        # check when we are selecting non-existing layout
+        mocked_localed_proxy.SetX11Keyboard.reset_mock()
+        mocked_localed_proxy.X11Layout = "fi"
+        mocked_localed_proxy.X11Variant = ""
+        mocked_localed_proxy.X11Options = ""
+        localed_wrapper._user_layouts_variants = user_defined
+
+        assert localed_wrapper.set_current_layout("cz") is False
+        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
+        mocked_localed_proxy.SetX11Keyboard.assert_not_called()
+
+        # check when the layout set is empty
+        mocked_localed_proxy.SetX11Keyboard.reset_mock()
+        mocked_localed_proxy.X11Layout = ""
+        mocked_localed_proxy.X11Variant = ""
+        localed_wrapper._user_layouts_variants = user_defined
+
+        assert localed_wrapper.set_current_layout("fr") is True
+        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
+        mocked_localed_proxy.SetX11Keyboard.assert_called_once_with(
+            "fr,cz,fi,us",
+            "pc105",  # hardcoded
+            ",qwerty,,euro",
+            "",
+            False,
+            False
+        )
+
+        # can't set layout when we don't have user defined set
+        mocked_localed_proxy.SetX11Keyboard.reset_mock()
+        mocked_localed_proxy.X11Layout = "cz, us"
+        mocked_localed_proxy.X11Variant = ""
+        mocked_localed_proxy.X11Options = ""
+        user_defined = []
+        localed_wrapper._user_layouts_variants = user_defined
+
+        assert localed_wrapper.set_current_layout("cz (qwerty)") is False
+        assert user_defined == localed_wrapper._user_layouts_variants  # must not change
+        mocked_localed_proxy.SetX11Keyboard.assert_not_called()
+
+    @patch("pyanaconda.modules.localization.localed.SystemBus")
+    @patch("pyanaconda.modules.localization.localed.LOCALED")
+    @patch("pyanaconda.modules.localization.localed.conf")
+    def test_compositor_localed_wrapper_signals(self, mocked_conf,
                                      mocked_localed_service,
                                      mocked_system_bus):
-        """Test signals from the localed wrapper
+        """Test signals from the compositor localed wrapper
 
         This one could be tricky. The issue is that this class has to store last known values to
         be able to recognize changes.
@@ -414,24 +498,24 @@ class LocaledWrapperTestCase(unittest.TestCase):
         mocked_localed_service.get_proxy.return_value = mocked_localed_proxy
         mocked_layouts_changed = Mock()
         mocked_selected_layout_changed = Mock()
-        localed_wrapper = LocaledWrapper()
+        localed_wrapper = CompositorLocaledWrapper()
         localed_wrapper.compositor_layouts_changed = mocked_layouts_changed
         localed_wrapper.compositor_selected_layout_changed = mocked_selected_layout_changed
 
         def _check_localed_wrapper_signals(last_known_state, compositor_state,
                                            expected_selected_signal, expected_layouts_signal):
-            """Test the localed wrapper signals are correctly emitted.
+            """Test the compositor localed wrapper signals are correctly emitted.
 
             :param last_known_state: State of the localed before the change. Used to resolve if
                                      selected layout has changed.
             :type last_known_state: [(str,str)] e.g.:[('cz', 'qwerty'), ('us','')...]
             :param compositor_state: New state the compositor will get into.
             :type compositor_state: {str: str} e.g.: {"X11Layout": "cz", "X11Variant": "qwerty"}
-            :param expected_selected_signal: Currently selected layout we expect LocaledWrapper
+            :param expected_selected_signal: Currently selected layout we expect CompositorLocaledWrapper
                                              will signal out. If signal shouldn't set None.
             :type expected_selected_signal: str
             :param expected_layouts_signal: Current configuration of the compositor signaled from
-                                            LocaledWrapper.
+                                            CompositorLocaledWrapper.
             :type expected_layouts_signal: [str] e.g.: ["cz", "us (euro)"]
             """
             mocked_layouts_changed.reset_mock()
