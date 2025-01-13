8bde90d4c8971caccdb9be7579e8e1c8d79149e2
Radek Vykydal <rvykydal@redhat.com>
2025-01-13 15:38:25 +0100

    security: add API to import certificates to Anaconda environment
    Resolves: RHEL-61430

--- pyanaconda/modules/security/certificates/certificates.py
+++ pyanaconda/modules/security/certificates/certificates.py
@@ -25,6 +25,7 @@ from pyanaconda.modules.common.base import KickstartBaseModule
 from pyanaconda.modules.common.constants.objects import CERTIFICATES
 from pyanaconda.modules.common.structures.security import CertificateData
 from pyanaconda.modules.security.certificates.certificates_interface import CertificatesInterface
+from pyanaconda.modules.security.certificates.installation import ImportCertificatesTask
 
 log = get_module_logger(__name__)
 
@@ -73,3 +74,13 @@ class CertificatesModule(KickstartBaseModule):
         # the properties changed signal here manually
         self.module_properties_changed.emit()
         log.debug("Certificates is set to %s.", certificates)
+
+    def import_with_task(self):
+        """Import certificates into the installer environment
+
+        :return: an installation task
+        """
+        return ImportCertificatesTask(
+            sysroot="/",
+            certificates=self.certificates,
+        )
---
+++ pyanaconda/modules/security/certificates/installation.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2025 Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+from pyanaconda.anaconda_loggers import get_module_logger
+from pyanaconda.modules.common.task import Task
+
+log = get_module_logger(__name__)
+
+
+class ImportCertificatesTask(Task):
+    """Task for importing certificates into a system."""
+
+    def __init__(self, sysroot, certificates):
+        """Create a new certificates import task.
+
+        :param str sysroot: a path to the root of the target system
+        :param certificates: list of certificate data holders
+        """
+        super().__init__()
+        self._sysroot = sysroot
+        self._certificates = certificates
+
+    @property
+    def name(self):
+        return "Import CA certificates"
+
+    def run(self):
+        for cert in self._certificates:
+            log.debug("Importing certificate with filename: %s dir: %s", cert.filename, cert.dir)
