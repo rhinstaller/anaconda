c2308af610dfee95efb4a6ac2ce837878e8f5632
Pavla Kratochvilova <pkratoch@redhat.com>
2025-01-17 12:37:48 +0100

    Add a second goal for second resolving in case of unavailable packages
    An unavailable package is not a critical error, therefore, it's reported
    only as a warning and it may be skipped.
    
    One option would be to check all the specs before adding them to goal,
    but that seems more error-prone.
    
    The other option, which was chosen in this commit, is to resolve
    the selection (up to) two times, the second time with skip_unavailable
    set to True. It is not possible to resolve the same goal twice with
    different settings, therefore, another goal is created and the specs are
    added to both.
    
    The tests are updated to match the behavior (no warnings with
    sip_unavailable, warnings and not errors without skip_unavailable).

--- tests/unit_tests/pyanaconda_tests/modules/payloads/payload/test_module_payload_dnf5_manager.py
+++ tests/unit_tests/pyanaconda_tests/modules/payloads/payload/test_module_payload_dnf5_manager.py
@@ -313,11 +313,11 @@ class DNFManagerTestCase(unittest.TestCase):
         )
 
         report = self.dnf_manager.resolve_selection()
-        assert report.error_messages == [
+        assert report.error_messages == []
+        assert report.warning_messages == [
             'No match for argument: p1',
             'No match for argument: g1',
         ]
-        assert report.warning_messages == []
 
     def test_ignore_missing_packages(self):
         """Test the ignore_missing_packages attribute."""
@@ -334,10 +334,7 @@ class DNFManagerTestCase(unittest.TestCase):
         report = self.dnf_manager.resolve_selection()
 
         assert report.error_messages == []
-        assert report.warning_messages == [
-            'No match for argument: p1',
-            'No match for argument: g1',
-        ]
+        assert report.warning_messages == []
 
     @pytest.mark.skip("Not implemented")
     def test_ignore_broken_packages(self):
