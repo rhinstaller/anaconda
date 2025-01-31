rhinstaller/anaconda

commit 7205b51748305f38fe51c48f24255529a8066807

Make network spoke complete also in connecting state.
This is fixup of commit 16dde604a1605e0a49caa818467b7d36285c7a65.
Network spoke has always been considered completed also if there was a
device in connecting state, not only when in connected state.

pyanaconda/ui/gui/spokes/network.py
@@ -1540,6 +1540,7 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalSpoke):
         # TODO: check also if source requires updates when implemented
         # If we can't configure network, don't require it
         return (not conf.system.can_configure_network
+                or self._network_module.IsConnecting
                 or self._network_module.Connected)
 
     @property
@@ -1692,6 +1693,7 @@ class NetworkStandaloneSpoke(StandaloneSpoke):
     def completed(self):
         return (not conf.system.can_configure_network
                 or self._network_module.Connected
+                or self._network_module.IsConnecting
                 or not (self.payload.source_type != conf.payload.default_source
                         and self.payload.needs_network))
 

pyanaconda/ui/tui/spokes/network.py
@@ -255,6 +255,7 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalTUISpoke):
         """ Check whether this spoke is complete or not."""
         # If we can't configure network, don't require it
         return (not conf.system.can_configure_network
+                or self._network_module.IsConnecting
                 or self._network_module.Connected)
 
     @property

commit c057a9cce6deaa4f2ced9177ee21f7146b1338f2

Fix calls to IsConnecting()
'Connected' is a property, whereas 'IsConnecting' is a method,
so without the '()' it's just equivalent to True.

Fixes: 7205b51748305f38fe51c48f24255529a8066807

Signed-off-by: Etienne Champetier <champetier.etienne@gmail.com>

pyanaconda/ui/gui/spokes/network.py
@@ -1559,7 +1559,7 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalSpoke):
         # TODO: check also if source requires updates when implemented
         # If we can't configure network, don't require it
         return (not conf.system.can_configure_network
-                or self._network_module.IsConnecting
+                or self._network_module.IsConnecting()
                 or self._network_module.Connected)
 
     @property
@@ -1712,7 +1712,7 @@ class NetworkStandaloneSpoke(StandaloneSpoke):
     def completed(self):
         return (not conf.system.can_configure_network
                 or self._network_module.Connected
-                or self._network_module.IsConnecting
+                or self._network_module.IsConnecting()
                 or not (self.payload.source_type != conf.payload.default_source
                         and self.payload.needs_network))
 

pyanaconda/ui/tui/spokes/network.py
@@ -259,7 +259,7 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalTUISpoke):
         """ Check whether this spoke is complete or not."""
         # If we can't configure network, don't require it
         return (not conf.system.can_configure_network
-                or self._network_module.IsConnecting
+                or self._network_module.IsConnecting()
                 or self._network_module.Connected)
 
     @property