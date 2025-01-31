rhinstaller/anaconda

commit 55e0317541b8ce7373c51b4ba8f9867f67a29388

Get kickstart data via DBus

pyanaconda/display.py
@@ -34,15 +34,15 @@ from pyanaconda.core import util, constants, hw
 from pyanaconda import vnc
 from pyanaconda.core.i18n import _
 from pyanaconda.flags import flags
-from pyanaconda.modules.common.constants.services import NETWORK
+from pyanaconda.modules.common.constants.objects import USER_INTERFACE
+from pyanaconda.modules.common.constants.services import NETWORK, RUNTIME
+from pyanaconda.modules.common.structures.vnc import VncData
 from pyanaconda.ui.tui.spokes.askvnc import AskVNCSpoke
 from pyanaconda.ui.tui import tui_quit_callback
 # needed for checking if the pyanaconda.ui.gui modules are available
 import pyanaconda.ui
 
 import blivet
-
-from pykickstart.constants import DISPLAY_MODE_TEXT
 
 from simpleline import App
 from simpleline.render.screen_handler import ScreenHandler
@@ -121,13 +121,15 @@ def ask_vnc_question(anaconda, vnc_server, message):
     spoke = AskVNCSpoke(anaconda.ksdata, message=message)
     ScreenHandler.schedule_screen(spoke)
     App.run()
+    ui_proxy = RUNTIME.get_proxy(USER_INTERFACE)
+    vnc_data = VncData.from_structure(ui_proxy.Vnc)
 
-    if anaconda.ksdata.vnc.enabled:
+    if vnc_data.enabled:
         if not anaconda.gui_mode:
             log.info("VNC requested via VNC question, switching Anaconda to GUI mode.")
         anaconda.display_mode = constants.DisplayModes.GUI
         flags.usevnc = True
-        vnc_server.password = anaconda.ksdata.vnc.password
+        vnc_server.password = vnc_data.password
 
 
 def check_vnc_can_be_started(anaconda):
@@ -154,7 +156,7 @@ def check_vnc_can_be_started(anaconda):
         vnc_startup_possible = False
 
     # disable VNC question if we were explicitly asked for text in kickstart
-    if anaconda.ksdata.displaymode.displayMode == DISPLAY_MODE_TEXT:
+    if anaconda.display_mode == constants.DisplayModes.TUI:
         error_messages.append("Not asking for VNC because text mode was explicitly asked for in kickstart")
         vnc_startup_possible = False
 
@@ -306,20 +308,23 @@ def setup_display(anaconda, options):
     if options.xdriver:
         write_xdriver(options.xdriver, root="/")
 
-    if anaconda.ksdata.vnc.enabled:
+    ui_proxy = RUNTIME.get_proxy(USER_INTERFACE)
+    vnc_data = VncData.from_structure(ui_proxy.Vnc)
+
+    if vnc_data.enabled:
         flags.usevnc = True
         if not anaconda.gui_mode:
             log.info("VNC requested via kickstart, switching Anaconda to GUI mode.")
             anaconda.display_mode = constants.DisplayModes.GUI
 
         if vnc_server.password == "":
-            vnc_server.password = anaconda.ksdata.vnc.password
+            vnc_server.password = vnc_data.password
 
         if vnc_server.vncconnecthost == "":
-            vnc_server.vncconnecthost = anaconda.ksdata.vnc.host
+            vnc_server.vncconnecthost = vnc_data.host
 
         if vnc_server.vncconnectport == "":
-            vnc_server.vncconnectport = anaconda.ksdata.vnc.port
+            vnc_server.vncconnectport = vnc_data.port
 
     # check if GUI without WebUI
     if anaconda.gui_mode and not anaconda.is_webui_supported:
@@ -357,7 +362,7 @@ def setup_display(anaconda, options):
                     "full control over the disk layout. Would you like "
                     "to use VNC mode instead?")
         ask_vnc_question(anaconda, vnc_server, message)
-        if not anaconda.ksdata.vnc.enabled:
+        if not vnc_data.enabled:
             # user has explicitly specified text mode
             flags.vncquestion = False
 
@@ -394,7 +399,8 @@ def setup_display(anaconda, options):
         if not anaconda.gui_startup_failed:
             do_extra_x11_actions(options.runres, gui_mode=anaconda.gui_mode)
 
-    if anaconda.tui_mode and anaconda.gui_startup_failed and flags.vncquestion and not anaconda.ksdata.vnc.enabled:
+    if anaconda.tui_mode and anaconda.gui_startup_failed and \
+            flags.vncquestion and not vnc_data.enabled:
         message = _("X was unable to start on your machine. Would you like to start VNC to connect to "
                     "this computer from another computer and perform a graphical installation or continue "
                     "with a text mode installation?")

anaconda.py
@@ -30,6 +30,8 @@ import time
 import signal
 import pid
 
+from pyanaconda.modules.common.structures.rescue import RescueData
+
 
 def exitHandler(rebootData):
     # Clear the list of watched PIDs.
@@ -395,9 +397,12 @@ if __name__ == "__main__":
 
     # Pick up any changes from interactive-defaults.ks that would
     # otherwise be covered by the dracut KS parser.
-    from pyanaconda.modules.common.constants.services import STORAGE
+    from pyanaconda.modules.common.constants.services import STORAGE, RUNTIME
     from pyanaconda.modules.common.constants.objects import BOOTLOADER
+
     bootloader_proxy = STORAGE.get_proxy(BOOTLOADER)
+    runtime_proxy = RUNTIME.get_proxy()
+    rescue_data = RescueData.from_structure(runtime_proxy.Rescue)
 
     if opts.leavebootorder:
         bootloader_proxy.KeepBootOrder = True
@@ -405,7 +410,7 @@ if __name__ == "__main__":
     if opts.nombr:
         bootloader_proxy.KeepMBR = True
 
-    if ksdata.rescue.rescue:
+    if rescue_data.rescue:
         flags.rescue_mode = True
 
     # reboot with kexec
@@ -413,7 +418,7 @@ if __name__ == "__main__":
         flags.kexec = True
 
     # Change the logging configuration based on the kickstart.
-    startup_utils.setup_logging_from_kickstart(ksdata)
+    startup_utils.setup_logging_from_kickstart()
 
     anaconda.ksdata = ksdata
 
@@ -458,15 +463,6 @@ if __name__ == "__main__":
 
     # now start the interface
     display.setup_display(anaconda, opts)
-
-    # we now know in which mode we are going to run so store the information
-    from pykickstart import constants as pykickstart_constants
-    display_mode_coversion_table = {
-        constants.DisplayModes.GUI: pykickstart_constants.DISPLAY_MODE_GRAPHICAL,
-        constants.DisplayModes.TUI: pykickstart_constants.DISPLAY_MODE_TEXT
-    }
-    ksdata.displaymode.displayMode = display_mode_coversion_table[anaconda.display_mode]
-    ksdata.displaymode.nonInteractive = not anaconda.interactive_mode
 
     # Initialize the default systemd target.
     startup_utils.initialize_default_systemd_target(text_mode=anaconda.tui_mode)

pyanaconda/rescue.py
@@ -20,8 +20,9 @@ from pyanaconda.core import util
 from pyanaconda.core.configuration.anaconda import conf
 from pyanaconda.core.constants import ANACONDA_CLEANUP, THREAD_STORAGE, QUIT_MESSAGE
 from pyanaconda.modules.common.constants.objects import DEVICE_TREE
-from pyanaconda.modules.common.constants.services import STORAGE
+from pyanaconda.modules.common.constants.services import STORAGE, RUNTIME
 from pyanaconda.modules.common.errors.storage import MountFilesystemError
+from pyanaconda.modules.common.structures.rescue import RescueData
 from pyanaconda.modules.common.structures.storage import OSData, DeviceFormatData
 from pyanaconda.modules.common.task import sync_run_task
 from pyanaconda.core.threads import thread_manager
@@ -558,12 +559,14 @@ class RootSelectionSpoke(NormalTUISpoke):
 
 def start_rescue_mode_ui(anaconda):
     """Start the rescue mode UI."""
+    runtime_proxy = RUNTIME.get_proxy()
+    rescue_data = RescueData.from_structure(runtime_proxy.Rescue)
 
     ksdata_rescue = None
-    if anaconda.ksdata.rescue.seen:
-        ksdata_rescue = anaconda.ksdata.rescue
+    if rescue_data.rescue:
+        ksdata_rescue = rescue_data
     scripts = anaconda.ksdata.scripts
-    rescue_nomount = anaconda.opts.rescue_nomount
+    rescue_nomount = rescue_data.nomount
     reboot = True
     if conf.target.is_image:
         reboot = False

pyanaconda/startup_utils.py
@@ -43,12 +43,13 @@ from pyanaconda.core.product import get_product_is_final_release, get_product_na
 from pyanaconda.core.service import start_service
 from pyanaconda.flags import flags
 from pyanaconda.localization import get_territory_locales, setup_locale, locale_has_translation
+from pyanaconda.modules.common.structures.logging import LoggingData
 from pyanaconda.screensaver import inhibit_screensaver
 from pyanaconda.modules.common.task import wait_for_task
 from pyanaconda.modules.common.structures.timezone import TimeSourceData, GeolocationData
 from pyanaconda.modules.common.constants.objects import STORAGE_CHECKER
 from pyanaconda.modules.common.constants.services import TIMEZONE, LOCALIZATION, SERVICES, \
-    SECURITY, STORAGE
+    SECURITY, STORAGE, RUNTIME
 from pyanaconda.modules.common.util import is_module_available
 from pyanaconda.core.threads import thread_manager
 
@@ -192,13 +193,13 @@ def setup_logging_from_options(options):
             log.error("Could not setup remotelog with %s", options.remotelog)
 
 
-def setup_logging_from_kickstart(data):
+def setup_logging_from_kickstart():
     """Configure logging according to the kickstart.
-
-    :param data: kickstart data
     """
-    host = data.logging.host
-    port = data.logging.port
+    runtime_proxy = RUNTIME.get_proxy()
+    logging_data = LoggingData.from_structure(runtime_proxy.Logging)
+    host = logging_data.host
+    port = logging_data.port
 
     if anaconda_logging.logger.remote_syslog is None and len(host) > 0:
         # not set from the command line, ok to use kickstart

commit 9ab8d962d347ba5327ec05d4d7b1e09a026b6273

Fix for inst.text was not asking for RDP
When user add inst.text on the kernel command line then Anaconda should
ask question if user wants to use RDP or switch to the text mode.
However, this behavior was broken on Rawhide.

The issue was introduced by commit
55e0317541b8ce7373c51b4ba8f9867f67a29388
which was moving the display modes to runtime module. In this module,
the check for rdp was changed from kickstart data logic to cmdline
logic.

pyanaconda/display.py
@@ -39,7 +39,8 @@ from pyanaconda.core.path import join_paths
 from pyanaconda.core.process_watchers import WatchProcesses
 from pyanaconda.flags import flags
 from pyanaconda.gnome_remote_desktop import GRDServer
-from pyanaconda.modules.common.constants.services import NETWORK
+from pyanaconda.modules.common.constants.objects import USER_INTERFACE
+from pyanaconda.modules.common.constants.services import NETWORK, RUNTIME
 from pyanaconda.mutter_display import MutterConfigError, MutterDisplay
 from pyanaconda.ui.tui import tui_quit_callback
 from pyanaconda.ui.tui.spokes.askrd import AskRDSpoke, RDPAuthSpoke
@@ -164,7 +165,8 @@ def check_rd_can_be_started(anaconda):
         rd_startup_possible = False
 
     # disable remote desktop question if we were explicitly asked for text in kickstart
-    if anaconda.display_mode == constants.DisplayModes.TUI:
+    proxy = RUNTIME.get_proxy(USER_INTERFACE)
+    if proxy.DisplayModeTextKickstarted:
         error_messages.append("Not asking for remote desktop session because text mode "
                               "was explicitly asked for in kickstart")
         rd_startup_possible = False