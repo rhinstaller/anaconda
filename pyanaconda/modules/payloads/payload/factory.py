0ccd9f6efc1b3926354af140d2ec341eac74cf36
Paweł Poławski <ppolawsk@redhat.com>
2025-03-24 16:26:27 +0100

    Add bootc kickstart command support

--- pyanaconda/modules/payloads/payload/factory.py
+++ pyanaconda/modules/payloads/payload/factory.py
@@ -52,6 +52,12 @@ class PayloadFactory:
             )
             return RPMOSTreeModule()
 
+        if payload_type == PayloadType.BOOTC:
+            from pyanaconda.modules.payloads.payload.bootc.bootc import (
+                BootcModule,
+            )
+            return BootcModule()
+
         raise ValueError("Unknown payload type: {}".format(payload_type))
 
     @classmethod
@@ -63,6 +69,9 @@ class PayloadFactory:
         """
         if data.ostreesetup.seen or data.ostreecontainer.seen:
             return PayloadType.RPM_OSTREE
+
+        if data.bootcsetup.seen:
+            return PayloadType.BOOTC
 
         if data.liveimg.seen:
             return PayloadType.LIVE_IMAGE
