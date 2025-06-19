7fea18b46a7a738cc3464121e095f272f2f965ef
Katerina Koukiou <k.koukiou@gmail.com>
2025-06-19 10:39:00 +0200

    Adjust to pykickstart adding support for RDP
    Relevant to: https://github.com/pykickstart/pykickstart/commit/5adbd63825446c24827c6de71129f6ee5ee25510
    
    (cherry picked from commit 067ff7704647feebf96616916dbf48114d4fa12b)
    
    Resolves: RHEL-96216

--- tests/unit_tests/pyanaconda_tests/modules/runtime/test_module_runtime.py
+++ tests/unit_tests/pyanaconda_tests/modules/runtime/test_module_runtime.py
@@ -44,7 +44,8 @@ class RuntimeInterfaceTestCase(unittest.TestCase):
                     "graphical",
                     "text",
                     "cmdline",
-                    "vnc"]
+                    "vnc",
+                    "rdp"]
         assert self.interface.KickstartCommands == commands
         sections = ['pre-install',
                     'post',
