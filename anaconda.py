e89a93cb4e9a71d3215bc8dc08e48477d53b212b
Katerina Koukiou <k.koukiou@gmail.com>
2025-07-03 08:16:17 +0200

    anaconda.py: remove duplicate import
    The STORAGE import happens already earlier in that file.

--- anaconda.py
+++ anaconda.py
@@ -506,8 +506,6 @@ if __name__ == "__main__":
     from pyanaconda.ui.lib.storage import ignore_oemdrv_disks
     ignore_oemdrv_disks()
 
-    from pyanaconda.modules.common.constants.services import STORAGE
-
     if not conf.target.is_directory:
         from pyanaconda.ui.lib.storage import reset_storage
 
