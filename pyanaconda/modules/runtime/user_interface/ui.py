530b588032b8a83d9fb008043301fa7a2c843637
Adam Kankovsky <adamkankovsky@gmail.com>
2025-07-10 09:29:56 +0200

    Enable kickstart RDP command in Anaconda
    Resolves: RHEL-96216

--- pyanaconda/modules/runtime/user_interface/ui.py
+++ pyanaconda/modules/runtime/user_interface/ui.py
@@ -17,11 +17,14 @@
 # License and may only be used or replicated with the express permission of
 # Red Hat, Inc.
 #
+from pykickstart.commands.displaymode import DISPLAY_MODE_TEXT
+
 from pyanaconda.anaconda_loggers import get_module_logger
 from pyanaconda.core.constants import (
     PASSWORD_POLICY_LUKS,
     PASSWORD_POLICY_ROOT,
     PASSWORD_POLICY_USER,
+    DisplayModes,
 )
 from pyanaconda.core.dbus import DBus
 from pyanaconda.core.product import get_product_is_final_release, get_product_values
@@ -30,6 +33,7 @@ from pyanaconda.modules.common.base import KickstartBaseModule
 from pyanaconda.modules.common.constants.objects import USER_INTERFACE
 from pyanaconda.modules.common.structures.policy import PasswordPolicy
 from pyanaconda.modules.common.structures.product import ProductData
+from pyanaconda.modules.common.structures.rdp import RdpData
 from pyanaconda.modules.runtime.user_interface.ui_interface import UIInterface
 
 log = get_module_logger(__name__)
@@ -45,9 +49,112 @@ class UIModule(KickstartBaseModule):
         self._password_policies = self.get_default_password_policies()
         self.password_policies_changed = Signal()
 
+        self.display_mode_changed = Signal()
+        self._displayMode = DisplayModes.TUI
+
+        self.display_mode_nonInteractive_changed = Signal()
+        self._displayMode_nonInteractive = False
+
+        self.display_mode_text_kickstarted_changed = Signal()
+        self._display_mode_text_kickstarted = False
+
+        self.rdp_changed = Signal()
+        self._rdp = RdpData()
+
     def publish(self):
         """Publish the module."""
         DBus.publish_object(USER_INTERFACE.object_path, UIInterface(self))
+
+    def process_kickstart(self, data):
+        """Process the kickstart data."""
+        self.set_display_mode(data.displaymode.displayMode)
+        self.set_display_mode_non_interactive(data.displaymode.nonInteractive)
+        # check if text mode was requested in kickstart
+        if data.displaymode.displayMode == DISPLAY_MODE_TEXT:
+            log.debug("Text mode requested by kickstart")
+            self._display_mode_text_kickstarted = True
+            self.display_mode_text_kickstarted_changed.emit()
+
+        rdp = RdpData()
+        rdp.enabled = data.rdp.enabled
+        rdp.username = data.rdp.username
+        rdp.password.set_secret(data.rdp.password)
+        self.set_rdp(rdp)
+
+    def setup_kickstart(self, data):
+        """Set up the kickstart data."""
+        data.displaymode.displayMode = self._displayMode
+        data.displaymode.nonInteractive = self._displayMode_nonInteractive
+        data.rdp.enabled = self._rdp.enabled
+        data.rdp.username = self._rdp.username
+        data.rdp.password = self._rdp.password.value
+
+    @property
+    def display_mode(self):
+        """DisplayMode mode for the installation.
+
+        :return: the displayMode mode for the installation
+        :rtype: DisplayModes enum
+        """
+        return self._displayMode
+
+    def set_display_mode(self, display_mode):
+        """Set displayMode mode for the installation.
+
+        :param display_mode: the displayMode mode for the installation
+        :type display_mode: DisplayModes enum
+        """
+        self._displayMode = display_mode
+        self.display_mode_changed.emit()
+        log.debug("Display mode will be: %s", display_mode)
+
+    @property
+    def display_mode_non_interactive(self):
+        """Non-interactive flag for display mode.
+
+        :return: the non-interactive flag for display mode
+        :rtype: bool
+        """
+        return self._displayMode_nonInteractive
+
+    def set_display_mode_non_interactive(self, non_interactive):
+        """Set the non-interactive flag for display mode.
+
+        :param non_interactive: the non-interactive flag for display mode.
+        :type non_interactive: bool
+        """
+        self._displayMode_nonInteractive = non_interactive
+        self.display_mode_nonInteractive_changed.emit()
+        log.debug("Display mode non-interactive set to: %s", str(non_interactive))
+
+    @property
+    def display_mode_text_kickstarted(self):
+        """Report if text mode was explicitely requested via kickstart.
+
+
+        #NOTE: No setter as this is only set once when parsing the kickstasrt.
+
+        :return: if text mode was requested by kickstart
+        :rtype: bool
+        """
+
+        return self._display_mode_text_kickstarted
+
+    @property
+    def rdp(self):
+        """The RdpData.
+        :return: an instance of RdpData
+        """
+        return self._rdp
+
+    def set_rdp(self, rdp):
+        """Set the RdpData structure.
+        :param rdp: RdpData structure.
+        :type rdp: object
+        """
+        self._rdp = rdp
+        self.rdp_changed.emit()
+        log.debug("RDP enabled set to: %s", rdp)
 
     @property
     def password_policies(self):
