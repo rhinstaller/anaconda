578685fe28b5dcc17cf51192fed7287a0e86810e
Adam Kankovsky <adamkankovsky@gmail.com>
2025-09-23 12:10:52 +0200

    Emit authselect in KS when fingerprint is enabled
    Resolves: RHEL-85175

--- pyanaconda/modules/security/security.py
+++ pyanaconda/modules/security/security.py
@@ -32,7 +32,7 @@ from pyanaconda.modules.security.kickstart import SecurityKickstartSpecification
 from pyanaconda.modules.security.security_interface import SecurityInterface
 from pyanaconda.modules.security.installation import ConfigureSELinuxTask, \
     RealmDiscoverTask, RealmJoinTask, ConfigureAuthselectTask, \
-    ConfigureAuthconfigTask, ConfigureFingerprintAuthTask
+    ConfigureAuthconfigTask, ConfigureFingerprintAuthTask, AUTHSELECT_ARGS
 
 from pyanaconda.anaconda_loggers import get_module_logger
 log = get_module_logger(__name__)
@@ -96,6 +96,9 @@ class SecurityService(KickstartService):
 
         if self.authselect:
             data.authselect.authselect = " ".join(self.authselect)
+        elif self.fingerprint_auth_enabled:
+            auth_args = AUTHSELECT_ARGS
+            data.authselect.authselect = " ".join(auth_args)
 
         if self.authconfig:
             data.authconfig.authconfig = " ".join(self.authconfig)
