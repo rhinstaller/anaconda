rhinstaller/anaconda

commit d8d74ffbf1b0ccc0d27007efbcea2d16ae986d92

pyanaconda: payload: return exception details for payload failures

pyanaconda/payload/manager.py
@@ -240,7 +240,7 @@ class PayloadManager(object):
             payload.add_driver_repos()
         except (OSError, DBusError, PayloadError) as e:
             log.error("PayloadError: %s", e)
-            self._error = self.ERROR_SETUP
+            self._error = "%s: %s" % (self.ERROR_SETUP, e)
             self._set_state(PayloadState.ERROR)
             payload.unsetup()
             return
@@ -252,7 +252,7 @@ class PayloadManager(object):
         # Check if that failed
         if not payload.base_repo:
             log.error("No base repo configured")
-            self._error = self.ERROR_MD
+            self._error = "%s: %s" % (self.ERROR_MD, e)
             self._set_state(PayloadState.ERROR)
             payload.unsetup()
             return

commit 6b64d451bdf7b5ab4d6f2a88493a22e7da01d583

Fix: UnboundLocalError: local variable 'e' referenced before assignment
This is a regression from: d8d74ffbf1b0ccc0d27007efbcea2d16ae986d92

Related to: RHEL-4705

pyanaconda/payload/manager.py
@@ -252,7 +252,7 @@ class PayloadManager(object):
         # Check if that failed
         if not payload.base_repo:
             log.error("No base repo configured")
-            self._error = "%s: %s" % (self.ERROR_MD, e)
+            self._error = self.ERROR_MD
             self._set_state(PayloadState.ERROR)
             payload.unsetup()
             return