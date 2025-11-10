e17f7a5ea88e26c2724987fab9e9037f135286ca
songmingliang <MerlinDust@foxmail.com>
2025-11-10 17:18:28 +0800

    Remove redundant 'dialog' parameters

--- pyanaconda/ui/tui/spokes/network.py
+++ pyanaconda/ui/tui/spokes/network.py
@@ -343,7 +343,7 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalTUISpoke):
         dialog = Dialog(_("Host Name"))
         self._container.add(TextWidget(_("Set host name")), callback=self._set_hostname_callback, data=dialog)
 
-        self._container.add(TextWidget(_("Apply host name")), callback=self._apply_hostname_callback, data=dialog)
+        self._container.add(TextWidget(_("Apply host name")), callback=self._apply_hostname_callback)
 
         for device_configuration in self.editable_configurations:
             iface = device_configuration.device_name
@@ -358,7 +358,7 @@ class NetworkSpoke(FirstbootSpokeMixIn, NormalTUISpoke):
         self.redraw()
         self.apply()
 
-    def _apply_hostname_callback(self, dialog):
+    def _apply_hostname_callback(self):
         self._network_module.SetCurrentHostname(self.hostname)
         self.redraw()
         self.apply()
