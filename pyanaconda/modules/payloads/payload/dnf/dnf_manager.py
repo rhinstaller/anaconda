50936874d51424b42931c598306d0377d111b89a
Pavla Kratochvilova <pkratoch@redhat.com>
2026-01-05 12:38:34 +0100

    dnf: Report transaction errors for individual TransactionItems
    The `transaction_has_errors` function prints the errors as a side-effect,
    so it should be always called, even if we already know the transaction
    ended with an error. Since python implements short-circuit evaluation
    of boolean expressions, the `transaction_has_errors` must be on the
    left-most side to ensure it is always called.
    
    Related: rhbz#2424523

--- pyanaconda/modules/payloads/payload/dnf/dnf_manager.py
+++ pyanaconda/modules/payloads/payload/dnf/dnf_manager.py
@@ -826,7 +826,8 @@ class DNFManager:
                 "The transaction finished with %s (%s)",
                 result, transaction.transaction_result_to_string(result)
             )
-            if result != 0 or transaction_has_errors(transaction):
+            has_item_errors = transaction_has_errors(transaction)
+            if has_item_errors or result != 0:
                 progress.error("The transaction process has ended with errors.")
         except BaseException as e:  # pylint: disable=broad-except
             progress.error("The transaction process has ended abruptly: {}\n{}".format(
