1ed8c6d4be2bb0deda13183482e83251b494576b
Katerina Koukiou <k.koukiou@gmail.com>
2026-01-14 12:40:08 +0100

    rescue: detect OSTree deployments in rescue mode and show deployment path
    For detecting OSTree installations, mount /boot and use the OSTree API
    to find the deployment directory. Show the deployment path in the rescue
    message instead of the sysroot path; this improves the instructions for
    chrooting into OSTree deployments.
    
    (cherry-picked from: a1b38331daa6534e2a492876b72e2cb855e80b1d)
    
    Resolves: RHEL-135123

--- pyanaconda/rescue.py
+++ pyanaconda/rescue.py
@@ -42,6 +42,7 @@ from pyanaconda.modules.common.constants.services import STORAGE
 from pyanaconda.modules.common.errors.storage import MountFilesystemError
 from pyanaconda.modules.common.structures.storage import DeviceFormatData, OSData
 from pyanaconda.modules.common.task import sync_run_task
+from pyanaconda.modules.payloads.payload.rpm_ostree.util import get_ostree_deployment_path
 from pyanaconda.threading import threadMgr
 from pyanaconda.ui.tui import tui_quit_callback
 from pyanaconda.ui.tui.spokes import NormalTUISpoke
@@ -202,6 +203,14 @@ class Rescue(object):
             task_proxy = STORAGE.get_proxy(task_path)
             sync_run_task(task_proxy)
             log.info("System has been mounted under: %s", conf.target.system_root)
+
+            # Mount /boot if it exists
+            # This is needed for OSTree deployments to be detected properly
+            boot_device = root.get_boot_device()
+            if boot_device:
+                boot_path = os.path.join(conf.target.system_root, "boot")
+                read_only = "ro" if self.ro else ""
+                self._device_tree_proxy.MountDevice(boot_device, boot_path, read_only)
         except MountFilesystemError as e:
             log.error("Mounting system under %s failed: %s", conf.target.system_root, e)
             self.status = RescueModeStatus.MOUNT_FAILED
