7110cc04f2a19ee54ca6fd9332171490ac2996dd
nathan9513-aps <nathan9512@proton.me>
2026-05-15 11:50:59 +0200

    rpm_ostree: fix GLib main context leak in PullRemoteAndDeleteTask
    When any GIO/OSTree operation inside PullRemoteAndDeleteTask.run() raises
    an exception after mainctx.push_thread_default(), the GLib main context
    for the thread is leaked because pop_thread_default() is never called.
    
    This was observed in the field when an I/O error occurred during OSTree
    fsck of a content object on a corrupted SquashFS image:
    
      kernel: SQUASHFS error: xz decompression failed, data probably corrupt
      kernel: SQUASHFS error: Failed to read block 0xb3fe281c: -5
    
      PayloadInstallationError: Failed to pull from repository: g-io-error-quark:
      fsck content object d5eda20e...: Error reading from file descriptor (0)
    
    The original code only wrapped pull_with_options() in a try/finally, but
    several operations between the context push and the pull can also raise
    -- most notably sysroot.load(cancellable), which reads from the physical
    root and is likely to fail on a corrupted medium.
    
    Move the try/finally block to start immediately after
    push_thread_default() so that pop_thread_default() is guaranteed to be
    called regardless of which operation fails.
    
    Also normalize the error message format from '%s' % e to
    '{}'.format(str(e)) for consistency with the rest of the file.
    
    Resolves: rhbz#2477408

--- pyanaconda/modules/payloads/payload/rpm_ostree/installation.py
+++ pyanaconda/modules/payloads/payload/rpm_ostree/installation.py
@@ -1009,59 +1009,62 @@ class PullRemoteAndDeleteTask(Task):
         mainctx = create_new_context()
         mainctx.push_thread_default()
 
-        cancellable = None
-
-        # Variable substitute the ref: https://pagure.io/atomic-wg/issue/299
-        ref = RpmOstree.varsubst_basearch(self._data.ref)
-
-        self.report_progress(
-            _("Starting pull of {branch_name} from {source}").format(
-                branch_name=ref, source=self._data.remote
-            )
-        )
-
-        progress = OSTree.AsyncProgress.new()
-        progress.connect('changed', self._pull_progress_cb)
-
-        # We use the UNTRUSTED flag as a way to help verification for potentially
-        # corrupted ISOs - this way ostree will validate the checksum of objects as
-        # it writes data.
-        pull_opts = {'refs': Variant('as', [ref]), 'flags': Variant('i', OSTree.RepoPullFlags.UNTRUSTED)}
-        # If we're doing a kickstart, we can at least use the content as a reference:
-        # See <https://github.com/rhinstaller/anaconda/issues/1117>
-        # The first path here is used by <https://pagure.io/fedora-lorax-templates>
-        # and the second by <https://github.com/projectatomic/rpm-ostree-toolbox/>
-        # FIXME extend tests to cover this part of code
-        if OSTree.check_version(2017, 8):
-            for path in ['/ostree/repo', '/install/ostree/repo']:
-                if os.path.isdir(path + '/objects'):
-                    pull_opts['localcache-repos'] = Variant('as', [path])
-                    break
-
-        sysroot_file = Gio.File.new_for_path(conf.target.physical_root)
-        sysroot = OSTree.Sysroot.new(sysroot_file)
-        sysroot.load(cancellable)
-        repo = sysroot.get_repo(None)[1]
-        # We don't support resuming from interrupted installs
-        repo.set_disable_fsync(True)
-
         try:
-            repo.pull_with_options(self._data.remote,
-                                   Variant('a{sv}', pull_opts),
-                                   progress, cancellable)
-        except GError as e:
-            raise PayloadInstallationError("Failed to pull from repository: %s" % e) from e
+            cancellable = None
 
-        log.info("ostree pull: %s", progress.get_status() or "")
-        self.report_progress(_("Preparing deployment of {}").format(ref))
+            # Variable substitute the ref: https://pagure.io/atomic-wg/issue/299
+            ref = RpmOstree.varsubst_basearch(self._data.ref)
 
-        # Now that we have the data pulled, delete the remote for now. This will allow a remote
-        # configuration defined in the tree (if any) to override what's in the kickstart.
-        # Otherwise, we'll re-add it in post.  Ideally, ostree would support a pull without adding
-        # a remote, but that would get quite complex.
-        repo.remote_delete(self._data.remote, None)
+            self.report_progress(
+                _("Starting pull of {branch_name} from {source}").format(
+                    branch_name=ref, source=self._data.remote
+                )
+            )
 
-        mainctx.pop_thread_default()
+            progress = OSTree.AsyncProgress.new()
+            progress.connect('changed', self._pull_progress_cb)
+
+            # We use the UNTRUSTED flag as a way to help verification for potentially
+            # corrupted ISOs - this way ostree will validate the checksum of objects as
+            # it writes data.
+            pull_opts = {'refs': Variant('as', [ref]), 'flags': Variant('i', OSTree.RepoPullFlags.UNTRUSTED)}
+            # If we're doing a kickstart, we can at least use the content as a reference:
+            # See <https://github.com/rhinstaller/anaconda/issues/1117>
+            # The first path here is used by <https://pagure.io/fedora-lorax-templates>
+            # and the second by <https://github.com/projectatomic/rpm-ostree-toolbox/>
+            # FIXME extend tests to cover this part of code
+            if OSTree.check_version(2017, 8):
+                for path in ['/ostree/repo', '/install/ostree/repo']:
+                    if os.path.isdir(path + '/objects'):
+                        pull_opts['localcache-repos'] = Variant('as', [path])
+                        break
+
+            sysroot_file = Gio.File.new_for_path(conf.target.physical_root)
+            sysroot = OSTree.Sysroot.new(sysroot_file)
+            sysroot.load(cancellable)
+            repo = sysroot.get_repo(None)[1]
+            # We don't support resuming from interrupted installs
+            repo.set_disable_fsync(True)
+
+            try:
+                repo.pull_with_options(self._data.remote,
+                                       Variant('a{sv}', pull_opts),
+                                       progress, cancellable)
+            except GError as e:
+                raise PayloadInstallationError(
+                    "Failed to pull from repository: {}".format(str(e))
+                ) from e
+
+            log.info("ostree pull: %s", progress.get_status() or "")
+            self.report_progress(_("Preparing deployment of {}").format(ref))
+
+            # Now that we have the data pulled, delete the remote for now. This will allow a remote
+            # configuration defined in the tree (if any) to override what's in the kickstart.
+            # Otherwise, we'll re-add it in post.  Ideally, ostree would support a pull without
+            # adding a remote, but that would get quite complex.
+            repo.remote_delete(self._data.remote, None)
+        finally:
+            mainctx.pop_thread_default()
 
     def _pull_progress_cb(self, async_progress):
         status = async_progress.get_status()
