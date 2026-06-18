efe8dc95499852075090964d817763ef8baf8ff6
Katerina Koukiou <k.koukiou@gmail.com>
2026-06-18 13:22:25 +0200

    rescue: Set system root to OSTree deployment path
    During a normal installation, SetSystemRootTask already bind-mounts the
    OSTree deployment at conf.target.system_root before %post scripts run.
    Rescue mode was missing this step: it mounted the physical root
    (/mnt/sysimage) but left system_root pointing at the physroot layout
    (ostree/, boot/, etc.) instead of the deployment directory that
    contains /bin/sh and the rest of the OS.
    
    Detect OSTree deployments in rescue mode and call set_system_root()
    with the deployment path, matching the normal installation flow.
    
    Adjust warning message as now user needs to do call `chroot /mnt/sysroot` only.
    
    (cherry-picked from: 073bebbfbe4a8ed95a8ebc7ade69a9a8fcae4c9d)
    
    Related: RHEL-135123

--- pyanaconda/rescue.py
+++ pyanaconda/rescue.py
@@ -35,6 +35,7 @@ from pyanaconda.core import util
 from pyanaconda.core.configuration.anaconda import conf
 from pyanaconda.core.constants import ANACONDA_CLEANUP, QUIT_MESSAGE, THREAD_STORAGE
 from pyanaconda.core.i18n import N_, _
+from pyanaconda.core.util import set_system_root
 from pyanaconda.flags import flags
 from pyanaconda.kickstart import runPostScripts
 from pyanaconda.modules.common.constants.objects import DEVICE_TREE
@@ -251,8 +252,10 @@ class Rescue(object):
         makeFStab()
 
         # Check if this is an OSTree/immutable system
-        if get_ostree_deployment_path(conf.target.system_root):
+        deployment_path = get_ostree_deployment_path(conf.target.physical_root)
+        if deployment_path:
             self.is_ostree = True
+            set_system_root(deployment_path)
 
         # run %post if we've mounted everything
         if not self.ro and self._scripts:
@@ -454,11 +457,6 @@ class RescueStatusAndShellSpoke(NormalTUISpoke):
                                      "command line for autorelabel to work properly.\n")
                                    if self._rescue.autorelabel else "")
 
-                # For OSTree installations, find the deployment directory for chroot
-                mountpoint = conf.target.system_root
-                deployment_path = get_ostree_deployment_path(mountpoint)
-                chroot_path = deployment_path if deployment_path else mountpoint
-
                 ostree_warning = (_("Warning: An immutable system has been detected. "
                                     "It is not recommended to make manual changes to the deployment "
                                     "directories as this may break system integrity. Only modify "
@@ -468,8 +466,8 @@ class RescueStatusAndShellSpoke(NormalTUISpoke):
                 text = TextWidget(_("Your system has been mounted under %(mountpoint)s.\n\n"
                                     "If you would like to make the root of your system the "
                                     "root of the active system, run the command:\n\n"
-                                    "\tchroot %(chroot_path)s\n\n")
-                                  % {"mountpoint": mountpoint, "chroot_path": chroot_path}
+                                    "\tchroot %(mountpoint)s\n\n")
+                                  % {"mountpoint": conf.target.system_root}
                                   + ostree_warning + autorelabel_msg + finish_msg)
             elif status == RescueModeStatus.MOUNT_FAILED:
                 if self._rescue.reboot:
